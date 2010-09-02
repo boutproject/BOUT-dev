bfdc933a21688937ff92048d673d7711810fb31d
Ben Dudson <bd512@york.ac.uk>
2010-09-02 22:55:03 +0100

    Started adding the GEM gyro-fluid model
    Only a skeleton at the moment, no real code implemented yet

---
+++ bout++/examples/gyro-gem/gem.cpp
@@ -0,0 +1,20 @@
+/****************************************************************
+ * GEM Gyro-fluid model
+ * 
+ ****************************************************************/
+
+#include "bout.h"
+
+Field3D Ni, Ne; // Gyro-center ion and electron densities
+
+Field3D phi;    // Electrostatic potential
+
+int physics_init(bool restarting)
+{
+
+}
+
+int physics_run(BoutReal time)
+{
+
+}
