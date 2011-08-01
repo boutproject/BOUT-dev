430693f639e3048a3f216a14e7a517c8bc29539e
Ben Dudson <bd512@york.ac.uk>
2011-08-01 14:03:38 +0100

    Dalf3 model complete
    Untested, but should all be there

--- examples/dalf3/dalf3.cxx
+++ examples/dalf3/dalf3.cxx
@@ -13,6 +13,9 @@
  *             Mode Structure"
  *             arXiv:physics/0207126  Feb 2001
  *
+ * NOTE: The normalisation used here is different to in the above
+ *       papers. See manual in doc/ subdirectory for details
+ *
  ****************************************************************/
 
 #include <bout.hxx>
@@ -29,20 +32,27 @@ const BoutReal Mi = 2.0*1.67262158e-27; // Ion mass
 const BoutReal Me = 9.1093816e-31;  // Electron mass
 const BoutReal Me_Mi = Me / Mi; // Electron mass / Ion mass
 
+// Poisson brackets: b0 x Grad(f) dot Grad(g) / B = [f, g]
+// Method to use: BRACKET_ARAKAWA, BRACKET_STD or BRACKET_SIMPLE
+const BRACKET_METHOD bm = BRACKET_STD;
+
 // Evolving quantities
-Field3D Vort, Ajpar, Pe, Upar;
+Field3D Vort, Ajpar, Pe, Vpar;
 
 Field3D phi, apar, jpar;
 
 Field2D B0, Pe0, Jpar0;
 Vector2D b0xcv;
 
-BoutReal C; // Collisional damping (resistivity)
-BoutReal beta_hat, mu_hat, eps_hat;
+Field2D eta; // Collisional damping (resistivity)
+BoutReal beta_hat, mu_hat;
 BoutReal mu_par;
 
 int phi_flags, apar_flags;
 bool ZeroElMass, estatic; 
+bool curv_kappa;
+bool flat_resist;
+BoutReal mul_resist;
 
 FieldGroup comms;
 
@@ -53,10 +63,10 @@ int physics_init(bool restarting) {
   
   GRID_LOAD(Jpar0);
   
-  Field2D Ni0, Te0, Ti0;
-  GRID_LOAD3(Ni0, Te0, Ti0);
+  Field2D Ni0, Te0;
+  GRID_LOAD2(Ni0, Te0);
   Ni0 *= 1e20; // To m^-3
-  Pe0 = Charge * Ni0 * (Te0 + Ti0); // Pressure in Pascals
+  Pe0 = Charge * Ni0 * Te0; // Electron pressure in Pascals
   SAVE_ONCE(Pe0);
   
   // Load curvature term
@@ -80,6 +90,20 @@ int physics_init(bool restarting) {
   mesh->get(hthe, "hthe"); // m
   mesh->get(I,    "sinty");// m^-2 T^-1
   
+  //////////////////////////////////////////////////////////////
+  // Options
+
+  Options *globalOptions = Options::getRoot();
+  Options *options = globalOptions->getSection("dalf3");
+  
+  OPTION(options, phi_flags, 0);
+  OPTION(options, apar_flags, 0);
+  OPTION(options, estatic, false);
+  OPTION(options, ZeroElMass, false);
+  OPTION(options, curv_kappa, false);
+  OPTION(options, flat_resist, false);
+  OPTION(options, mul_resist, 1.0);
+  
   // SHIFTED RADIAL COORDINATES
 
   if(mesh->ShiftXderivs) {
@@ -100,12 +124,6 @@ int physics_init(bool restarting) {
   BoutReal Tenorm = max(Te0, true);
   BoutReal Nenorm = max(Ni0, true);
   BoutReal Bnorm = max(B0, true);
-  BoutReal qnorm = 5; // Typical q
-  BoutReal Rnorm = max(Rxy, true);
-
-  // Perpendicular and parallel length scales
-  BoutReal Lperp = 1.;
-  BoutReal Lpar = 2.*PI*qnorm*Rnorm;
 
   // Sound speed in m/s
   BoutReal Cs = sqrt(Charge*Tenorm / Mi);
@@ -113,13 +131,41 @@ int physics_init(bool restarting) {
   // drift scale
   BoutReal rho_s = Cs * Mi / (Charge * Bnorm);
   
-  eps_hat = SQ(qnorm*Rnorm / Lperp);
-  beta_hat = 4.e-7*PI * Tenorm * Nenorm / (Bnorm*Bnorm) * eps_hat;
-  mu_hat = Me / Mi * eps_hat;
+  beta_hat = 4.e-7*PI * Charge*Tenorm * Nenorm / (Bnorm*Bnorm);
   
+  if(ZeroElMass) {
+    mu_hat = 0.;
+  }else
+    mu_hat = Me / Mi;
+  
+  // Spitzer resistivity 
+  if(flat_resist) {
+    // eta in Ohm-m. NOTE: ln(Lambda) = 20
+    eta = 0.51*1.03e-4*20.*pow(Tenorm, -1.5);
+  }else {
+    eta = 0.51*1.03e-4*20.*(Te0^(-1.5)); 
+  }
 
+  // Plasma quantities
   Jpar0 /= Nenorm*Charge*Cs;
-  
+  Pe0 /= Nenorm*Charge*Tenorm;
+
+  // Coefficients
+  eta *= Charge * Nenorm / Bnorm;
+
+  b0xcv.x /= Bnorm;
+  b0xcv.y *= rho_s*rho_s;
+  b0xcv.z *= rho_s*rho_s;
+
+  // Metrics
+  Rxy /= rho_s;
+  hthe /= rho_s;
+  I *= rho_s*rho_s*Bnorm;
+  Bpxy /= Bnorm;
+  Btxy /= Bnorm;
+  B0 /= Bnorm;
+
+  mesh->dx /= rho_s*rho_s*Bnorm;
   
   ///////////////////////////////////////////////////
   // CALCULATE METRICS
@@ -143,22 +189,8 @@ int physics_init(bool restarting) {
   
   mesh->geometry(); // Calculate quantities from metric tensor
 
-  //////////////////////////////////////////////////////////////
-  // Options
-
-  Options *globalOptions = Options::getRoot();
-  Options *options = globalOptions->getSection("dalf3");
-  
-  OPTION(options, phi_flags, 0);
-  OPTION(options, estatic, false);
-  OPTION(options, ZeroElMass, false);
-  
-  if(ZeroElMass) {
-    mu_hat = 0.;
-  }
-
-  SOLVE_FOR3(Vort, Pe, Upar);
-  comms.add(Vort, Pe, Upar);
+  SOLVE_FOR3(Vort, Pe, Vpar);
+  comms.add(Vort, Pe, Vpar);
   if(!(estatic && ZeroElMass)) {
     SOLVE_FOR(Ajpar);
     comms.add(Ajpar);
@@ -171,22 +203,27 @@ int physics_init(bool restarting) {
 }
 
 // Curvature operator
-const Field3D Kappa(const Field3D &x) {
+const Field3D Kappa(const Field3D &f) {
+  if(curv_kappa) {
+    // Use the b0xcv vector from grid file
+    return -2.*b0xcv*Grad(f) / B0;
+  }
   
+  return -2.*bracket(log(B0), f, bm);
 }
 
 const Field3D Grad_parP_LtoC(const Field3D &f) {
-  return Grad_par_LtoC(f);
+  return Grad_par_LtoC(f) - beta_hat * bracket(apar, f);
 }
 
 const Field3D Grad_parP_CtoL(const Field3D &f) {
-  return Grad_par_CtoL(f);
+  return Grad_par_CtoL(f) - beta_hat * bracket(apar, f);
 }
 
 int physics_run(BoutReal time) {
 
   // Invert vorticity to get electrostatic potential
-  phi = invert_laplace(Vort, phi_flags);
+  phi = invert_laplace(Vort*B0, phi_flags);
   phi.applyBoundary();
   
   // Communicate evolving variables and phi
@@ -198,7 +235,7 @@ int physics_run(BoutReal time) {
     apar = 0.;
     if(ZeroElMass) {
       // Not evolving Ajpar
-      jpar = Grad_par_CtoL(Pe - phi) / C;
+      jpar = Grad_par_CtoL(Pe - phi) / eta;
       jpar.applyBoundary();
     }else {
       jpar = Ajpar / mu_hat;
@@ -223,30 +260,37 @@ int physics_run(BoutReal time) {
 
   // Vorticity equation
   ddt(Vort) = 
-    - b0xGrad_dot_Grad(phi, Vort)    // ExB advection
-    + (B0^3)*Grad_parP_LtoC(jpar/B0)
-    - B0*B0*Kappa(Pe)
+    - bracket(phi, Vort, bm)    // ExB advection
+    + B0*B0*Grad_parP_LtoC(jpar/B0)
+    - B0*Kappa(Pe)
     ;
   
+  // Parallel Ohm's law
   if(!(estatic && ZeroElMass)) {
     // beta_hat*apar + mu_hat*jpar
     ddt(Ajpar) =
-      - mu_hat*b0xGrad_dot_Grad(phi, jpar)
+      - mu_hat*bracket(phi, jpar, bm)
       + Grad_parP_CtoL(Pe0 + Pe - phi)
-      - C*jpar
+      - eta*jpar
       ;
   }
   
+  // Parallel velocity
+  ddt(Vpar) = 
+    - bracket(phi, Vpar, bm)
+    - Grad_parP_CtoL(Pe + Pe0) 
+    + mu_par * Grad2_par2(Vpar)
+    ;
+
+  // Electron pressure
   ddt(Pe) =
-    - b0xGrad_dot_Grad(phi, Pe + Pe0)
-    + B0*Grad_parP_LtoC( (jpar - Upar)/B0 )
-    - Kappa(Pe - phi)
+    - bracket(phi, Pe + Pe0, bm)
+    + (Pe0 + Pe) * (
+                    Kappa(phi - Pe)
+                    + B0*Grad_parP_LtoC( (jpar - Vpar)/B0 )
+                    )
     ;
   
-  ddt(Upar) = 
-    - b0xGrad_dot_Grad(phi, Upar)
-    - Grad_parP_CtoL(Pe + Pe0) / eps_hat
-    + (mu_par/eps_hat) * Grad2_par2(Upar)
-    ;
+  return 0;
 }
 
