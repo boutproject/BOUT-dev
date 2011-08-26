b119bbfe1108173f43686c810dafc5aa2b3198bf
Ben Dudson <bd512@york.ac.uk>
2011-08-25 18:43:33 -0700

    1.0 - RELEASE
    Making a release for the 2011 BOUT++ workshop

--- include/bout.hxx
+++ include/bout.hxx
@@ -53,7 +53,7 @@
 
 #include "where.hxx"
 
-const BoutReal BOUT_VERSION = 0.85;  ///< Version number
+const BoutReal BOUT_VERSION = 1.0;  ///< Version number
 
 // BOUT++ functions (bout++.cpp). Call to add a variable to evolve
 
