92ebf388b0b4bd19ec979c741551ed3777ad22fb
Ben Dudson <bd512@york.ac.uk>
2011-11-06 21:09:39 +0000

    Removing showdata and plotdata from boututils/init
    Not imported by default, but can still be imported using
    
    from boututils.showdata import *

--- tools/pylib/boututils/__init__.py
+++ tools/pylib/boututils/__init__.py
@@ -7,15 +7,15 @@
 print "Loading data utilities"
 
 # Load routines from separate files
-try:
-    from showdata import showdata
-except:
-    print "No showdata"
+#try:
+#    from showdata import showdata
+#except:
+#    print "No showdata"
 
-try:
-    from plotdata import plotdata
-except:
-    print "No plotdata"
+#try:
+#    from plotdata import plotdata
+#except:
+#    print "No plotdata"
 
 try:
     from datafile import DataFile
