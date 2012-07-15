7f2beb59840709bc8c9c48550f325e4bd41eefc6
Ben Dudson <bd512@york.ac.uk>
2012-07-15 23:44:25 +0100

    Adding code for new mesh

---
+++ src/mesh/impls/quilt/quiltdomain.cxx
@@ -0,0 +1,2 @@
+
+#include "quiltdomain.hxx"
---
+++ src/mesh/impls/quilt/quiltdomain.hxx
@@ -0,0 +1,21 @@
+/*
+ * Extend the Domain class, to allow extra data
+ * to be attached to each domain
+ */
+
+class QuiltDomain;
+
+#ifndef __QUILTDOMAIN_H__
+#define __QUILTDOMAIN_H__
+
+#include "../domain.hxx"
+
+class QuiltDomain : public Domain {
+public:
+  QuiltDomain(int NX, int NY) : Domain(NX,NY) {}
+  ~QuiltDomain();
+  
+  int proc;
+};
+
+#endif // __QUILTDOMAIN_H__
