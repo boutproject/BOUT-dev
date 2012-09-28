8d245767ca431f69ae9369099c63ea987696156e
Ben Dudson <bd512@york.ac.uk>
2012-09-28 13:52:03 +0100

    Added test-laplace2 example
    For testing different Laplacian inversion methods.
    Creates two Laplacian inversion solvers by using
    different options for each.
    
    Size of the mesh is determined by settings in the BOUT.inp file
    rather than a binary grid file. Makes changing mesh sizes straightforward.

---
+++ examples/test-laplace2/test_laplace.cxx
@@ -0,0 +1,43 @@
+/*
+ * Laplacian inversion
+ * 
+ */
+
+#include <bout.hxx>
+#include <boutmain.hxx>
+#include <invert_laplace.hxx>
+#include <field_factory.hxx>
+
+int physics_init(bool restarting) {
+  FieldFactory f;
+  
+  Options *options = Options::getRoot();
+
+  Field3D input = f.create3D("(1-gauss(x-0.5,0.2))*gauss(z-pi)");
+  Field2D a = f.create2D("gauss(x)");
+  Field2D c = f.create2D("sin(x) * gauss(x-0.5)");
+  SAVE_ONCE3(input, a, c);
+  
+  // Create two solvers, using different options
+  class Laplacian *solver1 = Laplacian::create(options->getSection("solver1"));
+  class Laplacian *solver2 = Laplacian::create(options->getSection("solver2"));
+  
+  Field3D result1 = solver1->solve(input);
+  Field3D result2 = solver2->solve(input);
+
+  SAVE_ONCE2(result1, result2);
+  
+  dump.write();
+  dump.close();
+  
+  output << "\nFinished running test. Triggering error to quit\n\n";
+  
+  MPI_Barrier(BoutComm::get()); // Wait for all processors to write data
+  
+  return 1;
+}
+
+int physics_run(BoutReal t) {
+  // Doesn't do anything
+  return 1;
+}
