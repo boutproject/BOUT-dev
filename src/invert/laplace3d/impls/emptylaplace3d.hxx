bb1dddefddfb7d6032c74790f008c1ba0224457f
Ben Dudson <bd512@york.ac.uk>
2013-08-28 18:08:43 +0100

    Added Laplace3D solver interface
    New class of solvers for full 3D problem. Interface follows
    the same pattern (factory) as other solvers.
    
    No implementations yet. Added bare bones of a PETSc implementation

---
+++ src/invert/laplace3d/impls/emptylaplace3d.hxx
@@ -0,0 +1,19 @@
+
+#ifndef __EMPTYLAPLACE3D_H__
+#define __EMPTYLAPLACE3D_H__
+
+#include <bout/invert/laplace3d.hxx>
+#include <boutexception.hxx>
+
+class EmptyLaplace3D : public Laplace3D {
+public:
+  EmptyLaplace3D(Options *opt = NULL) {throw BoutException("Laplace3D solver not available");}
+  void setCoefA(const Field2D &f) {}
+  void setCoefB(const Field2D &f) {}
+  void setCoefC(const Field2D &f) {}
+  void setCoefD(const Field2D &f) {}
+ 
+  const Field3D solve(const Field3D &b) {return b;}
+};
+
+#endif // __EMPTYLAPLACE3D_H__
---
+++ src/invert/laplace3d/laplace3d.cxx
@@ -0,0 +1,8 @@
+
+#include <bout/invert/laplace3d.hxx>
+
+#include "laplace3d_factory.hxx"
+
+Laplace3D* Laplace3D::create(Options *opts) {
+  return Laplace3DFactory::getInstance()->createLaplace3D(opts);
+}
