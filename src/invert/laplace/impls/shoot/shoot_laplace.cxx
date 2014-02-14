0114892f2873651fa9d5bdc80d228b062c73c544
Ben Dudson <bd512@york.ac.uk>
2014-02-14 14:24:10 +0000

    Laplacian inversion using shooting method
    Attempt to set boundary conditions only on outer boundary
    then shoot inwards. Appears to be very sensitive to noise,
    and easily blows up for high-k modes.
    
    Test case in examples/test-laplace2 using input shoot/BOUT.inp
    
    Just for testing; not recommended for use

---
+++ src/invert/laplace/impls/shoot/shoot_laplace.cxx
@@ -0,0 +1,172 @@
+/*!
+ * \file shoot_laplace.cxx
+ *
+ * \brief Laplacian solver using shooting method
+ *  
+ * CHANGELOG
+ * =========
+ * 
+ * Feb 2014: Ben Dudson <benjamin.dudson@york.ac.uk>
+ *         * Initial version
+ * 
+ **************************************************************************
+ * Copyright 2014 B.D.Dudson
+ *
+ * Contact: Ben Dudson, benjamin.dudson@york.ac.uk
+ * 
+ * This file is part of BOUT++.
+ *
+ * BOUT++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BOUT++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BOUT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "shoot_laplace.hxx"
+#include <globals.hxx>
+#include <fft.hxx>
+#include <bout/constants.hxx>
+
+LaplaceShoot::LaplaceShoot(Options *opt) : Laplacian(opt), A(0.0), C(1.0), D(1.0) {
+  nmode = maxmode + 1; // Number of Z modes. maxmode set in invert_laplace.cxx from options
+  
+  // Allocate memory
+  int size = (mesh->ngz-1)/2 + 1;
+  km = new dcomplex[size];
+  kc = new dcomplex[size];
+  kp = new dcomplex[size];
+  
+  for(int i=0;i<size;i++) {
+    km[i] = 0.0;
+    kc[i] = 0.0;
+    kp[i] = 0.0;
+  }
+
+  rhsk = new dcomplex[size];
+
+  buffer = new BoutReal[4*maxmode];
+}
+
+LaplaceShoot::~LaplaceShoot() {
+  delete[] km;
+  delete[] kc;
+  delete[] kp;
+  
+  delete[] rhsk;
+  
+  delete[] buffer;
+}
+
+const FieldPerp LaplaceShoot::solve(const FieldPerp &rhs) {
+  FieldPerp x;  // Result
+  x.allocate();
+  
+  int jy = rhs.getIndex();  // Get the Y index
+  x.setIndex(jy);
+  
+  // Get the width of the boundary
+  
+  int inbndry = 2, outbndry=2;
+  if(flags & INVERT_BNDRY_ONE) {
+    inbndry = outbndry = 1;
+  }
+  if(flags & INVERT_BNDRY_IN_ONE)
+    inbndry = 1;
+  if(flags & INVERT_BNDRY_OUT_ONE)
+    outbndry = 1;
+  
+  int xs, xe;
+  xs = mesh->xstart; // Starting X index
+  if(mesh->firstX())
+    xs = inbndry;
+  xe = mesh->xend;  // Last X index
+  if(mesh->lastX())
+    xe = mesh->ngx-outbndry-1;
+
+  if(mesh->lastX()) {
+    // Set initial value and gradient to zero
+    // by setting kc and kp
+    
+    for(int i=0;i<maxmode;i++) {
+      kc[i] = 0.0;
+      kp[i] = 0.0;
+    }
+    
+    for(int ix=xe;ix<mesh->ngx;ix++)
+      for(int iz=0;iz<mesh->ngz-1;iz++) {
+        x[ix][iz] = 0.0;
+      }
+      
+  }else {
+    // Wait for processor outer X
+    comm_handle handle = mesh->irecvXOut(buffer, 4*maxmode, jy);
+    mesh->wait(handle);
+    
+    // Copy into kc, kp
+    for(int i=0;i<maxmode;i++) {
+      kc[i] = dcomplex(buffer[4*i], buffer[4*i+1]);
+      kp[i] = dcomplex(buffer[4*i+2], buffer[4*i+3]);
+    }
+    
+    // Calculate solution at xe using kc
+    ZFFT_rev(kc, mesh->zShift(xe, jy), x[xe]);
+  }
+  
+  // kc and kp now set to result at x and x+1 respectively
+  // Use b at x to get km at x-1
+  // Loop inwards from edge
+  for(int ix=xe; ix >= xs; ix--) {
+    ZFFT(rhs[ix], mesh->zShift(ix, jy), rhsk);
+    
+    for(int kz=0; kz<maxmode; kz++) {
+      BoutReal kwave=kz*2.0*PI/(mesh->zlength); // wave number is 1/[rad]
+      
+      // Get the coefficients
+      dcomplex a,b,c;
+      tridagCoefs(ix, jy, kwave, a, b, c, &C, &D);
+      b += A(ix,jy);
+      
+      // a*km + b*kc + c*kp = rhsk
+      
+      km[kz] = (rhsk[kz] - b*kc[kz] - c*kp[kz]) / a;
+    }
+    
+    // Inverse FFT to get x[ix-1]
+    ZFFT_rev(km, mesh->zShift(ix, jy), x[ix-1]);
+    
+    // Cycle km->kc->kp
+    
+    dcomplex *tmp;
+    tmp = kp; kp = kc; kc = km; km = tmp;
+  }
+  
+  // Finished on this processor. Send data to next inner processor
+  if(!mesh->firstX()) {
+    // Should be able to send dcomplex buffers. For now copy into BoutReal buffer
+    for(int i=0;i<maxmode;i++) {
+      buffer[4*i]     = kc[i].Real();
+      buffer[4*i + 1] = kc[i].Imag();
+      buffer[4*i + 2] = kp[i].Real();
+      buffer[4*i + 3] = kp[i].Imag();
+    }
+    mesh->sendXIn(buffer, 4*maxmode, jy);
+  }else {
+    // Set inner boundary
+    for(int ix=xs-2;ix>=0;ix--) {
+      for(int iz=0;iz<mesh->ngz-1;iz++) {
+        x[ix][iz] = x[xs-1][iz];
+      }
+    }
+  }
+  
+  return x;
+}
