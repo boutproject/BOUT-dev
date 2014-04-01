633e9772f9362647aa140cc17f6b1d2665573206
Ben Dudson <bd512@york.ac.uk>
2014-04-01 16:27:27 +0100

    Added RK3 SSP method
    Solver:type rk3ssp
    
    3rd order explicit RK scheme, Strong Stability Preserving
    
    www.cscamm.umd.edu/tadmor/pub/linear-stability/Gottlieb-Shu-Tadmor.SIREV-01.pdf
    
    Spatial TVD schemes can become oscillatory if discretised
    in time with a non-SSP scheme. These schemes aim to retain
    the stability properties of the Euler method, but with higher
    accuracy.

---
+++ src/solver/impls/rk3-ssp/rk3-ssp.hxx
@@ -0,0 +1,77 @@
+/**************************************************************************
+ * 3rd-order Runge Kutta Strong Stability Preserving (SSP)
+ * 
+ * http://www.cscamm.umd.edu/tadmor/pub/linear-stability/Gottlieb-Shu-Tadmor.SIREV-01.pdf
+ * 
+ * Sigal Gottlieb, Chi-Wang Shu, and Eitan Tadmor, Strong stability-preserving 
+ * high-order time discretization methods,
+ * SIAM Rev. 43 (2001), no. 1, 89-112 (electronic). MR 2002f:65132
+ *
+ * Always available, since doesn't depend on external library
+ * 
+ **************************************************************************
+ * Copyright 2010 B.D.Dudson, S.Farley, M.V.Umansky, X.Q.Xu
+ *
+ * Contact: Ben Dudson, bd512@york.ac.uk
+ * 
+ * This file is part of BOUT++.
+ *
+ * BOUT++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BOUT++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BOUT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+class RK3SSP;
+
+#ifndef __RK3SSP_SOLVER_H__
+#define __RK3SSP_SOLVER_H__
+
+#include "mpi.h"
+
+#include <bout_types.hxx>
+#include <bout/solver.hxx>
+
+class RK3SSP : public Solver {
+ public:
+  RK3SSP(Options *opt = NULL);
+  ~RK3SSP();
+  
+  void setMaxTimestep(BoutReal dt);
+  BoutReal getCurrentTimestep() {return timestep; }
+  
+  int init(bool restarting, int nout, BoutReal tstep);
+  
+  int run();
+ private:
+
+  BoutReal max_timestep; // Maximum timestep
+  int mxstep; // Maximum number of internal steps between outputs
+  
+  BoutReal *f;
+  
+  BoutReal out_timestep; // The output timestep
+  int nsteps; // Number of output steps
+  
+  BoutReal timestep; // The internal timestep
+
+  int nlocal, neq; // Number of variables on local processor and in total
+  
+  void take_step(BoutReal curtime, BoutReal dt, 
+                 BoutReal *start, BoutReal *result); // Take a single step to calculate f1
+  
+  BoutReal *u1, *u2, *u3, *L; // Time-stepping arrays
+  
+};
+
+#endif // __RK4_SOLVER_H__
+
