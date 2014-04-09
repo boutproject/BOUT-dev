910fe212b1dc3be6625197e2cb35dac921537ad4
Ben Dudson <bd512@york.ac.uk>
2014-04-09 16:27:31 +0100

    Added example of using DataFormat

---
+++ examples/test-dataformat/test_dataformat.cxx
@@ -0,0 +1,21 @@
+
+#include <dataformat.hxx>
+#include <output.hxx>
+
+int main() {
+  const string izfilename="sample.nc";
+
+  // Create a file format handler
+  DataFormat *izfile = data_format(izfilename.c_str());
+
+  izfile->openr(izfilename, 0);
+
+  if(!izfile->is_valid()) {
+    output << "\tERROR: Could not open file " << izfilename << endl;
+  }
+
+  izfile->close();
+
+  return 0;
+}
+
