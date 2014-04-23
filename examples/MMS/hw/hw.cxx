61720f29c09727fc93d6ca4db70c526acef561cd
Ben Dudson <bd512@york.ac.uk>
2014-04-23 13:27:49 +0100

    Added MMS test of Hasegawa-Wakatani system
    For now imposing a phi solution, rather than solving
    from vorticity, so only testing differencing methods
    
    With 1st order upwinding for bracket method,
    get 1st order convergence
    
    Using Arakawa method, get no convergence.
    Could be because it needs some dissipation?
    Appears to blow up around boundaries first. Problem
    there?

---
+++ examples/MMS/hw/hw.cxx
@@ -0,0 +1,127 @@
+
+#include <boutmain.hxx>
+#include <smoothing.hxx>
+#include <invert_laplace.hxx>
+#include <derivs.hxx>
+#include <field_factory.hxx>
+#include <bout/constants.hxx>
+
+Field3D n, vort;  // Evolving density and vorticity
+Field3D phi;
+
+BoutReal alpha, kappa, Dvort, Dn;
+bool modified; // Modified H-W equations?
+
+class Laplacian* phiSolver; // Laplacian solver for vort -> phi
+
+// Poisson brackets: b0 x Grad(f) dot Grad(g) / B = [f, g]
+// Method to use: BRACKET_ARAKAWA, BRACKET_STD or BRACKET_SIMPLE
+BRACKET_METHOD bm; // Bracket method for advection terms
+
+// Simple implementation of 4th order perpendicular Laplacian
+Field3D Delp4(const Field3D &var) {
+  Field3D tmp;
+  tmp = Delp2(var, 0.0);
+  mesh->communicate(tmp);
+  tmp.applyBoundary("neumann");
+  return Delp2(tmp, 0.0);
+  
+  //return Delp2(var);
+}
+
+int physics_init(bool restart) {
+  
+  Options *options = Options::getRoot()->getSection("hw");
+  OPTION(options, alpha, 1.0);
+  OPTION(options, kappa, 0.1);
+  OPTION(options, Dvort, 1e-2);
+  OPTION(options, Dn,    1e-2);
+
+  OPTION(options, modified, false);
+  
+  ////// Set mesh spacing
+  Options *meshoptions = Options::getRoot()->getSection("mesh");
+
+  BoutReal Lx;
+  meshoptions->get("Lx",Lx,1.0);
+
+  /*this assumes equidistant grid*/
+  int nguard = mesh->xstart;
+  mesh->dx = Lx/(mesh->GlobalNx - 2*nguard);
+  mesh->dz = TWOPI*Lx/(mesh->GlobalNx - 2*nguard);
+  /////
+
+  SOLVE_FOR2(n, vort);
+  SAVE_REPEAT(phi);
+
+  phiSolver = Laplacian::create();
+  phi = 0.; // Starting phi
+  
+  // Use default flags 
+
+  // Choose method to use for Poisson bracket advection terms
+  int bracket;
+  OPTION(options, bracket, 0);
+  switch(bracket) {
+  case 0: {
+    bm = BRACKET_STD; 
+    output << "\tBrackets: default differencing\n";
+    break;
+  }
+  case 1: {
+    bm = BRACKET_SIMPLE; 
+    output << "\tBrackets: simplified operator\n";
+    break;
+  }
+  case 2: {
+    bm = BRACKET_ARAKAWA; 
+    output << "\tBrackets: Arakawa scheme\n";
+    break;
+  }
+  case 3: {
+    bm = BRACKET_CTU; 
+    output << "\tBrackets: Corner Transport Upwind method\n";
+    break;
+  }
+  default:
+    output << "ERROR: Invalid choice of bracket method. Must be 0 - 3\n";
+    return 1;
+  }
+
+  return 0;
+}
+
+int physics_run(BoutReal time) {
+  
+  // Solve for potential
+  //phi = phiSolver->solve(vort, phi);
+  phi = FieldFactory::get()->create3D("phi:solution", Options::getRoot(), mesh, CELL_CENTRE, time);
+  
+  // Communicate variables
+  mesh->communicate(n, vort, phi);
+
+  // Modified H-W equations, with zonal component subtracted from resistive coupling term
+  Field3D nonzonal_n = n;
+  Field3D nonzonal_phi = phi;
+  if(modified) {
+    // Subtract average in Y and Z
+    nonzonal_n -= averageY(n.DC());
+    nonzonal_phi -= averageY(phi.DC());
+  }
+  
+  ddt(n) = 
+    - bracket(phi, n, bm) 
+    + alpha*(nonzonal_phi - nonzonal_n)
+    - kappa*DDZ(phi)
+    //- Dn*Delp4(n)
+    ;
+  
+  ddt(vort) = 
+    - bracket(phi, vort, bm)
+    + alpha*(nonzonal_phi - nonzonal_n)
+    //- Dvort*Delp4(vort)
+    ;
+  
+  return 0;
+}
+
---
+++ examples/MMS/hw/mms.py
@@ -0,0 +1,108 @@
+#
+# Generate the test case using SymPy
+#
+# Equations are:
+#
+# d/dt(n) = -[phi,n] + alpha*(phi - n) - kappa*DDZ(phi) + Sn
+# d/dt(vort) = -[phi,vort] + alpha*(phi - n)  + Svort
+#
+# Delp2(phi) = vort + Sphi
+
+from sympy import symbols, cos, sin, diff
+
+####
+
+def bracket(f, g):
+    """
+    Calculates [f,g] symbolically
+    """
+    
+    dfdx = diff(f, x)
+    dfdz = diff(f, z)
+
+    dgdx = diff(g, x)
+    dgdz = diff(g, z)
+    
+    return dfdz * dgdx - dfdx * dgdz
+
+def DDZ(f):
+    return diff(f, z)
+
+def Delp2(f):
+    """ Laplacian in X-Z
+    """
+    d2fdx2 = diff(f, x, 2)
+    d2fdz2 = diff(f, z, 2)
+
+    return d2fdx2 + d2fdz2
+
+def Delp4(f):
+    d4fdx4 = diff(f, x, 4)
+    d4fdz4 = diff(f, z, 4)
+
+    return d4fdx4 + d4fdz4
+
+####
+
+# Parameters
+alpha = 1.0
+kappa = 0.5
+Dn = 0.0
+Dvort = 0.0
+
+# Define symbols
+
+x = symbols('x')
+z = symbols('z')
+t = symbols('t')
+
+# Define the manufactured solution
+
+n = 0.9 + 0.9*x + 0.2*cos(10*t)*sin(5.*x**2 - 2*z)
+vort = 0.9 + 0.7*x + 0.2*cos(7*t)*sin(2.*x**2 - 3*z)
+phi = 0.5*x - cos(7*t)*sin(3.*x**2 - 3*z)
+
+# Calculate gradients in x for boundaries
+
+dndx = diff(n, x)
+dvortdx = diff(vort, x)
+dphidx = diff(phi, x)
+
+# Calculate RHS function
+
+dndt = (
+      - bracket(phi, n)
+      + alpha*(phi - n)
+      - kappa*DDZ(phi)
+   #   - Dn*Delp4(n)
+      )
+
+dvortdt = (
+      - bracket(phi, vort)
+      + alpha*(phi - n)
+   #   - Dvort*Delp4(vort)
+      )
+
+# Calculate sources
+
+Sn = diff(n, t) - dndt
+Svort = diff(vort, t) - dvortdt
+
+Sphi = Delp2(phi) - vort
+
+#######################
+
+print("N:")
+print("solution = " + str(n))
+print("d/dx     = " + str(dndx))
+print("source   = " + str(Sn))
+
+print("\nVort:")
+print("solution = " + str(vort))
+print("d/dx     = " + str(dvortdx))
+print("source   = " + str(Svort))
+
+print("\nPhi:")
+print("solution = " + str(phi))
+print("d/dx     = " + str(dphidx))
+print("source   = " + str(Sphi))
