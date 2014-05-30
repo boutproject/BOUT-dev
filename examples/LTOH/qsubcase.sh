4727095fdd6ad19075eeede42c55d797313e2b1a
brey <breyiannis@gmail.com>
2014-05-30 11:51:16 +0900

    clean up

--- examples/LTOH/qsubcase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-QSUB=mpich2sub # Common values are qsub, mpisub  
-NP=32
-
-#-compile/build local executable
-make
-
-$QSUB $NP ./elm_pb
--- examples/LTOH/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make
-
-mpirun ./elm_pb -d test
-
-
-echo "Finished ELM-pb test case"
-
