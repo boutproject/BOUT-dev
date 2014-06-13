dabddc7949fc285262a59cd888ae8dccd9ed0d7e
David Dickinson <d7919@users.noreply.github.com>
2014-06-13 14:32:24 +0100

    Adding + and += operators to FieldGroup class to allow FieldGroups to be merged.

--- src/field/fieldgroup.cxx
+++ src/field/fieldgroup.cxx
@@ -25,3 +25,16 @@ FieldGroup::FieldGroup(FieldData &f1, FieldData &f2, FieldData &f3, FieldData &f
   fvec.push_back(&f1); fvec.push_back(&f2); fvec.push_back(&f3); fvec.push_back(&f4);
   fvec.push_back(&f5); fvec.push_back(&f6);
 }
+
+FieldGroup& FieldGroup::operator+(const FieldGroup &other){
+  vector<FieldData*> vec;
+  vec=other.get();
+  for(int i=0;i<vec.size();i++){
+    this->add(*(vec[i]));
+  };
+  return *this;
+};
+
+FieldGroup& FieldGroup::operator+=(const FieldGroup &other){
+  return (*this)+other;
+};
