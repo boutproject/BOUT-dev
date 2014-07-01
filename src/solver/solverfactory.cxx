c3f364fc899fc67014f049930e7cefa7d9abf3f0
David Dickinson <d7919@users.noreply.github.com>
2014-07-01 10:44:29 +0100

    Adding support for petsc-3.4 as a solver. This involves fairly minimal changes from the code used for petsc-3.3. This doesn't attempt to use any new features introduced in 3.4 but tries to ensure the same behaviour as runs with earlier petsc versions.

--- src/solver/solverfactory.cxx
+++ src/solver/solverfactory.cxx
@@ -4,6 +4,7 @@
 #include "impls/petsc-3.1/petsc-3.1.hxx"
 #include "impls/petsc-3.2/petsc-3.2.hxx"
 #include "impls/petsc-3.3/petsc-3.3.hxx"
+#include "impls/petsc-3.4/petsc-3.4.hxx"
 #include "impls/petsc/petsc.hxx"
 #include "impls/ida/ida.hxx"
 #include "impls/pvode/pvode.hxx"
