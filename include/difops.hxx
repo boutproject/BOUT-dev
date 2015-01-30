ba0bb7722bf11bfc2700270b65729f9d86ac3277
Luke Easy <le590@york.ac.uk>
2015-01-30 16:19:04 +0000

    Changes:
    Previously the bracket operator always returned a CELL_CENTRE field.  This would cause problems on when using using the bracket on staggered fields.  Now, bracket will a return a field whose position is the same of the two input fields.  An error will be produced if the two input fields are not in the same location.

--- include/difops.hxx
+++ include/difops.hxx
@@ -129,9 +129,10 @@ const Field3D b0xGrad_dot_Grad(const Field3D &phi, const Field3D &A, CELL_LOC ou
 
 // Poisson bracket methods
 enum BRACKET_METHOD {BRACKET_STD=0, BRACKET_SIMPLE=1, BRACKET_ARAKAWA=2, BRACKET_CTU=3};
-const Field2D bracket(const Field2D &f, const Field2D &g, BRACKET_METHOD method = BRACKET_STD, Solver *solver = NULL);
-const Field3D bracket(const Field2D &f, const Field3D &g, BRACKET_METHOD method = BRACKET_STD, Solver *solver = NULL);
-const Field3D bracket(const Field3D &f, const Field2D &g, BRACKET_METHOD method = BRACKET_STD, Solver *solver = NULL);
-const Field3D bracket(const Field3D &f, const Field3D &g, BRACKET_METHOD method = BRACKET_STD, Solver *solver = NULL);
+const Field2D bracket(const Field2D &f, const Field2D &g, BRACKET_METHOD method = BRACKET_STD, CELL_LOC outloc=CELL_DEFAULT, Solver *solver = NULL);
+const Field3D bracket(const Field2D &f, const Field3D &g, BRACKET_METHOD method = BRACKET_STD, CELL_LOC outloc=CELL_DEFAULT, Solver *solver = NULL);
+const Field3D bracket(const Field3D &f, const Field2D &g, BRACKET_METHOD method = BRACKET_STD, CELL_LOC outloc=CELL_DEFAULT, Solver *solver = NULL);
+const Field3D bracket(const Field3D &f, const Field3D &g, BRACKET_METHOD method = BRACKET_STD, CELL_LOC outloc=CELL_DEFAULT, Solver *solver = NULL);
+
 
 #endif /* __DIFOPS_H__ */
