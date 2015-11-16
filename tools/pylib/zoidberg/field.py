84d3f234c59745d03eb1cf8c2ab2576b94207de7
Peter Hill <zed.three@gmail.com>
2015-11-16 14:12:08 +0000

    Add zoidberg class to read VMEC files

--- tools/pylib/zoidberg/field.py
+++ tools/pylib/zoidberg/field.py
@@ -212,3 +212,174 @@ class Stellarator(MagneticField):
         self.Bzfunc = lambdify((self.x, self.z, self.phi), Bz, "numpy")
 
         super().__init__(self.grid, self.Bxfunc, self.Bzfunc)
+
+
+class VMEC(object):
+    """Read a VMEC equilibrium file
+    """
+
+    def __weird_average(self, field):
+        result = np.zeros_like(field)
+        result[:,0]    = field[:,1] - 0.5*field[:,2]
+        result[:,2:-2] = 0.5 * (field[:,2:-2] + field[:,3:-1])
+        result[:,-1]   = 2*field[:,-2] - field[:,-3]
+        return result
+
+    def cfunct(self, field):
+        ns = field.shape[0]
+        lt = self.theta.size
+        lz = self.zeta.size
+        # Create mode x angle arrays
+        mt = self.xm[:,np.newaxis]*self.theta
+        nz = self.xn[:,np.newaxis]*self.zeta
+        # Create Trig Arrays
+        cosmt = np.cos(mt)
+        sinmt = np.sin(mt)
+        cosnz = np.cos(nz)
+        sinnz = np.sin(nz)
+        # Calculate the transform
+        f = np.zeros((ns,lt,lz))
+        for k, field_slice in enumerate(field):
+            rmn = np.repeat(field_slice[:,np.newaxis], lt, axis=1)
+            a = rmn*cosmt
+            b = np.dot(a.T, cosnz)
+            # print("a: {}, b: {}".format(a.shape, b.shape))
+            c = rmn*sinmt
+            d = np.dot(c.T, sinnz)
+            # print("c: {}, d: {}".format(c.shape, d.shape))
+            f[k,:,:] = b - d
+        return f
+
+    def sfunct(self, field):
+        ns = field.shape[0]
+        lt = self.theta.size
+        lz = self.zeta.size
+        # Create mode x angle arrays
+        mt = self.xm[:,np.newaxis]*self.theta
+        nz = self.xn[:,np.newaxis]*self.zeta
+        # Create Trig Arrays
+        cosmt = np.cos(mt)
+        sinmt = np.sin(mt)
+        cosnz = np.cos(nz)
+        sinnz = np.sin(nz)
+        # Calculate the transform
+        f = np.zeros((ns,lt,lz))
+        for k, field_slice in enumerate(field):
+            rmn = np.repeat(field_slice[:,np.newaxis], lt, axis=1)
+            a = rmn*sinmt
+            b = np.dot(a.T, cosnz)
+            c = rmn*cosmt
+            d = np.dot(c.T, sinnz)
+            f[k,:,:] = b + d
+        return f
+
+    def __init__(self, grid, vmec_file, ntheta=None, nzeta=None, nr=32, nz=32):
+        # Only needed here
+        from boututils.datafile import DataFile
+        from scipy.interpolate import griddata, RegularGridInterpolator
+        
+        # Read necessary stuff
+        with DataFile(vmec_file, write=False) as f:
+            self.xm = f['xm'].T
+            self.xn = f['xn'].T
+            ns = int(f['ns'])
+            xm_big = np.repeat(self.xm[:,np.newaxis], ns, axis=1)
+            xn_big = np.repeat(self.xn[:,np.newaxis], ns, axis=1)
+            # s and c seem to swap meanings here...
+            rumns = -f['rmnc'].T*xm_big
+            rvmns = -f['rmnc'].T*xn_big
+            zumnc = f['zmns'].T*xm_big
+            zvmnc = f['zmns'].T*xn_big
+
+            try:
+                iasym = f['iasym']
+            except KeyError:
+                iasym = 0
+
+            if iasym:
+                rumnc = -f['rmns'].T*xm_big
+                rvmnc = -f['rmns'].T*xn_big
+                zumns = f['zmnc'].T*xm_big 
+                zvmns = f['zmnc'].T*xn_big 
+
+            bsupumnc = self.__weird_average(f['bsupumnc'].T).T
+            bsupvmnc = self.__weird_average(f['bsupvmnc'].T).T
+
+            if ntheta is None:
+                self.ntheta = int(f['mpol'])
+            else:
+                self.ntheta = ntheta
+
+            if nzeta is None:
+                self.nzeta = int(f['ntor']) + 1
+            else:
+                self.nzeta = nzeta
+
+            self.theta = np.linspace(0, 2*np.pi, self.ntheta)
+            self.zeta  = np.linspace(0, 2*np.pi, self.nzeta)
+            # R, Z on (s, theta, zeta)
+            self.r_stz = self.cfunct(f['rmnc'])
+            self.z_stz = self.sfunct(f['zmns'])
+
+            bu = self.cfunct(bsupumnc)
+            bv = self.cfunct(bsupvmnc)
+
+            drdu = self.sfunct(rumns.T)
+            drdv = self.sfunct(rvmns.T)
+            dzdu = self.cfunct(zumnc.T)
+            dzdv = self.cfunct(zvmnc.T)
+            if iasym:
+                self.r_stz = self.r_stz + self.sfunct(f['rmnc'])
+                drdu = drdu + self.cfunct(rumnc.T)
+                drdv = drdv + self.cfunct(rvmnc.T)
+                self.z_stz = self.z_stz + self.cfunct(f['zmnc'])
+                dzdu = dzdu + self.sfunct(zumns.T)
+                dzdv = dzdv + self.sfunct(zvmns.T)
+
+        # Convert to bR, bZ, bphi
+        self.br = bu*drdu + bv*drdv
+        self.bphi = self.r_stz*bv
+        self.bz = bu*dzdu + bv*dzdv
+        # self.br_pol = bu*drdu;
+        # self.bz_pol = bu*dzdu;
+
+        # Make a new rectangular grid in (R,Z)
+        self.r_1D = np.linspace(self.r_stz.min(), self.r_stz.max(), nr)
+        self.z_1D = np.linspace(self.z_stz.min(), self.z_stz.max(), nr)
+        self.R_2D, self.Z_2D = np.meshgrid(self.r_1D, self.z_1D, indexing='ij')
+
+        # Interpolate 
+        self.br_rz = np.zeros( (nr, nz, self.nzeta) )
+        self.bz_rz = np.zeros( (nr, nz, self.nzeta) )
+        self.bphi_rz = np.zeros( (nr, nz, self.nzeta) )
+
+        for k, (br, bz, bphi, r, z) in enumerate(zip(self.br.T, self.bz.T, self.bphi.T, self.r_stz.T, self.z_stz.T)):
+            points = np.column_stack( (r.flatten(), z.flatten()) )
+            self.br_rz[...,k] = griddata(points, br.flatten(), (self.R_2D, self.Z_2D),
+                                         method='linear', fill_value=0.0)
+            self.bz_rz[...,k] = griddata(points, bz.flatten(), (self.R_2D, self.Z_2D),
+                                         method='linear', fill_value=0.0)
+            self.bphi_rz[...,k] = griddata(points, bphi.flatten(), (self.R_2D, self.Z_2D),
+                                           method='linear', fill_value=1.0)
+
+        # zeta_grid = np.repeat(self.zeta[np.newaxis, :], self.R[1], axis=0)
+        # zeta_grid = np.repeat(zeta_grid[np.newaxis, :, :], ns, axis=0)
+        points = ( self.r_1D, self.z_1D, self.zeta )
+        self.br_interp = RegularGridInterpolator(points, self.br_rz, bounds_error=False, fill_value=0.0)
+        self.bz_interp = RegularGridInterpolator(points, self.bz_rz, bounds_error=False, fill_value=0.0)
+        self.bphi_interp = RegularGridInterpolator(points, self.bphi_rz, bounds_error=False, fill_value=1.0)
+
+        def Bxfunc(x, z, phi):
+            phi = np.mod(phi, 2.*np.pi)
+            return self.br_interp((x, z, phi))
+
+        def Bzfunc(x, z, phi):
+            phi = np.mod(phi, 2.*np.pi)
+            return self.bz_interp((x, z, phi))
+
+        def bphifunc(x, z, phi):
+            phi = np.mod(phi, 2.*np.pi)
+            return self.bphi_interp((x, z, phi))
+
+        self.magnetic_field = MagneticField(grid, Bxfunc, Bzfunc)
+======= end
