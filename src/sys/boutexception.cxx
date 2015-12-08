5c5ebc74e10e08b71c42401a2144d205160096d9
David Schwörer <schword2@mail.dcu.ie>
2015-12-08 20:17:44 +0000

    basic backtrace in exception

--- src/sys/boutexception.cxx
+++ src/sys/boutexception.cxx
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <stdarg.h>
 #include <output.hxx>
+#define BACKTRACE
+#ifdef BACKTRACE
+#include <execinfo.h>
+#endif
 
 void BoutParallelThrowRhsFail(int &status, const char* message) {
   int allstatus;
@@ -17,10 +21,53 @@ BoutException::~BoutException() throw()
 {
 }
 
+void BoutException::Backtrace(){
+#ifdef BACKTRACE
+  void *trace[64];
+  char **messages = (char **)NULL;
+  int i, trace_size = 0;
+  
+  trace_size = backtrace(trace, 64);
+  messages = backtrace_symbols(trace, trace_size);
+  /* skip first stack frame (points here) */
+  output.write("\n[bt] Execution path:\n");
+  for (i=1; i<trace_size; ++i)
+    {
+      output.write("[bt] #%d %s\n", i, messages[i]);
+      
+      /* find first occurence of '(' or ' ' in message[i] and assume
+       * everything before that is the file name. (Don't go beyond 0 though
+       * (string terminator)*/
+      size_t p = 0;
+      while(messages[i][p] != '(' && messages[i][p] != ' '
+	    && messages[i][p] != 0)
+	++p;
+      
+      char syscom[256];
+      sprintf(syscom,"addr2line %p -Cfpie %.*s", trace[i], p, messages[i]);
+      //last parameter is the file name of the symbol
+      FILE * fp = popen(syscom, "r");
+      if (fp!= NULL){
+	char out[1024];
+	fgets(out, sizeof(out)-1, fp);
+	if (pclose(fp) == 0){
+	  output.write(out);
+	}
+      } else {
+	output.write(syscom);
+      }
+    }
+#else
+  output.write("Backtrace not enabled!\n");
+#endif
+}
+
 BoutException::BoutException(const char* s, ...)
 {
+  this->Backtrace();
+  
   va_list ap;  // List of arguments
-
+  
   if(s == (const char*) NULL)
     return;
   
