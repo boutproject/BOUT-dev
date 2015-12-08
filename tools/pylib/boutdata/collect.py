acd12ab2da85b0483c3d8414ff954d7db1ce1a59
David Schwörer <schword2@mail.dcu.ie>
2015-12-08 21:38:33 +0000

    readded future statements

--- tools/pylib/boutdata/collect.py
+++ tools/pylib/boutdata/collect.py
@@ -1,5 +1,5 @@
-#from __future__ import print_function
-#from __future__ import division
+from __future__ import print_function
+from __future__ import division
 try:
     from builtins import str
 except:
