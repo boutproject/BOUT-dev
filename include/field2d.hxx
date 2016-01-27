98803aa184161ff8e0c092a65c20b5f605443b15
Ben Dudson <bd512@york.ac.uk>
2016-01-27 04:53:49 +0000

    Removing unused ioSupport functions
    getData(int)
    zeroComponent(int)
    getSuffix(int)
    getMark()
    setMark()
    ioSupport()
    
    This fixes issue #136

--- include/field2d.hxx
+++ include/field2d.hxx
@@ -168,16 +168,7 @@ class Field2D : public Field, public FieldData {
   int  getData(int x, int y, int z, BoutReal *rptr) const;
   int  setData(int x, int y, int z, void *vptr);
   int  setData(int x, int y, int z, BoutReal *rptr);
-
-  bool ioSupport() { return true; } ///< This class supports I/O operations
-  BoutReal *getData(int component) { 
-    BoutReal **d = getData();
-    return *d;
-  }
-  void zeroComponent(int component){
-    *this = 0.0;
-  }
-
+  
 #ifdef CHECK
   bool checkData(bool vital = true) const; ///< Checks if the data is all valid.
   void doneComms() { bndry_xin = bndry_xout = bndry_yup = bndry_ydown = true; }
