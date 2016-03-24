c0128adc5ff826f4331efab3c3b33e7b23ff121f
David Schwörer <schword2@mail.dcu.ie>
2016-03-24 02:44:17 +0000

    added single loop iteration for speed test

--- examples/test-fielditerator/speed/speed.cxx
+++ examples/test-fielditerator/speed/speed.cxx
@@ -43,10 +43,23 @@ int physics_init(bool restarting) {
     }
   }
   double t3=omp_get_wtime();
+  {
+    const int mymax=mesh->ngx*mesh->ngy*mesh->ngz;
+    double * data=d3[0][0];
+    for (int i=0;i<iter;++i){
+      double myrand=rand();
+#pragma omp parallel
+      for (int j=0;j < mymax;++j){
+	data[j]=myrand;
+      }
+    }
+  }
+  double t4=omp_get_wtime();
 
   output.write("\n\n\n\t***** RESULTS *****\n\n  ***********************************\n");
   output.write("    FieldIteratorCIndex: %f\n",t2-t1);
   output.write("    triple for loop    : %f\n",t3-t2);
+  output.write("    single for loop    : %f\n",t4-t3);
   output.write("  ***********************************\n\n\n\n\n\n");
 
   
