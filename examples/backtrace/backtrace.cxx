6e24ea3b6771a86a7c0c448292d0d420264ab59d
David Schwörer <schword2@mail.dcu.ie>
2016-04-19 20:55:11 +0100

    Update example/backtrace to generate an FPE if enabled

--- examples/backtrace/backtrace.cxx
+++ examples/backtrace/backtrace.cxx
@@ -7,7 +7,11 @@
 
 
 int physics_init(bool restarting) {
-
+  BoutReal a=1;
+  BoutReal b=0;
+  BoutReal c = a/b;
+  output.write("c is %f\n",c);
+  
   BoutException("Tomatoes are red?\n");
   
   return 1;
