709e791e82c1e40d7956be3df76436913b8e0b71
Ben Dudson <bd512@york.ac.uk>
2016-08-15 10:35:13 +0100

    BoutMesh throws error if ShiftAngle not found
    If MYG > 0 and TwistShift=true, so ShiftAngle is used, then ShiftAngle
    should be defined. Previously it was calculated from zShift
    and a warning was printed, but this could be easily missed.
    
    Now an error is thrown unless the options shiftangle_from_zshift
    is set to true (in [mesh] section).

--- src/mesh/impls/bout/boutmesh.hxx
+++ src/mesh/impls/bout/boutmesh.hxx
@@ -112,6 +112,8 @@ class BoutMesh : public Mesh {
   BoutReal Vol_Integral(const Field2D &var);
 
  private:
+  Options *meshoptions; ///< Handle for mesh options
+
   string gridname;
   int nx, ny;        ///< Size of the grid in the input file
   int MX, MY;        ///< size of the grid excluding boundary regions
