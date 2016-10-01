70195c6911f3e03891e1f1e7967123b0dc6bdc67
David Schwörer <schword2@mail.dcu.ie>
2016-10-01 01:12:35 +0100

    new test: test-dataiterator2

---
+++ examples/test-dataiterator2/test_dataiterator.cxx
@@ -0,0 +1,46 @@
+/*
+ * Test Cyclic Reduction parallel solver
+ *
+ */
+
+#include <bout.hxx>
+#include <boutmain.hxx>
+
+//#include <fielditerator.hxx>
+#include <assert.h>
+
+#include <unistd.h>
+
+Field3D n;
+
+
+int physics_init(bool restarting) {
+
+  /**********************************************
+   * * *                                    * * *
+   * * *          Field3D test              * * *
+   * * *                                    * * *
+   **********************************************/
+  
+  Field3D d3=1;
+  Field2D d2=1;
+  FieldPerp dp;
+
+  #include "test3d.cxx"
+  
+  /**********************************************
+   * * *                                    * * *
+   * * *          Field2D test              * * *
+   * * *                                    * * *
+   **********************************************/
+
+
+  SOLVE_FOR(n);
+  return 0;
+}
+
+int physics_run(BoutReal t) {
+  // Doesn't do anything
+  ddt(n)=0;
+  return 0;
+}
