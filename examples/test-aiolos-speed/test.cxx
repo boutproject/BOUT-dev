3bca490d0890a706eb41db7cdfd8019c9d0d3f1e
David Schwörer <schword2@mail.dcu.ie>
2016-10-14 14:30:22 +0100

    simple speedtest for aiolos

---
+++ examples/test-aiolos-speed/test.cxx
@@ -0,0 +1,31 @@
+#include <boutmain.hxx>
+#include <derivs.hxx>
+#include <field_factory.hxx>
+
+#include <assert.h>
+
+Field3D n;
+
+void compare(Field3D a, Field3D b);
+
+int physics_init(bool restart) {
+  n=1;
+  int iter;
+  OPTION(Options::getRoot(),iter,10);
+  for (int i =0; i < iter; ++i){
+    mesh->indexDDX(n,CELL_DEFAULT,DIFF_DEFAULT);
+    mesh->indexDDY(n,CELL_DEFAULT,DIFF_DEFAULT);
+    mesh->indexDDZ(n,CELL_DEFAULT,DIFF_DEFAULT,true);
+    mesh->indexD2DX2(n,CELL_DEFAULT,DIFF_DEFAULT);
+    mesh->indexD2DY2(n,CELL_DEFAULT,DIFF_DEFAULT);
+    mesh->indexD2DZ2(n,CELL_DEFAULT,DIFF_DEFAULT);
+  }
+  
+  return 0;
+}
+
+
+int physics_run(BoutReal time) {
+  ddt(n)=0;
+  return 0;
+}
