07d99098591e398a5b73d3ef6b36f40f9d7b499d
Michael Løiten <michael.l.magnussen@gmail.com>
2016-11-08 09:52:35 +0100

    Bugfix of bugix
    (Sorry for the spam)

--- tools/pylib/bout_runners/bout_runners.py
+++ tools/pylib/bout_runners/bout_runners.py
@@ -676,10 +676,8 @@ class basic_runner(object):
                     # Call the post processing function
                     self._call_post_processing_function(\
                             function = post_processing_function,\
-                            folders  = tuple(self._dmp_folder),\
+                            folders  = (self._dmp_folder,),\
                             **kwargs)
-                    # Reset the seq_of_dmp_folders
-                    seq_of_dmp_folders = []
                 else:
                     # Append the dmp folder to the list of dmp folders
                     seq_of_dmp_folders.append(self._dmp_folder)
