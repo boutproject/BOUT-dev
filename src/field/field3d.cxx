13abf476dda53cbcee0c8e18d76f112f8e40829d
David Schwörer <schword2@mail.dcu.ie>
2016-11-24 18:54:25 +0000

    Draft for faster field operation

--- src/field/field3d.cxx
+++ src/field/field3d.cxx
@@ -837,15 +837,15 @@ F3D_OP_FPERP(*);
     return result;                                                  \
   }
 
-F3D_OP_FIELD(+, Field3D);   // Field3D + Field3D
-F3D_OP_FIELD(-, Field3D);   // Field3D - Field3D
-F3D_OP_FIELD(*, Field3D);   // Field3D * Field3D
-F3D_OP_FIELD(/, Field3D);   // Field3D / Field3D
+//F3D_OP_FIELD(+, Field3D);   // Field3D + Field3D
+//F3D_OP_FIELD(-, Field3D);   // Field3D - Field3D
+//F3D_OP_FIELD(*, Field3D);   // Field3D * Field3D
+//F3D_OP_FIELD(/, Field3D);   // Field3D / Field3D
 
-F3D_OP_FIELD(+, Field2D);   // Field3D + Field2D
-F3D_OP_FIELD(-, Field2D);   // Field3D - Field2D
-F3D_OP_FIELD(*, Field2D);   // Field3D * Field2D
-F3D_OP_FIELD(/, Field2D);   // Field3D / Field2D
+//F3D_OP_FIELD(+, Field2D);   // Field3D + Field2D
+//F3D_OP_FIELD(-, Field2D);   // Field3D - Field2D
+//F3D_OP_FIELD(*, Field2D);   // Field3D * Field2D
+//F3D_OP_FIELD(/, Field2D);   // Field3D / Field2D
 
 #define F3D_OP_REAL(op)                                         \
   const Field3D operator op(const Field3D &lhs, BoutReal rhs) { \
