1e068dee84510fd5b0b5195ebb98373ea6cd6a38
David Schwörer <schword2@mail.dcu.ie>
2017-02-10 17:40:17 +0000

    fix init of dataiterator in openmp case

--- include/bout/dataiterator.hxx
+++ include/bout/dataiterator.hxx
@@ -80,8 +80,8 @@ public:
     xend(xe),     yend(ye),     zend(ze),
     xmax(xend),   ymax(yend),   zmax(zend),
 #else
-    xmin(xstart), ymin(ys),     zmin(zs),
-    xmax(xend),   ymax(ye),     zmax(ze),
+    xmin(xs),     ymin(ys),     zmin(zs),
+    xmax(xe),     ymax(ye),     zmax(ze),
 #endif
     isEnd(false)
   {
