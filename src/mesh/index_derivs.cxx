306d342878dee2ddc9bbb42381f52801282994c8
David Schwörer <schword2@mail.dcu.ie>
2017-02-21 17:57:44 +0000

    throw if diffmethod is unknown

--- src/mesh/index_derivs.cxx
+++ src/mesh/index_derivs.cxx
@@ -719,28 +719,19 @@ DIFF_METHOD lookupFunc(DiffLookup *table, const string &label) {
   matchtype = DIFF_DEFAULT;
 
   // Loop through the name lookup table
-  int i = 0;
-  do {
-    if((toupper(DiffNameTable[i].label[0]) == toupper(label[0])) && isImplemented(table, DiffNameTable[i].method)) {
-      matchtype = DiffNameTable[i].method;
-
-      if(strcasecmp(label.c_str(), DiffNameTable[i].label) == 0) {// Whole match
-	return matchtype;
-      }
+  for (int i = 0; DiffNameTable[i].method != DIFF_DEFAULT ; ++i ){
+    if(strcasecmp(label.c_str(), DiffNameTable[i].label) == 0) {// Whole match
+      return matchtype;
     }
-    i++;
-  }while(DiffNameTable[i].method != DIFF_DEFAULT);
+  }
   
-  // No exact match, so return matchtype.
-
-  if(matchtype == DIFF_DEFAULT) {
-    // No type match either. Return the first value in the table
-    matchtype = table[0].method;
-    output << " No match for '" << label << "' -> ";
-  }else
-    output << " Type match for '" << label << "' ->";
-
-  return matchtype;
+  // No exact match, so throw
+  std::string avail{};
+  for (int i = 0; DiffNameTable[i].method != DIFF_DEFAULT ; ++i ){
+    avail += DiffNameTable[i].label;
+    avail += "\n";
+  }
+  throw BoutException("Unknown option %s.\nAvailable options are:\n%s",label.c_str(),avail.c_str());
 }
 
 void printFuncName(DIFF_METHOD method) {
