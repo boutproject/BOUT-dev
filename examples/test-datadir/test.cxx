43b6aa00f3551c86c87eb9bde77119d792a10eae
David Schwörer <schword2@mail.dcu.ie>
2017-04-20 17:25:15 +0100

    improve test datadir

--- examples/test-datadir/test.cxx
+++ examples/test-datadir/test.cxx
@@ -1,15 +1,5 @@
-#include <boutmain.hxx>
+#include <bout.hxx>
 
-Field3D n;
-
-int physics_init(bool restart) {
-  SOLVE_FOR(n); 
-  output.write("Everything is fine");
-  return 0;
-}
-
-
-int physics_run(BoutReal time) {
-  ddt(n)=0;
-  return 0;
+int main(int argc, char **argv) {
+  return BoutInitialise(argc, argv);
 }
