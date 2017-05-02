0bd7b721bd4862979528d5d8b82f25d858192ceb
David Dickinson <d7919@users.noreply.github.com>
2017-05-02 12:41:16 +0100

    Adding nyInterp and withRepeat options to pol_slice
    The withRepeat input can be used to indicate if the data file includes
    the duplicate z grid point (e.g. for compatibility with bout++ < v4).
    
    The nyInterp input can be used to request interpolation in the
    y (parallel) direction prior to constructing the poloidal cross
    section. The idea here is that we tend to expect things to be slowly
    varying in the parallel direction so interpolation should be reasonable,
    and we often want more points in the poloidal direction than actually
    used in the simulation for nice figures.

--- tools/pylib/boutdata/pol_slice.py
+++ tools/pylib/boutdata/pol_slice.py
@@ -20,8 +20,12 @@ except ImportError:
     print("=> Set $PYTHONPATH variable to include BOUT++ pylib")
     raise SystemExit
 
-def pol_slice(var3d, gridfile, n=1, zangle=0.0):
-    """ data2d = pol_slice(data3d, 'gridfile', n=1, zangle=0.0) """
+def pol_slice(var3d, gridfile, n=1, zangle=0.0, withRepeat=False, nyInterp=None):
+    """ data2d = pol_slice(data3d, 'gridfile', n=1, zangle=0.0) 
+    By default data3d will not include the repeated z point. If it does
+    contain this point one should set withRepeat=True to get the correct result.
+    Set nyInterp to the number of y (theta) points you wish to use in the final result.
+    """
     n = int(n)
     zangle = float(zangle)
 
@@ -31,6 +35,10 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0):
         return None
 
     nx, ny, nz = s
+
+    #Fix to account for repeat point which is now usually dropped
+    if not withRepeat:
+        nz = nz+1 #Increase the nz value to include the repeated point, which is not present in var3d
 
     dz = 2.*np.pi / float(n * (nz-1))
 
@@ -64,6 +72,43 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0):
         print("ERROR: pol_slice couldn't read grid file")
         return None
 
+    #Decide if we've asked to do interpolation
+    doInterp = False
+    if nyInterp is not None:
+        if ny != nyInterp:
+            doInterp = True
+
+    #Setup interpolation if requested
+    if doInterp:
+        try:
+            from numpy import linspace, meshgrid, append, newaxis
+        except ImportError:
+            print("ERROR: NumPy module not available")
+            raise
+        try:
+            from scipy.ndimage import map_coordinates
+        except ImportError:
+            print("ERROR: SciPy module not available")
+            raise
+
+        varTmp = var3d
+        #These are the index space co-ordinates of the output arrays
+        xOut = linspace(0,nx-1,nx)
+        yOut = linspace(0,ny-1,nyInterp)
+        zOut = linspace(0,nz-1,nz)
+
+        #Use meshgrid to create 3 length N arrays (N=total number of points)
+        xx,yy,zz = meshgrid(xOut,yOut,zOut,indexing='ij')
+        #Interpolate to output positions and make the correct shape
+        var3d = map_coordinates(input=varTmp,coordinates=[xx,yy,zz],cval=-999)
+
+        #As above
+        xx,yy = meshgrid(xOut,yOut,indexing='ij')
+        zShift = map_coordinates(zShift,[xx,yy],cval=-999)
+
+        #Update shape
+        ny = nyInterp
+
     var2d = np.zeros([nx, ny])
 
     ######################################
@@ -81,10 +126,36 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0):
     zm = (z0 - 1 + (nz-1)) % (nz-1)
 
     # There may be some more cunning way to do this indexing
-    for x in np.arange(nx):
-        for y in np.arange(ny):
-            var2d[x,y] = 0.5*p[x,y]*(p[x,y]-1.0) * var3d[x,y,zm[x,y]] + \
-                         (1.0 - p[x,y]*p[x,y])   * var3d[x,y,z0[x,y]] + \
-                         0.5*p[x,y]*(p[x,y]+1.0) * var3d[x,y,zp[x,y]]
+    # for x in np.arange(nx):
+    #     for y in np.arange(ny):
+    #         var2d[x,y] = 0.5*p[x,y]*(p[x,y]-1.0) * var3d[x,y,zm[x,y]] + \
+    #                      (1.0 - p[x,y]*p[x,y])   * var3d[x,y,z0[x,y]] + \
+    #                      0.5*p[x,y]*(p[x,y]+1.0) * var3d[x,y,zp[x,y]]
+
+    # var2dList = [x.T for x in var3d.T]
+    # var2d = 0.5*p*(p-1.0) * np.choose(zm,var2dList) + \
+    #         (1.0 - p*p)   * np.choose(z0,var2dList) + \
+    #         0.5*p*(p+1.0) * np.choose(zp,var2dList) 
+
+    #For some reason numpy imposes a limit of 32 entries to choose
+    #so if nz>32 we have to use a different approach
+    if nz >= 32:
+        #Untested
+        # v3m = np.array([var3d.T[zm.T[I]][I] for I in ndi.ndindex(zm.T.shape)]).T
+        # v30 = np.array([var3d.T[z0.T[I]][I] for I in ndi.ndindex(z0.T.shape)]).T
+        # v3p = np.array([var3d.T[zp.T[I]][I] for I in ndi.ndindex(zp.T.shape)]).T
+        # var2d = 0.5*p*(p-1.0) * v3m + \
+        #         (1.0 - p*p) * v30 + \
+        #         0.5*p*(p+1.0) * v3p
+        for x in np.arange(nx):
+            for y in np.arange(ny):
+                var2d[x,y] = 0.5*p[x,y]*(p[x,y]-1.0) * var3d[x,y,zm[x,y]] + \
+                             (1.0 - p[x,y]*p[x,y])   * var3d[x,y,z0[x,y]] + \
+                             0.5*p[x,y]*(p[x,y]+1.0) * var3d[x,y,zp[x,y]]
+    else:
+        var2d = 0.5*p*(p-1.0) * np.choose(zm.T,var3d.T).T + \
+                (1.0 - p*p) * np.choose(z0.T,var3d.T).T + \
+                0.5*p*(p+1.0) * np.choose(zp.T,var3d.T).T
+
 
     return var2d
