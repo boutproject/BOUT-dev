80bb9230b94d97137b916c9c188baab0c790f2ae
Peter Hill <zed.three@gmail.com>
2017-06-13 14:22:39 +0100

    Finish tests for RangeIterator

--- tests/sys/test_range.cxx
+++ tests/sys/test_range.cxx
@@ -109,7 +109,32 @@ TEST(RangeTest, JoinRangeOperator) {
   }
 }
 
-TEST(RangeTest, RemoveRangeOperator) {
+TEST(RangeTest, RemoveRangeOperatorTotalOverlap) {
+  std::vector<int> full_range = {0, 1, 2, 3, 4};
+  RangeIterator range_a(0, 10);
+  RangeIterator range_b(0, 10);
+  range_a -= range_b;
+
+  range_a.first();
+  EXPECT_TRUE(range_a.isDone());
+}
+
+TEST(RangeTest, RemoveRangeOperatorTotalOverlapWithJoined) {
+  std::vector<int> full_range = {11, 12, 13, 14, 15};
+  int count = 0;
+  RangeIterator range_a(11, 15);
+  RangeIterator range_b(0, 10);
+  RangeIterator range_c(0, 10, range_a);
+  range_c -= range_b;
+
+  for (range_c.first(); !range_c.isDone(); ++range_c, ++count) {
+    EXPECT_EQ(*range_c, full_range[count]);
+    ASSERT_TRUE(*range_c > 10);
+    ASSERT_TRUE(*range_c <= 15);
+  }
+}
+
+TEST(RangeTest, RemoveRangeOperatorUpper) {
   std::vector<int> full_range = {0, 1, 2, 3, 4};
   int count = 0;
   RangeIterator range_a(0, 10);
@@ -119,6 +144,33 @@ TEST(RangeTest, RemoveRangeOperator) {
   for (range_a.first(); !range_a.isDone(); ++range_a, ++count) {
     EXPECT_EQ(*range_a, full_range[count]);
     ASSERT_TRUE(*range_a <= 4);
+  }
+}
+
+TEST(RangeTest, RemoveRangeOperatorLower) {
+  std::vector<int> full_range = {6, 7, 8, 9, 10};
+  int count = 0;
+  RangeIterator range_a(0, 10);
+  RangeIterator range_b(0, 5);
+  range_a -= range_b;
+
+  for (range_a.first(); !range_a.isDone(); ++range_a, ++count) {
+    EXPECT_EQ(*range_a, full_range[count]);
+    ASSERT_TRUE(*range_a > 5);
+    ASSERT_TRUE(*range_a <= 10);
+  }
+}
+
+TEST(RangeTest, RemoveRangeOperatorMiddle) {
+  std::vector<int> full_range = {0, 1, 2, 8, 9, 10};
+  int count = 0;
+  RangeIterator range_a(0, 10);
+  RangeIterator range_b(3, 7);
+  range_a -= range_b;
+
+  for (range_a.first(); !range_a.isDone(); ++range_a, ++count) {
+    EXPECT_EQ(*range_a, full_range[count]);
+    ASSERT_TRUE(count <= 6);
   }
 }
 
@@ -164,9 +216,19 @@ TEST(RangeTest, RangesIntersectAll) {
   RangeIterator range_a(5, 7);
   RangeIterator range_b(10, 20);
   RangeIterator range_c(0, 10, range_b);
+  RangeIterator range_d(15, 17);
+  RangeIterator range_e(25, 27);
 
   EXPECT_TRUE(range_a.intersects(range_c, true));
   EXPECT_TRUE(range_c.intersects(range_a, true));
+  EXPECT_TRUE(range_c.intersects(range_d, true));
+  // intersects not commutative when all=true?
+  // EXPECT_TRUE(range_d.intersects(range_c, true));
+
+  EXPECT_FALSE(range_e.intersects(range_c, true));
+  EXPECT_FALSE(range_c.intersects(range_e, true));
+  EXPECT_FALSE(range_c.intersects(range_e, true));
+  EXPECT_FALSE(range_e.intersects(range_c, true));
 }
 
 TEST(RangeTest, Equality) {
@@ -177,4 +239,37 @@ TEST(RangeTest, Equality) {
   EXPECT_FALSE(range_a != range_a);
   EXPECT_TRUE(range_a != range_b);
   EXPECT_FALSE(range_a == range_b);
+}
+
+TEST(RangeTest, Assignment) {
+  RangeIterator range_a(0, 1);
+  RangeIterator range_b(2, 3);
+
+  range_a = range_b;
+  range_a.first();
+  EXPECT_EQ(*range_a, 2);
+}
+
+TEST(RangeTest, Copy) {
+  RangeIterator range_a(0, 1);
+  RangeIterator range_b(range_a);
+
+  range_b.first();
+  EXPECT_EQ(*range_a, 0);
+}
+
+TEST(RangeTest, CopyJoined) {
+  RangeIterator range_a(2, 3);
+  RangeIterator range_b(0, 1, &range_a);
+
+  range_b.first();
+  EXPECT_EQ(*range_b, 0);
+}
+
+TEST(RangeTest, CopyJoinedNullRange) {
+  RangeIterator range_a(2, 3);
+  RangeIterator range_b(1, 0, range_a);
+
+  range_b.first();
+  EXPECT_EQ(*range_b, 2);
 }
