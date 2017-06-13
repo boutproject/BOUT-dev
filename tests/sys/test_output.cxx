45f728d1fe1c6a2a88b7878c22bd4590fefba76c
Peter Hill <zed.three@gmail.com>
2017-06-13 17:21:13 +0100

    Add more tests for Output; adapt `Output::print()` for testing
    `Output::print()` used `vprintf` instead of removing `file` from
    `multioutbuf`, which made it difficult to test. This commit changes it
    to mirror `Output::enable/disable`.

--- tests/sys/test_output.cxx
+++ tests/sys/test_output.cxx
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "output.hxx"
 
+#include <cstdio>
 #include <string>
 
 // stdout redirection code from https://stackoverflow.com/a/4043813/2043465
@@ -44,16 +45,88 @@ TEST_F(OutputTest, JustStdOutGlobalInstance) {
   EXPECT_EQ(buffer.str(), "Hello, world!\n");
 }
 
+TEST_F(OutputTest, OpenFile) {
+  Output local_output;
+
+  // Get a filename for a temporary file
+  char *filename = std::tmpnam(nullptr);
+
+  std::string test_output = "To stdout and file\n";
+
+  local_output.open(filename);
+  local_output << test_output;
+
+  std::ifstream test_file(filename);
+  std::stringstream test_buffer;
+  test_buffer << test_file.rdbuf();
+
+  EXPECT_EQ(test_output, test_buffer.str());
+  EXPECT_EQ(test_output, buffer.str());
+}
+
+TEST_F(OutputTest, JustPrint) {
+  Output local_output;
+
+  // Get a filename for a temporary file
+  char *filename = std::tmpnam(nullptr);
+
+  std::string test_output = "To stdout only\n";
+
+  local_output.open(filename);
+  local_output.print(test_output.c_str());
+
+  std::ifstream test_file(filename);
+  std::stringstream test_buffer;
+  test_buffer << test_file.rdbuf();
+
+  EXPECT_EQ("", test_buffer.str());
+  EXPECT_EQ(test_output, buffer.str());
+}
+
 TEST_F(OutputTest, DisableEnableStdout) {
   Output local_output;
 
+  // Get a filename for a temporary file
+  char *filename = std::tmpnam(nullptr);
+
+  std::string file_only = "To file only\n";
+  std::string file_and_stdout = "To stdout and file\n";
+
+  // Open temporary file and close stdout
+  local_output.open(filename);
   local_output.disable();
-  local_output << "disabled\n";
 
-  EXPECT_NE(buffer.str(), "disabled\n");
+  local_output << file_only;
 
+  std::ifstream test_file(filename);
+  std::stringstream test_buffer;
+  test_buffer << test_file.rdbuf();
+
+  EXPECT_EQ(file_only, test_buffer.str());
+  EXPECT_EQ("", buffer.str());
+
+  // Enable stdout again
   local_output.enable();
-  local_output << "enabled\n";
+  local_output << file_and_stdout;
 
-  EXPECT_EQ(buffer.str(), "enabled\n");
+  test_buffer << test_file.rdbuf();
+
+  // File should contain both outputs, stdout only latter
+  EXPECT_EQ(file_only + file_and_stdout, test_buffer.str());
+  EXPECT_EQ(file_and_stdout, buffer.str());
+}
+
+TEST_F(OutputTest, CleanupAndGetInstance) {
+  Output *local_output = Output::getInstance();
+  EXPECT_NE(local_output, nullptr);
+
+  local_output->cleanup();
+
+  // Get a new instance
+  Output *new_output = Output::getInstance();
+  EXPECT_NE(local_output, new_output);
+
+  *new_output << "Hello, world!\n";
+
+  EXPECT_EQ(buffer.str(), "Hello, world!\n");
 }
