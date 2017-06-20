604a0215a36756a5bd3d0f173af47088dbf619bf
Ben Dudson <bd512@york.ac.uk>
2017-06-20 17:25:02 +0100

    Adding elliptic grid generator
    Creates a grid between two arbitrarily shaped boundaries
    by solving a nonlinear elliptic equation
    
    $ python poloidal_grid.py

--- tools/pylib/zoidberg/poloidal_grid.py
+++ tools/pylib/zoidberg/poloidal_grid.py
@@ -2,6 +2,7 @@
 Routines for generating structured meshes on poloidal domains
 """
 
+import numpy as np
 from numpy import pi, linspace, sqrt, cos, sin, append, zeros, argmin
 from scipy.interpolate import splrep, splev, interp1d
 from scipy.integrate import cumtrapz
@@ -144,7 +145,7 @@ def circle(R0=1.0, r= 0.5, n=20):
     """
     Creates a pair of RZline objects, for inner and outer boundaries
 
-    >>> inner, outer = circular_boundaries()
+    >>> inner = circle()
     """
     # Define an angle coordinate
     theta = linspace(0,2*pi,n, endpoint=False)
@@ -272,16 +273,154 @@ def grid_weighted_distance(inner, outer, nx, ny, show=True, equidistant=False):
     plt.plot(Rpos, Zpos, 'o')
     plt.show()
     
+def grid_elliptic(inner, outer, nx, ny, show=True, tol=1e-10):
+    """
+    Create a structured grid between inner and outer boundaries
+    using elliptic method
     
+    Coordinates x = x(R, Z) and y = y(R,Z)
+    obey an elliptic equation
+
+    d^2x/dR^2 + d^2x/dZ^2 = 0
+    
+    d^2y/dR^2 + d^2y/dZ^2 = 0
+    
+    where here x is in in the domain (0,1) and y in (0,2pi)
+    
+    The above equations are inverted, giving:
+
+    a*R_xx - 2*b*R_xy + c*R+yy = 0
+    
+    a*Z_xx - 2*b*Z_xy + c*Z_yy = 0
+
+    where 
+
+    a = R_y^2 + Z_y^2
+    b = R_y*R_x + Z_x*Z_y
+    c = R_x^2 + Z_x^2
+    
+    This is a nonlinear system of equations which is solved
+    iteratively.
+    
+    See:
+    https://www.nada.kth.se/kurser/kth/2D1263/l2.pdf
+    https://en.wikipedia.org/wiki/Principles_of_grid_generation
+    """
+    
+    assert nx >= 2
+    assert ny > 1
+    
+    R = zeros((nx, ny))
+    Z = zeros((nx, ny))
+
+    # Order both boundaries by distance
+    inner = inner.orderByDistance()
+    outer = outer.orderByDistance()
+    
+    # Generate angle values (y coordinate), 
+    # which should now be equally spaced
+    # in distance along inner and outer boundaries
+    thetavals = linspace(0, 2*pi, ny, endpoint=False)
+
+    # Radial coordinate
+    xvals = linspace(0, 1.0, nx, endpoint=True)
+    
+    # Interpolate coordinates of inner and outer boundary
+    Rinner = inner.Rvalue(thetavals)
+    Zinner = inner.Zvalue(thetavals)
+    
+    Router = outer.Rvalue(thetavals)
+    Zouter = outer.Zvalue(thetavals)
+    
+    # Interpolate in x between inner and outer
+    # to get starting guess for a grid
+    for i in range(nx):
+        R[i,:] = xvals[i]*Router + (1.-xvals[i])*Rinner
+        Z[i,:] = xvals[i]*Zouter + (1.-xvals[i])*Zinner
+    
+    dx = xvals[1] - xvals[0]
+    dy = thetavals[1] - thetavals[0]
+
+    if show:
+        plt.plot(inner.R, inner.Z)
+        plt.plot(outer.R, outer.Z)
+        #plt.plot(R, Z, 'o') # Starting locations
+        
+    # Start solver loop
+    while True:
+        # Calculate coefficients, which exclude boundary points
+        # Note that the domain is periodic in y so roll arrays
+        
+        R_xm = R[:-2,:]   # R(x-1,y)
+        R_xp = R[2:, :]   # R(x+1,y)
+        R_ym = np.roll(R, 1) # R(x, y-1)
+        R_yp = np.roll(R,-1) # R(x, y+1)
+        R_xmym = R_ym[:-2,:] # R(x-1, y-1)
+        R_xpym = R_ym[2:,:]  # R(x+1, y-1)
+        R_xmyp = R_yp[:-2,:] # R(x-1, y+1)
+        R_xpyp = R_yp[2:,:]  # R(x+1, y+1)
+        R_ym = R_ym[1:-1,:]  # Now chop off boundaries
+        R_yp = R_yp[1:-1,:]  # This is done to minimise number of rolls
+        
+        Z_xm = Z[:-2,:]
+        Z_xp = Z[2:, :]
+        Z_ym = np.roll(Z, 1)
+        Z_yp = np.roll(Z,-1)
+        Z_xmym = Z_ym[:-2,:] 
+        Z_xpym = Z_ym[2:,:]
+        Z_xmyp = Z_yp[:-2,:]
+        Z_xpyp = Z_yp[2:,:]
+        Z_ym = Z_ym[1:-1,:]
+        Z_yp = Z_yp[1:-1,:]
+        
+
+        dRdy = (R_yp - R_ym)/(2.*dy)
+        dRdx = (R_xp - R_xm)/(2.*dx)
+        
+        dZdy = (Z_yp - Z_ym)/(2.*dy)
+        dZdx = (Z_xp - Z_xm)/(2.*dx)
+        
+        a = dRdy**2 + dZdy**2
+        b = dRdy*dRdx + dZdx*dZdy
+        c = dRdx**2 + dZdx**2
+        
+        # Now solve a*R_xx - 2*b*R_xy + c*R_yy = 0
+        # For now using Jacobi update
+    
+        a_dx2 = a/dx**2
+        b_dxdy = b/(2.*dx*dy)
+        c_dy2 = c/dy**2
+        inv_diag = 1./(2*a/dx**2 + 2*c/dy**2)
+        
+        Rold = R.copy()
+        Zold = Z.copy()
+        
+        R[1:-1,:] = ( a_dx2*(R_xm + R_xp) - b_dxdy*(R_xpyp - R_xmyp - R_xpym + R_xmym) + c_dy2*(R_ym + R_yp) ) * inv_diag
+        
+        Z[1:-1,:] = ( a_dx2*(Z_xm + Z_xp) - b_dxdy*(Z_xpyp - Z_xmyp - Z_xpym + Z_xmym) + c_dy2*(Z_ym + Z_yp) ) * inv_diag
+        
+
+        maxchange_sq = np.amax((R-Rold)**2 + (Z-Zold)**2)
+        #print(maxchange_sq)
+        
+        if maxchange_sq < tol:
+            break
+
+    if show:
+        plt.plot(R,Z)
+        plt.plot(np.transpose(R), np.transpose(Z))
+        plt.show()
+        
+    return R, Z
 
 if __name__ == "__main__":
-    inner = circle(R0=1.5, r=1.0, n=50)
-    outer = circle(R0=1.0, r=2.0, n=50)
+    #inner = circle(R0=1.5, r=1.0, n=100)
+    #outer = circle(R0=1.0, r=2.0, n=100)
 
-    #inner = shaped_line(R0=3.0, a=0.5, elong=1.0, triang=0.0, indent=1.0, n=50)
-    #outer = shaped_line(R0=2.8, a=1.5, elong=1.0, triang=0.0, indent=0.2, n=50)
+    inner = shaped_line(R0=3.0, a=0.5, elong=1.0, triang=0.0, indent=1.0, n=50)
+    outer = shaped_line(R0=2.8, a=1.5, elong=1.0, triang=0.0, indent=0.2, n=50)
     #outer = shaped_line(R0=3.0, a=1.0, elong=1.0, triang=0.0, indent=1.0, n=50)
     
-    grid_weighted_distance(inner, outer, 10, 20, show=True, equidistant=False)
-        
+    R, Z = grid_elliptic(inner, outer, 100, 100, show=True)
+    
     
