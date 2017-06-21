65c7cd2d57230415799510e3a34bff8ed370ecde
Ben Dudson <bd512@york.ac.uk>
2017-06-21 17:39:08 +0100

    Work in progress on new Grid class
    Represents a collection of poloidal grids, and provides
    ways to iterate through them.

--- tools/pylib/zoidberg/grid.py
+++ tools/pylib/zoidberg/grid.py
@@ -1,7 +1,10 @@
 from __future__ import division
-from builtins import object
+#from builtins import object
 
 import numpy as np
+
+# Import classes representing poloidal grids
+from poloidal_grid import RectangularPoloidalGrid, StructuredPoloidalGrid
 
 class Grid(object):
     def __init__(self, nx, ny, nz,
@@ -60,3 +63,126 @@ class Grid(object):
     def __repr__(self):
         return "Grid(nx={nx}, ny={ny}, nz={nz}, Lx={Lx}, Ly={Ly}, Lz={Lz}, name='{name}')".format(
             nx=self.nx, ny=self.ny, nz=self.nz, Lx=self.Lx, Ly=self.Ly, Lz=self.Lz, name=self.name)
+
+
+class NewGrid(object):
+    """
+    Represents a 3D grid, consisting of a collection of poloidal grids
+    """
+    def __init__(self, poloidal_grids, ycoords, ymin, Ly, yperiodic=False, name="fci_grid"):
+        """
+        Inputs
+        ------
+        
+        poloidal_grids
+        ycoords
+
+        Examples
+        --------
+
+        grid = Grid()
+        
+        To iterate over the poloidal grids, and get the grids to either side:
+        
+        for i in range(grid.numberOfPoloidalGrids()):
+            pol, y = grid.getPoloidalGrid(i)
+            pol_next, y_next = grid.getPoloidalGrid(i+1)
+            pol_last, y_last = grid.getPoloidalGrid(i-1)
+        
+        The getPoloidalGrid() method ensures that y_last <= y <= y_next
+        """
+        
+        try:
+            ngrids = len(poloidal_grids)
+            
+            # Check this is the same length as ycoords
+            assert len(ycoords) == ngrids
+        except TypeError:
+            # No len(), assume single poloidal grid
+            ngrids = 1
+
+        self.poloidal_grids = poloidal_grids
+        self._ngrids = ngrids  # This is an implementation detail, whether we have one or multiple separate grids
+        self.ycoords = np.asfarray(ycoords)
+        self.ymin = ymin
+        self.Ly = Ly
+        self.yperiodic = yperiodic
+        
+    def __repr__(self):
+        return "Grid({0}, {1}:{2}:{3}, yperiodic={4})".format(self.poloidal_grids, 
+                                                              np.amin(self.ycoords), np.amax(self.ycoords), self.ycoords.size, 
+                                                              self.yperiodic)
+    def numberOfPoloidalGrids(self):
+        """
+        Returns the number of poloidal grids i.e. number of points in Y
+        """
+        return self.ycoords.size
+
+    def getPoloidalGrid(self, yindex):
+        """
+        Returns the poloidal grid and y value at the given y index
+        
+        This handles negative values and values out of range, if the domain is periodic
+        
+        """
+        yindex = int(yindex)
+        
+        ny = self.ycoords.size
+
+        if (yindex >= 0) and (yindex < ny):
+            # Within index range, so just return
+            if self._ngrids == 1:
+                # Only one grid
+                return self.poloidal_grids, self.ycoords[yindex]
+            else:
+                return self.poloidal_grids[index], self.ycoords[yindex]
+                
+        # Out of range
+        if self.yperiod is None:
+            # Not periodic
+            if yindex < 0:
+                return None, ymin   # Hit the lower end in Y
+            return None, ymin + Ly  # Hit the upper end in Y
+        
+        # Periodic domain
+        
+        if yindex < 0:
+            periods = np.ceil(yindex / ny) # Number of periods
+            
+            
+        
+
+def rectangular_grid(nx, ny, nz,
+                     Lx=1.0, Ly=10., Lz=1.0,
+                     xmin=0.0, ymin=0.0, zmin=0.0,
+                     yperiodic=False):
+    """
+    Create a rectangular grid in (x,y,z). Here y is along the
+    magnetic field (typically toroidal angle), and (x,z) are in
+    the poloidal plane
+    
+    nx,ny,nz   Number of points in x,y,z
+    Lx,Ly,Lz   Size of the domain in x,y,z
+    xmin, ymin, zmin   The lower edge of the domain
+    yperiodic   Determines if the y direction is periodic
+    
+    
+    """
+    
+    # In this simple case we only need one RectangularPoloidalGrid
+    # to represent all poloidal planes
+    
+    poloidal_grid = RectangularPoloidalGrid(nx, nz, Lx, Lz, 
+                                            xmin + 0.5*Lx,
+                                            zmin + 0.5*Lz)
+    
+    return NewGrid( poloidal_grid, 
+                    np.linspace(ymin, ymin+Ly, endpoint = not yperiodic), 
+                    ymin, Ly,
+                    yperiodic=yperiodic )
+    
+    
+if __name__ == "__main__":
+    
+    grid = rectangular_grid(10,10,10)
+    print(grid)
