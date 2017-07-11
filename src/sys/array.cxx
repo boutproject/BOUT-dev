a8530c7bdfbff5f4c75f7b483cfd1a3ef39df895
Ben Dudson <bd512@york.ac.uk>
2017-07-11 12:47:13 +0100

    Moving Array static data into functions
    This should remove warnings like:
    
        ../../include/bout/array.hxx:301:10: warning: instantiation of variable 'Array<double>::use_store' required here, but no definition is available [-Wundefined-var-template]
          if(use_store) {
    
    but has the following effects:
    
    * Static members no longer need to be separately instantiated in
      the array.cxx file, and that file can be removed from the build
      -> Array is now just the header file
    
    * Cleanup is now a bit more complicated, since it must operate
      on a function-local variable. This is done by passing an argument
    
    * There may be a small performance hit, not tested yet. All
      conditionals should be true most of the time, and the functions
      return as soon as possible
    
    * Array::useStore() is now a public member, so store backing can
      be disabled by calling e.g. Array<BoutReal>::useStore(false);

--- src/sys/array.cxx
+++ src/sys/array.cxx
@@ -7,18 +7,6 @@
 #include <bout_types.hxx>
 #include <dcomplex.hxx>
 
-template<>
-std::map< int, std::vector<Array<double>::ArrayData* > > Array<double>::store = {}; // NB: C++11
-
-template<>
-bool Array<double>::use_store = true;
-
-template<>
-std::map< int, std::vector<Array<dcomplex>::ArrayData* > > Array<dcomplex>::store = {};
-
-template<>
-bool Array<dcomplex>::use_store = true;
-
 
 #ifdef UNIT
 /*
