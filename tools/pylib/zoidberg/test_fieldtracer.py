343205300e913fa1404737b41c6255e64889cd3f
Ben Dudson <bd512@york.ac.uk>
2017-07-11 08:17:35 +0100

    Unit test for reversible field tracer
    Same as the test for the standard field tracer

--- tools/pylib/zoidberg/test_fieldtracer.py
+++ tools/pylib/zoidberg/test_fieldtracer.py
@@ -23,6 +23,27 @@ def test_slab():
     assert np.allclose(coords[:,1,0], 0.3)
     assert np.allclose(coords[:,2,0], 0.4)
     assert np.allclose(coords[1,:,1], 0.5 + 1.5*0.1/0.5) # Z coord
+
+def test_FieldTracerReversible_slab():
+    mag_field = field.Slab(By=0.5, Bz=0.1, Bzprime = 0.0)
+    
+    tracer = fieldtracer.FieldTracerReversible(mag_field)
+
+    coords = tracer.follow_field_lines( 0.2, 0.3, [1.5, 2.5] )
+    
+    assert coords.shape == (2,2)
+    assert np.allclose(coords[:,0], 0.2)  # X coordinate
+    
+    assert np.allclose(coords[0,1], 0.3)
+    assert np.allclose(coords[1,1], 0.3 + 0.1/0.5) # Z coordinate
+
+    coords = tracer.follow_field_lines( [0.2,0.3,0.4], 0.5, [1.0,2.5] )
+
+    assert coords.shape == (2,3,2)
+    assert np.allclose(coords[:,0,0], 0.2)
+    assert np.allclose(coords[:,1,0], 0.3)
+    assert np.allclose(coords[:,2,0], 0.4)
+    assert np.allclose(coords[1,:,1], 0.5 + 1.5*0.1/0.5) # Z coord
     
 def test_poincare():
     mag_field = field.Slab(By=0.5, Bz=0.1, Bzprime = 0.0)
