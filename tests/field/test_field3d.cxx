d72ce837f58d1b68f41c1f91b80b2ce4180d316e
Peter Hill <zed.three@gmail.com>
2017-07-12 12:07:00 +0100

    Add lots of tests for Field3D

--- tests/field/test_field3d.cxx
+++ tests/field/test_field3d.cxx
@@ -1,38 +1,92 @@
 #include "gtest/gtest.h"
 
-#include "field3d.hxx"
+#include "bout/constants.hxx"
 #include "bout/mesh.hxx"
+#include "boutexception.hxx"
+#include "field3d.hxx"
 #include "test_extras.hxx"
+#include "unused.hxx"
 
-// Global mesh
+#include <cmath>
+#include <set>
+#include <vector>
+
+/// Global mesh
 extern Mesh *mesh;
 
+/// Test fixture to make sure the global mesh is our fake one
 class Field3DTest : public ::testing::Test {
-public:
-  Field3DTest() {
-    // Make sure the global mesh is our fake one
-    if (mesh == nullptr) {
+protected:
+  static void SetUpTestCase() {
+    // Delete any existing mesh
+    if (mesh != nullptr) {
       delete mesh;
       mesh = nullptr;
     }
     mesh = new FakeMesh(nx, ny, nz);
   }
-  ~Field3DTest() {
+
+  static void TearDownTestCase() {
     delete mesh;
     mesh = nullptr;
   }
 
-  int nx = 3;
-  int ny = 5;
-  int nz = 7;
+public:
+  static const int nx;
+  static const int ny;
+  static const int nz;
 };
+
+const int Field3DTest::nx = 3;
+const int Field3DTest::ny = 5;
+const int Field3DTest::nz = 7;
+
+TEST_F(Field3DTest, IsReal) {
+  Field3D field;
+
+  EXPECT_TRUE(field.isReal());
+}
+
+TEST_F(Field3DTest, Is3D) {
+  Field3D field;
+
+  EXPECT_TRUE(field.is3D());
+}
+
+TEST_F(Field3DTest, ByteSize) {
+  Field3D field;
+
+  EXPECT_EQ(field.byteSize(), sizeof(BoutReal));
+}
+
+TEST_F(Field3DTest, BoutRealSize) {
+  Field3D field;
+
+  EXPECT_EQ(field.BoutRealSize(), 1);
+}
 
 TEST_F(Field3DTest, Allocate) {
   Field3D field;
 
+  EXPECT_FALSE(field.isAllocated());
+
   field.allocate();
 
   EXPECT_TRUE(field.isAllocated());
+}
+
+TEST_F(Field3DTest, IsFinite) {
+  Field3D field;
+
+  EXPECT_FALSE(finite(field));
+
+  field = 1.0;
+
+  EXPECT_TRUE(finite(field));
+
+  field(1, 1, 1) = std::nan("");
+
+  EXPECT_FALSE(finite(field));
 }
 
 TEST_F(Field3DTest, GetGridSizes) {
@@ -45,10 +99,165 @@ TEST_F(Field3DTest, GetGridSizes) {
   EXPECT_EQ(field.getNz(), nz);
 }
 
+TEST_F(Field3DTest, CreateOnGivenMesh) {
+  int test_nx = 2;
+  int test_ny = 3;
+  int test_nz = 5;
+
+  FakeMesh *fieldmesh = new FakeMesh(test_nx, test_ny, test_nz);
+
+  Field3D field(fieldmesh);
+
+  field.allocate();
+
+  EXPECT_EQ(field.getNx(), test_nx);
+  EXPECT_EQ(field.getNy(), test_ny);
+  EXPECT_EQ(field.getNz(), test_nz);
+
+  delete fieldmesh;
+}
+
+/// This test is split into two parts: a very basic sanity check first
+/// (do we visit the right number of elements?), followed by a
+/// slightly more complex check one which checks certain indices are
+/// actually hit. The latter check works as follows: assign a constant
+/// value to the whole field, apart from at certain points, which a
+/// different "sentinel" value is used. When we iterate over the
+/// field, check those sentinel values are in the correct locations,
+/// and the sum over the whole field includes them.
+///
+/// A more rigorous test would assign a different power of two to each
+/// grid point, and check the sum is correct.
+TEST_F(Field3DTest, IterateOverWholeField) {
+  Field3D field;
+
+  field.allocate();
+
+  // Basic test first: do we visit the correct number of elements?
+  int count = 0;
+  for (auto &UNUSED(i) : field) {
+    ++count;
+  }
+
+  // If this fails, no point doing second part
+  ASSERT_EQ(count, nx * ny * nz);
+
+  field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+  const int num_sentinels = 3;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect
+  std::set<std::vector<int>> test_indices;
+  const std::vector<int> index0 = {0, 0, 0};
+  test_indices.insert(index0);
+  const std::vector<int> index1 = {1, 2, 3};
+  test_indices.insert(index1);
+  const std::vector<int> index2 = {2, 1, 4};
+  test_indices.insert(index2);
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (auto &i : field) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x, i.y, i.z});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum, ((nx * ny * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(test_indices == result_indices);
+}
+
+TEST_F(Field3DTest, Indexing) {
+  Field3D field;
+
+  field.allocate();
+
+  for (int i = 0; i < nx; ++i) {
+    for (int j = 0; j < ny; ++j) {
+      for (int k = 0; k < nz; ++k) {
+        field(i, j, k) = i + j + k;
+      }
+    }
+  }
+
+  EXPECT_DOUBLE_EQ(field(2, 2, 2), 6);
+}
+
+#if CHECK > 2
+TEST_F(Field3DTest, CheckNotEmpty) {
+  Field3D field;
+
+  EXPECT_THROW(field(0, 0, 0), BoutException);
+}
+
+TEST_F(Field3DTest, BoundsCheck) {
+  Field3D field;
+
+  field = 1.0;
+  EXPECT_THROW(field(-1, 0, 0), BoutException);
+  EXPECT_THROW(field(0, -1, 0), BoutException);
+  EXPECT_THROW(field(0, 0, -1), BoutException);
+  EXPECT_THROW(field(nx, 0, 0), BoutException);
+  EXPECT_THROW(field(0, ny, 0), BoutException);
+  EXPECT_THROW(field(0, 0, nz), BoutException);
+}
+
+TEST_F(Field3DTest, ConstCheckNotEmpty) {
+  const Field3D field;
+
+  EXPECT_THROW(field(0, 0, 0), BoutException);
+}
+
+TEST_F(Field3DTest, ConstBoundsCheck) {
+  const Field3D field = 1.0;
+
+  EXPECT_THROW(field(-1, 0, 0), BoutException);
+  EXPECT_THROW(field(0, -1, 0), BoutException);
+  EXPECT_THROW(field(0, 0, -1), BoutException);
+  EXPECT_THROW(field(nx, 0, 0), BoutException);
+  EXPECT_THROW(field(0, ny, 0), BoutException);
+  EXPECT_THROW(field(0, 0, nz), BoutException);
+}
+
+TEST_F(Field3DTest, CheckData) {
+  Field3D field;
+
+  EXPECT_THROW(checkData(field), BoutException);
+
+  field = 1.0;
+
+  EXPECT_NO_THROW(checkData(field));
+
+  field(1, 1, 1) = std::nan("");
+
+  EXPECT_THROW(checkData(field), BoutException);
+}
+
+#endif // CHECK > 2
+
 TEST_F(Field3DTest, CreateFromBoutReal) {
   Field3D field(1.0);
 
   EXPECT_TRUE(IsField3DEqualBoutReal(field, 1.0));
+}
+
+TEST_F(Field3DTest, CreateFromField3D) {
+  Field3D field(99.0);
+  Field3D result(field);
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(result, 99.0));
 }
 
 TEST_F(Field3DTest, AssignFromBoutReal) {
@@ -59,7 +268,53 @@ TEST_F(Field3DTest, AssignFromBoutReal) {
   EXPECT_TRUE(IsField3DEqualBoutReal(field, 2.0));
 }
 
-TEST_F(Field3DTest, Add) {
+TEST_F(Field3DTest, UnaryMinus) {
+  Field3D field;
+
+  field = 2.0;
+  field = -field;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(field, -2.0));
+}
+
+TEST_F(Field3DTest, AddEqualsBoutReal) {
+  Field3D a;
+
+  a = 1.0;
+  a += 5.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 6.0));
+}
+
+TEST_F(Field3DTest, AddEqualsField3D) {
+  Field3D a, b;
+
+  a = 2.0;
+  b = 3.0;
+  a += b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 5.0));
+}
+
+TEST_F(Field3DTest, AddField3DBoutReal) {
+  Field3D a, b;
+
+  a = 1.0;
+  b = a + 2.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 3.0));
+}
+
+TEST_F(Field3DTest, AddBoutRealField3D) {
+  Field3D a, b;
+
+  a = 1.0;
+  b = 3.0 + a;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 4.0));
+}
+
+TEST_F(Field3DTest, AddField3DField3D) {
   Field3D a, b, c;
 
   a = 1.0;
@@ -67,4 +322,283 @@ TEST_F(Field3DTest, Add) {
   c = a + b;
 
   EXPECT_TRUE(IsField3DEqualBoutReal(c, 3.0));
+}
+
+TEST_F(Field3DTest, MultiplyEqualsBoutReal) {
+  Field3D a;
+
+  a = 2.0;
+  a *= 1.5;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 3.0));
+}
+
+TEST_F(Field3DTest, MultiplyEqualsField3D) {
+  Field3D a, b;
+
+  a = 2.5;
+  b = 4.0;
+  a *= b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 10.0));
+}
+
+TEST_F(Field3DTest, MultiplyField3DBoutReal) {
+  Field3D a, b;
+
+  a = 1.5;
+  b = a * 2.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 3.0));
+}
+
+TEST_F(Field3DTest, MultiplyBoutRealField3D) {
+  Field3D a, b;
+
+  a = 2.5;
+  b = 3.0 * a;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 7.5));
+}
+
+TEST_F(Field3DTest, MultiplyField3DField3D) {
+  Field3D a, b, c;
+
+  a = 4.0;
+  b = 8.0;
+  c = a * b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(c, 32.0));
+}
+
+TEST_F(Field3DTest, SubtractEqualsBoutReal) {
+  Field3D a;
+
+  a = 1.0;
+  a -= 5.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, -4.0));
+}
+
+TEST_F(Field3DTest, SubtractEqualsField3D) {
+  Field3D a, b;
+
+  a = 2.0;
+  b = 7.0;
+  a -= b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, -5.0));
+}
+
+TEST_F(Field3DTest, SubtractField3DBoutReal) {
+  Field3D a, b;
+
+  a = 10.0;
+  b = a - 2.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 8.0));
+}
+
+TEST_F(Field3DTest, SubtractBoutRealField3D) {
+  Field3D a, b;
+
+  a = 10.0;
+  b = 3.0 - a;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, -7.0));
+}
+
+TEST_F(Field3DTest, SubtractField3DField3D) {
+  Field3D a, b, c;
+
+  a = 10.0;
+  b = 20.0;
+  c = a - b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(c, -10.0));
+}
+
+TEST_F(Field3DTest, DivideEqualsBoutReal) {
+  Field3D a;
+
+  a = 2.5;
+  a /= 5.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 0.5));
+}
+
+TEST_F(Field3DTest, DivideEqualsField3D) {
+  Field3D a, b;
+
+  a = 5.0;
+  b = 2.5;
+  a /= b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(a, 2.0));
+}
+
+TEST_F(Field3DTest, DivideField3DBoutReal) {
+  Field3D a, b;
+
+  a = 3.0;
+  b = a / 2.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 1.5));
+}
+
+TEST_F(Field3DTest, DivideBoutRealField3D) {
+  Field3D a, b;
+
+  a = 2.5;
+  b = 10.0 / a;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 4.0));
+}
+
+TEST_F(Field3DTest, DivideField3DField3D) {
+  Field3D a, b, c;
+
+  a = 32.0;
+  b = 8.0;
+  c = a / b;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(c, 4.0));
+}
+
+TEST_F(Field3DTest, PowBoutRealField3D) {
+  Field3D a, b;
+  a = 5.0;
+  b = pow(2.0, a);
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 32.0));
+}
+
+TEST_F(Field3DTest, PowField3DBoutReal) {
+  Field3D a, b;
+  a = 5.0;
+  b = pow(a, 2.0);
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(b, 25.0));
+}
+
+TEST_F(Field3DTest, PowField3DField3D) {
+  Field3D a, b, c;
+  a = 2.0;
+  b = 6.0;
+  c = pow(a, b);
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(c, 64.0));
+}
+
+TEST_F(Field3DTest, Sqrt) {
+  Field3D field;
+
+  field = 16.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(sqrt(field), 4.0));
+}
+
+TEST_F(Field3DTest, Abs) {
+  Field3D field;
+
+  field = -31.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(abs(field), 31.0));
+}
+
+TEST_F(Field3DTest, Exp) {
+  Field3D field;
+
+  field = 2.5;
+  const BoutReal expected = 12.182493960703473;
+  EXPECT_TRUE(IsField3DEqualBoutReal(exp(field), expected));
+}
+
+TEST_F(Field3DTest, Log) {
+  Field3D field;
+
+  field = 12.182493960703473;
+  const BoutReal expected = 2.5;
+  EXPECT_TRUE(IsField3DEqualBoutReal(log(field), expected));
+}
+
+TEST_F(Field3DTest, LogExp) {
+  Field3D field;
+
+  field = 2.5;
+  const BoutReal expected = 2.5;
+  EXPECT_TRUE(IsField3DEqualBoutReal(log(exp(field)), expected));
+}
+
+TEST_F(Field3DTest, Sin) {
+  Field3D field;
+
+  field = PI / 2.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(sin(field), 1.0));
+
+  field = PI;
+  EXPECT_TRUE(IsField3DEqualBoutReal(sin(field), 0.0));
+}
+
+TEST_F(Field3DTest, Cos) {
+  Field3D field;
+
+  field = PI / 2.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(cos(field), 0.0));
+
+  field = PI;
+  EXPECT_TRUE(IsField3DEqualBoutReal(cos(field), -1.0));
+}
+
+TEST_F(Field3DTest, Tan) {
+  Field3D field;
+
+  field = PI / 4.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(tan(field), 1.0));
+
+  field = PI;
+  EXPECT_TRUE(IsField3DEqualBoutReal(tan(field), 0.0));
+}
+
+TEST_F(Field3DTest, Sinh) {
+  Field3D field;
+
+  field = 1.0;
+  const BoutReal expected = 1.1752011936438014;
+  EXPECT_TRUE(IsField3DEqualBoutReal(sinh(field), expected));
+
+  field = -1.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(sinh(field), -expected));
+}
+
+TEST_F(Field3DTest, Cosh) {
+  Field3D field;
+
+  field = 1.0;
+  const BoutReal expected = 1.5430806348152437;
+  EXPECT_TRUE(IsField3DEqualBoutReal(cosh(field), expected));
+
+  field = -1.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(cosh(field), expected));
+}
+
+TEST_F(Field3DTest, Tanh) {
+  Field3D field;
+
+  field = 1.0;
+  const BoutReal expected = 0.761594155955764;
+  EXPECT_TRUE(IsField3DEqualBoutReal(tanh(field), expected));
+
+  field = -1.0;
+  EXPECT_TRUE(IsField3DEqualBoutReal(tanh(field), -expected));
+}
+
+TEST_F(Field3DTest, Floor) {
+  Field3D field;
+
+  field = 50.0;
+  field(1, 1, 1) = 49.9;
+  field(2, 3, 4) = -20;
+
+  const BoutReal floor_value = 50.0;
+
+  EXPECT_TRUE(IsField3DEqualBoutReal(floor(field, floor_value), floor_value));
 }
