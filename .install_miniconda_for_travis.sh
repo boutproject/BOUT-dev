d31449557a18a40043377f4f4f1ac751c944d886
David Dickinson <d7919@users.noreply.github.com>
2017-08-15 14:34:28 +0100

    Fix for broken conda build

--- .install_miniconda_for_travis.sh
+++ .install_miniconda_for_travis.sh
@@ -2,16 +2,17 @@
 TARGET_DIR=$HOME/miniconda
 
 export PATH="${TARGET_DIR}/bin:$PATH"
+
 #Check to see if the target directory already exists (thanks to caching)
 #if it does then we can skip the install of conda etc.
 if [ ! -e ${TARGET_DIR} ]
 then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p ${TARGET_DIR}
 else
     echo "Using cached version of conda"
 fi
 
+bash miniconda.sh -b -p ${TARGET_DIR}
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
