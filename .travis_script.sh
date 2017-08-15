6e509d51f145f60a7b231799fc3bd85cf25d4e00
David Dickinson <d7919@users.noreply.github.com>
2017-08-15 13:35:02 +0100

    Tidy up use of parallel make by using MAKEFLAGS

--- .travis_script.sh
+++ .travis_script.sh
@@ -17,8 +17,8 @@ then
     exit $conf
 fi
 export PYTHONPATH=$(pwd)/tools/pylib/:$PYTHONPATH
-njobs=4
-time make -j ${njobs} || exit
-time make -j ${njobs} check-unit-tests || exit
-time make -j ${njobs} check-integrated-tests || exit
-time make -j ${njobs} check-mms-tests || exit
+export MAKEFLAGS="-j 2"
+time make || exit
+time make check-unit-tests || exit
+time make check-integrated-tests || exit
+time make check-mms-tests || exit
