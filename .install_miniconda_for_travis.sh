107385ca848369277add10eb257dd1af86c15eeb
David Dickinson <d7919@users.noreply.github.com>
2017-08-15 14:45:31 +0100

    Testing for conda caching

--- .install_miniconda_for_travis.sh
+++ .install_miniconda_for_travis.sh
@@ -3,21 +3,25 @@ TARGET_DIR=$HOME/miniconda
 
 export PATH="${TARGET_DIR}/bin:$PATH"
 
+mkdir -p download
+
 #Check to see if the target directory already exists (thanks to caching)
 #if it does then we can skip the install of conda etc.
-# if [ ! -e ${TARGET_DIR} ]
-# then
-#     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-# else
-#     echo "Using cached version of conda"
-# fi
+if [[ ! -e download/miniconda.sh ]]
+then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O download/miniconda.sh
+else
+    echo "Using cached version of miniconda"
+fi
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p ${TARGET_DIR}
+bash download/miniconda.sh -b -p ${TARGET_DIR} -f
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 conda create -q -n test-environment python=3.5 numpy scipy netcdf4
+ls
+echo "------------"
+ls ${TARGET_DIR}/bin
 source activate test-environment
