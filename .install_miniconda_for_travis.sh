f29f02254055db7aa98fc952bc165d4b8dc95084
David Dickinson <d7919@users.noreply.github.com>
2017-08-15 14:28:34 +0100

    Changing approach to caching of miniconda to try to fix broken build

--- .install_miniconda_for_travis.sh
+++ .install_miniconda_for_travis.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 TARGET_DIR=$HOME/miniconda
 
+export PATH="${TARGET_DIR}/bin:$PATH"
 #Check to see if the target directory already exists (thanks to caching)
 #if it does then we can skip the install of conda etc.
 if [ ! -e ${TARGET_DIR} ]
 then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     bash miniconda.sh -b -p ${TARGET_DIR}
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
-    # Useful for debugging any issues with conda
-    conda info -a
-    conda create -q -n test-environment python=3.5 numpy scipy netcdf4
 else
     echo "Using cached version of conda"
 fi
+
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+conda create -q -n test-environment python=3.5 numpy scipy netcdf4
+source activate test-environment
