879e4ec93ffaee1805ad13f6758ad11d324baacb
David Dickinson <d7919@users.noreply.github.com>
2017-08-16 10:02:04 +0100

    Disabling caching of miniconda/conda
    Seems to actually slow things down, presumably because the install
    doesn't actually involve much in the way of cpu cycles but is actually
    network limited anyway.

--- .install_miniconda_for_travis.sh
+++ .install_miniconda_for_travis.sh
@@ -5,28 +5,15 @@ export PATH="${TARGET_DIR}/bin:$PATH"
 
 mkdir -p ${DOWNLOAD_TARGET}
 
-#Check to see if the miniconda script already exists (thanks to caching)
-#if it does then we can skip the download of conda
-if [[ ! -e ${DOWNLOAD_TARGET}/miniconda.sh ]]
-then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${DOWNLOAD_TARGET}/miniconda.sh
-else
-    echo "Using cached version of miniconda.sh"
-fi
+#Fetch the miniconda installer
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${DOWNLOAD_TARGET}/miniconda.sh
 
-#Check to see if the miniconda build already exists (thanks to caching)
-#if it does then we can skip the install of conda etc.
-if [[ ! -e ${TARGET_DIR}/bin/activate ]]
-then
-    bash ${DOWNLOAD_TARGET}/miniconda.sh -b -p ${TARGET_DIR} -f
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
-    # Useful for debugging any issues with conda
-    conda info -a
-    conda create -q -n test-environment python=3.5 numpy scipy netcdf4
-else
-    echo "Using cached version of minconda test-environment"
-fi
-ls -l ${TARGET_DIR}/bin
-source ${TARGET_DIR}/bin/activate test-environment
+#Install conda
+bash ${DOWNLOAD_TARGET}/miniconda.sh -b -p ${TARGET_DIR} -f
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+conda create -q -n test-environment python=3.5 numpy scipy netcdf4
+source activate test-environment
