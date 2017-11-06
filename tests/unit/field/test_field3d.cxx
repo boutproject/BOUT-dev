0864b93f359972acbe8057bb385e0ba8064bd317
Peter Hill <zed.three@gmail.com>
2017-11-06 17:38:12 +0000

    Add Field3D tests for SDI

--- tests/unit/field/test_field3d.cxx
+++ tests/unit/field/test_field3d.cxx
@@ -225,6 +225,87 @@ TEST_F(Field3DTest, IterateOverRGN_ALL) {
   EXPECT_TRUE(test_indices == result_indices);
 }
 
+TEST_F(Field3DTest, IterateOverRGN_ALL_SDI) {
+  Field3D field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+  const int num_sentinels = test_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (const auto &i : field.sdi_region(RGN_ALL)) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x(), i.y(), i.z()});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum, ((nx * ny * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(test_indices == result_indices);
+}
+
+TEST_F(Field3DTest, IterateOverRGN_ALL_SDI_NonRangeBased) {
+  Field3D field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+  const int num_sentinels = test_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (auto i = field.sdi_region(RGN_ALL).begin(), end = field.sdi_region(RGN_ALL).end();
+       i < end; ++i) {
+    sum += field(*i);
+    if (field(*i) == sentinel) {
+      result_indices.insert({i->x(), i->y(), i->z()});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum, ((nx * ny * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(test_indices == result_indices);
+}
+
 TEST_F(Field3DTest, IterateOverRGN_NOBNDRY) {
   Field3D field = 1.0;
 
@@ -261,6 +342,52 @@ TEST_F(Field3DTest, IterateOverRGN_NOBNDRY) {
     sum += field[i];
     if (field[i] == sentinel) {
       result_indices.insert({i.x, i.y, i.z});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum,
+            (((nx - 2) * (ny - 2) * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(region_indices == result_indices);
+}
+
+TEST_F(Field3DTest, IterateOverRGN_NOBNDRY_SDI) {
+  Field3D field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect.
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+
+  // This is the set of indices actually inside the region we want
+  std::set<std::vector<int>> region_indices;
+  region_indices.insert({1, 1, 0});
+  region_indices.insert({1, 1, 1});
+  const int num_sentinels = region_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto &index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (const auto &i : field.sdi_region(RGN_NOBNDRY)) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x(), i.y(), i.z()});
       ++found_sentinels;
     }
   }
@@ -318,6 +445,53 @@ TEST_F(Field3DTest, IterateOverRGN_NOX) {
   EXPECT_TRUE(region_indices == result_indices);
 }
 
+TEST_F(Field3DTest, IterateOverRGN_NOX_SDI) {
+  Field3D field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect.
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+
+  // This is the set of indices actually inside the region we want
+  std::set<std::vector<int>> region_indices;
+  region_indices.insert({1, 0, 0});
+  region_indices.insert({1, 1, 0});
+  region_indices.insert({1, 0, 1});
+  region_indices.insert({1, 1, 1});
+  const int num_sentinels = region_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (const auto &i : field.sdi_region(RGN_NOX)) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x(), i.y(), i.z()});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum, (((nx - 2) * ny * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(region_indices == result_indices);
+}
+
 TEST_F(Field3DTest, IterateOverRGN_NOY) {
   Field3D field;
 
@@ -358,6 +532,55 @@ TEST_F(Field3DTest, IterateOverRGN_NOY) {
     sum += field[i];
     if (field[i] == sentinel) {
       result_indices.insert({i.x, i.y, i.z});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum, ((nx * (ny - 2) * nz) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(region_indices == result_indices);
+}
+
+TEST_F(Field3DTest, IterateOverRGN_NOY_SDI) {
+  Field3D field;
+
+  field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect.
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+
+  // This is the set of indices actually inside the region we want
+  std::set<std::vector<int>> region_indices;
+  region_indices.insert({0, 1, 0});
+  region_indices.insert({1, 1, 0});
+  region_indices.insert({0, 1, 1});
+  region_indices.insert({1, 1, 1});
+  const int num_sentinels = region_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (const auto &i : field.sdi_region(RGN_NOY)) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x(), i.y(), i.z()});
       ++found_sentinels;
     }
   }
