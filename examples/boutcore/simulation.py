9399add2697c689efb58d880742879b4c2f1f50b
David Schwörer <schword2@mail.dcu.ie>
2017-11-09 22:52:37 +0000

    add simple example boutcore simulation

---
+++ examples/boutcore/simulation.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import boutcore
+import numpy as np
+boutcore.init(["mesh:n=2"])
+mesh=boutcore.Mesh.getGlobal();
+dens=boutcore.Field3D.fromMesh(mesh)
+dens.set(0)
+
+def rhs(time):
+    n_ddt=dens.ddt()
+    n_ddt.set(1)
+
+model=boutcore.PhysicsModel()
+model.setRhs(rhs)
+model.solve_for(n=dens)
+model.solve()
+
