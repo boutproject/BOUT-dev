04fe521f0989047257fc25242a4adb1063deabdc
David Schwörer <schword2@mail.dcu.ie>
2018-01-22 20:27:21 +0000

    add script to generate input files

---
+++ tools/pylib/_boutcore_build/geninput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import boutcore as bc
+import numpy as np
+bc.init("-d input -f ../data/BOUT.inp".split(" "))
+
+f=bc.Field3D.fromMesh(None)
+f.setAll(np.array([[[1.]]]))
+f2=bc.Field3D.fromMesh(None)
+f2.setAll(np.array([[[2.]]]))
+print(f.getAll())
+dump=bc.Datafile() # get global :(
+dump.add(f3d=f,f2d=f2,save_repeat=True)
+dump.write()
+
