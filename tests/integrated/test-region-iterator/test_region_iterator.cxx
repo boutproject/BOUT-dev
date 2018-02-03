016ef53c4feb3b93b16244fafe3b78fc132de176
David Dickinson <d7919@users.noreply.github.com>
2018-02-03 15:29:19 +0000

    Extend region iterator test to use a mesh provided Region

--- tests/integrated/test-region-iterator/test_region_iterator.cxx
+++ tests/integrated/test-region-iterator/test_region_iterator.cxx
@@ -33,6 +33,27 @@ int physics_init(bool restarting) {
   }
   if(nerr != 0 ) throw BoutException("Unexpected values found in 'b', count %d",nerr);
 
+
+  Field3D d=1.0, e=1.0, f=2.0;
+  BLOCK_REGION_LOOP(mesh->getRegion3D("RGN_NOBNDRY"),i,
+			   d[i] = 3.0;
+			   e[i] = f[i];
+			   );
+
+  //Check expected results
+  nerr=0;
+  //Expect to find differences just in the boundaries, so work out how many boundary points there area
+  const int nerrExpected = (2*mesh->xstart*mesh->LocalNy + 2*mesh->ystart*(mesh->LocalNx-mesh->xstart*2))*mesh->LocalNz;
+  for (const auto &i: d.region(RGN_ALL)){
+    if (d[i] != 3.0) nerr++;
+  }
+  if(nerr != nerrExpected ) throw BoutException("Unexpected values found in 'd', count %d",nerr);
+  nerr=0;
+  for (const auto &i: e.region(RGN_ALL)){
+    if (e[i] != f[i]) nerr++;
+  }
+  if(nerr != nerrExpected ) throw BoutException("Unexpected values found in 'e', count %d",nerr);
+
   SOLVE_FOR(n);
   return 0;
 }
