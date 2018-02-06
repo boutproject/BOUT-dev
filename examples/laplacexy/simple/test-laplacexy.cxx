6ef178bb93c5016511539caa8815c64abf97f092
John Omotani <john.omotani@ukaea.uk>
2018-02-06 10:33:45 +0000

    examples/laplacexy/simple bugfix
    Enclose laplacexy object in braces so that its destructor is called
    before BoutFinalise. Avoids PETSc error when cleaning up communicators.

--- examples/laplacexy/simple/test-laplacexy.cxx
+++ examples/laplacexy/simple/test-laplacexy.cxx
@@ -6,19 +6,21 @@
 int main(int argc, char** argv) {
   BoutInitialise(argc, argv);
   
-  /// Create a LaplaceXY object
-  LaplaceXY laplacexy(mesh);
-  
-  /// Generate rhs function
-  Field2D rhs = FieldFactory::get()->create2D("laplacexy:rhs", Options::getRoot(), mesh);
-  
-  /// Solution
-  Field2D x = 0.0;
-  
-  x = laplacexy.solve(rhs, x);
-  
-  SAVE_ONCE2(rhs, x);
-  dump.write();  // Save output file
+  { // braces make sure laplacexy's destructor is called before BoutFinalise(). Otherwise PETSc 3.7 raises an error.
+    /// Create a LaplaceXY object
+    LaplaceXY laplacexy(mesh);
+    
+    /// Generate rhs function
+    Field2D rhs = FieldFactory::get()->create2D("laplacexy:rhs", Options::getRoot(), mesh);
+    
+    /// Solution
+    Field2D x = 0.0;
+    
+    x = laplacexy.solve(rhs, x);
+    
+    SAVE_ONCE2(rhs, x);
+    dump.write();  // Save output file
+  }
   
   BoutFinalise();
   return 0;
