6430f526f78261ac14c7190740a703428377f87b
David Schwörer <schword2@mail.dcu.ie>
2018-02-06 16:02:13 +0000

    add unit tests for region flag for min/max

--- tests/unit/field/test_field3d.cxx
+++ tests/unit/field/test_field3d.cxx
@@ -1168,6 +1168,7 @@ TEST_F(Field3DTest, Min) {
   const BoutReal min_value = 40.0;
 
   EXPECT_EQ(min(field, false), min_value);
+  EXPECT_EQ(min(field, false,RGN_ALL), -99.0);
 }
 
 TEST_F(Field3DTest, Max) {
@@ -1183,6 +1184,7 @@ TEST_F(Field3DTest, Max) {
   const BoutReal max_value = 60.0;
 
   EXPECT_EQ(max(field, false), max_value);
+  EXPECT_EQ(max(field, false,RGN_ALL), 99.0);
 }
 
 TEST_F(Field3DTest, DC) {
