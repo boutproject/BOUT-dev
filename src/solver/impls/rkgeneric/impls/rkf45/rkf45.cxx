e8603757019a8f31ada056cc0ec19acfedca19e5
David Dickinson <d7919@users.noreply.github.com>
2018-02-19 23:23:43 +0000

    Replacing matrix and new with Matrix and Array in rkgeneric

--- src/solver/impls/rkgeneric/impls/rkf45/rkf45.cxx
+++ src/solver/impls/rkgeneric/impls/rkf45/rkf45.cxx
@@ -11,18 +11,18 @@ RKF45Scheme::RKF45Scheme(Options *options):RKScheme(options){
   OPTION(options, followHighOrder, followHighOrder);
 
   //Allocate coefficient arrays
-  stageCoeffs = matrix<BoutReal>(numStages,numStages);
-  resultCoeffs = matrix<BoutReal>(numStages,numOrders);
-  timeCoeffs = new BoutReal[numStages];
+  stageCoeffs = Matrix<BoutReal>(numStages, numStages);
+  resultCoeffs = Matrix<BoutReal>(numStages, numOrders);
+  timeCoeffs = Array<BoutReal>(numStages);
 
   //Zero out arrays (shouldn't be needed, but do for testing)
   for(int i=0;i<numStages;i++){
     timeCoeffs[i]=0.;
     for(int j=0;j<numStages;j++){
-      stageCoeffs[i][j]=0.;
+      stageCoeffs(i, j) = 0.;
     }
     for(int j=0;j<numOrders;j++){
-      resultCoeffs[i][j]=0.;
+      resultCoeffs(i, j) = 0.;
     }
   }
 
@@ -30,37 +30,49 @@ RKF45Scheme::RKF45Scheme(Options *options):RKScheme(options){
   //Set coefficients : stageCoeffs
   //////////////////////////////////
   //Level 0
-  stageCoeffs[0][0] = 0.0;
+  stageCoeffs(0, 0) = 0.0;
   //Level 1
-  stageCoeffs[1][0] = 1.0/4.0;
+  stageCoeffs(1, 0) = 1.0 / 4.0;
   //Level 2
-  stageCoeffs[2][0] = 3.0/32.0; stageCoeffs[2][1] = 9.0/32.0;
+  stageCoeffs(2, 0) = 3.0 / 32.0;
+  stageCoeffs(2, 1) = 9.0 / 32.0;
   //Level 3
-  stageCoeffs[3][0] = 1932.0/2197.0; stageCoeffs[3][1] = -7200.0/2197.0; 
-  stageCoeffs[3][2] = 7296.0/2197.0;
+  stageCoeffs(3, 0) = 1932.0 / 2197.0;
+  stageCoeffs(3, 1) = -7200.0 / 2197.0;
+  stageCoeffs(3, 2) = 7296.0 / 2197.0;
   //Level 4
-  stageCoeffs[4][0] = 439.0/216.0; stageCoeffs[4][1] = -8.0;
-  stageCoeffs[4][2] = 3680.0/513.0; stageCoeffs[4][3] = -845.0/4104.0;
+  stageCoeffs(4, 0) = 439.0 / 216.0;
+  stageCoeffs(4, 1) = -8.0;
+  stageCoeffs(4, 2) = 3680.0 / 513.0;
+  stageCoeffs(4, 3) = -845.0 / 4104.0;
   //Level 5
-  stageCoeffs[5][0] = -8.0/27.0; stageCoeffs[5][1] = 2.0;
-  stageCoeffs[5][2] = -3544.0/2565.0; stageCoeffs[5][3] = 1859.0/4104.0;
-  stageCoeffs[5][4] = -11.0/40.0;
+  stageCoeffs(5, 0) = -8.0 / 27.0;
+  stageCoeffs(5, 1) = 2.0;
+  stageCoeffs(5, 2) = -3544.0 / 2565.0;
+  stageCoeffs(5, 3) = 1859.0 / 4104.0;
+  stageCoeffs(5, 4) = -11.0 / 40.0;
 
   //////////////////////////////////
   //Set coefficients : resultCoeffs
   //////////////////////////////////
   //Level 0
-  resultCoeffs[0][0] = 16.0/135.0; resultCoeffs[0][1] = 25.0/216.0;
+  resultCoeffs(0, 0) = 16.0 / 135.0;
+  resultCoeffs(0, 1) = 25.0 / 216.0;
   //Level 1
-  resultCoeffs[1][0] = 0.0; resultCoeffs[1][1] = 0.0;
+  resultCoeffs(1, 0) = 0.0;
+  resultCoeffs(1, 1) = 0.0;
   //Level 2
-  resultCoeffs[2][0] = 6656.0/12825.0; resultCoeffs[2][1] = 1408.0/2565.0;
+  resultCoeffs(2, 0) = 6656.0 / 12825.0;
+  resultCoeffs(2, 1) = 1408.0 / 2565.0;
   //Level 3
-  resultCoeffs[3][0] = 28561.0/56430.0; resultCoeffs[3][1] = 2197.0/4104.0;
+  resultCoeffs(3, 0) = 28561.0 / 56430.0;
+  resultCoeffs(3, 1) = 2197.0 / 4104.0;
   //Level 4
-  resultCoeffs[4][0] = -9.0/50.0; resultCoeffs[4][1] = -1.0/5.0;
+  resultCoeffs(4, 0) = -9.0 / 50.0;
+  resultCoeffs(4, 1) = -1.0 / 5.0;
   //Level 5
-  resultCoeffs[5][0] = 2.0/55.0; resultCoeffs[5][1] = 0.0;
+  resultCoeffs(5, 0) = 2.0 / 55.0;
+  resultCoeffs(5, 1) = 0.0;
 
   //////////////////////////////////
   //Set coefficients : timeCoeffs
