526fbeed0b6f1375e9c15d08b3070a5f49aad338
David Schwörer <schword2@mail.dcu.ie>
2018-02-21 21:46:34 +0000

    More inline comments

--- tools/pylib/_boutcore_build/helper.py
+++ tools/pylib/_boutcore_build/helper.py
@@ -1,4 +1,4 @@
-def resolve_slice(ind,num):
+def _resolve_slice(ind,num):
     if isinstance(ind,slice):
         ret=list(ind.indices(num))
         d=ret[1]-ret[0]
@@ -14,10 +14,10 @@ def resolve_slice(ind,num):
             raise IndexError("%d is out of range [%d:%d]"%(ind,0,num))
         return [ind,ind+1,1]
         
-def resolve_slices(inds,nums):
+def _resolve_slices(inds,nums):
     if len(inds)!= len(nums):
         raise IndexError("Expexted %d dimensions, but got %d dimensions."%(len(nums),len(inds)))
     ret=[]
     for i in range(len(inds)):
-        ret.append(resolve_slice(inds[i],nums[i]))
+        ret.append(_resolve_slice(inds[i],nums[i]))
     return ret
