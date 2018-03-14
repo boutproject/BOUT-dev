47108d20d98f659eeda75d49c9fd27532742834f
David Dickinson <d7919@users.noreply.github.com>
2018-03-14 12:18:00 +0000

    Add getopts parsing to .travis_script.sh
    This allows us to keep all the details of how to build/run/do coverage
    in this script, making the yml config a bit simpler and reducing
    duplicated code.
    
    Also splitting env vars across lines to improve readability.

--- .travis_script.sh
+++ .travis_script.sh
@@ -18,8 +18,69 @@ then
 fi
 export PYTHONPATH=$(pwd)/tools/pylib/:$PYTHONPATH
 export MAKEFLAGS="-j 2 -k"
+
+#Default flags
+COVERAGE=0
+UNIT=0
+INTEGRATED=0
+MMS=0
+TESTS=0
+
+usage() {
+    echo "$0 options are: "
+    #Just pull out special comments from this file
+    grep "\#\#\#" $0}
+}
+#Handle input flags
+while getopts "cuim" arg;
+do
+    case $arg in
+	c) ### Run the coverage-post job tasks
+	    COVERAGE=1
+	    ;;
+	u) ### Run the unit tests
+	    UNIT=1
+	    TESTS=1
+	    ;;
+	i) ### Run the integrated tests
+	    INTEGRATED=1
+	    TESTS=1
+	    ;;
+	m) ### Run the mms tests
+	    MMS=1
+	    TESTS=1
+	    ;;
+	*) ### Show usage message
+	    usage
+	    ;;
+    esac
+done
+
 time make $MAIN_TARGET|| exit
-time make build-check || exit
-time make check-unit-tests || exit
-time make check-integrated-tests || exit
-time make check-mms-tests || exit
+if [ ${TEST} == 1 ]
+then
+    time make build-check || exit
+fi
+
+if [ ${UNIT} == 1]
+then
+    time make check-unit-tests || exit
+fi
+
+if [ ${INTEGRATED} == 1]
+then
+    time make check-integrated-tests || exit
+fi
+
+if [ ${MMS} == 1]
+then
+   time make check-mms-tests || exit
+fi
+
+if [ ${COVERAGE} == 1]
+   # Ensure that there is a corresponding .gcda file for every .gcno file
+   # This is to try and make the coverage report slightly more accurate
+   # It still won't include, e.g. any solvers we don't build with though
+   find . -name "*.gcno" -exec sh -c 'touch -a "${1%.gcno}.gcda"' _ {} \;
+   bash <(curl -s https://codecov.io/bash)
+then
