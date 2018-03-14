20e4c847252f74fa2dbc72006549dd7d9c4a6f51
David Schwörer <schword2@mail.dcu.ie>
2018-03-14 15:02:06 +0000

    Return something if it was promissed

--- include/flexible.hxx
+++ include/flexible.hxx
@@ -50,21 +50,24 @@ public:
     // maybe this should be false?
     set(f,true);
     ASSERT1(fields[mainid]!=nullptr);
+    return *this;
   }
   Flexible<F> & operator=(F&& f) {
     set(f,true);
     ASSERT1(fields[mainid]!=nullptr);
+    return *this;
   }
   Flexible<F> & operator=(BoutReal d) {
     (*((F*)fields[mainid]))=d;
     clean(false);
     ASSERT1(fields[mainid]!=nullptr);
+    return *this;
   }
   /// Set a part of the Flexible Field.
   /// If the main field is set, then, all other fields are
   /// invalidated. If an other location is set, then, it is assumed
   /// that the this is in sync with the main field.
-  const F & set(const F & field, bool copy=true){
+  Flexible<F> & set(const F & field, bool copy=true){
     uint loc = getId(field.getLocation());
     if (loc == mainid){
       clean(true);
@@ -78,6 +81,7 @@ public:
       fields[loc]=&field;
     }
     owner[loc]=copy; // did we just copy?
+    return *this;
   };
   //DEPRECATED(operator const F &() ) {
   operator const F &() {
@@ -139,6 +143,7 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator*=(const Field2D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -147,10 +152,12 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator*=(BoutReal rhs) {
     ((F*)fields[mainid])->operator*=(rhs);
     clean(false);
+    return *this;
   };
   Flexible<F>& operator/=(const Field3D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -159,6 +166,7 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator/=(const Field2D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -167,10 +175,12 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator/=(BoutReal rhs) {
     ((F*)fields[mainid])->operator/=(rhs);
     clean(false);
+    return *this;
   };
   Flexible<F>& operator+=(const Field3D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -179,6 +189,7 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator+=(const Field2D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -187,10 +198,12 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator+=(BoutReal rhs) {
     ((F*)fields[mainid])->operator+=(rhs);
     clean(false);
+    return *this;
   };
   Flexible<F>& operator-=(const Field3D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -199,6 +212,7 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator-=(const Field2D & rhs) {
     if (mainid == getId(rhs.getLocation())){
@@ -207,10 +221,12 @@ public:
       throw BoutException("Not yet implemtented!");
     }
     clean(false);
+    return *this;
   };
   Flexible<F>& operator-=(BoutReal rhs) {
     ((F*)fields[mainid])->operator-=(rhs);
     clean(false);
+    return *this;
   };
 
 private:
