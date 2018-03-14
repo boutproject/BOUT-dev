39b93b572ceb72625c55744a1d5d88764d7ce7b4
David Schwörer <schword2@mail.dcu.ie>
2018-03-14 19:29:42 +0000

    Fix: Add missing braces

--- src/mesh/interpolation.cxx
+++ src/mesh/interpolation.cxx
@@ -249,7 +249,7 @@ const Field2D interp_to(const Field2D &var, CELL_LOC loc) {
 
       switch(dir) {
       case CELL_XLOW: {
-        if (mesh->startx < 2)
+        if (mesh->xstart < 2){
           throw BoutException("Cannot interpolate in X direction\n"
                               " - Not enough boundary cells\n"
                               " - at least 2 are needed!");
