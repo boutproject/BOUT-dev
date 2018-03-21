794a343e9e32c4ccf08eb2a666d868d4d9713e89
David Schwörer <schword2@mail.dcu.ie>
2018-03-21 22:52:15 +0000

    Adopt boutcore test for interpolation fixes
    The test included a call to interp_to
    That throws now, if there are not enought boundary cells.
    Thus the test was needed to be adopted

--- tools/pylib/_boutcore_build/geninput.py
+++ tools/pylib/_boutcore_build/geninput.py
@@ -5,11 +5,11 @@ import numpy as np
 bc.init("-d input -f ../data/BOUT.inp".split(" "))
 
 f=bc.Field3D.fromMesh(None)
-f.setAll(np.array([[[1.]]]))
+zeros=np.zeros((6,6,2))
+f.set(zeros+1.)
 f2=bc.Field3D.fromMesh(None)
-f2.setAll(np.array([[[2.]]]))
-print(f.getAll())
+f2.set(zeros+2.)
 dump=bc.Datafile() # get global :(
-dump.add(f3d=f,f2d=f2,save_repeat=True)
+dump.add(f3d=f,f2d=f2,save_repeat=False)
 dump.write()
 
