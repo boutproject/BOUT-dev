64edbdb1690a7a52e7c0b44896865b4cb0a36448
Ben Dudson <bd512@york.ac.uk>
2018-04-10 14:36:27 +0100

    Set manual author to The BOUT++ team
    Many people contribute(d) to it, so this is a lazy way
    to acknowledge all of them.

--- manual/sphinx/conf.py
+++ manual/sphinx/conf.py
@@ -103,7 +103,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'BOUT++'
 copyright = '2017, B. Dudson'
-author = 'B. Dudson'
+author = 'The BOUT++ team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
