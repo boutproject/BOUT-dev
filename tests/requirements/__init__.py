ccd15413a219f47b8ca6caf85274a89185c39b09
Ben Dudson <bd512@york.ac.uk>
2018-04-11 22:11:46 +0100

    Add documentation, small fixes
    * Small section in the manual on requirements
    
    * Section in README.md file
    
    * Docstring in Requirements class
    
    * Exclude "__init__.pyc" by skipping all file names containing ".py"
    
    * If ``verbose = True``, print values from bout-config and those
      set in calls to ``add``.

--- tests/requirements/__init__.py
+++ tests/requirements/__init__.py
@@ -5,6 +5,25 @@ import re
 from boututils.run_wrapper import shell
 
 class Requirements(object):
+    """
+    Checks if test cases meet their specified requirements. 
+    Test scripts can contain a line starting with 
+    #requires or #Requires, optionally followed by a colon,
+    then a Python expression which combines boolean values.
+
+    Examples: 
+
+    #requires netcdf
+
+    #requires not make
+
+    #Requires not (travis and mpich)
+    
+    The individual requirements (netcdf, make, travis, mpich etc.)
+    are gathered from the bout-config scipt, or from executable
+    scripts in the "requirements" directory (where this code is).
+    
+    """
     def __init__(self,path=None,verbose=False):
         selflocation=os.path.realpath(__file__)
         selflocation=selflocation.rsplit("/",1)[0]
@@ -12,14 +31,16 @@ class Requirements(object):
         if path == None:
             path=selflocation
 
-        # Get list of files in subdirectory, excluding common temporaries
+        # Get list of files in subdirectory, excluding common temporaries,
+        # hidden files, and python .py and .pyc files
         requirements_list = [ x for x in os.listdir(path)
                               if not (("#" in x) or ("~" in x)
-                                      or (x[0] == ".") or (x == "__init__.py")) ]
+                                      or (x[0] == ".") or (".py" in x)) ]
 
         if verbose:
             print("======= Requirement checks ========")
 
+        self._verbose = verbose
         self._requirements = {}
         for requirement in requirements_list:
             status,out = shell(os.path.join(path, requirement), pipe=True)
@@ -40,7 +61,9 @@ class Requirements(object):
                     print("Error parsing "+match+" - %s is not \"yes\"/\"no\""%match[1])
                 else:
                     self._requirements[key]=value
-
+                    if verbose:
+                        print("{0} => {1}".format(key, value))
+                    
         # Now have dictionary of requirements, true/false.
 
     def check(self,script):
@@ -65,6 +88,8 @@ class Requirements(object):
     def add(self,requirement, value):
         """Add a requirement value to the dictionary"""
         self._requirements[requirement]=value
+        if self._verbose:
+            print("{0} => {1}".format(requirement, value))
 
 
     def has(self,requirement):
