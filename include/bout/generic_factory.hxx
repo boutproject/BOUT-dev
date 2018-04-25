68d46960b6bcaa1c537ace97930e02e58f652560
Peter Hill <zed.three@gmail.com>
2018-04-25 11:06:42 +0100

    Add generic Factory class

---
+++ include/bout/generic_factory.hxx
@@ -0,0 +1,99 @@
+/// \file Base type for factories
+
+#pragma once
+#ifndef __BOUT_GENERIC_FACTORY_H__
+#define __BOUT_GENERIC_FACTORY_H__
+
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+
+/// Generic Factory, adapted from Modern C++ Design/Loki by A. Alexandrescu
+///
+/// Use with RegisterInFactory to provide a generic way of creating
+/// new derived types at runtime
+///
+/// Example:
+///
+///     class Base {};
+///     class Derived : public Base {};
+///     RegisterInFactory<Base, Derived> register("derived_type");
+///     auto foo = Factory<Base>::getInstance().create("derived_type");
+///
+/// TODO: Use std::unique_ptr<BaseType> instead of BaseType*
+///
+/// @tparam BaseType       The base class that this factory creates
+/// @tparam IdentifierType How different types are identified (default string)
+/// @tparam TypeCreator    The function signature for creating a new BaseType
+///
+/// MIT Licence
+template <typename BaseType, typename IdentifierType = std::string,
+          typename TypeCreator = std::function<BaseType *()>>
+class Factory {
+public:
+  /// Add a new type \p name to the factory
+  ///
+  /// @param[in] name     An identifier for this type
+  /// @param[in] creator  A function for creating this type
+  /// @returns true if the type was successfully added
+  bool add(const IdentifierType &name, TypeCreator creator) {
+    return type_map.insert(std::make_pair(name, creator)).second;
+  }
+
+  /// Remove a type \p name from the factory
+  ///
+  /// @param[in] name  The identifier for the type to be removed
+  /// @returns true if the type was successfully removed
+  bool remove(const IdentifierType &name) { return type_map.erase(name) == 1; }
+
+  /// Create a new object of type \p name
+  ///
+  /// @param[in] name  The identifier for the type to be created
+  /// @returns a pointer to the new object
+  BaseType *create(const IdentifierType &name) {
+    auto index = type_map.find(name);
+    if (index != std::end(type_map)) {
+      return index->second();
+    }
+    throw std::runtime_error("Could not find " + name);
+  }
+
+  /// Get the singleton instance
+  static Factory &getInstance() {
+    static Factory instance;
+    return instance;
+  }
+
+private:
+  std::map<IdentifierType, TypeCreator> type_map;
+  Factory() {}
+};
+
+/// Helper class for adding new types to Factory
+///
+/// Example:
+///
+///     class Base {};
+///     class Derived : public Base {};
+///     RegisterInFactory<Base, Derived> register("derived_type");
+///     auto foo = Factory<Base>::getInstance().create("derived_type");
+///
+/// Adapted from
+/// http://www.drdobbs.com/conversations-abstract-factory-template/184403786
+///
+/// @tparam BaseType       Which factory to add \p DerivedType to
+/// @tparam DerivedType    The new type to add to Factory<BaseType>
+/// @tparam IdentifierType How different types are identified (default string)
+template <typename BaseType, typename DerivedType, typename IdentifierType = std::string>
+class RegisterInFactory {
+public:
+  RegisterInFactory(const IdentifierType &name) {
+    Factory<BaseType>::getInstance().add(name,
+                                         []() -> BaseType * { return new DerivedType; });
+  }
+};
+
+#endif // __BOUT_GENERIC_FACTORY_H__
---
+++ tests/unit/include/bout/test_generic_factory.cxx
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+
+#include "bout/generic_factory.hxx"
+
+#include <string>
+
+class Base {
+public:
+  Base() {}
+  virtual std::string foo() { return "Base"; }
+};
+
+class Derived1 : public Base {
+public:
+  Derived1() {}
+  std::string foo() override { return "Derived1"; }
+};
+
+class Derived2 : public Base {
+public:
+  Derived2() {}
+  std::string foo() override { return "Derived2"; }
+};
+
+namespace {
+RegisterInFactory<Base, Base> registerme("base");
+RegisterInFactory<Base, Derived1> registerme1("derived1");
+RegisterInFactory<Base, Derived2> registerme2("derived2");
+} // namespace
+
+TEST(GenericFactory, RegisterAndCreate) {
+
+  auto base_ = Factory<Base>::getInstance().create("base");
+  EXPECT_EQ(base_->foo(), "Base");
+
+  auto derived1_ = Factory<Base>::getInstance().create("derived1");
+  EXPECT_EQ(derived1_->foo(), "Derived1");
+
+  auto derived2_ = Factory<Base>::getInstance().create("derived2");
+  EXPECT_EQ(derived2_->foo(), "Derived2");
+}
