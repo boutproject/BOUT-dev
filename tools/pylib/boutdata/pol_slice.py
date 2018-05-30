d40ec0e1dd954a876d56df4618e27e1771eb5376
Peter Hill <zed.three@gmail.com>
2018-05-30 17:03:08 +0100

    Simplify some of the interpolation in pol_slice

--- tools/pylib/boutdata/pol_slice.py
+++ tools/pylib/boutdata/pol_slice.py
@@ -38,8 +38,6 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0, nyInterp=None):
 
     nx, ny, nz = s
 
-    dz = 2.*np.pi / float(n * nz)
-
     # Open the grid file
     with DataFile(gridfile) as gf:
         # Check the grid size is correct
@@ -63,37 +61,24 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0, nyInterp=None):
                 print("ERROR: Neither qinty nor zShift found")
                 return None
 
-    #Decide if we've asked to do interpolation
-    doInterp = False
-    if nyInterp is not None:
-        if ny != nyInterp:
-            doInterp = True
-
-    #Setup interpolation if requested
-    if doInterp:
-
+    # Decide if we've asked to do interpolation
+    if nyInterp is not None and nyInterp != ny:
         varTmp = var3d
-        #These are the index space co-ordinates of the output arrays
-        xOut = np.linspace(0,nx-1,nx)
-        yOut = np.linspace(0,ny-1,nyInterp)
-        zOut = np.linspace(0,nz-1,nz)
 
-        #Use meshgrid to create 3 length N arrays (N=total number of points)
-        xx,yy,zz = np.meshgrid(xOut,yOut,zOut,indexing='ij')
-        #Interpolate to output positions and make the correct shape
-        var3d = map_coordinates(input=varTmp,coordinates=[xx,yy,zz],cval=-999)
+        # Interpolate to output positions and make the correct shape
+        # np.mgrid gives us an array of indices
+        var3d = map_coordinates(varTmp, coordinates=np.mgrid[0:nx, 0:nyInterp, 0:nz],
+                                cval=-999)
+        zShift = map_coordinates(zShift, np.mgrid[0:nx, 0:nyInterp], cval=-999)
 
-        #As above
-        xx,yy = np.meshgrid(xOut,yOut,indexing='ij')
-        zShift = map_coordinates(zShift,[xx,yy],cval=-999)
-
-        #Update shape
+        # Update shape
         ny = nyInterp
 
     var2d = np.zeros([nx, ny])
 
     ######################################
     # Perform 2D slice
+    dz = 2.*np.pi / float(n * nz)
     zind = (zangle - zShift) / dz
     z0f = np.floor(zind)
     z0 = z0f.astype(int)
@@ -106,18 +91,17 @@ def pol_slice(var3d, gridfile, n=1, zangle=0.0, nyInterp=None):
     zp = (z0 + 1) % (nz-1)
     zm = (z0 - 1 + (nz-1)) % (nz-1)
 
-    #For some reason numpy imposes a limit of 32 entries to choose
-    #so if nz>32 we have to use a different approach. This limit may change with numpy version
+    # For some reason numpy imposes a limit of 32 entries to choose
+    # so if nz>32 we have to use a different approach. This limit may change with numpy version
     if nz >= 32:
         for x in np.arange(nx):
             for y in np.arange(ny):
-                var2d[x,y] = 0.5*p[x,y]*(p[x,y]-1.0) * var3d[x,y,zm[x,y]] + \
-                             (1.0 - p[x,y]*p[x,y])   * var3d[x,y,z0[x,y]] + \
-                             0.5*p[x,y]*(p[x,y]+1.0) * var3d[x,y,zp[x,y]]
+                var2d[x, y] = (0.5*p[x, y]*(p[x, y]-1.0) * var3d[x, y, zm[x, y]] +
+                               (1.0 - p[x, y]*p[x, y]) * var3d[x, y, z0[x, y]] +
+                               0.5*p[x, y]*(p[x, y]+1.0) * var3d[x, y, zp[x, y]])
     else:
-        var2d = 0.5*p*(p-1.0) * np.choose(zm.T,var3d.T).T + \
-                (1.0 - p*p) * np.choose(z0.T,var3d.T).T + \
-                0.5*p*(p+1.0) * np.choose(zp.T,var3d.T).T
-
+        var2d = (0.5*p*(p-1.0) * np.choose(zm.T, var3d.T).T +
+                 (1.0 - p*p) * np.choose(z0.T, var3d.T).T +
+                 0.5*p*(p+1.0) * np.choose(zp.T, var3d.T).T)
 
     return var2d
