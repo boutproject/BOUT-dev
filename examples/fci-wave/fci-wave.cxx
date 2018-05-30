c6070597c1a4c638c1e008e8a7707b7feacf8a0b
Ben Dudson <bd512@york.ac.uk>
2018-05-30 11:34:51 +0100

    Applying boundary in Div_par operator
    Uses boundary condition on input variable to apply boundary
    to variable / B

--- examples/fci-wave/fci-wave.cxx
+++ examples/fci-wave/fci-wave.cxx
@@ -13,9 +13,22 @@ private:
     
     f_B.splitYupYdown();
     mesh->getParallelTransform().integrateYUpDown(f_B);
-    //mesh->getParallelTransform().calcYUpDown(f_B);
-    
-    f_B.applyParallelBoundary("parallel_dirichlet");
+
+    // integrateYUpDown replaces all yup/down points, so the boundary conditions
+    // now need to be applied. If Bxyz has neumann parallel boundary conditions
+    // then the boundary condition is simpler since f = 0 gives f_B=0 boundary condition.
+
+    /// Loop over the mesh boundary regions
+    for (const auto &reg : mesh->getBoundariesPar()) {
+      Field3D &f_B_next = f_B.ynext(reg->dir);
+      const Field3D &f_next = f.ynext(reg->dir);
+      const Field3D &B_next = Bxyz.ynext(reg->dir);
+      
+      for (reg->first(); !reg->isDone(); reg->next()) {
+        f_B_next(reg->x, reg->y+reg->dir, reg->z) =
+          f_next(reg->x, reg->y+reg->dir, reg->z) / B_next(reg->x, reg->y, reg->z);
+      }
+    }
 
     Field3D result;
     result.allocate();
@@ -44,8 +57,10 @@ protected:
 
     // Get the magnetic field
     mesh->get(Bxyz, "B");
-    
+
+    // Neumann boundaries simplifies parallel derivatives
     Bxyz.applyBoundary("neumann");
+    Bxyz.applyParallelBoundary("parallel_neumann");
     SAVE_ONCE(Bxyz);
     
     SOLVE_FOR2(n,nv);
@@ -60,10 +75,17 @@ protected:
 
     n.applyParallelBoundary();
     nv.applyParallelBoundary();
+
+    // Calculate momentum flux
+    Field3D momflux = SQ(nv)/floor(n, 1e-4);
+    momflux.splitYupYdown();
+    momflux.yup().allocate();
+    momflux.ydown().allocate();
+    momflux.applyParallelBoundary("parallel_dirichlet");
     
     // Momentum
     ddt(nv) =
-      - Div_par_integrate(SQ(nv)/floor(n, 1e-4))
+      - Div_par_integrate(momflux)
       - Grad_par(n)
       + Grad2_par2(nv)
       ;
