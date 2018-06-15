d528de23c253fdd151979394f7a33b82c8b045b1
Peter Hill <zed.three@gmail.com>
2018-06-15 10:54:43 +0100

    Guard against double-frees of PETSc objects
    PETSc registers some objects on creation, and will destroy them itself
    on calls to PetscFinalize. If an object survives past our call to
    BoutFinalise, which calls PetscFinalize unconditionally, we may get
    double-frees when we try to destroy PETSc objects ourselves.
    
    Solution is to check if PetscFinalize has already been called, and if
    so, don't destroy those objects.

--- src/invert/laplacexy/laplacexy.cxx
+++ src/invert/laplacexy/laplacexy.cxx
@@ -498,13 +498,17 @@ void LaplaceXY::setCoefs(const Field2D &A, const Field2D &B) {
 }
 
 LaplaceXY::~LaplaceXY() {
-  KSPDestroy( &ksp );
-  VecDestroy( &xs );
-  VecDestroy( &bs );
-  MatDestroy( &MatA );
-  
-  // Delete tridiagonal solver
-  delete cr;
+  PetscBool is_finalised;
+  PetscFinalized(&is_finalised);
+
+  if (!is_finalised) {
+    // PetscFinalize may already have destroyed this object
+    KSPDestroy(&ksp);
+  }
+
+  VecDestroy(&xs);
+  VecDestroy(&bs);
+  MatDestroy(&MatA);
 }
 
 const Field2D LaplaceXY::solve(const Field2D &rhs, const Field2D &x0) {
