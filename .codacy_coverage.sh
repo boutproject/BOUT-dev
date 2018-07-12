07aafa87cdbd9d1a207605b4156cf78bde19d70a
David Dickinson <d7919@users.noreply.github.com>
2018-07-12 11:57:14 +0100

    Replace --forceLanguage with -f

--- .codacy_coverage.sh
+++ .codacy_coverage.sh
@@ -1,6 +1,5 @@
-
 #First install the reporter tool
-wget -O ~/codacy-coverage-reporter-assembly-latest.jar https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/4.0.0/codacy-coverage-reporter-4.0.0-assembly.jar
+wget -q -O ~/codacy-coverage-reporter-assembly-latest.jar https://oss.sonatype.org/service/local/repositories/releases/content/com/codacy/codacy-coverage-reporter/4.0.0/codacy-coverage-reporter-4.0.0-assembly.jar > /dev/null
 
 #Analyse the existing gcov files and output in compatible xml format
 #The -g option says to use the existing gcov files, the -k options says to not delete the gcov files
@@ -8,4 +7,4 @@ wget -O ~/codacy-coverage-reporter-assembly-latest.jar https://oss.sonatype.org/
 gcovr --root . -g -k -j 2 --xml -o gcovr_report.xml
 
 #Do the upload
-java -jar ~/codacy-coverage-reporter-assembly-latest.jar report --language CPP -r gcovr_report.xml --partial
+java -jar ~/codacy-coverage-reporter-assembly-latest.jar report -f --language CPP -r gcovr_report.xml --partial
