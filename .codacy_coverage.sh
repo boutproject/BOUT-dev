46480b207d60e2823a2576bcd6c6d47a9d29130d
David Dickinson <d7919@users.noreply.github.com>
2018-07-12 10:40:18 +0100

    Start trying to build coverage for codacy

---
+++ .codacy_coverage.sh
@@ -0,0 +1,11 @@
+
+#First install the reporter tool
+wget -O ~/codacy-coverage-reporter-assembly-latest.jar $(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)
+
+#Analyse the existing gcov files and output in compatible xml format
+#The -g option says to use the existing gcov files, the -k options says to not delete the gcov files
+#The -j option is like make's -j
+gcovr --root . -g -k -j 2 --xml
+
+#Do the upload
+java -jar ~/codacy-coverage-reporter-assembly-latest.jar report --language CPP --forceLanguage -r {XML}
