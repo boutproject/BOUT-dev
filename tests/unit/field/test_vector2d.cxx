406ef66ab184db6fba477c1331adc3c755875a5a
David Schwörer <schword2@mail.dcu.ie>
2018-07-17 18:55:22 +0100

    Update tests

--- tests/unit/field/test_vector2d.cxx
+++ tests/unit/field/test_vector2d.cxx
@@ -28,10 +28,10 @@ protected:
     mesh = new FakeMesh(nx, ny, nz);
     mesh->createDefaultRegions();
 
-    mesh->addBoundary(new BoundaryRegionXIn("core", 1, ny - 2));
-    mesh->addBoundary(new BoundaryRegionXOut("sol", 1, ny - 2));
-    mesh->addBoundary(new BoundaryRegionYUp("upper_target", 1, nx - 2));
-    mesh->addBoundary(new BoundaryRegionYDown("lower_target", 1, nx - 2));
+    mesh->addBoundary(new BoundaryRegionXIn("core", 1, ny - 2, mesh));
+    mesh->addBoundary(new BoundaryRegionXOut("sol", 1, ny - 2, mesh));
+    mesh->addBoundary(new BoundaryRegionYUp("upper_target", 1, nx - 2, mesh));
+    mesh->addBoundary(new BoundaryRegionYDown("lower_target", 1, nx - 2, mesh));
   }
 
   static void TearDownTestCase() {
