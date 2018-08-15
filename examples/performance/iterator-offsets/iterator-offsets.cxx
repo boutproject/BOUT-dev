3665bfa8e62606ca3b1bb8d06fc72af405d29998
Peter Hill <zed.three@gmail.com>
2018-08-15 17:30:49 +0100

    Add various new cases to iterator-offset perf test

--- examples/performance/iterator-offsets/iterator-offsets.cxx
+++ examples/performance/iterator-offsets/iterator-offsets.cxx
@@ -8,7 +8,6 @@
 #include <chrono>
 #include <iostream>
 #include <iterator>
-#include <time.h>
 #include <vector>
 #include <iomanip>
 
@@ -16,9 +15,21 @@
 #include "bout/region.hxx"
 #include <bout/indexoffset.hxx>
 
-typedef std::chrono::time_point<std::chrono::steady_clock> SteadyClock;
-typedef std::chrono::duration<double> Duration;
+using SteadyClock = std::chrono::time_point<std::chrono::steady_clock>;
+using Duration = std::chrono::duration<double>;
 using namespace std::chrono;
+
+#define NEW_BLOCK_REGION_LOOP(index, region)                                             \
+  BOUT_OMP(parallel for)                                                                 \
+  for (auto block = region.getBlocks().cbegin(); block < region.getBlocks().cend();      \
+       ++block)                                                                          \
+    for (auto index = block->first; index < block->second; ++index)
+
+#define NEW_BOUT_REGION_LOOP_PARALLEL_SECTION(index, region, omp_pragmas)                \
+  BOUT_OMP(omp_pragmas)                                                                  \
+  for (auto block = region.getBlocks().cbegin(); block < region.getBlocks().cend();      \
+       ++block)                                                                          \
+    for (auto index = block->first; index < block->second; ++index)
 
 #define ITERATOR_TEST_BLOCK(NAME, ...)		\
   {__VA_ARGS__								\
@@ -34,7 +45,7 @@ int main(int argc, char **argv) {
   BoutInitialise(argc, argv);
   std::vector<std::string> names;
   std::vector<Duration> times;
-  
+
   //Get options root
   Options *globalOptions = Options::getRoot();
   Options *modelOpts = globalOptions->getSection("performanceIterator");
@@ -44,138 +55,107 @@ int main(int argc, char **argv) {
   OPTION(modelOpts, profileMode, false);
   OPTION(modelOpts, includeHeader, false);
 
-  ConditionalOutput time_output(Output::getInstance());
+  ConditionalOutput time_output{Output::getInstance()};
   time_output.enable(true);
-  
-  Field3D a = 1.0;
-  Field3D b = 2.0;
+
+  const Field3D a{1.0};
+  const Field3D b{2.0};
 
   Field3D result;
   result.allocate();
 
-  //Some small setup for C loop case
-  BoutReal *ad = &a(0,0,0);
-  BoutReal *bd = &b(0,0,0);
-  BoutReal *rd = &result(0,0,0);
-  
   const int len = mesh->LocalNx*mesh->LocalNy*mesh->LocalNz;
 
-  //Raw C loop
-///  ITERATOR_TEST_BLOCK("C loop",
-///		      for(int j=0;j<len;++j) {
-///			rd[j] = ad[j] + bd[j];
-///		      };
-///		      );
-#ifdef _OPENMP  
-///  ITERATOR_TEST_BLOCK("C loop (omp)",
-///		      BOUT_OMP(parallel for)
-///		      for(int j=0;j<len;++j) {
-///			rd[j] = ad[j] + bd[j];
-///		      };
-///		    );
-#endif
-  
   // Nested loops over block data
-  ITERATOR_TEST_BLOCK("Nested loop",
-		    for(int i=0;i<mesh->LocalNx;++i) {
-		      for(int j=mesh->ystart;j<mesh->yend;++j) {
-			for(int k=0;k<mesh->LocalNz;++k) {
-			  result(i,j,k) = (a(i,j+1,k) - a(i,j-1,k))/(2.*mesh->coordinates()->dy(i,j));
-			}
-		      }
-		    }
-		    );
+  ITERATOR_TEST_BLOCK(
+    "Nested loop",
+    for(int i=0;i<mesh->LocalNx;++i) {
+      for(int j=mesh->ystart;j<mesh->yend;++j) {
+        for(int k=0;k<mesh->LocalNz;++k) {
+          result(i,j,k) = (a(i,j+1,k) - a(i,j-1,k))/(2.*mesh->coordinates()->dy(i,j));
+        }
+      }
+    }
+    );
 
-#ifdef _OPENMP  
-  ITERATOR_TEST_BLOCK("Nested loop (omp)",
-		      BOUT_OMP(parallel for)
-		      for(int i=0;i<mesh->LocalNx;++i) {
-			for(int j=mesh->ystart;j<mesh->yend;++j) {
-			  for(int k=0;k<mesh->LocalNz;++k) {
-			    result(i,j,k) = (a(i,j+1,k) - a(i,j-1,k))/(2.*mesh->coordinates()->dy(i,j));
-			  }
-		      }
-		      }
-		      );
+#ifdef _OPENMP
+  ITERATOR_TEST_BLOCK(
+    "Nested loop (omp)",
+    BOUT_OMP(parallel for)
+    for(int i=0;i<mesh->LocalNx;++i) {
+      for(int j=mesh->ystart;j<mesh->yend;++j) {
+        for(int k=0;k<mesh->LocalNz;++k) {
+          result(i,j,k) = (a(i,j+1,k) - a(i,j-1,k))/(2.*mesh->coordinates()->dy(i,j));
+        }
+      }
+    }
+    );
 #endif
-  
-  // DataIterator using begin(), end()
-///  IndexRange r{0, mesh->LocalNx, mesh->ystart, mesh->yend, 0, mesh->LocalNz};
-///  ITERATOR_TEST_BLOCK("DI begin/end",
-///		    for(DataIterator i = r.begin(), rend=r.end(); i != rend; ++i){
-///		      result[i] = (a[i.yp()] - a[i.ym()])/(2.*mesh->coordinates()->dy[i]);
-///		    }
-///		    );
 
-  // DataIterator with done()
-///  IndexRange r{0, mesh->LocalNx, mesh->ystart, mesh->yend, 0, mesh->LocalNz};
-///  ITERATOR_TEST_BLOCK("DI begin/done",
-///		    for(DataIterator i = r.begin(); !i.done() ; ++i){
-///		      result(i.x,i.y,i.z);// = a(i.x,i.y,i.z) + b(i.x,i.y,i.z);
-///		    }
-///		    );
-  
   // Range based for DataIterator with indices
-  ITERATOR_TEST_BLOCK("C++11 range-based for",
-                    BOUT_OMP(parallel)
-		    for(auto i : result.region(RGN_NOY)){
-		      result(i.x,i.y,i.z) = (a(i.x,i.y+1,i.z) - a(i.x,i.y-1,i.z))/(2.*mesh->coordinates()->dy(i.x,i.y));
-		    }
-		    );
+  ITERATOR_TEST_BLOCK(
+    "C++11 range-based for (omp)",
+    BOUT_OMP(parallel)
+    for(auto i : result.region(RGN_NOY)){
+      result(i.x,i.y,i.z) = (a(i.x,i.y+1,i.z) - a(i.x,i.y-1,i.z))/(2.*mesh->coordinates()->dy(i.x,i.y));
+    }
+    );
 
-  // Range based DataIterator 
-  ITERATOR_TEST_BLOCK("C++11 range-based for [i]", 
-                    BOUT_OMP(parallel)
-		    for(const auto &i : result.region(RGN_NOY)){
-		      result[i] = (a[i.yp()] - a[i.ym()])/(2.*mesh->coordinates()->dy[i]);
-		    }
-		    );
+  // Range based DataIterator
+  ITERATOR_TEST_BLOCK(
+    "C++11 range-based for [i] (omp)",
+    BOUT_OMP(parallel)
+    for(const auto &i : result.region(RGN_NOY)){
+      result[i] = (a[i.yp()] - a[i.ym()])/(2.*mesh->coordinates()->dy[i]);
+    }
+    );
 
-  ITERATOR_TEST_BLOCK("C++11 range-based for [i] with offset", 
-		    IndexOffset<Ind3D> offset(*mesh);
-                    BOUT_OMP(parallel)
-{
-		    for(const auto &i : mesh->getRegion3D("RGN_NOY")){
-		      result[i] = (a[offset.yp(i)] - a[offset.ym(i)])/(2.*mesh->coordinates()->dy[i]);
-		    }
-}
-		    );
+  ITERATOR_TEST_BLOCK(
+    "C++11 range-based for [i] with offset (omp)",
+    const IndexOffset<Ind3D> offset(*mesh);
+    BOUT_OMP(parallel)
+    {
+      for(const auto &i : mesh->getRegion3D("RGN_NOY")){
+        result[i] = (a[offset.yp(i)] - a[offset.ym(i)])/(2.*mesh->coordinates()->dy[i]);
+      }
+    }
+    );
 
-
-  ITERATOR_TEST_BLOCK("C++11 range-based for [i] with stencil", 
-                    BOUT_OMP(parallel)
-                    {
-                    stencil s;
-		    for(const auto &i : result.region(RGN_NOY)){
-	              s.mm = nan("");
-		      s.m = a[i.ym()];
-		      s.c = a[i];
-		      s.p = a[i.yp()];
-		      s.pp = nan("");
-                      result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
-		    }
-                    }
-		    );
-  
-  // DataIterator over fields
-///  ITERATOR_TEST_BLOCK("DI (done) [i]",
-///                    BOUT_OMP(parallel)
-///		    for(DataIterator d = result.iterator(); !d.done(); d++)
-///		      result[d] = a[d] + b[d];
-///		    );
-
-  //Raw C loop
-///  ITERATOR_TEST_BLOCK("C loop repeat",
-///		    for(int j=0;j<len;++j) {
-///		      rd[j] = ad[j] + bd[j];
-///		    };
-///		    );
+#ifdef _OPENMP
+  ITERATOR_TEST_BLOCK(
+    "C++11 range-based for [i] with stencil (omp)",
+    BOUT_OMP(parallel)
+    {
+      stencil s;
+      for(const auto &i : result.region(RGN_NOY)){
+        s.mm = nan("");
+        s.m = a[i.ym()];
+        s.c = a[i];
+        s.p = a[i.yp()];
+        s.pp = nan("");
+        result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
+      }
+    }
+    );
+#endif
+  ITERATOR_TEST_BLOCK(
+    "C++11 range-based for [i] with stencil (serial)",
+    stencil s;
+    for(const auto &i : result.region(RGN_NOY)){
+      s.mm = nan("");
+      s.m = a[i.ym()];
+      s.c = a[i];
+      s.p = a[i.yp()];
+      s.pp = nan("");
+      result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
+    }
+    );
 
   // Region macro
   ITERATOR_TEST_BLOCK(
-      "Region (serial)",
+      "Region with stencil (serial)",
       stencil s;
-      IndexOffset<Ind3D> offset(*mesh);
+      const IndexOffset<Ind3D> offset(*mesh);
       BLOCK_REGION_LOOP_SERIAL(mesh->getRegion3D("RGN_NOY"), i,
         s.mm = nan("");
         s.m = a[offset.ym(i)];
@@ -184,15 +164,18 @@ int main(int argc, char **argv) {
         s.pp = nan("");
 
         result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
-);
-		      );
+        );
+    );
 #ifdef _OPENMP
-  ITERATOR_TEST_BLOCK("Region (omp)",
-IndexOffset<Ind3D> offset(*mesh);
-BOUT_OMP(parallel)
-{
+
+  ITERATOR_TEST_BLOCK(
+    "Region with stencil (outside) (parallel section omp)",
+    const IndexOffset<Ind3D> offset(*mesh);
+    BOUT_OMP(parallel)
+    {
       stencil s;
-      BLOCK_REGION_LOOP_PARALLEL_SECTION(mesh->getRegion3D("RGN_NOY"), i,
+      BLOCK_REGION_LOOP_PARALLEL_SECTION(
+        mesh->getRegion3D("RGN_NOY"), i,
         s.mm = nan("");
         s.m = a[offset.ym(i)];
         s.c = a[i];
@@ -200,11 +183,89 @@ BOUT_OMP(parallel)
         s.pp = nan("");
 
         result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
-);
-}
-		      );
+        );
+    }
+    );
+
+   ITERATOR_TEST_BLOCK(
+    "Region with stencil (inside) (parallel section omp)",
+    BOUT_OMP(parallel)
+    {
+      const IndexOffset<Ind3D> offset(*mesh);
+      stencil s;
+      BLOCK_REGION_LOOP_PARALLEL_SECTION(
+        mesh->getRegion3D("RGN_NOY"), i,
+        s.mm = nan("");
+        s.m = a[offset.ym(i)];
+        s.c = a[i];
+        s.p = a[offset.yp(i)];
+        s.pp = nan("");
+
+        result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
+        );
+    }
+    );
+
+  ITERATOR_TEST_BLOCK(
+    "Region with stencil & raw ints (parallel section omp)",
+    const auto nz = mesh->LocalNz;
+    BOUT_OMP(parallel)
+    {
+      stencil s;
+      BLOCK_REGION_LOOP_PARALLEL_SECTION(
+        mesh->getRegion3D("RGN_NOY"), i,
+        s.mm = nan("");
+        s.m = a[i.ind - (nz)];
+        s.c = a[i.ind];
+        s.p = a[i.ind + (nz)];
+        s.pp = nan("");
+
+        result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
+        );
+    }
+    );
+
+  ITERATOR_TEST_BLOCK(
+    "Region with stencil (single loop omp)",
+    BOUT_OMP(parallel)
+    {
+      const IndexOffset<Ind3D> offset{*mesh};
+      stencil s;
+      const auto &region = mesh->getRegion3D("RGN_NOY").getIndices();
+      BOUT_OMP(for schedule(guided))
+        for (auto i = region.cbegin(); i < region.cend(); ++i) {
+          s.mm = nan("");
+          s.m = a[offset.ym(*i)];
+          s.c = a[*i];
+          s.p = a[offset.yp(*i)];
+          s.pp = nan("");
+
+          result[*i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[*i]);
+        }
+    }
+    );
+
+   ITERATOR_TEST_BLOCK(
+    "Region with stencil (inside) (parallel section omp)",
+    BOUT_OMP(parallel)
+    {
+      const IndexOffset<Ind3D> offset(*mesh);
+      stencil s;
+      NEW_BOUT_REGION_LOOP_PARALLEL_SECTION(i, mesh->getRegion3D("RGN_NOY"), for schedule(guided) nowait) {
+        s.mm = nan("");
+        s.m = a[offset.ym(i)];
+        s.c = a[i];
+        s.p = a[offset.yp(i)];
+        s.pp = nan("");
+
+        result[i] = (s.p - s.m)/(2.*mesh->coordinates()->dy[i]);
+      }
+    }
+    );
+
 #endif
-  
+
+
   if(profileMode){
     int nthreads=0;
 #ifdef _OPENMP
@@ -216,9 +277,9 @@ BOUT_OMP(parallel)
       time_output << "\n------------------------------------------------\n";
       time_output << "Case legend";
       time_output <<"\n------------------------------------------------\n";
-      
-      for (int i = 0 ; i < names.size(); i++){	
-	time_output << std::setw(width) << "Case " << i << ".\t" << names[i] << "\n";
+
+      for (int i = 0 ; i < names.size(); i++){
+    time_output << std::setw(width) << "Case " << i << ".\t" << names[i] << "\n";
       }
       time_output << "\n";
       time_output << std::setw(width) << "Nprocs" << "\t";
@@ -228,8 +289,8 @@ BOUT_OMP(parallel)
       time_output << std::setw(width) << "Nx (global)" << "\t";
       time_output << std::setw(width) << "Ny (global)" << "\t";
       time_output << std::setw(width) << "Nz (global)" << "\t";
-      for (int i = 0 ; i < names.size(); i++){	
-	time_output << std::setw(width) << "Case " << i << "\t";
+      for (int i = 0 ; i < names.size(); i++){
+    time_output << std::setw(width) << "Case " << i << "\t";
       }
       time_output << "\n";
     }
@@ -241,7 +302,7 @@ BOUT_OMP(parallel)
     time_output << std::setw(width) << mesh->GlobalNx << "\t";
     time_output << std::setw(width) << mesh->GlobalNy << "\t";
     time_output << std::setw(width) << mesh->GlobalNz << "\t";
-    for (int i = 0 ; i < names.size(); i++){	
+    for (int i = 0 ; i < names.size(); i++){
       time_output << std::setw(width) << times[i].count()/NUM_LOOPS << "\t";
     }
     time_output << "\n";
