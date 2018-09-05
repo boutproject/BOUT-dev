6209443634466c8cd286e86608976ab0e5046d88
David Dickinson <d7919@users.noreply.github.com>
2018-09-05 16:01:41 +0100

    Adding a brief example to demonstrate invert_operator usage

---
+++ examples/invert_operator/invert_operator.cxx
@@ -0,0 +1,47 @@
+
+#include <bout/invert_operator.hxx>
+#include <bout/physicsmodel.hxx>
+#include <derivs.hxx>
+
+Field3D minus(const Field3D &input) { return -1.0 * input; };
+Field3D delp(const Field3D &input) { return input + Delp2(input); };
+
+class HW : public PhysicsModel {
+private:
+  Field3D n;
+  class InvertOperator<Field3D> *mySolver;
+
+protected:
+  int init(bool restart) {
+
+    SOLVE_FOR(n);
+
+    mySolver = new InvertOperator<Field3D>();
+
+    mySolver->setup(delp);
+
+    Field3D output = 3.0;
+    n = -2.0;
+    output = mySolver->invert(n);
+
+    output_warn << endl;
+    output_warn << "Max difference is " << max(abs(output - n), true) << endl;
+    output_warn << endl;
+    auto pass = mySolver->verify(n) == 0 ? "False" : "True";
+    output_warn << "Has test passed ? " << pass << endl;
+    output_warn << endl;
+
+    return 0;
+  }
+
+  int rhs(BoutReal time) {
+    ddt(n) = 0.;
+    return 0;
+  }
+
+public:
+  ~HW() { delete mySolver; };
+};
+
+// Define a main() function
+BOUTMAIN(HW);
