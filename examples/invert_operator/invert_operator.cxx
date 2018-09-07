394dd96f899a6dab81507a7439f1c1c99249278c
David Dickinson <d7919@users.noreply.github.com>
2018-09-07 17:04:35 +0100

    Don't use new in the example

--- examples/invert_operator/invert_operator.cxx
+++ examples/invert_operator/invert_operator.cxx
@@ -9,7 +9,6 @@ Field3D delp(const Field3D &input) { return input + Delp2(input); };
 class HW : public PhysicsModel {
 private:
   Field3D n;
-  InvertOperator<Field3D> *mySolver;
   
   struct myOp : public OperatorWrapper {
     BoutReal factor=1.;
@@ -22,33 +21,37 @@ protected:
 
     SOLVE_FOR(n);
 
-    mySolver = new InvertOperator<Field3D>();
-
-    // mySolver->setup(delp);
-    mySolver->setup(myDelp);
+    InvertOperator<Field3D> mySolver(myDelp);
     // Above could also be:
-    // mySolver->setup(delp);
+    // mySolver(delp);
     // or even a Lambda
-    // mySolver->setup([](const Field3D &input) { return input + Delp2(input); });
+    // mySolver([](const Field3D &input) { return input + Delp2(input); });
+
+    mySolver.setup();
 
     // Note mySolve takes a copy of the passed functor so updates to the local
     // instance won't have any effect, but the function _can_ be changed (currently)
     // as it is a public member, so the following should work.
     myDelp.factor = 3.0;
-    mySolver->func = myDelp;
+    mySolver.setOperatorFunction(myDelp);
+    
     Field3D output = 3.0;
     n = -2.0;
     for(int i=0; i<10000; i++){
-      output = mySolver->invert(n);
+      // myDelp.factor = i+1;
+      // mySolver.setOperatorFunction(myDelp);
+      output = mySolver.invert(n);
+      // auto pass = mySolver.verify(n) == 0 ? "False" : "True";
+      // output_warn << "Has test passed ? " << pass << std::endl;
     }
     output_warn << endl;
     output_warn << "Max difference is " << max(abs(output - n), true) << std::endl;
     output_warn << std::endl;
-    auto pass = mySolver->verify(n) == 0 ? "False" : "True";
+    auto pass = mySolver.verify(n) == 0 ? "False" : "True";
     output_warn << "Has test passed ? " << pass << std::endl;
     output_warn << std::endl;
 
-    mySolver->reportTime();
+    mySolver.reportTime();
     return 0;
   }
 
@@ -58,7 +61,6 @@ protected:
   }
 
 public:
-  ~HW() { delete mySolver; };
 };
 
 // Define a main() function
