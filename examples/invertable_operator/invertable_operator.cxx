8e92083b6fa074542c4697edeea6c4c2bfe8399f
David Dickinson <d7919@users.noreply.github.com>
2018-09-12 14:02:33 +0100

    Clang format InvertableOperator example file

--- examples/invertable_operator/invertable_operator.cxx
+++ examples/invertable_operator/invertable_operator.cxx
@@ -1,9 +1,9 @@
 #include <invert_laplace.hxx>
 #include <msg_stack.hxx>
 
-#include <bout/sys/timer.hxx>
 #include <bout/invertable_operator.hxx>
 #include <bout/physicsmodel.hxx>
+#include <bout/sys/timer.hxx>
 #include <derivs.hxx>
 
 #include <boundary_region.hxx>
@@ -14,21 +14,21 @@ Field3D delp(const Field3D &input) { return input + Delp2(input); };
 class HW : public PhysicsModel {
 private:
   Field3D n, solutionInv, solutionLap;
-  
+
   struct myOp {
-    BoutReal factor=1.;
-    Field3D operator()(const Field3D &input) {return factor*input + Delp2(input); } ;
+    BoutReal factor = 1.;
+    Field3D operator()(const Field3D &input) { return factor * input + Delp2(input); };
   };
   myOp myDelp;
 
   struct myLaplacian {
-    Field3D D=1.0, C=1.0, A=0.0;
+    Field3D D = 1.0, C = 1.0, A = 0.0;
 
     // Drop C term for now
     Field3D operator()(const Field3D &input) {
       TRACE("myLaplacian::operator()");
-      Timer timer("invertable_operator_operate");      
-      Field3D result = A*input + D*Delp2(input);
+      Timer timer("invertable_operator_operate");
+      Field3D result = A * input + D * Delp2(input);
 
       // Ensure boundary points are set appropriately as given by the input field.
       result.setBoundaryTo(input);
@@ -37,7 +37,29 @@ private:
     };
   };
 
-  
+  struct my3DLaplacian {
+    Field3D A = 1.0, B = 1.0, C = 0.0, D = 0.0;
+    bool withDiv = false;
+
+    // Drop C term for now
+    Field3D operator()(const Field3D &input) {
+      TRACE("myLaplacian::operator()");
+      Timer timer("invertable_operator_operate");
+      Field3D result = A * input + B * Laplace_perp(input);
+      if (withDiv) {
+        auto tmp = C * Grad_perp(input);
+        input.getMesh()->communicate(tmp);
+        result += Div(tmp);
+      }
+      result += D * Laplace(input);
+
+      // Ensure boundary points are set appropriately as given by the input field.
+      result.setBoundaryTo(input);
+
+      return result;
+    };
+  };
+
   myLaplacian mm;
   InvertableOperator<Field3D> mySolver;
   // Above could also be:
@@ -46,17 +68,17 @@ private:
   // mySolver([](const Field3D &input) { return input + Delp2(input); });
 
   class Laplacian *phiSolverXZ;
-  
-  const int nits=1000;    
-  
+
+  const int nits = 10;
+
 protected:
   int init(bool restart) {
     SOLVE_FOR(n);
     SOLVE_FOR(solutionLap);
     SOLVE_FOR(solutionInv);
 
-    mm.A=1.0e-1;
-    mm.D=1.0;
+    mm.A = 1.0e-1;
+    mm.D = 1.0;
 
     // Note mySolve takes a copy of the passed functor so updates to the local
     // instance won't have any effect, but the function _can_ be changed (currently)
@@ -65,14 +87,13 @@ protected:
     mySolver.setOperatorFunction(mm);
     mySolver.setup();
 
-
     phiSolverXZ = Laplacian::create();
     phiSolverXZ->setCoefA(mm.A);
     phiSolverXZ->setCoefC(1.0);
     phiSolverXZ->setCoefD(mm.D);
 
     n.applyBoundary("dirichlet");
-    
+
     return 0;
   }
 
@@ -81,15 +102,21 @@ protected:
     ddt(solutionInv) = 0.;
     ddt(solutionLap) = 0.;
 
-    try{
-    for(int i=0; i<nits; i++){
-      solutionInv = mySolver.invert(n);
-    }
-    }catch(BoutException){
+    // First run to get the solution with zero initial guess
+    solutionInv = mySolver.invert(n, 0.0);
+    mySolver.reportTime();
+
+    try {
+      for (int i = 0; i < nits; i++) {
+        //    solutionInv = mySolver.invert(n);
+        solutionInv = mySolver.invert(n, solutionInv);
+        // mesh->communicate(solutionInv);
+      }
+    } catch (BoutException) {
     };
 
     mesh->communicate(solutionInv);
-    
+
     output_warn << std::endl;
     auto pass = mySolver.verify(n) == 0 ? "False" : "True";
     output_warn << "Has test passed ? " << pass << std::endl;
@@ -97,19 +124,20 @@ protected:
 
     {
       Timer timer("sol_lap");
-      try{
-      for(int i=0; i<nits; i++){    
-    	solutionLap = phiSolverXZ->solve(n);
-      }
-      }catch(BoutException){
+      try {
+        for (int i = 0; i < nits; i++) {
+          solutionLap = phiSolverXZ->solve(n);
+        }
+      } catch (BoutException) {
       };
     }
 
-    output_warn<<"Max diff undo Invertable is "<<max(abs(mm(solutionInv)-n),true)<<endl;
-    output_warn<<"MAX DIFF SOL "<<max(abs(solutionLap-solutionInv),true)<<endl;
+    output_warn << "Max diff undo Invertable is " << max(abs(mm(solutionInv) - n), true)
+                << endl;
+    output_warn << "MAX DIFF SOL " << max(abs(solutionLap - solutionInv), true) << endl;
 
     mySolver.reportTime();
-    output_warn<<"Laplacian time is "<<Timer::resetTime("sol_lap")<<endl;
+    output_warn << "Laplacian time is " << Timer::resetTime("sol_lap") << endl;
 
     return 0;
   }
