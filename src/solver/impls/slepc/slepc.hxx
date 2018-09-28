6458aae9cb821592a887b1ed4f156958cd10d0f3
Ben Dudson <bd512@york.ac.uk>
2018-09-28 20:48:35 +0100

    Change Solver::add to take std::string
    `Solver::add` and `Solver::constrain` previously took
    variable names as `const char*`. This made generating variable names
    quite clunky e.g. `(d.name+"_x").c_str()`.
    Now changed to `const std::string&`.
    
    Some reformatting, particularly of `SlepcSolver`, and adding
    `override` to `SlepcSolver` definitions.

--- src/solver/impls/slepc/slepc.hxx
+++ src/solver/impls/slepc/slepc.hxx
@@ -33,22 +33,22 @@ class SlepcSolver;
 
 #include <slepc.h>
 
-#include <field2d.hxx>
-#include <field3d.hxx>
-#include <vector2d.hxx>
-#include <vector3d.hxx>
 #include "bout/solverfactory.hxx"
 #include <bout/solver.hxx>
+#include <field2d.hxx>
+#include <field3d.hxx>
 #include <utils.hxx>
+#include <vector2d.hxx>
+#include <vector3d.hxx>
 
-#include <vector>
-#include <bout/slepclib.hxx>
 #include <bout/petsclib.hxx>
+#include <bout/slepclib.hxx>
+#include <vector>
 
 #define OPT_SIZE 40
 
-//Define a name to use with SolverType to indicate SlepcSolver
-//is in charge of advancing fields
+// Define a name to use with SolverType to indicate SlepcSolver
+// is in charge of advancing fields
 #define SOLVERSLEPCSELF "self"
 
 namespace {
@@ -58,150 +58,196 @@ RegisterSolver<SlepcSolver> registersolverslepc("slepc");
 using std::vector;
 
 class SlepcSolver : public Solver {
- public:
+public:
   SlepcSolver(Options *options);
   ~SlepcSolver();
 
   int advanceStep(Mat &matOperator, Vec &inData, Vec &outData);
   int compareEigs(PetscScalar ar, PetscScalar ai, PetscScalar br, PetscScalar bi);
-  void monitor(PetscInt its, PetscInt nconv, PetscScalar eigr[], PetscScalar eigi[], PetscReal errest[], PetscInt nest);
+  void monitor(PetscInt its, PetscInt nconv, PetscScalar eigr[], PetscScalar eigi[],
+               PetscReal errest[], PetscInt nest);
 
-  //These contain slepc specific code and call the advanceSolver code
+  // These contain slepc specific code and call the advanceSolver code
   int init(int NOUT, BoutReal TIMESTEP) override;
   int run() override;
 
   ////////////////////////////////////////
   /// OVERRIDE
   ///      Here we override *all* other
-  ///      virtual functions in order to 
-  ///      pass through control to the 
+  ///      virtual functions in order to
+  ///      pass through control to the
   ///      actual solver (advanceSolver)
   ///      This is only required if allow
   ///      use of additional solver
   ////////////////////////////////////////
-  
-  void setModel(PhysicsModel *model) { // New API
+
+  void setModel(PhysicsModel *model) override { // New API
     Solver::setModel(model);
-    if(!selfSolve){advanceSolver->setModel(model);}
+    if (!selfSolve) {
+      advanceSolver->setModel(model);
+    }
   }
-  
-  void setRHS(rhsfunc f) {  // Old API
+
+  void setRHS(rhsfunc f) override { // Old API
     Solver::setRHS(f);
-    if(!selfSolve){advanceSolver->setRHS(f);}
+    if (!selfSolve) {
+      advanceSolver->setRHS(f);
+    }
   }
-  
+
   //////Following overrides all just pass through to advanceSolver
 
-  //Override virtual add functions in order to pass through to advanceSolver
-  void add(Field2D &v, const char* name){
-    Solver::add(v,name);
-    if(!selfSolve){advanceSolver->add(v,name);}
+  // Override virtual add functions in order to pass through to advanceSolver
+  void add(Field2D &v, const std::string &name) override {
+    Solver::add(v, name);
+    if (!selfSolve) {
+      advanceSolver->add(v, name);
+    }
   }
-  void add(Field3D &v, const char* name){
-    Solver::add(v,name);
-    if(!selfSolve){advanceSolver->add(v,name);}
+  void add(Field3D &v, const std::string &name) override {
+    Solver::add(v, name);
+    if (!selfSolve) {
+      advanceSolver->add(v, name);
+    }
   }
-  void add(Vector2D &v, const char* name){
-    Solver::add(v,name);
-    if(!selfSolve){advanceSolver->add(v,name);}
+  void add(Vector2D &v, const std::string &name) override {
+    Solver::add(v, name);
+    if (!selfSolve) {
+      advanceSolver->add(v, name);
+    }
   }
-  void add(Vector3D &v, const char* name){
-    Solver::add(v,name);
-    if(!selfSolve){advanceSolver->add(v,name);}
-  }
-
-  //Set operations
-  void setJacobian(Jacobian j){if(!selfSolve){advanceSolver->setJacobian(j);}}
-  void setSplitOperator(rhsfunc fC, rhsfunc fD){
-    if(selfSolve){Solver::setSplitOperator(fC,fD);}else{advanceSolver->setSplitOperator(fC,fD);}
-  }
-
-  //Constraints
-  bool constraints(){
-    if(selfSolve){return false;}else{return advanceSolver->constraints();}
-  }
-  void constraint(Field2D &v, Field2D &C_v, const char* name){
-    if(!selfSolve){advanceSolver->constraint(v,C_v,name);}
-  }
-  void constraint(Field3D &v, Field3D &C_v, const char* name){
-    if(!selfSolve){advanceSolver->constraint(v,C_v,name);}
-  }
-  void constraint(Vector2D &v, Vector2D &C_v, const char* name){
-    if(!selfSolve){advanceSolver->constraint(v,C_v,name);}
-  }
-  void constraint(Vector3D &v, Vector3D &C_v, const char* name){
-    if(!selfSolve){advanceSolver->constraint(v,C_v,name);}
+  void add(Vector3D &v, const std::string &name) override {
+    Solver::add(v, name);
+    if (!selfSolve) {
+      advanceSolver->add(v, name);
+    }
   }
 
-  //Override count operations
-  int n2Dvars() const{
-    if(selfSolve){return Solver::n2Dvars();}else{return advanceSolver->n2Dvars();}
+  // Set operations
+  void setJacobian(Jacobian j) override {
+    if (!selfSolve) {
+      advanceSolver->setJacobian(j);
+    }
   }
-  int n3Dvars() const{
-    if(selfSolve){return Solver::n3Dvars();}else{return advanceSolver->n3Dvars();}
+  void setSplitOperator(rhsfunc fC, rhsfunc fD) override {
+    if (selfSolve) {
+      Solver::setSplitOperator(fC, fD);
+    } else {
+      advanceSolver->setSplitOperator(fC, fD);
+    }
   }
-  //Time steps
-  void setMaxTimestep(BoutReal dt){
-    if(selfSolve){Solver::setMaxTimestep(dt);}else{advanceSolver->setMaxTimestep(dt);}
+
+  // Constraints
+  bool constraints() override {
+    if (selfSolve) {
+      return false;
+    } else {
+      return advanceSolver->constraints();
+    }
   }
-  BoutReal getCurrentTimestep(){
-    if(selfSolve){return Solver::max_dt;}{return advanceSolver->getCurrentTimestep();}
+  void constraint(Field2D &v, Field2D &C_v, const std::string &name) override {
+    if (!selfSolve) {
+      advanceSolver->constraint(v, C_v, name);
+    }
+  }
+  void constraint(Field3D &v, Field3D &C_v, const std::string &name) override {
+    if (!selfSolve) {
+      advanceSolver->constraint(v, C_v, name);
+    }
+  }
+  void constraint(Vector2D &v, Vector2D &C_v, const std::string &name) override {
+    if (!selfSolve) {
+      advanceSolver->constraint(v, C_v, name);
+    }
+  }
+  void constraint(Vector3D &v, Vector3D &C_v, const std::string &name) override {
+    if (!selfSolve) {
+      advanceSolver->constraint(v, C_v, name);
+    }
+  }
+
+  // Override count operations
+  int n2Dvars() const override {
+    if (selfSolve) {
+      return Solver::n2Dvars();
+    } else {
+      return advanceSolver->n2Dvars();
+    }
+  }
+  int n3Dvars() const override {
+    if (selfSolve) {
+      return Solver::n3Dvars();
+    } else {
+      return advanceSolver->n3Dvars();
+    }
+  }
+  // Time steps
+  void setMaxTimestep(BoutReal dt) override {
+    if (selfSolve) {
+      Solver::setMaxTimestep(dt);
+    } else {
+      advanceSolver->setMaxTimestep(dt);
+    }
+  }
+  BoutReal getCurrentTimestep() override {
+    if (selfSolve) {
+      return Solver::max_dt;
+    }
+    { return advanceSolver->getCurrentTimestep(); }
   }
 
   int compareState;
 
-  void slepcToBout(PetscScalar &reEigIn, PetscScalar &imEigIn,
-		   BoutReal &reEigOut, BoutReal &imEigOut,
-		   bool force=false);
+  void slepcToBout(PetscScalar &reEigIn, PetscScalar &imEigIn, BoutReal &reEigOut,
+                   BoutReal &imEigOut, bool force = false);
 
   Mat shellMat; //"Shell" matrix operator
 private:
   MPI_Comm comm;
-  EPS eps; //Slepc solver handle
+  EPS eps; // Slepc solver handle
 
-  ST st; //Spectral transform object
-  PetscBool stIsShell; //Is the ST a shell object?
+  ST st;               // Spectral transform object
+  PetscBool stIsShell; // Is the ST a shell object?
 
-  Solver* advanceSolver; //Pointer to actual solver used to advance fields
+  Solver *advanceSolver; // Pointer to actual solver used to advance fields
 
   void vecToFields(Vec &inVec);
   void fieldsToVec(Vec &outVec);
-  
+
   void createShellMat();
   void createEPS();
 
   void analyseResults();
-  void boutToSlepc(BoutReal &reEigIn, BoutReal &imEigIn,
-		   PetscScalar &reEigOut, PetscScalar &imEigOut,
-		   bool force=false);
+  void boutToSlepc(BoutReal &reEigIn, BoutReal &imEigIn, PetscScalar &reEigOut,
+                   PetscScalar &imEigOut, bool force = false);
 
-  SlepcLib slib;// Handles initialize / finalize
-  bool ddtMode; //If true then slepc deals with the ddt operator
-  bool selfSolve; //If true then we don't create an advanceSolver
+  SlepcLib slib;  // Handles initialize / finalize
+  bool ddtMode;   // If true then slepc deals with the ddt operator
+  bool selfSolve; // If true then we don't create an advanceSolver
   bool eigenValOnly;
 
-  //For selfSolve=true
+  // For selfSolve=true
   Array<BoutReal> f0, f1;
 
-  //Timestep details
+  // Timestep details
   int nout;
   BoutReal tstep;
 
-  //Used for SLEPc options
-  int nEig,maxIt;
-  int mpd;     // Maximum projected dimension
-  PetscReal tol,target;
+  // Used for SLEPc options
+  int nEig, maxIt;
+  int mpd; // Maximum projected dimension
+  PetscReal tol, target;
   BoutReal targRe, targIm;
   bool userWhich;
 
-  //Generic options
-  bool useInitial; //If true then set the first vector in subspace to be initial conditions
+  // Generic options
+  bool useInitial; // If true then set the first vector in subspace to be initial
+                   // conditions
   bool debugMonitor;
 
-  //Grid size
+  // Grid size
   PetscInt localSize;
 };
-
 
 #endif // __SLEPC_SOLVER_H__
 
