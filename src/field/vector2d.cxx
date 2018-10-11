f72611ca6d0e85920c5d8a726ac3a7878dfa552c
John Omotani <john.omotani@ukaea.uk>
2018-10-11 12:28:09 +0100

    Remove uses of global mesh from boundary conditions
    Requires adding a Mesh* pointer and getDataMesh() method to FieldData,
    so that FieldData::setBoundary() can use the local mesh.
    
    getDataMesh() method is needed in case the FieldData is constructed
    before the global mesh is created (i.e. when Field3D, etc. are declared
    in global scope). It must not be called getMesh() because that would
    clash with the method of Field.

--- src/field/vector2d.cxx
+++ src/field/vector2d.cxx
@@ -36,10 +36,12 @@
 #include <interpolation.hxx>
 
 Vector2D::Vector2D(Mesh *localmesh)
-    : x(localmesh), y(localmesh), z(localmesh), covariant(true), deriv(nullptr), location(CELL_CENTRE) {}
+    : FieldData(localmesh), x(localmesh), y(localmesh), z(localmesh),
+      covariant(true), deriv(nullptr), location(CELL_CENTRE) {}
 
 Vector2D::Vector2D(const Vector2D &f)
-    : x(f.x), y(f.y), z(f.z), covariant(f.covariant), deriv(nullptr), location(CELL_CENTRE) {}
+    : FieldData(f.fielddatamesh), x(f.x), y(f.y), z(f.z), covariant(f.covariant),
+      deriv(nullptr), location(CELL_CENTRE) {}
 
 Vector2D::~Vector2D() {
   if (deriv != nullptr) {
@@ -147,6 +149,8 @@ Vector2D* Vector2D::timeDeriv() {
 /////////////////// ASSIGNMENT ////////////////////
 
 Vector2D & Vector2D::operator=(const Vector2D &rhs) {
+  fielddatamesh = rhs.fielddatamesh;
+
   x = rhs.x;
   y = rhs.y;
   z = rhs.z;
