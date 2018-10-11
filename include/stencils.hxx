e7c2a29d527e36ae24b6163989e7f1c14b38a5c8
David Dickinson <d7919@users.noreply.github.com>
2018-10-11 11:34:12 +0100

    Adding templated routine `populateStencil` which should hopefully be usable to populate any stencil

--- include/stencils.hxx
+++ include/stencils.hxx
@@ -37,4 +37,46 @@ struct stencil {
   BoutReal mm = BoutNaN, m = BoutNaN, c = BoutNaN, p = BoutNaN, pp = BoutNaN; 
 };
 
+
+template<typename FieldType, int NGuard, DIRECTION direction, STAGGER stagger>
+void inline populateStencil(stencil &s, const FieldType& f, const typename FieldType::ind_type i){
+  static_assert(NGuard == 1 || NGuard == 2,
+		"populateStencil currently only supports one or two guard cells"
+		);
+
+  switch(stagger) {
+  case(STAGGER::None):
+    if (NGuard == 2) s.mm = f[i.template minus<2, direction>()];
+    s.m = f[i.template minus<1, direction>()];
+    s.c = f[i];
+    s.p = f[i.template plus<1, direction>()];
+    if (NGuard == 2) s.pp = f[i.template plus<2, direction>()];
+    break;
+  case(STAGGER::C2L):
+    if (NGuard == 2) s.mm = f[i.template minus<2, direction>()];
+    s.m = f[i.template minus<1, direction>()];
+    s.c = f[i];
+    s.p = s.c;
+    s.pp = f[i.template plus<1, direction>()];
+    break;
+  case(STAGGER::L2C):
+    s.mm = f[i.template minus<1, direction>()];
+    s.m = f[i];
+    s.c = s.m;
+    s.p = f[i.template plus<1, direction>()];
+    if (NGuard == 2) s.pp = f[i.template plus<2, direction>()];
+    break;
+  }
+  return;
+}
+
+template<typename FieldType, int NGuard, DIRECTION direction, STAGGER stagger>
+stencil inline populateStencil(const FieldType& f, const typename FieldType::ind_type i){
+  static_assert(NGuard == 1 || NGuard == 2,
+		"populateStencil currently only supports one or two guard cells"
+		);
+  stencil s;
+  populateStencil<FieldType, NGuard, direction, stagger>(s, f, i);
+  return s;
+}
 #endif /* __STENCILS_H__ */
