e7120a08b9060ae8ab5baf5096bf6df79a356025
John Omotani <john.omotani@ukaea.uk>
2018-10-14 22:50:55 +0100

    Fix MMS/single-null to use shifted option in mms_alternate.py correctly

--- tests/MMS/single-null/runtest.py
+++ tests/MMS/single-null/runtest.py
@@ -10,14 +10,6 @@ import numpy
 import sympy
 from sys import exit
 
-# geometry for simple circular tokamak
-tokamak = SimpleTokamak(psiN0=.95, lower_legs_fraction=0.25)
-# rescale x and y coordinates so dx and dy are not constants
-tokamak.set_scalex(1 + .1*sin(2*pi*metric.x+metric.y))
-tokamak.set_scaley(1 + .1*sin(2*pi*metric.x-metric.y))
-# re-calculate metric terms
-tokamak.metric()
-
 min_exponent = 6
 max_exponent = 7
 ngrids = numpy.logspace(min_exponent, max_exponent, num=max_exponent-min_exponent+1, base=2, dtype=int)
@@ -28,9 +20,9 @@ plot_error = False
 
 boutcore.init('-q -q -q -q')
 
-testfunc = sin(2*pi*metric.x + metric.y + metric.z + metric.zShift)
-boutcore_operator = boutcore.Grad_par
-symbolic_operator = Grad_par
+testfunc = sin(2*pi*metric.x + metric.y + metric.z)
+boutcore_operator = boutcore.DDX
+symbolic_operator = DDX
 order = 2
 stagger = None
 dimensions = 'xyz'
@@ -39,6 +31,20 @@ method = None
 results = []
 for twistshift, paralleltransform in [('true', 'identity'), ('false', 'shifted')]:
     print('twistshift='+twistshift+' paralleltransform='+paralleltransform)
+
+    # geometry for simple circular tokamak
+    if paralleltransform == 'identity':
+        tokamak = SimpleTokamak(psiN0=.95, lower_legs_fraction=0.25)
+    elif paralleltransform == 'shifted':
+        tokamak = SimpleTokamak(psiN0=.95, lower_legs_fraction=0.25, shifted=True)
+    else:
+        raise ValueError('Unhandled paralleltransform='+paralleltransform)
+    # rescale x and y coordinates so dx and dy are not constants
+    tokamak.set_scalex(1 + .1*sin(2*pi*metric.x+metric.y))
+    tokamak.set_scaley(1 + .1*sin(2*pi*metric.x-metric.y))
+    # re-calculate metric terms
+    tokamak.metric()
+
     error_list = []
     for n in ngrids:
         print('n =',n)
@@ -106,7 +112,7 @@ for twistshift, paralleltransform in [('true', 'identity'), ('false', 'shifted')
             outloc = stagger[1]
 
         # calculate result of differential operator using BOUT++ implementation
-        bout_input = boutcore.create3D(exprToStr(testfunc), mesh, outloc=inloc)
+        bout_input = boutcore.create3D(exprToStr(testfunc.subs(metric.z, metric.z + metric.zShift)), mesh, outloc=inloc)
         mesh.communicate(bout_input)
 
         if method is None:
@@ -115,7 +121,10 @@ for twistshift, paralleltransform in [('true', 'identity'), ('false', 'shifted')
             bout_result = boutcore_operator(bout_input, outloc=outloc, method=method)
         mesh.communicate(bout_result)
         # calculate result of differential operator symbolically, then convert to boutcore.Field3D/Field2D
-        analytic_func = symbolic_operator(testfunc)
+        if paralleltransform == 'identity':
+            analytic_func = symbolic_operator(testfunc.subs(metric.z, metric.z + metric.zShift))
+        elif paralleltransform == 'shifted':
+            analytic_func = symbolic_operator(testfunc).subs(metric.z, metric.z + metric.zShift)
         analytic_result = boutcore.create3D(exprToStr(analytic_func), mesh, outloc=outloc)
 
         # calculate max error
@@ -130,8 +139,8 @@ for twistshift, paralleltransform in [('true', 'identity'), ('false', 'shifted')
 
     if order-.1 < convergence < order+.2:
         results.append('pass')
-        results.append('pass: '+str(boutcore_operator)+' is  working correctly for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
-        #results.append('Proc #'+str(mesh.getYProcIndex())+' --- pass: '+str(boutcore_operator)+' is  working correctly for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
+        #results.append('pass: '+str(boutcore_operator)+' is  working correctly for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
+        results.append('Proc #'+str(mesh.getYProcIndex())+' --- pass: '+str(boutcore_operator)+' is  working correctly for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
     else:
         if plot_error:
             yproc = mesh.getYProcIndex()
@@ -141,8 +150,8 @@ for twistshift, paralleltransform in [('true', 'identity'), ('false', 'shifted')
             pyplot.show()
             from boututils.showdata import showdata
             showdata(error, titles=["proc = "+str(yproc)])
-        results.append(str(boutcore_operator)+' is not working for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
-        #results.append('Proc #'+str(mesh.getYProcIndex())+' --- '+str(boutcore_operator)+' is not working for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
+        #results.append(str(boutcore_operator)+' is not working for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
+        results.append('Proc #'+str(mesh.getYProcIndex())+' --- '+str(boutcore_operator)+' is not working for '+inloc+'->'+outloc+' twistshift='+twistshift+' paralleltransform='+paralleltransform+' '+str(method)+'. Expected '+str(order)+', got '+str(convergence)+'.')
 
 # check results of tests
 fail = False
