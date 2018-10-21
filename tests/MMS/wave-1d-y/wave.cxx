8a1d624183759ddeeddb7c50e9702dff325fdf08
John Omotani <john.omotani@ukaea.uk>
2018-10-21 17:36:11 +0100

    Test StaggerGrids=false and both dirichlet_smooth/neumann in MMS/wave-1d-y

--- tests/MMS/wave-1d-y/wave.cxx
+++ tests/MMS/wave-1d-y/wave.cxx
@@ -5,12 +5,19 @@
 class Wave1D : public PhysicsModel {
 private:
   Field3D f, g; // Evolving variables
+  CELL_LOC maybe_ylow;
   
 protected:
   int init(bool restarting) {
 
-    g.setLocation(CELL_YLOW); // g staggered 
-    
+    if(mesh->StaggerGrids) {
+      maybe_ylow = CELL_YLOW;
+    } else {
+      maybe_ylow = CELL_CENTRE;
+    }
+
+    g.setLocation(maybe_ylow); // g staggered
+
     // Tell BOUT++ to solve f and g
     bout_solve(f, "f");
     bout_solve(g, "g");
@@ -23,7 +30,7 @@ protected:
     
     // Central differencing
     ddt(f) = DDY(g, CELL_CENTRE);
-    ddt(g) = DDY(f, CELL_YLOW);
+    ddt(g) = DDY(f, maybe_ylow);
     
     return 0;
   }
