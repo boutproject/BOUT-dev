cb702c3228d68293047cf7c7ee9d6837c9e9a093
David Dickinson <d7919@users.noreply.github.com>
2018-11-09 11:48:54 +0000

    Optimisations to certain vector routines
    ToCovariant/Contravariant now use explicit loop rather than multiple
    field operations. Can provide significant reduction in time.
    
    V_dot_grad for two vector arguments now written in terms of a templated
    routine to avoid large amounts of duplicated code. The templated routine
    also introduces a loop over the mesh rather than using field operations.
    
    Together these changes should reduce the time for vector based models be
    a reasonable fraction (e.g. the orszag-tang example).

--- src/field/vector3d.cxx
+++ src/field/vector3d.cxx
@@ -34,6 +34,7 @@
 #include <boundary_op.hxx>
 #include <boutexception.hxx>
 #include <bout/assert.hxx>
+#include <bout/scorepwrapper.hxx>
 #include <interpolation.hxx>
 
 Vector3D::Vector3D(Mesh *localmesh)
@@ -56,59 +57,106 @@ Vector3D::~Vector3D() {
   }
 }
 
-void Vector3D::toCovariant() {  
+void Vector3D::toCovariant() {
+  SCOREP0();  
   if(!covariant) {
     Mesh *localmesh = x.getMesh();
-    Field3D gx(localmesh), gy(localmesh), gz(localmesh);
 
-    Coordinates *metric_x, *metric_y, *metric_z;
     if (location == CELL_VSHIFT) {
+      Coordinates *metric_x, *metric_y, *metric_z;
       metric_x = localmesh->getCoordinates(CELL_XLOW);
       metric_y = localmesh->getCoordinates(CELL_YLOW);
       metric_z = localmesh->getCoordinates(CELL_ZLOW);
+
+      // Fields at different locations so we need to interpolate
+      // Note : Could reduce peak memory requirement here by just
+      // dealing with the three components seperately. This would
+      // require the use of temporary fields to hold the intermediate
+      // result so would likely only reduce memory usage by one field
+      const auto y_at_x = interp_to(y, x.getLocation());
+      const auto z_at_x = interp_to(z, x.getLocation());
+      const auto x_at_y = interp_to(x, y.getLocation());
+      const auto z_at_y = interp_to(z, y.getLocation());
+      const auto x_at_z = interp_to(x, z.getLocation());
+      const auto y_at_z = interp_to(y, z.getLocation());
+
+      // multiply by g_{ij}
+      BOUT_FOR(i, localmesh->getRegion3D("RGN_ALL")){
+	x[i] = metric_x->g_11[i]*x[i] + metric_x->g_12[i]*y_at_x[i] + metric_x->g_13[i]*z_at_x[i];
+	y[i] = metric_y->g_22[i]*y[i] + metric_y->g_12[i]*x_at_y[i] + metric_y->g_23[i]*z_at_y[i];
+	z[i] = metric_z->g_33[i]*z[i] + metric_z->g_13[i]*x_at_z[i] + metric_z->g_23[i]*y_at_z[i];
+      };
     } else {
-      metric_x = localmesh->getCoordinates(location);
-      metric_y = localmesh->getCoordinates(location);
-      metric_z = localmesh->getCoordinates(location);
+      const auto metric = localmesh->getCoordinates(location);
+
+      // Need to use temporary arrays to store result
+      Field3D gx(localmesh), gy(localmesh), gz(localmesh);
+      gx.allocate(); gy.allocate(); gz.allocate();
+
+      BOUT_FOR(i, localmesh->getRegion3D("RGN_ALL")){
+	gx[i] = metric->g_11[i]*x[i] + metric->g_12[i]*y[i] + metric->g_13[i]*z[i];
+	gy[i] = metric->g_22[i]*y[i] + metric->g_12[i]*x[i] + metric->g_23[i]*z[i];
+	gz[i] = metric->g_33[i]*z[i] + metric->g_13[i]*x[i] + metric->g_23[i]*y[i];
+      };
+
+      x = gx;
+      y = gy;
+      z = gz;
     }
 
-    // multiply by g_{ij}
-    gx = x*metric_x->g_11 + metric_x->g_12*interp_to(y, x.getLocation()) + metric_x->g_13*interp_to(z, x.getLocation());
-    gy = y*metric_y->g_22 + metric_y->g_12*interp_to(x, y.getLocation()) + metric_y->g_23*interp_to(z, y.getLocation());
-    gz = z*metric_z->g_33 + metric_z->g_13*interp_to(x, z.getLocation()) + metric_z->g_23*interp_to(y, z.getLocation());
-
-    x = gx;
-    y = gy;
-    z = gz;
-    
     covariant = true;
   }
 }
 void Vector3D::toContravariant() {  
+  SCOREP0();
   if(covariant) {
     // multiply by g^{ij}
     Mesh *localmesh = x.getMesh();
     Field3D gx(localmesh), gy(localmesh), gz(localmesh);
 
-    Coordinates *metric_x, *metric_y, *metric_z;
     if (location == CELL_VSHIFT) {
+      Coordinates *metric_x, *metric_y, *metric_z;
+    
       metric_x = localmesh->getCoordinates(CELL_XLOW);
       metric_y = localmesh->getCoordinates(CELL_YLOW);
       metric_z = localmesh->getCoordinates(CELL_ZLOW);
+
+      // Fields at different locations so we need to interpolate
+      // Note : Could reduce peak memory requirement here by just
+      // dealing with the three components seperately. This would
+      // require the use of temporary fields to hold the intermediate
+      // result so would likely only reduce memory usage by one field
+      const auto y_at_x = interp_to(y, x.getLocation());
+      const auto z_at_x = interp_to(z, x.getLocation());
+      const auto x_at_y = interp_to(x, y.getLocation());
+      const auto z_at_y = interp_to(z, y.getLocation());
+      const auto x_at_z = interp_to(x, z.getLocation());
+      const auto y_at_z = interp_to(y, z.getLocation());
+
+      // multiply by g_{ij}
+      BOUT_FOR(i, localmesh->getRegion3D("RGN_ALL")){
+	x[i] = metric_x->g11[i]*x[i] + metric_x->g12[i]*y_at_x[i] + metric_x->g13[i]*z_at_x[i];
+	y[i] = metric_y->g22[i]*y[i] + metric_y->g12[i]*x_at_y[i] + metric_y->g23[i]*z_at_y[i];
+	z[i] = metric_z->g33[i]*z[i] + metric_z->g13[i]*x_at_z[i] + metric_z->g23[i]*y_at_z[i];
+      };
+
     } else {
-      metric_x = localmesh->getCoordinates(location);
-      metric_y = localmesh->getCoordinates(location);
-      metric_z = localmesh->getCoordinates(location);
+      const auto metric = localmesh->getCoordinates(location);
+
+      // Need to use temporary arrays to store result
+      Field3D gx(localmesh), gy(localmesh), gz(localmesh);
+      gx.allocate(); gy.allocate(); gz.allocate();
+
+      BOUT_FOR(i, localmesh->getRegion3D("RGN_ALL")){
+	gx[i] = metric->g11[i]*x[i] + metric->g12[i]*y[i] + metric->g13[i]*z[i];
+	gy[i] = metric->g22[i]*y[i] + metric->g12[i]*x[i] + metric->g23[i]*z[i];
+	gz[i] = metric->g33[i]*z[i] + metric->g13[i]*x[i] + metric->g23[i]*y[i];
+      };
+
+      x = gx;
+      y = gy;
+      z = gz;
     }
-
-    // multiply by g_{ij}
-    gx = x*metric_x->g11 + metric_x->g12*interp_to(y, x.getLocation()) + metric_x->g13*interp_to(z, x.getLocation());
-    gy = y*metric_y->g22 + metric_y->g12*interp_to(x, y.getLocation()) + metric_y->g23*interp_to(z, y.getLocation());
-    gz = z*metric_z->g33 + metric_z->g13*interp_to(x, z.getLocation()) + metric_z->g23*interp_to(y, z.getLocation());
-
-    x = gx;
-    y = gy;
-    z = gz;
     
     covariant = false;
   }
@@ -149,6 +197,7 @@ Vector3D* Vector3D::timeDeriv() {
 /////////////////// ASSIGNMENT ////////////////////
 
 Vector3D & Vector3D::operator=(const Vector3D &rhs) {
+  SCOREP0();
   x = rhs.x;
   y = rhs.y;
   z = rhs.z;
@@ -160,6 +209,7 @@ Vector3D & Vector3D::operator=(const Vector3D &rhs) {
 }
 
 Vector3D & Vector3D::operator=(const Vector2D &rhs) {
+  SCOREP0();  
   x = rhs.x;
   y = rhs.y;
   z = rhs.z;
@@ -173,6 +223,7 @@ Vector3D & Vector3D::operator=(const Vector2D &rhs) {
 
 Vector3D & Vector3D::operator=(const BoutReal val)
 {
+  SCOREP0();
   x = val;
   y = val;
   z = val;
@@ -499,6 +550,7 @@ CELL_LOC Vector3D::getLocation() const {
 }
 
 void Vector3D::setLocation(CELL_LOC loc) {
+  SCOREP0();  
   TRACE("Vector3D::setLocation");
   if (loc == CELL_DEFAULT) {
     loc = CELL_CENTRE;
