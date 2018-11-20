a05406c8f3672b0bb46d59c03dbc59d8c28b5efa
Peter Hill <zed.three@gmail.com>
2018-11-20 16:54:14 +0000

    Split BoundaryFactory tests into smaller tests; use a fixture

--- tests/unit/mesh/test_boundary_factory.cxx
+++ tests/unit/mesh/test_boundary_factory.cxx
@@ -24,69 +24,90 @@ public:
   void apply(Field3D &UNUSED(f)) override {}
 };
 
-TEST(BoundaryFactoryTests, IsSingleton) {
+class BoundaryFactoryTest : public ::testing::Test {
+public:
+  BoundaryFactoryTest() {
+    if (mesh != nullptr) {
+      delete mesh;
+      mesh = nullptr;
+    }
+    mesh = new FakeMesh(3, 3, 3);
+
+    fac->add(new TestBoundary(), "testboundary");
+
+    region = new BoundaryRegionXIn{"test_region", 0, 1, mesh};
+  }
+
+  ~BoundaryFactoryTest() {
+    delete mesh;
+    mesh = nullptr;
+
+    delete region;
+
+    BoundaryFactory::cleanup();
+
+    if (boundary != nullptr) {
+      delete boundary;
+      boundary = nullptr;
+    }
+  }
+
+  BoundaryFactory* fac{BoundaryFactory::getInstance()};
+  BoundaryRegionXIn* region{nullptr};
+  BoundaryOpBase* boundary{nullptr};
+};
+
+TEST_F(BoundaryFactoryTest, IsSingleton) {
   BoundaryFactory* fac1 = BoundaryFactory::getInstance();
   BoundaryFactory* fac2 = BoundaryFactory::getInstance();
 
   EXPECT_EQ(fac1, fac2);
 }
 
-TEST(BoundaryFactoryTests, CreateTestBoundary) {
-  mesh = new FakeMesh(3, 3, 3);
-  
-  BoundaryFactory* fac = BoundaryFactory::getInstance();
-  fac->add(new TestBoundary(), "testboundary");
-  
-  BoundaryRegionXIn region("test_region", 0, 1, mesh);
+TEST_F(BoundaryFactoryTest, CreateTestBoundaryNoBrackets) {
+  boundary = fac->create("testboundary", region);
 
-  // Check no brackets
-  
-  auto *boundary = fac->create("testboundary", &region);
+  EXPECT_TRUE(boundary != nullptr);
 
-  EXPECT_TRUE( boundary != nullptr );
-  
   EXPECT_EQ(typeid(*boundary), typeid(TestBoundary));
+}
 
-  delete boundary;
-  
-  // Positional arguments
+TEST_F(BoundaryFactoryTest, CreateTestBoundaryPositionalArguments) {
+  boundary = fac->create("testboundary(a, 1)", region);
+  EXPECT_TRUE(boundary != nullptr);
 
-  boundary = fac->create("testboundary(a, 1)", &region);
-  EXPECT_TRUE( boundary != nullptr );
+  auto* tb = dynamic_cast<TestBoundary*>(boundary);
 
-  TestBoundary *tb = dynamic_cast<TestBoundary*>(boundary);
+  EXPECT_EQ(tb->args.size(), 2);
+  EXPECT_EQ(tb->args.front(), "a");
+  EXPECT_EQ(tb->args.back(), "1");
+  EXPECT_TRUE(tb->keywords.empty());
+}
 
-  EXPECT_EQ( tb->args.size(), 2 );
-  EXPECT_EQ( tb->args.front(), "a" );
-  EXPECT_EQ( tb->args.back(), "1" );
-  EXPECT_TRUE( tb->keywords.empty() );
-  
-  delete boundary;
+TEST_F(BoundaryFactoryTest, CreateTestBoundaryKeywords) {
+  boundary = fac->create("testboundary(key=1, b=value)", region);
+  EXPECT_TRUE(boundary != nullptr);
 
-  // Test keywords
-  boundary = fac->create("testboundary(key=1, b=value)", &region);
-  EXPECT_TRUE( boundary != nullptr );
+  auto* tb = dynamic_cast<TestBoundary*>(boundary);
 
-  tb = dynamic_cast<TestBoundary*>(boundary);
-  
-  EXPECT_TRUE( tb->args.empty() );
-  EXPECT_EQ( tb->keywords.size(), 2 );
-  EXPECT_EQ( tb->keywords.at("key"), "1" );
-  EXPECT_EQ( tb->keywords.at("b"), "value" );
+  EXPECT_TRUE(tb->args.empty());
+  EXPECT_EQ(tb->keywords.size(), 2);
+  EXPECT_EQ(tb->keywords.at("key"), "1");
+  EXPECT_EQ(tb->keywords.at("b"), "value");
+}
 
-  delete boundary;
+TEST_F(BoundaryFactoryTest, CreateTestBoundaryPositionalArgumentsAndKeywords) {
+  boundary = fac->create(
+      "testboundary(0.23, key =1+2 , something(),b=value ,a + sin(1.2))", region);
+  EXPECT_TRUE(boundary != nullptr);
 
-  // Mix of positional args and keywords
-  boundary = fac->create("testboundary(0.23, key =1+2 , something(),b=value ,a + sin(1.2))", &region);
-  EXPECT_TRUE( boundary != nullptr );
-
-  tb = dynamic_cast<TestBoundary*>(boundary);
+  auto* tb = dynamic_cast<TestBoundary*>(boundary);
 
   std::list<std::string> args_expected = {"0.23", "something()", "a + sin(1.2)"};
-  
-  EXPECT_EQ( tb->args, args_expected );
-  
-  EXPECT_EQ( tb->keywords.size(), 2 );
-  EXPECT_EQ( tb->keywords.at("key"), "1+2" );
-  EXPECT_EQ( tb->keywords.at("b"), "value" );
+
+  EXPECT_EQ(tb->args, args_expected);
+
+  EXPECT_EQ(tb->keywords.size(), 2);
+  EXPECT_EQ(tb->keywords.at("key"), "1+2");
+  EXPECT_EQ(tb->keywords.at("b"), "value");
 }
