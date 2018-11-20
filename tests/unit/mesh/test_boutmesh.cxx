b9ae93d9d44a83c4561035b4c416082f670dccb6
John Omotani <john.omotani@ukaea.uk>
2018-11-20 14:42:50 +0000

    Test addCoordinates in BoutMeshTest
    Unit tests for addCoordinates() methods.

--- tests/unit/mesh/test_boutmesh.cxx
+++ tests/unit/mesh/test_boutmesh.cxx
@@ -4,43 +4,96 @@
 #include "bout/mesh.hxx"
 #include "output.hxx"
 #include "unused.hxx"
+#include <unordered_map>
 
 class FakeGridDataSource : public GridDataSource {
 public:
   FakeGridDataSource(){};
   ~FakeGridDataSource(){};
   bool hasVar(const string &UNUSED(name)) { return false; };
-  bool get(Mesh *UNUSED(m), int &UNUSED(ival), const string &UNUSED(name)) {
-    return true;
+  bool get(Mesh *UNUSED(m), int &ival, const string &name) {
+    if (intvars.count(name)>0) {
+      ival = intvars.at(name);
+      return true;
+    }
+    return false;
   };
   bool get(Mesh *UNUSED(m), BoutReal &UNUSED(rval), const string &UNUSED(name)) {
-    return true;
+    return false;
   }
   bool get(Mesh *UNUSED(m), Field2D &UNUSED(var), const string &UNUSED(name),
            BoutReal UNUSED(def) = 0.0) {
-    return true;
+    return false;
   }
   bool get(Mesh *UNUSED(m), Field3D &UNUSED(var), const string &UNUSED(name),
            BoutReal UNUSED(def) = 0.0) {
-    return true;
+    return false;
   }
   bool get(Mesh *UNUSED(m), vector<int> &UNUSED(var), const string &UNUSED(name),
            int UNUSED(len), int UNUSED(offset) = 0,
            Direction UNUSED(dir) = GridDataSource::X) {
-    return true;
+    return false;
   }
   bool get(Mesh *UNUSED(m), vector<BoutReal> &UNUSED(var), const string &UNUSED(name),
            int UNUSED(len), int UNUSED(offset) = 0,
            Direction UNUSED(dir) = GridDataSource::X) {
-    return true;
+    return false;
   }
+
+  std::unordered_map<std::string, int> intvars { {"nx", 6}, {"ny", 7 }, {"nz", 5}};
 };
 
-TEST(BoutMeshTest, NullOptionsCheck) {
+/// Test fixture with a BoutMesh
+class BoutMeshTest : public ::testing::Test {
+protected:
+  static void SetUpTestCase() {
+    output_info.disable();
+    output_warn.disable();
+    output_progress.disable();
+  }
+
+  static void TearDownTestCase() {
+    output_info.enable();
+    output_warn.enable();
+    output_progress.enable();
+  }
+
+public:
+  BoutMeshTest() : source(), localmesh(Mesh::create(&source)) {
+    localmesh->StaggerGrids = true;
+    output_info.disable();
+    localmesh->load();
+  }
+  FakeGridDataSource source;
+  Mesh* localmesh;
+};
+
+TEST_F(BoutMeshTest, NullOptionsCheck) {
   // Temporarily turn off outputs to make test quiet
-  output_info.disable();
-  output_warn.disable();
   EXPECT_NO_THROW(BoutMesh mesh(new FakeGridDataSource, nullptr));
-  output_info.enable();
-  output_warn.enable();
+}
+
+TEST_F(BoutMeshTest, AddCoordinatesToMeshCENTRE) {
+  EXPECT_NO_THROW(localmesh->addCoordinates(CELL_CENTRE));
+  EXPECT_NO_THROW(localmesh->getCoordinates(CELL_CENTRE)->geometry());
+  EXPECT_NO_THROW(localmesh->addCoordinates(CELL_YLOW));
+  EXPECT_THROW(localmesh->getCoordinates(CELL_CENTRE)->geometry(), BoutException);
+}
+
+TEST_F(BoutMeshTest, AddCoordinatesToMeshXLOW) {
+  EXPECT_NO_THROW(localmesh->addCoordinates(CELL_XLOW));
+  EXPECT_THROW(localmesh->addCoordinates(CELL_XLOW), BoutException);
+  EXPECT_THROW(localmesh->getCoordinates(CELL_XLOW)->geometry(), BoutException);
+}
+
+TEST_F(BoutMeshTest, AddCoordinatesToMeshYLOW) {
+  EXPECT_NO_THROW(localmesh->addCoordinates(CELL_YLOW));
+  EXPECT_THROW(localmesh->addCoordinates(CELL_YLOW), BoutException);
+  EXPECT_THROW(localmesh->getCoordinates(CELL_YLOW)->geometry(), BoutException);
+}
+
+TEST_F(BoutMeshTest, AddCoordinatesToMeshZLOW) {
+  EXPECT_NO_THROW(localmesh->addCoordinates(CELL_ZLOW));
+  EXPECT_THROW(localmesh->addCoordinates(CELL_ZLOW), BoutException);
+  EXPECT_THROW(localmesh->getCoordinates(CELL_ZLOW)->geometry(), BoutException);
 }
