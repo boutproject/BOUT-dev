54a8690a6ca018e4dfc290664bd6b7c4a88c213c
Peter Hill <zed.three@gmail.com>
2018-11-23 15:33:32 +0000

    Add a macro for marking things as maybe unused

--- include/unused.hxx
+++ include/unused.hxx
@@ -35,4 +35,19 @@
 # define UNUSED(x) x
 #endif
 
+/// Mark a function parameter as possibly unused in the function body
+///
+/// Unlike `UNUSED`, this has to go around the type as well:
+///
+///    MAYBE_UNUSED(int foo);
+#ifdef __has_cpp_attribute
+#if __has_cpp_attribute(maybe_unused)
+# define MAYBE_UNUSED(x) [[maybe_unused]] x
+#endif
+#elif defined(__GNUC__)
+# define MAYBE_UNUSED(x) x __attribute__((unused))
+#else
+# define MAYBE_UNUSED(x) x
+#endif
+
 #endif //__UNUSED_H__
