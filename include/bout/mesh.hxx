620f3709dc26fe7d95562ff7df862cb99ab12251
David Dickinson <d7919@users.noreply.github.com>
2018-12-06 14:35:58 +0000

    Provide a default case to some switch statements

--- include/bout/mesh.hxx
+++ include/bout/mesh.hxx
@@ -451,6 +451,7 @@ class Mesh {
   /// Returns the non-CELL_CENTRE location
   /// allowed as a staggered location
   CELL_LOC getAllowedStaggerLoc(DIRECTION direction) const {
+    AUTO_TRACE();
     switch (direction) {
     case (DIRECTION::X):
       return CELL_XLOW;
@@ -460,12 +461,15 @@ class Mesh {
       return CELL_YLOW;
     case (DIRECTION::Z):
       return CELL_ZLOW;
+    case default:
+      throw BoutException("Unhandled direction encountered in getAllowedStaggerLoc");
     }
   };
 
   /// Returns the number of grid points in the
   /// particular direction
   int getNpoints(DIRECTION direction) const {
+    AUTO_TRACE();
     switch (direction) {
     case (DIRECTION::X):
       return LocalNx;
@@ -475,12 +479,15 @@ class Mesh {
       return LocalNy;
     case (DIRECTION::Z):
       return LocalNz;
+    case default:
+      throw BoutException("Unhandled direction encountered in getNpoints");
     }
   };
 
   /// Returns the number of guard points in the
   /// particular direction
   int getNguard(DIRECTION direction) const {
+    AUTO_TRACE();
     switch (direction) {
     case (DIRECTION::X):
       return xstart;
@@ -490,6 +497,8 @@ class Mesh {
       return ystart;
     case (DIRECTION::Z):
       return 2;
+    case default:
+      throw BoutException("Unhandled direction encountered in getNguard");
     }
   };
 
