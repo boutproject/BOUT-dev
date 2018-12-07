9ef339bae1a07a4208b67cca8f72b6d3ae10843e
David Dickinson <d7919@users.noreply.github.com>
2018-12-07 20:30:26 +0000

    Fix unit tests for introduction of zstart/zend and RGN_NOZ

--- tests/unit/field/test_field3d.cxx
+++ tests/unit/field/test_field3d.cxx
@@ -578,10 +578,58 @@ TEST_F(Field3DTest, IterateOverRGN_NOY) {
 }
 
 TEST_F(Field3DTest, IterateOverRGN_NOZ) {
-  Field3D field = 1.0;
+  const int mzguard = 0;
+  Field3D field;
 
-  // This is not a valid region for Field3D
-  EXPECT_THROW(field.getRegion(RGN_NOZ), BoutException);
+  field = 1.0;
+
+  const BoutReal sentinel = -99.0;
+
+  // We use a set in case for some reason the iterator doesn't visit
+  // each point in the order we expect.
+  std::set<std::vector<int>> test_indices;
+  test_indices.insert({0, 0, 0});
+  test_indices.insert({0, 0, 1});
+  test_indices.insert({0, 1, 0});
+  test_indices.insert({1, 0, 0});
+  test_indices.insert({0, 1, 1});
+  test_indices.insert({1, 0, 1});
+  test_indices.insert({1, 1, 0});
+  test_indices.insert({1, 1, 1});
+
+  // This is the set of indices actually inside the region we want
+  std::set<std::vector<int>> region_indices;
+  region_indices.insert({0, 0, 0});
+  region_indices.insert({0, 0, 1});
+  region_indices.insert({0, 1, 0});
+  region_indices.insert({1, 0, 0});
+  region_indices.insert({0, 1, 1});
+  region_indices.insert({1, 0, 1});
+  region_indices.insert({1, 1, 0});
+  region_indices.insert({1, 1, 1});
+  const int num_sentinels = region_indices.size();
+
+  // Assign sentinel value to watch out for to our chosen points
+  for (const auto index : test_indices) {
+    field(index[0], index[1], index[2]) = sentinel;
+  }
+
+  int found_sentinels = 0;
+  BoutReal sum = 0.0;
+  std::set<std::vector<int>> result_indices;
+
+  for (const auto& i : field.getRegion(RGN_NOZ)) {
+    sum += field[i];
+    if (field[i] == sentinel) {
+      result_indices.insert({i.x(), i.y(), i.z()});
+      ++found_sentinels;
+    }
+  }
+
+  EXPECT_EQ(found_sentinels, num_sentinels);
+  EXPECT_EQ(sum,
+            ((nx * (ny) * (nz - mzguard)) - num_sentinels) + (num_sentinels * sentinel));
+  EXPECT_TRUE(region_indices == result_indices);
 }
 
 TEST_F(Field3DTest, IterateOver2DRGN_ALL) {
