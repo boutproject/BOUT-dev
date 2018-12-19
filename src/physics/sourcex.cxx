7f5255fd86a00bebaa918cd7ca0da1631aa24c95
John Omotani <john.omotani@ukaea.uk>
2018-12-19 10:20:12 +0000

    Remove uses of global mesh in sourcex.cxx

--- src/physics/sourcex.cxx
+++ src/physics/sourcex.cxx
@@ -16,57 +16,63 @@ BoutReal TanH(BoutReal a) {
 }
 
 // create radial buffer zones to set jpar zero near radial boundaries
-const Field2D source_tanhx(const Field2D &UNUSED(f), BoutReal swidth, BoutReal slength) {
-  Field2D result;
+const Field2D source_tanhx(const Field2D &f, BoutReal swidth, BoutReal slength) {
+  Mesh* localmesh = f.getMesh();
+
+  Field2D result(localmesh);
   result.allocate();
 
   // create a radial buffer zone to set jpar zero near radial boundary
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal lx = mesh->GlobalX(i.x()) - slength;
+    BoutReal lx = localmesh->GlobalX(i.x()) - slength;
     BoutReal dampl = TanH(lx / swidth);
     result[i] = 0.5 * (1.0 - dampl);
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
 
 // create radial buffer zones to set jpar zero near radial boundaries
-const Field2D source_expx2(const Field2D &UNUSED(f), BoutReal swidth, BoutReal slength) {
-  Field2D result;
+const Field2D source_expx2(const Field2D &f, BoutReal swidth, BoutReal slength) {
+  Mesh* localmesh = f.getMesh();
+
+  Field2D result(localmesh);
 
   result.allocate();
 
   // create a radial buffer zone to set jpar zero near radial boundary
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal lx = mesh->GlobalX(i.x()) - slength;
+    BoutReal lx = localmesh->GlobalX(i.x()) - slength;
     BoutReal dampl = exp(-lx * lx / swidth / swidth);
     result[i] = dampl;
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
 
 // create radial buffer zones to set jpar zero near radial boundaries
-const Field3D sink_tanhx(const Field2D &UNUSED(f0), const Field3D &f, BoutReal swidth,
+const Field3D sink_tanhx(const Field2D &f0, const Field3D &f, BoutReal swidth,
                          BoutReal slength, bool UNUSED(BoutRealspace)) {
-  Field3D result;
+  Mesh* localmesh = f0.getMesh();
+
+  Field3D result(localmesh);
   result.allocate();
 
   // create a radial buffer zone to set jpar zero near radial boundary
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal rlx = 1. - mesh->GlobalX(i.x()) - slength;
+    BoutReal rlx = 1. - localmesh->GlobalX(i.x()) - slength;
     BoutReal dampr = TanH(rlx / swidth);
     result[i] = 0.5 * (1.0 - dampr) * f[i];
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
@@ -75,12 +81,14 @@ const Field3D sink_tanhx(const Field2D &UNUSED(f0), const Field3D &f, BoutReal s
 const Field3D mask_x(const Field3D &f, bool UNUSED(BoutRealspace)) {
   TRACE("mask_x");
 
-  Field3D result;
+  Mesh* localmesh = f.getMesh();
+
+  Field3D result(localmesh);
   result.allocate();
 
   // create a radial buffer zone to set jpar zero near radial boundary
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal lx = mesh->GlobalX(i.x());
+    BoutReal lx = localmesh->GlobalX(i.x());
     BoutReal dampl = TanH(lx / 40.0);
     BoutReal dampr = TanH((1. - lx) / 40.0);
 
@@ -88,49 +96,54 @@ const Field3D mask_x(const Field3D &f, bool UNUSED(BoutRealspace)) {
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
 
 // create radial buffer zones to set jpar zero near radial boundaries
-const Field3D sink_tanhxl(const Field2D &UNUSED(f0), const Field3D &f, BoutReal swidth,
+const Field3D sink_tanhxl(const Field2D &f0, const Field3D &f, BoutReal swidth,
                           BoutReal slength, bool UNUSED(BoutRealspace)) {
   TRACE("sink_tanhx");
 
-  Field3D result;
+  Mesh* localmesh = f0.getMesh();
+
+  Field3D result(localmesh);
 
   result.allocate();
 
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal lx = mesh->GlobalX(i.x()) - slength;
+    BoutReal lx = localmesh->GlobalX(i.x()) - slength;
     BoutReal dampl = TanH(lx / swidth);
 
     result[i] = 0.5 * (1.0 - dampl) * f[i];
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
 
 // create radial buffer zones to set jpar zero near radial boundaries
-const Field3D sink_tanhxr(const Field2D &UNUSED(f0), const Field3D &f, BoutReal swidth,
+const Field3D sink_tanhxr(const Field2D &f0, const Field3D &f, BoutReal swidth,
                           BoutReal slength, bool UNUSED(BoutRealspace)) {
   TRACE("sink_tanhxr");
-  Field3D result;
+
+  Mesh* localmesh = f0.getMesh();
+
+  Field3D result(localmesh);
   result.allocate();
 
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal rlx = 1. - mesh->GlobalX(i.x()) - slength;
+    BoutReal rlx = 1. - localmesh->GlobalX(i.x()) - slength;
     BoutReal dampr = TanH(rlx / swidth);
 
     result[i] = 0.5 * (1.0 - dampr) * f[i];
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
@@ -139,7 +152,9 @@ const Field3D sink_tanhxr(const Field2D &UNUSED(f0), const Field3D &f, BoutReal 
 const Field3D buff_x(const Field3D &f, bool UNUSED(BoutRealspace)) {
   TRACE("buff_x");
 
-  Field3D result;
+  Mesh* localmesh = f.getMesh();
+
+  Field3D result(localmesh);
   result.allocate();
 
   const BoutReal dampl = 1.e0;
@@ -148,7 +163,7 @@ const Field3D buff_x(const Field3D &f, bool UNUSED(BoutRealspace)) {
   const BoutReal deltar = 0.05;
 
   BOUT_FOR(i, result.getRegion("RGN_ALL")) {
-    BoutReal lx = mesh->GlobalX(i.x());
+    BoutReal lx = localmesh->GlobalX(i.x());
     BoutReal rlx = 1. - lx;
 
     result[i] = (dampl * exp(-(lx * lx) / (deltal * deltal)) +
@@ -157,7 +172,7 @@ const Field3D buff_x(const Field3D &f, bool UNUSED(BoutRealspace)) {
   }
 
   // Need to communicate boundaries
-  mesh->communicate(result);
+  localmesh->communicate(result);
 
   return result;
 }
