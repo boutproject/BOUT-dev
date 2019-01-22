5ee830759ba9c600f8b56cc078a1bb1320704002
Brendan Shanahan <brendan.shanahan@ipp.mpg.de>
2019-01-22 14:53:50 +0100

    Update test for laplacexz * Add basic test for off-diagonal metrics * Add runtest

--- tests/integrated/test-laplacexz/test-laplacexz.cxx
+++ tests/integrated/test-laplacexz/test-laplacexz.cxx
@@ -12,12 +12,31 @@
 
 #include <bout/invert/laplacexz.hxx>
 #include <field_factory.hxx>
+#include <derivs.hxx>
 
 int main(int argc, char** argv) {
   BoutInitialise(argc, argv);
 
   LaplaceXZ *inv = LaplaceXZ::create(mesh);
+  Coordinates *coord = mesh->getCoordinates();
 
+  coord->g13 = 1.8;  //test off-diagonal components with nonzero value
+
+  // create some input field
+  Field3D f = FieldFactory::get()->create3D("f", Options::getRoot(), mesh); 
+
+  // Calculate the Laplacian with non-zero g13
+  Field3D g = coord->g11*D2DX2(f) + coord->g13*D2DXDZ(f) + coord->g33*D2DZ2(f);
+
+  inv->setCoefs(Field2D(1.0),Field2D(0.0));
+  
+  Field3D f2 = inv->solve(g,0.0);    // Invert the Laplacian.
+  
+  SAVE_ONCE3(f,f2,g);
+
+  coord->g13 = 0.0;  //reset to 0.0 for original laplacexz test
+
+  // Now the normal test.
   output.write("Setting coefficients\n");
 
   inv->setCoefs(Field3D(1.0),Field3D(0.0));
