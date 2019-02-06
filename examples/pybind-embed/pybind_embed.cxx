0b9f2faf1e9454b85a08478911889e81d13ebb2c
David Dickinson <d7919@users.noreply.github.com>
2019-02-06 20:08:29 +0000

    Show use of python module in example

--- examples/pybind-embed/pybind_embed.cxx
+++ examples/pybind-embed/pybind_embed.cxx
@@ -10,19 +10,30 @@ int myFunc(){
 };
 
 int main() {
-  // Get access to a python interpreter with lifetime of the current scope
-  py::scoped_interpreter guard{};
+  {
+    // Get access to a python interpreter with lifetime of the current scope
+    py::scoped_interpreter guard{};
 
-  // Construct a python dictionary -- note this includes a call
-  // to a native C++ method as well as including constants.
-  auto kwargs = py::dict("name"_a="BOUT++ user",
-                         "number"_a=myFunc(),
-                         "floatNumber"_a=1.2345                         
-                         );
-  // Format the string using the dictionary constructed above
-  auto message = "Hello, {name}! The answer is {number} and not {floatNumber}"_s.format(**kwargs);
-  // Display message
-  py::print(message);
+    // Construct a python dictionary -- note this includes a call
+    // to a native C++ method as well as including constants.
+    auto kwargs = py::dict("name"_a="BOUT++ user",
+                           "number"_a=myFunc(),
+                           "floatNumber"_a=1.2345                         
+                           );
+    // Format the string using the dictionary constructed above
+    auto message = "Hello, {name}! The answer is {number} and not {floatNumber}"_s.format(**kwargs);
+    // Display message
+    py::print(message);
+  }
+
+  {
+    // Get access to another python interpreter
+    py::scoped_interpreter guard{};
+
+    py::module sys = py::module::import("sys");
+    py::print("The python path is:");
+    py::print(sys.attr("path"));
+  }
 }
 #else
 #error Must configure with `--with-pybind11` to be able to build pybind_embed example.
