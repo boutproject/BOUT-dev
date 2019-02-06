b1d4c42542547089dfee7c2b1427cbd46b55d1f9
David Dickinson <d7919@users.noreply.github.com>
2019-02-06 19:44:22 +0000

    Adding simple example demonstrating the use of pybind11 for calling python from C++

---
+++ examples/pybind-embed/README.md
@@ -0,0 +1,4 @@
+# Example: PyBind_embed
+
+A very simple example demonstrating the use of pybind11 to embed a
+python interpreter inside a C++ program.
---
+++ examples/pybind-embed/pybind_embed.cxx
@@ -0,0 +1,29 @@
+#ifdef BOUT_HAS_PYBIND11
+#include <pybind11/embed.h>
+
+namespace py = pybind11;
+using namespace py::literals;
+
+// Provide a simple function that returns an int
+int myFunc(){
+  return 42;
+};
+
+int main() {
+  // Get access to a python interpreter with lifetime of the current scope
+  py::scoped_interpreter guard{};
+
+  // Construct a python dictionary -- note this includes a call
+  // to a native C++ method as well as including constants.
+  auto kwargs = py::dict("name"_a="BOUT++ user",
+                         "number"_a=myFunc(),
+                         "floatNumber"_a=1.2345                         
+                         );
+  // Format the string using the dictionary constructed above
+  auto message = "Hello, {name}! The answer is {number} and not {floatNumber}"_s.format(**kwargs);
+  // Display message
+  py::print(message);
+}
+#else
+#error Must configure with `--with-pybind11` to be able to build pybind_embed example.
+#endif
