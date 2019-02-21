a434e436991a431eb92aafe5a105ff016a1676e0
John Omotani <john.omotani@ukaea.uk>
2019-02-21 23:46:52 +0000

    Update ShiftedMetric unit tests
    Include updates using yDirectionType.

--- tests/unit/mesh/parallel/test_shiftedmetric.cxx
+++ tests/unit/mesh/parallel/test_shiftedmetric.cxx
@@ -37,6 +37,14 @@ public:
                        {2., 4., 6., 8., 10., 12., 14.},
                        {3., 6., 9., 12., 15., 18., 21.}});
 
+    static_cast<FakeMesh*>(mesh)->setCoordinates(std::make_shared<Coordinates>(
+        mesh, Field2D{1.0}, Field2D{1.0}, BoutReal{1.0}, Field2D{1.0}, Field2D{0.0},
+        Field2D{1.0}, Field2D{1.0}, Field2D{1.0}, Field2D{0.0}, Field2D{0.0},
+        Field2D{0.0}, Field2D{1.0}, Field2D{1.0}, Field2D{1.0}, Field2D{0.0},
+        Field2D{0.0}, Field2D{0.0}, Field2D{0.0}, Field2D{0.0}, false));
+
+    mesh->setParallelTransform(Mesh::PTptr(new ShiftedMetric(*mesh, zShift)));
+
     Field3D input_temp{mesh};
 
     // input values have been slightly shuffled to ensure that input is not
@@ -68,12 +76,6 @@ public:
                             {1., 2., 4., 3., 5.}}});
 
     input = std::move(input_temp);
-
-    static_cast<FakeMesh*>(mesh)->setCoordinates(std::make_shared<Coordinates>(
-        mesh, Field2D{1.0}, Field2D{1.0}, BoutReal{1.0}, Field2D{1.0}, Field2D{0.0},
-        Field2D{1.0}, Field2D{1.0}, Field2D{1.0}, Field2D{0.0}, Field2D{0.0},
-        Field2D{0.0}, Field2D{1.0}, Field2D{1.0}, Field2D{1.0}, Field2D{0.0},
-        Field2D{0.0}, Field2D{0.0}, Field2D{0.0}, Field2D{0.0}, false));
   }
 
   ~ShiftedMetricTest() {
@@ -90,8 +92,6 @@ public:
 };
 
 TEST_F(ShiftedMetricTest, ToFieldAligned) {
-  ShiftedMetric shifted{*mesh, zShift};
-
   Field3D expected{mesh};
 
   fillField(expected, {{{2., 3., 4., 5., 1.},
@@ -118,14 +118,17 @@ TEST_F(ShiftedMetricTest, ToFieldAligned) {
                         {4., 5., 1., 3., 2.},
                         {2., 4., 3., 5., 1.}}});
 
-  EXPECT_TRUE(IsFieldEqual(shifted.toFieldAligned(input, RGN_ALL), expected, "RGN_ALL",
+  EXPECT_TRUE(IsFieldEqual(mesh->toFieldAligned(input), expected, "RGN_ALL",
                            FFTTolerance));
 }
 
 TEST_F(ShiftedMetricTest, FromFieldAligned) {
-  ShiftedMetric shifted{*mesh, zShift};
+  // reset input.yDirectionType so that fromFieldAligned is not a null
+  // operation
+  input.setDirectionY(DIRECTION::YAligned);
 
-  Field3D expected{mesh};
+  Field3D expected{mesh, CELL_CENTRE};
+  expected.setDirectionY(DIRECTION::YOrthogonal);
 
   fillField(expected, {{{5., 1., 2., 3., 4.},
                         {4., 5., 2., 1., 3.},
@@ -151,9 +154,13 @@ TEST_F(ShiftedMetricTest, FromFieldAligned) {
                         {2., 4., 5., 1., 3.},
                         {5., 1., 2., 4., 3.}}});
 
+  Field3D result = mesh->fromFieldAligned(input);
+
   // Loosen tolerance a bit due to FFTs
-  EXPECT_TRUE(IsFieldEqual(shifted.fromFieldAligned(input, RGN_ALL), expected, "RGN_ALL",
+  EXPECT_TRUE(IsFieldEqual(mesh->fromFieldAligned(input), expected, "RGN_ALL",
                            FFTTolerance));
+  EXPECT_TRUE(fieldsCompatible(result, expected));
+  EXPECT_FALSE(fieldsCompatible(result, input));
 }
 
 TEST_F(ShiftedMetricTest, CalcYUpDown) {
@@ -177,9 +184,7 @@ TEST_F(ShiftedMetricTest, CalcYUpDown) {
   output_info.enable();
 
   // Actual interesting bit here!
-  ShiftedMetric shifted{*mesh, zShift};
-  shifted.calcYUpDown(input);
-
+  mesh->getParallelTransform().calcYUpDown(input);
   // Expected output values
 
   Field3D expected_up_1{mesh};
