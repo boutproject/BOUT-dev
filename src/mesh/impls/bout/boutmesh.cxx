d8e5faa0af873908783709604067a9a96632f04e
John Omotani <john.omotani@ukaea.uk>
2019-03-20 12:18:24 +0000

    Write out ny_inner in BoutMesh::outputVars()
    This specifies where (in index space) the upper divertor target plates
    are, if they are present.

--- src/mesh/impls/bout/boutmesh.cxx
+++ src/mesh/impls/bout/boutmesh.cxx
@@ -2571,6 +2571,7 @@ void BoutMesh::outputVars(Datafile &file) {
   file.add(jyseps1_2, "jyseps1_2", false);
   file.add(jyseps2_1, "jyseps2_1", false);
   file.add(jyseps2_2, "jyseps2_2", false);
+  file.add(ny_inner, "ny_inner", false);
 
   getCoordinates()->outputVars(file);
 }
