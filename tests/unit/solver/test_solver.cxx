ed2573610c52400ad572840fd3f4adcad3c21793
Peter Hill <peter.hill@york.ac.uk>
2019-03-27 15:36:01 +0000

    Add some more Solver unit tests

--- tests/unit/solver/test_solver.cxx
+++ tests/unit/solver/test_solver.cxx
@@ -12,7 +12,9 @@
 namespace {
 class FakeSolver : public Solver {
 public:
-  FakeSolver(Options* options) : Solver(options) {}
+  FakeSolver(Options* options) : Solver(options) {
+    has_constraints = true;
+  }
   ~FakeSolver() = default;
   int run() { return (*options)["number"].withDefault(42); }
 };
@@ -22,8 +24,19 @@ RegisterSolver<FakeSolver> register_fake("fake_solver");
 
 class SolverTest : public FakeMeshFixture {
 public:
-  SolverTest() : FakeMeshFixture() {}
-  virtual ~SolverTest() = default;
+  SolverTest() : FakeMeshFixture() {
+    Options::root()["field"]["function"] = "1.0";
+    Options::root()["field"]["solution"] = "2.0";
+    Options::root()["another_field"]["function"] = "3.0";
+    Options::root()["another_field"]["solution"] = "4.0";
+    Options::root()["vector_x"]["function"] = "5.0";
+    Options::root()["vector_y"]["function"] = "6.0";
+    Options::root()["vector_z"]["function"] = "7.0";
+    Options::root()["another_vectorx"]["function"] = "8.0";
+    Options::root()["another_vectory"]["function"] = "9.0";
+    Options::root()["another_vectorz"]["function"] = "10.0";
+  }
+  virtual ~SolverTest() { Options::cleanup(); }
 
   WithQuietOutput quiet_info{output_info};
   WithQuietOutput quiet_progress{output_progress};
@@ -115,51 +128,280 @@ TEST_F(SolverTest, AddField2D) {
   Options options;
   FakeSolver solver{&options};
 
-  Field2D field{};
-  EXPECT_NO_THROW(solver.add(field, "field"));
+  Field2D field1{}, field2{};
+  EXPECT_NO_THROW(solver.add(field1, "field"));
   EXPECT_EQ(solver.n2Dvars(), 1);
   EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(field1, 1.0));
 
 #if CHECK > 0
-  EXPECT_THROW(solver.add(field, "field"), BoutException);
+  EXPECT_THROW(solver.add(field2, "field"), BoutException);
   EXPECT_EQ(solver.n2Dvars(), 1);
   EXPECT_EQ(solver.n3Dvars(), 0);
 #endif
 
-  EXPECT_NO_THROW(solver.add(field, "another_field"));
+  EXPECT_NO_THROW(solver.add(field2, "another_field"));
   EXPECT_EQ(solver.n2Dvars(), 2);
   EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(field2, 3.0));
+}
+
+TEST_F(SolverTest, AddField2DMMS) {
+  Options options;
+  options["mms"] = true;
+  options["mms_initialise"] = true;
+  FakeSolver solver{&options};
+
+  Field2D field1{}, field2{};
+  EXPECT_NO_THROW(solver.add(field1, "field"));
+  EXPECT_EQ(solver.n2Dvars(), 1);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(field1, 2.0));
+
+#if CHECK > 0
+  EXPECT_THROW(solver.add(field2, "field"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 1);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+#endif
+
+  EXPECT_NO_THROW(solver.add(field2, "another_field"));
+  EXPECT_EQ(solver.n2Dvars(), 2);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(field2, 4.0));
 }
 
 TEST_F(SolverTest, AddField3D) {
   Options options;
   FakeSolver solver{&options};
 
-  Field3D field{};
-  EXPECT_NO_THROW(solver.add(field, "field"));
+  Field3D field1{}, field2{};
+  EXPECT_NO_THROW(solver.add(field1, "field"));
   EXPECT_EQ(solver.n2Dvars(), 0);
   EXPECT_EQ(solver.n3Dvars(), 1);
+  EXPECT_TRUE(IsFieldEqual(field1, 1.0));
 
 #if CHECK > 0
-  EXPECT_THROW(solver.add(field, "field"), BoutException);
+  EXPECT_THROW(solver.add(field2, "field"), BoutException);
   EXPECT_EQ(solver.n2Dvars(), 0);
   EXPECT_EQ(solver.n3Dvars(), 1);
 #endif
 
-  EXPECT_NO_THROW(solver.add(field, "another_field"));
+  EXPECT_NO_THROW(solver.add(field2, "another_field"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 2);
+  EXPECT_TRUE(IsFieldEqual(field2, 3.0));
+}
+
+TEST_F(SolverTest, AddField3DMMS) {
+  Options options;
+  options["mms"] = true;
+  options["mms_initialise"] = true;
+  FakeSolver solver{&options};
+
+  Field3D field1{}, field2{};
+  EXPECT_NO_THROW(solver.add(field1, "field"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 1);
+  EXPECT_TRUE(IsFieldEqual(field1, 2.0));
+
+#if CHECK > 0
+  EXPECT_THROW(solver.add(field2, "field"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 1);
+#endif
+
+  EXPECT_NO_THROW(solver.add(field2, "another_field"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 2);
+  EXPECT_TRUE(IsFieldEqual(field2, 4.0));
+}
+
+TEST_F(SolverTest, AddVector2D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Vector2D vector1{}, vector2{};
+  EXPECT_NO_THROW(solver.add(vector1, "vector"));
+  EXPECT_EQ(solver.n2Dvars(), 3);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(vector1.x, 5.0));
+  EXPECT_TRUE(IsFieldEqual(vector1.y, 6.0));
+  EXPECT_TRUE(IsFieldEqual(vector1.z, 7.0));
+
+#if CHECK > 0
+  EXPECT_THROW(solver.add(vector2, "vector"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 3);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+#endif
+
+  vector2.covariant = false;
+  EXPECT_NO_THROW(solver.add(vector2, "another_vector"));
+  EXPECT_EQ(solver.n2Dvars(), 6);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_TRUE(IsFieldEqual(vector2.x, 8.0));
+  EXPECT_TRUE(IsFieldEqual(vector2.y, 9.0));
+  EXPECT_TRUE(IsFieldEqual(vector2.z, 10.0));
+}
+
+TEST_F(SolverTest, AddVector3D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Vector3D vector1{}, vector2{};
+  EXPECT_NO_THROW(solver.add(vector1, "vector"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 3);
+  EXPECT_TRUE(IsFieldEqual(vector1.x, 5.0));
+  EXPECT_TRUE(IsFieldEqual(vector1.y, 6.0));
+  EXPECT_TRUE(IsFieldEqual(vector1.z, 7.0));
+
+#if CHECK > 0
+  EXPECT_THROW(solver.add(vector2, "vector"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 3);
+#endif
+
+  vector2.covariant = false;
+  EXPECT_NO_THROW(solver.add(vector2, "another_vector"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 6);
+  EXPECT_TRUE(IsFieldEqual(vector2.x, 8.0));
+  EXPECT_TRUE(IsFieldEqual(vector2.y, 9.0));
+  EXPECT_TRUE(IsFieldEqual(vector2.z, 10.0));
+}
+
+TEST_F(SolverTest, ConstraintField2D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Field2D field1{}, field2{};
+  EXPECT_NO_THROW(solver.constraint(field1, field1, "field"));
+  EXPECT_EQ(solver.n2Dvars(), 1);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+
+#if CHECK > 0
+  EXPECT_THROW(solver.constraint(field2, field2, "field"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 1);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_THROW(solver.constraint(field2, field2, ""), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 1);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+#endif
+
+  EXPECT_NO_THROW(solver.constraint(field2, field2, "another_field"));
+  EXPECT_EQ(solver.n2Dvars(), 2);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+}
+
+TEST_F(SolverTest, ConstraintField3D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Field3D field1{}, field2{};
+  EXPECT_NO_THROW(solver.constraint(field1, field1, "field"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 1);
+
+#if CHECK > 0
+  EXPECT_THROW(solver.constraint(field2, field2, "field"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 1);
+  EXPECT_THROW(solver.constraint(field2, field2, ""), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 1);
+#endif
+
+  EXPECT_NO_THROW(solver.constraint(field2, field2, "another_field"));
   EXPECT_EQ(solver.n2Dvars(), 0);
   EXPECT_EQ(solver.n3Dvars(), 2);
 }
 
-TEST_F(SolverTest, Init) {
+TEST_F(SolverTest, ConstraintVector2D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Vector2D vector1{}, vector2{};
+  EXPECT_NO_THROW(solver.constraint(vector1, vector1, "vector"));
+  EXPECT_EQ(solver.n2Dvars(), 3);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+
+#if CHECK > 0
+  EXPECT_THROW(solver.constraint(vector2, vector2, "vector"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 3);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+  EXPECT_THROW(solver.constraint(vector2, vector2, ""), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 3);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+#endif
+
+  vector2.covariant = false;
+  EXPECT_NO_THROW(solver.constraint(vector2, vector2, "another_vector"));
+  EXPECT_EQ(solver.n2Dvars(), 6);
+  EXPECT_EQ(solver.n3Dvars(), 0);
+}
+
+TEST_F(SolverTest, ConstraintVector3D) {
+  Options options;
+  FakeSolver solver{&options};
+
+  Vector3D vector1{}, vector2{};
+  EXPECT_NO_THROW(solver.constraint(vector1, vector1, "vector"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 3);
+
+#if CHECK > 0
+  EXPECT_THROW(solver.constraint(vector2, vector2, "vector"), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 3);
+  EXPECT_THROW(solver.constraint(vector2, vector2, ""), BoutException);
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 3);
+#endif
+
+  vector2.covariant = false;
+  EXPECT_NO_THROW(solver.constraint(vector2, vector2, "another_vector"));
+  EXPECT_EQ(solver.n2Dvars(), 0);
+  EXPECT_EQ(solver.n3Dvars(), 6);
+}
+
+TEST_F(SolverTest, NoInitTwice) {
   Options options;
   FakeSolver solver{&options};
 
   EXPECT_NO_THROW(solver.init(0, 0));
   EXPECT_THROW(solver.init(0, 0), BoutException);
+}
 
-  Field2D field{};
-  EXPECT_THROW(solver.add(field, "field"), BoutException);
+TEST_F(SolverTest, NoAddAfterInit) {
+  Options options;
+  FakeSolver solver{&options};
+
+  EXPECT_NO_THROW(solver.init(0, 0));
+
+  Field2D field1{};
+  EXPECT_THROW(solver.add(field1, "field"), BoutException);
+  Field3D field2{};
+  EXPECT_THROW(solver.add(field2, "field"), BoutException);
+  Vector2D vector1{};
+  EXPECT_THROW(solver.add(vector1, "vector"), BoutException);
+  Vector3D vector2{};
+  EXPECT_THROW(solver.add(vector2, "vector"), BoutException);
+}
+
+TEST_F(SolverTest, NoConstraintsAfterInit) {
+  Options options;
+  FakeSolver solver{&options};
+
+  EXPECT_NO_THROW(solver.init(0, 0));
+
+  Field2D field1{};
+  EXPECT_THROW(solver.constraint(field1, field1, "field"), BoutException);
+  Field3D field2{};
+  EXPECT_THROW(solver.constraint(field2, field2, "field"), BoutException);
+  Vector2D vector1{};
+  EXPECT_THROW(solver.constraint(vector1, vector1, "vector"), BoutException);
+  Vector3D vector2{};
+  EXPECT_THROW(solver.constraint(vector2, vector2, "vector"), BoutException);
 }
 
 TEST_F(SolverTest, SplitOperator) {
