239fd68aa19324cc92806c72447b09e54cf94bc6
Peter Hill <peter.hill@york.ac.uk>
2019-04-08 11:26:54 +0100

    Vary test fields in as many dimensions as possible

--- tests/unit/field/test_where.cxx
+++ tests/unit/field/test_where.cxx
@@ -1,95 +1,120 @@
 #include "gtest/gtest.h"
 
 #include "field.hxx"
-#include "where.hxx"
 #include "test_extras.hxx"
+#include "where.hxx"
 
 /// Global mesh
-namespace bout{
-namespace globals{
-extern Mesh *mesh;
+namespace bout {
+namespace globals {
+extern Mesh* mesh;
 } // namespace globals
 } // namespace bout
 
 // The unit tests use the global mesh
 using namespace bout::globals;
 
+namespace {
+constexpr auto le0 = 4.0;
+constexpr auto gt0 = 2.0;
+} // namespace
+
 // Reuse the "standard" fixture for FakeMesh
 class WhereTest : public FakeMeshFixture {
 public:
-  WhereTest() : FakeMeshFixture(), expected_2d(mesh), expected_3d(mesh) {
-    fillField(expected_2d,
-              {{4., 4., 4., 2., 2.}, {4., 4., 4., 2., 2.}, {4., 4., 4., 2., 2.}});
+  WhereTest()
+      : FakeMeshFixture(), expected_2d2d(mesh), expected_2d3d(mesh), test_2d(mesh) {
+    fillField(expected_2d2d, {{le0, le0, le0, gt0, gt0},
+                              {gt0, le0, le0, le0, le0},
+                              {le0, le0, gt0, gt0, gt0}});
 
-    fillField(expected_3d, {{{4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {2., 2., 2., 2., 2., 2., 2.},
-                             {2., 2., 2., 2., 2., 2., 2.}},
+    fillField(expected_2d3d, {{{le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {gt0, gt0, gt0, gt0, gt0, gt0, gt0},
+                               {gt0, gt0, gt0, gt0, gt0, gt0, gt0}},
 
-                            {{4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {2., 2., 2., 2., 2., 2., 2.},
-                             {2., 2., 2., 2., 2., 2., 2.}},
+                              {{gt0, gt0, gt0, gt0, gt0, gt0, gt0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0}},
 
-                            {{4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {4., 4., 4., 4., 4., 4., 4.},
-                             {2., 2., 2., 2., 2., 2., 2.},
-                             {2., 2., 2., 2., 2., 2., 2.}}});
+                              {{le0, le0, le0, le0, le0, le0, le0},
+                               {le0, le0, le0, le0, le0, le0, le0},
+                               {gt0, gt0, gt0, gt0, gt0, gt0, gt0},
+                               {gt0, gt0, gt0, gt0, gt0, gt0, gt0},
+                               {gt0, gt0, gt0, gt0, gt0, gt0, gt0}}});
 
-    test_2d = makeField<Field2D>(
-        [](Field2D::ind_type i) { return i.y() - (WhereTest::ny / 2); });
-    test_3d = makeField<Field3D>(
-        [](Field3D::ind_type i) { return i.y() - (WhereTest::ny / 2); });
+    fillField(test_2d,
+              {{-2., -1., 0., 1., 2.}, {1., 0., -1., -2., -3.}, {-1., 0., 1., 2., 3.}});
   }
 
   virtual ~WhereTest() = default;
 
-  Field2D expected_2d;
-  Field3D expected_3d;
+  Field2D expected_2d2d;
+  Field3D expected_2d3d;
 
   Field2D test_2d;
-  Field3D test_3d;
 };
 
 TEST_F(WhereTest, Field2DField3DField3D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{2.0}, Field3D{4.0}), expected_3d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{gt0}, Field3D{le0}), expected_2d3d));
 }
 
 TEST_F(WhereTest, Field2DField3DBoutReal) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{2.0}, 4.0), expected_3d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{gt0}, le0), expected_2d3d));
 }
 
 TEST_F(WhereTest, Field2DBoutRealField3D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, 2.0, Field3D{4.0}), expected_3d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, gt0, Field3D{le0}), expected_2d3d));
 }
 
 TEST_F(WhereTest, Field2DField3DField2D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{2.0}, Field2D{4.0}), expected_3d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field3D{gt0}, Field2D{le0}), expected_2d3d));
 }
 
 TEST_F(WhereTest, Field2DField2DField3D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{2.0}, Field3D{4.0}), expected_3d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{gt0}, Field3D{le0}), expected_2d3d));
 }
 
 TEST_F(WhereTest, Field2DField2DField2D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{2.0}, Field2D{4.0}), expected_2d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{gt0}, Field2D{le0}), expected_2d2d));
 }
 
 TEST_F(WhereTest, Field2DField2DBoutReal) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{2.0}, 4.0), expected_2d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, Field2D{gt0}, le0), expected_2d2d));
 }
 
 TEST_F(WhereTest, Field2DBoutRealField2D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, 2.0, Field2D{4.0}), expected_2d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, gt0, Field2D{le0}), expected_2d2d));
 }
 
 TEST_F(WhereTest, Field2DBoutRealBoutReal) {
-  EXPECT_TRUE(IsFieldEqual(where(test_2d, 2.0, 4.0), expected_2d));
+  EXPECT_TRUE(IsFieldEqual(where(test_2d, gt0, le0), expected_2d2d));
 }
 
 TEST_F(WhereTest, Field3DBoutRealField3D) {
-  EXPECT_TRUE(IsFieldEqual(where(test_3d, 2.0, Field3D{4.0}), expected_3d));
+  auto test_3d = makeField<Field3D>([](Field3D::ind_type i) {
+    return (i.y() - (ny / 2)) + (i.x() - (nx / 2)) + (i.z() - (nz / 2));
+  });
+
+  Field3D expected_3d3d;
+  fillField(expected_3d3d, {{{le0, le0, le0, le0, le0, le0, le0},
+                             {le0, le0, le0, le0, le0, le0, gt0},
+                             {le0, le0, le0, le0, le0, gt0, gt0},
+                             {le0, le0, le0, le0, gt0, gt0, gt0},
+                             {le0, le0, le0, gt0, gt0, gt0, gt0}},
+                            {{le0, le0, le0, le0, le0, le0, gt0},
+                             {le0, le0, le0, le0, le0, gt0, gt0},
+                             {le0, le0, le0, le0, gt0, gt0, gt0},
+                             {le0, le0, le0, gt0, gt0, gt0, gt0},
+                             {le0, le0, gt0, gt0, gt0, gt0, gt0}},
+                            {{le0, le0, le0, le0, le0, gt0, gt0},
+                             {le0, le0, le0, le0, gt0, gt0, gt0},
+                             {le0, le0, le0, gt0, gt0, gt0, gt0},
+                             {le0, le0, gt0, gt0, gt0, gt0, gt0},
+                             {le0, gt0, gt0, gt0, gt0, gt0, gt0}}});
+
+  EXPECT_TRUE(IsFieldEqual(where(test_3d, gt0, Field3D{le0}), expected_3d3d));
 }
