e778d0c4f6e3fbc26cfbf857c557461e3932e80e
Peter Hill <peter.hill@york.ac.uk>
2019-05-08 14:24:46 +0100

    Implement FakeMesh::GlobalZ correctly

--- tests/unit/test_extras.hxx
+++ tests/unit/test_extras.hxx
@@ -271,13 +271,13 @@ public:
   std::vector<BoundaryRegionPar *> getBoundariesPar() { return std::vector<BoundaryRegionPar *>(); }
   BoutReal GlobalX(int jx) const { return jx; }
   BoutReal GlobalY(int jy) const { return jy; }
-  BoutReal GlobalZ(int jz) const { return jz; }
+  BoutReal GlobalZ(int jz) const { return jz / static_cast<BoutReal>(LocalNz); }
   BoutReal GlobalX(BoutReal jx) const { return jx; }
   BoutReal GlobalY(BoutReal jy) const { return jy; }
-  BoutReal GlobalZ(BoutReal jz) const { return jz; }
+  BoutReal GlobalZ(BoutReal jz) const { return jz / static_cast<BoutReal>(LocalNz); }
   int XGLOBAL(int UNUSED(xloc)) const { return 0; }
   int YGLOBAL(int UNUSED(yloc)) const { return 0; }
-  int ZGLOBAL(int UNUSED(zloc)) const { return 0; }  
+  int ZGLOBAL(int UNUSED(zloc)) const { return 0; }
 
   void initDerivs(Options * opt){
     StaggerGrids=true;
