ad4bc06f5798139eefd330034b68cfc5e9051a4e
John Omotani <john.omotani@ukaea.uk>
2019-05-23 16:50:56 +0100

    Integrated test for twist-shift

---
+++ tests/integrated/test-twistshift/test-twistshift.cxx
@@ -0,0 +1,32 @@
+#include "bout.hxx"
+#include "field_factory.hxx"
+
+int main(int argc, char** argv) {
+  BoutInitialise(argc, argv);
+
+  Field3D test = FieldFactory::get()->create3D("test");
+
+  Field3D test_aligned = toFieldAligned(test);
+
+  // zero guard cells to check that communication is doing something
+  for (int x=0; x<mesh->LocalNx; x++) {
+    for (int z=0; z<mesh->LocalNz; z++) {
+      for (int y=0; y<mesh->ystart; y++) {
+        test_aligned(x, y, z) = 0.;
+      }
+      for (int y=mesh->yend+1; y<mesh->LocalNy; y++) {
+        test_aligned(x, y, z) = 0.;
+      }
+    }
+  }
+
+  mesh->communicate(test_aligned);
+
+  Field3D result = fromFieldAligned(test_aligned);
+
+  SAVE_ONCE(test, test_aligned, result);
+
+  dump.write();
+
+  BoutFinalise();
+}
