69c4871b6414e87e7c11bc4d811afdf0a99e2513
Peter Hill <peter.hill@york.ac.uk>
2019-05-23 14:12:53 +0100

    Add test for BoutMesh decomposition on single processor
    This test will fail; next commit fixes bug

--- tests/unit/mesh/test_boutmesh.cxx
+++ tests/unit/mesh/test_boutmesh.cxx
@@ -4,6 +4,7 @@
 #include "bout/mesh.hxx"
 #include "output.hxx"
 #include "unused.hxx"
+#include "bout/griddata.hxx"
 
 #include "test_extras.hxx"
 
@@ -14,4 +15,20 @@ TEST(BoutMeshTest, NullOptionsCheck) {
   EXPECT_NO_THROW(BoutMesh mesh(new FakeGridDataSource, nullptr));
   output_info.enable();
   output_warn.enable();
+// Not a great test as it's not specific to the thing we want to test,
+// and also takes a whopping ~300ms!
+TEST(BoutMeshTest, SingleCoreDecomposition) {
+  WithQuietOutput info{output_info};
+  WithQuietOutput warn{output_warn};
+  WithQuietOutput progress{output_progress};
+
+  Options options{};
+  options["ny"] = 1;
+  options["nx"] = 4;
+  options["nz"] = 1;
+  options["MXG"] = 1;
+  options["MYG"] = 0;
+
+  BoutMesh mesh{new GridFromOptions{&options}, &options};
+  EXPECT_NO_THROW(mesh.load());
 }
