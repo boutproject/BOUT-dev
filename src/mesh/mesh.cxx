aa42cc4bfc0948615c8d7e14101856b6c6ca48b4
Joseph Parker <joseph.parker@stfc.ac.uk>
2019-06-21 11:02:13 +0100

    Set default value in cases where there is no mesh

--- src/mesh/mesh.cxx
+++ src/mesh/mesh.cxx
@@ -61,8 +61,11 @@ int Mesh::get(std::string &sval, const std::string &name) {
 int Mesh::get(int &ival, const std::string &name, int def) {
   TRACE("Mesh::get(ival, %s)", name.c_str());
 
-  if (source == nullptr or !source->get(this, ival, name, def))
+  if (source == nullptr or !source->get(this, ival, name, def)) {
+    // set ival to default in source==nullptr too:
+    ival = def;
     return 1;
+  }
 
   return 0;
 }
