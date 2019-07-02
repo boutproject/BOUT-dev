1e81b361cdb438b8f0a8cf085a63114dba8e05b9
Peter Hill <peter.hill@york.ac.uk>
2019-07-02 15:07:37 +0100

    Clang-format HeatFluxSNB

--- examples/conduction-snb/conduction-snb.cxx
+++ examples/conduction-snb/conduction-snb.cxx
@@ -3,19 +3,19 @@
 #include <bout/snb.hxx>
 #include <bout.hxx>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   using bout::HeatFluxSNB;
 
   BoutInitialise(argc, argv);
 
   // Read the density and temperature profiles
-  Options &opt = Options::root();
+  Options& opt = Options::root();
 
   Field3D Ne = opt["Ne"].doc("Electron density in m^-3").as<Field3D>();
   Field3D Te = opt["Te"].doc("Electron temperature in eV").as<Field3D>();
 
   mesh->communicate(Ne, Te);
-  
+
   // Calculate divergence of heat flux
   HeatFluxSNB snb;
   Field3D Div_Q_SH;
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
   // Save to the output
   SAVE_ONCE(Ne, Te, Div_Q, Div_Q_SH);
   dump.write();
-  
+
   BoutFinalise();
   return 0;
 }
--- include/bout/snb.hxx
+++ include/bout/snb.hxx
@@ -1,5 +1,5 @@
-#include "../options.hxx"
-#include "../invert_parderiv.hxx"
+#include "invert_parderiv.hxx"
+#include "options.hxx"
 
 #include <memory>
 
@@ -9,9 +9,11 @@ namespace bout {
 ///
 /// Useful references:
 ///
-///   Braginskii equations by R.Fitzpatrick: http://farside.ph.utexas.edu/teaching/plasma/Plasmahtml/node35.html
-/// 
-///   J.P.Brodrick et al 2017: https://doi.org/10.1063/1.5001079 and https://arxiv.org/abs/1704.08963
+///   Braginskii equations by R.Fitzpatrick:
+///   http://farside.ph.utexas.edu/teaching/plasma/Plasmahtml/node35.html
+///
+///   J.P.Brodrick et al 2017: https://doi.org/10.1063/1.5001079 and
+///   https://arxiv.org/abs/1704.08963
 ///
 ///   Shurtz, Nicolai and Busquet 2000: https://doi.org/10.1063/1.1289512
 ///
@@ -21,17 +23,19 @@ public:
   HeatFluxSNB() : HeatFluxSNB(Options::root()["snb"]) {}
 
   /// Construct using options in given section.
-  explicit HeatFluxSNB(Options &options) {
+  explicit HeatFluxSNB(Options& options) {
     invertpar = std::unique_ptr<InvertPar>{InvertPar::Create()};
 
     // Read options. Note that the defaults are initialised already
-    r = options["r"].doc("Scaling of the electron-electron mean free path")
-      .withDefault(r);
-    beta_max = options["beta_max"].doc("Maximum energy group to consider (multiple of eT)")
-      .withDefault(beta_max);
+    r = options["r"]
+            .doc("Scaling of the electron-electron mean free path")
+            .withDefault(r);
+    beta_max = options["beta_max"]
+                   .doc("Maximum energy group to consider (multiple of eT)")
+                   .withDefault(beta_max);
     ngroups = options["ngroups"].doc("Number of energy groups").withDefault(ngroups);
   }
-  
+
   ~HeatFluxSNB() = default;
 
   HeatFluxSNB(HeatFluxSNB&&) = default;
@@ -49,26 +53,27 @@ public:
   ///
   /// Returns the divergence of heat flux in units of eV per cubic meter per second
   /// -> multiply by e=1.602e-19 to get Watts per cubic meter.
-  Field3D divHeatFlux(const Field3D &Te, const Field3D &Ne, Field3D *Div_Q_SH_out = nullptr);
-  
+  Field3D divHeatFlux(const Field3D& Te, const Field3D& Ne,
+                      Field3D* Div_Q_SH_out = nullptr);
+
 private:
   /// Parallel inversion of tridiagonal matrices
   std::unique_ptr<InvertPar> invertpar{nullptr};
-  
-  BoutReal Z{1}; ///< Average ion charge (1 = Hydrogen)
-  BoutReal r{2}; ///< Electron-electron mean free path scaling factor
+
+  BoutReal Z{1};           ///< Average ion charge (1 = Hydrogen)
+  BoutReal r{2};           ///< Electron-electron mean free path scaling factor
   BoutReal beta_max{10.0}; ///< Maximum energy group to consider (multiple of eT)
-  int ngroups{40}; ///< Number of energy groups
+  int ngroups{40};         ///< Number of energy groups
 
   /// Indefinite integral of beta^4 * exp(-beta)
   /// with constant set to zero
   BoutReal int_beta4_exp(BoutReal beta) {
-    return - exp(- beta) * (24 + beta * (24 + beta * (12 + beta * (4 + beta))));
+    return -exp(-beta) * (24 + beta * (24 + beta * (12 + beta * (4 + beta))));
   }
-  
+
   /// (1/24) * Integral of beta^4 * exp(-beta) from beta_min to beta_max
   BoutReal groupWeight(BoutReal beta_min, BoutReal beta_max) {
-    return (1./24) * (int_beta4_exp(beta_max) - int_beta4_exp(beta_min));
+    return (1. / 24) * (int_beta4_exp(beta_max) - int_beta4_exp(beta_min));
   }
 };
 
