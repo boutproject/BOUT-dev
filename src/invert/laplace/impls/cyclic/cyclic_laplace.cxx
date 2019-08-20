fb03bc1582bb1fff6572a2719204df769e57855a
John Omotani <john.omotani@ukaea.uk>
2019-08-20 17:31:26 +0100

    Implement the INVERT_ZERO_DC flag in LaplaceCyclic

--- src/invert/laplace/impls/cyclic/cyclic_laplace.cxx
+++ src/invert/laplace/impls/cyclic/cyclic_laplace.cxx
@@ -232,9 +232,19 @@ FieldPerp LaplaceCyclic::solve(const FieldPerp& rhs, const FieldPerp& x0) {
       auto k1d = Array<dcomplex>((localmesh->LocalNz) / 2 +
                                  1); // ZFFT routine expects input of this length
 
+      int startz = 0;
+      if(global_flags & INVERT_ZERO_DC) {
+        // No DC component
+        startz = 1;
+      }
+
       BOUT_OMP(for nowait)
       for (int ix = xs; ix <= xe; ix++) {
-        for (int kz = 0; kz < nmode; kz++)
+        if (startz == 1) {
+          k1d[0] = 0.;
+        }
+
+        for (int kz = startz; kz < nmode; kz++)
           k1d[kz] = xcmplx(kz, ix - xs);
 
         for (int kz = nmode; kz < (localmesh->LocalNz) / 2 + 1; kz++)
@@ -440,13 +450,23 @@ Field3D LaplaceCyclic::solve(const Field3D& rhs, const Field3D& x0) {
       auto k1d = Array<dcomplex>((localmesh->LocalNz) / 2 +
                                  1); // ZFFT routine expects input of this length
 
+      int startz = 0;
+      if(global_flags & INVERT_ZERO_DC) {
+        // No DC component
+        startz = 1;
+      }
+
       BOUT_OMP(for nowait)
       for (int ind = 0; ind < nxny; ++ind) { // Loop over X and Y
         // ind = (ix - xs)*(ye - ys + 1) + (iy - ys)
         int ix = xs + ind / ny;
         int iy = ys + ind % ny;
 
-        for (int kz = 0; kz < nmode; kz++)
+        if (startz == 1) {
+          k1d[0] = 0.;
+        }
+
+        for (int kz = startz; kz < nmode; kz++)
           k1d[kz] = xcmplx3D((iy - ys) * nmode + kz, ix - xs);
 
         for (int kz = nmode; kz < localmesh->LocalNz / 2 + 1; kz++)
