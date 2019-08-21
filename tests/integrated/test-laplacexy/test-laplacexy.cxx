70fe131a53589760ffef4ff171c44f18c2853031
John Omotani <john.omotani@ukaea.uk>
2019-08-21 23:47:46 +0100

    Integrated test for LaplaceXY

---
+++ tests/integrated/test-laplacexy/test-laplacexy.cxx
@@ -0,0 +1,82 @@
+/**************************************************************************
+ * Testing Perpendicular Laplacian inversion using PETSc solvers
+ *
+ **************************************************************************
+ * Copyright 2019 J.T. Omotani, B.D. Dudson
+ *
+ * Contact: Ben Dudson, bd512@york.ac.uk
+ *
+ * This file is part of BOUT++.
+ *
+ * BOUT++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BOUT++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BOUT++.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+#include <bout.hxx>
+#include <bout/constants.hxx>
+#include <bout/invert/laplacexy.hxx>
+#include <initialprofiles.hxx>
+#include <options.hxx>
+
+int main(int argc, char** argv) {
+
+  BoutInitialise(argc, argv);
+  
+  LaplaceXY laplacexy;
+
+  // Solving equations of the form
+  // Div(A Grad_perp(f)) + B*f = rhs
+  // A*Laplace_perp(f) + Grad_perp(A).Grad_perp(f) + B*f = rhs
+  Field2D f, a, b, sol;
+  Field2D error, absolute_error; //Absolute value of relative error: abs((f - sol)/f)
+  BoutReal max_error; //Output of test
+
+  initial_profile("f", f);
+  initial_profile("a", a);
+  initial_profile("b", b);
+
+  // Apply boundary conditions to f, so the boundary cells match the way boundary
+  // conditions will be applied to sol
+  f.setBoundary("f");
+  f.applyBoundary();
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+  Field2D rhs = a*Laplace_perp(f) + Grad_perp(a)*Grad_perp(f) + b*f;
+
+  laplacexy.setCoefs(a, b);
+  
+  sol = laplacexy.solve(rhs, 0.);
+  error = (f - sol)/f;
+  absolute_error = f - sol;
+  max_error = max(abs(absolute_error), true);
+
+  output<<"Magnitude of maximum absolute error is "<<max_error<<endl;
+
+  dump.add(a, "a");
+  dump.add(b, "b");
+  dump.add(f, "f");
+  dump.add(sol, "sol");
+  dump.add(error, "error");
+  dump.add(absolute_error, "absolute_error");
+  dump.add(max_error, "max_error");
+
+  dump.write();
+  dump.close();
+
+  MPI_Barrier(BoutComm::get()); // Wait for all processors to write data
+
+  BoutFinalise();
+  return 0;
+}
