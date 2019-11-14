88099d07159a5195b8ff7e828498859c3e4c2579
Peter Hill <peter.hill@york.ac.uk>
2019-11-14 13:39:40 +0000

    Replace ParDerivFactory with StandardFactory
    Also:
    - renamed to InvertParFactory to be in line with other factories
    - moved into invert_parderiv header

--- include/invert_parderiv.hxx
+++ include/invert_parderiv.hxx
@@ -35,9 +35,41 @@
 #include "field2d.hxx"
 #include "options.hxx"
 #include "unused.hxx"
+#include "bout/generic_factory.hxx"
 
 // Parderiv implementations
-#define PARDERIVCYCLIC "cyclic"
+constexpr auto PARDERIVCYCLIC = "cyclic";
+
+class InvertPar;
+
+class InvertParFactory
+    : public StandardFactory<InvertPar, InvertParFactory,
+                             std::function<std::unique_ptr<InvertPar>(Options*, Mesh*)>> {
+public:
+  static constexpr auto type_name = "InvertPar";
+  static constexpr auto section_name = "parderiv";
+  static constexpr auto option_name = "type";
+  static constexpr auto default_type = PARDERIVCYCLIC;
+
+  ReturnType create(Options* options = nullptr, Mesh* mesh = nullptr) {
+    return StandardFactory::create(getType(options), options, mesh);
+  }
+};
+
+template <class DerivedType>
+class RegisterInStandardFactory<InvertPar, DerivedType, InvertParFactory> {
+public:
+  RegisterInStandardFactory(const std::string& name) {
+    InvertParFactory::getInstance().add(
+        name, [](Options* options, Mesh* mesh) -> std::unique_ptr<InvertPar> {
+          return std::make_unique<DerivedType>(options, mesh);
+        });
+  }
+};
+
+template <class DerivedType>
+using RegisterInvertPar =
+    RegisterInStandardFactory<InvertPar, DerivedType, InvertParFactory>;
 
 /// Base class for parallel inversion solvers
 /*!
@@ -49,7 +81,7 @@
  * Example
  * -------
  *
- * InvertPar *inv = InvertPar::Create();
+ * auto inv = InvertPar::Create();
  * inv->setCoefA(1.0);
  * inv->setCoefB(-0.1);
  * 
@@ -72,7 +104,9 @@ public:
    * 
    * Note: For consistency this should be renamed "create" and take an Options* argument
    */
-  static InvertPar* Create(Mesh *mesh_in = nullptr);
+  static std::unique_ptr<InvertPar> Create(Mesh *mesh_in = nullptr) {
+    return InvertParFactory::getInstance().create(nullptr, mesh_in);
+  }
   
   /*!
    * Solve the system of equations
--- src/invert/parderiv/invert_parderiv.cxx
+++ src/invert/parderiv/invert_parderiv.cxx
@@ -28,11 +28,7 @@
  ************************************************************************/
 
 #include <invert_parderiv.hxx>
-#include "parderiv_factory.hxx"
-
-InvertPar* InvertPar::Create(Mesh* mesh_in) {
-  return ParDerivFactory::getInstance()->createInvertPar(mesh_in);
-}
+#include "impls/cyclic/cyclic.hxx"
 
 const Field2D InvertPar::solve(const Field2D &f) {
   Field3D var(f);
@@ -40,5 +36,3 @@ const Field2D InvertPar::solve(const Field2D &f) {
   var = solve(var);
   return DC(var);
 }
-
-  
