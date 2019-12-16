cfb7888b495aec5031ed3bbe80436c007191f782
Peter Hill <zed.three@gmail.com>
2019-12-16 10:41:53 +0000

    Fix MACRO_FOR_EACH for MSVC using intermediate expansion

--- include/bout/macro_for_each.hxx
+++ include/bout/macro_for_each.hxx
@@ -13,30 +13,32 @@
 //    https://github.com/cormacc/va_args_iterators
 //
 
+/// Intermediate expansion needed for MSVC due to non-compliant preprocessor
+#define BOUT_EXPAND(x) x
 
 /// _me_x set of macros expand a number of arguments without ';' between them
 #define _me_1(_call, x) _call(x)
-#define _me_2(_call, x, ...) _call(x) _me_1(_call, __VA_ARGS__)
-#define _me_3(_call, x, ...) _call(x) _me_2(_call, __VA_ARGS__)
-#define _me_4(_call, x, ...) _call(x) _me_3(_call, __VA_ARGS__)
-#define _me_5(_call, x, ...) _call(x) _me_4(_call, __VA_ARGS__)
-#define _me_6(_call, x, ...) _call(x) _me_5(_call, __VA_ARGS__)
-#define _me_7(_call, x, ...) _call(x) _me_6(_call, __VA_ARGS__)
-#define _me_8(_call, x, ...) _call(x) _me_7(_call, __VA_ARGS__)
-#define _me_9(_call, x, ...) _call(x) _me_8(_call, __VA_ARGS__)
-#define _me_10(_call, x, ...) _call(x) _me_9(_call, __VA_ARGS__)
+#define _me_2(_call, x, ...) _call(x) BOUT_EXPAND(_me_1(_call, __VA_ARGS__))
+#define _me_3(_call, x, ...) _call(x) BOUT_EXPAND(_me_2(_call, __VA_ARGS__))
+#define _me_4(_call, x, ...) _call(x) BOUT_EXPAND(_me_3(_call, __VA_ARGS__))
+#define _me_5(_call, x, ...) _call(x) BOUT_EXPAND(_me_4(_call, __VA_ARGS__))
+#define _me_6(_call, x, ...) _call(x) BOUT_EXPAND(_me_5(_call, __VA_ARGS__))
+#define _me_7(_call, x, ...) _call(x) BOUT_EXPAND(_me_6(_call, __VA_ARGS__))
+#define _me_8(_call, x, ...) _call(x) BOUT_EXPAND(_me_7(_call, __VA_ARGS__))
+#define _me_9(_call, x, ...) _call(x) BOUT_EXPAND(_me_8(_call, __VA_ARGS__))
+#define _me_10(_call, x, ...) _call(x) BOUT_EXPAND(_me_9(_call, __VA_ARGS__))
 
 /// _fe_x set of macros expand a number of arguments with ';' between them
 #define _fe_1(_call, x) _call(x);
-#define _fe_2(_call, x, ...) _call(x); _fe_1(_call, __VA_ARGS__)
-#define _fe_3(_call, x, ...) _call(x); _fe_2(_call, __VA_ARGS__)
-#define _fe_4(_call, x, ...) _call(x); _fe_3(_call, __VA_ARGS__)
-#define _fe_5(_call, x, ...) _call(x); _fe_4(_call, __VA_ARGS__)
-#define _fe_6(_call, x, ...) _call(x); _fe_5(_call, __VA_ARGS__)
-#define _fe_7(_call, x, ...) _call(x); _fe_6(_call, __VA_ARGS__)
-#define _fe_8(_call, x, ...) _call(x); _fe_7(_call, __VA_ARGS__)
-#define _fe_9(_call, x, ...) _call(x); _fe_8(_call, __VA_ARGS__)
-#define _fe_10(_call, x, ...) _call(x); _fe_9(_call, __VA_ARGS__)
+#define _fe_2(_call, x, ...) _call(x); BOUT_EXPAND(_fe_1(_call, __VA_ARGS__))
+#define _fe_3(_call, x, ...) _call(x); BOUT_EXPAND(_fe_2(_call, __VA_ARGS__))
+#define _fe_4(_call, x, ...) _call(x); BOUT_EXPAND(_fe_3(_call, __VA_ARGS__))
+#define _fe_5(_call, x, ...) _call(x); BOUT_EXPAND(_fe_4(_call, __VA_ARGS__))
+#define _fe_6(_call, x, ...) _call(x); BOUT_EXPAND(_fe_5(_call, __VA_ARGS__))
+#define _fe_7(_call, x, ...) _call(x); BOUT_EXPAND(_fe_6(_call, __VA_ARGS__))
+#define _fe_8(_call, x, ...) _call(x); BOUT_EXPAND(_fe_7(_call, __VA_ARGS__))
+#define _fe_9(_call, x, ...) _call(x); BOUT_EXPAND(_fe_8(_call, __VA_ARGS__))
+#define _fe_10(_call, x, ...) _call(x); BOUT_EXPAND(_fe_9(_call, __VA_ARGS__))
 
 /// When called with __VA_ARGS__ first, this evaluates to an argument which depends
 /// on the length of __VA_ARGS__. This is used to find the appropriate macro to
@@ -63,10 +65,10 @@
 ///     to avoid surprising results.
 /// 
 #define MACRO_FOR_EACH(mac, ...)                                        \
-  _GET_FOR_EACH_EXPANSION(__VA_ARGS__,                                  \
+  BOUT_EXPAND(_GET_FOR_EACH_EXPANSION(__VA_ARGS__,                      \
                           _me_10, _me_9, _me_8, _me_7, _me_6, _me_5,    \
                           _me_4, _me_3, _me_2, _me_1)                   \
-  (mac, __VA_ARGS__)
+  (mac, __VA_ARGS__))
 
 /// Apply a function (first argument) to each
 /// of the following arguments.
@@ -88,9 +90,9 @@
 ///     to avoid surprising results.
 ///
 #define MACRO_FOR_EACH_FN(fn, ...)                                      \
-  _GET_FOR_EACH_EXPANSION(__VA_ARGS__,                                  \
+  BOUT_EXPAND(_GET_FOR_EACH_EXPANSION(__VA_ARGS__,                      \
                           _fe_10, _fe_9, _fe_8, _fe_7, _fe_6, _fe_5,    \
                           _fe_4, _fe_3, _fe_2, _fe_1)                   \
-  (fn, __VA_ARGS__)
+  (fn, __VA_ARGS__))
 
 #endif
