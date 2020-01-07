02bf743e2c898c54a9dba3b4e500ace4a920240d
John Omotani <john.omotani@ukaea.uk>
2020-01-07 14:28:35 +0000

    Communicate the corner cells

--- src/mesh/impls/bout/boutmesh.hxx
+++ src/mesh/impls/bout/boutmesh.hxx
@@ -270,24 +270,34 @@ private:
   /// Used to keep track of communications between send and receive
   struct CommHandle {
     /// Array of receive requests. One for each possible neighbour; one each way in X, two
-    /// each way in Y
-    MPI_Request request[6];
+    /// each way in Y. Plus four for the corners.
+    MPI_Request request[10] = {MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL};
     /// Array of send requests (for non-blocking send). One for each possible neighbour;
-    /// one each way in X, two each way in Y
-    MPI_Request sendreq[6];
+    /// one each way in X, two each way in Y. Plus four for the corners.
+    MPI_Request sendreq[10] = {MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL, MPI_REQUEST_NULL, MPI_REQUEST_NULL,
+                               MPI_REQUEST_NULL};
     /// Length of the buffers used to send/receive (in BoutReals)
-    int xbufflen, ybufflen;
+    int xbufflen, ybufflen, cornerbufflen;
     /// Sending buffers
-    Array<BoutReal> umsg_sendbuff, dmsg_sendbuff, imsg_sendbuff, omsg_sendbuff;
+    Array<BoutReal> umsg_sendbuff, dmsg_sendbuff, imsg_sendbuff, omsg_sendbuff,
+                    lowin_corner_sendbuff, upin_corner_sendbuff, lowout_corner_sendbuff,
+                    upout_corner_sendbuff;
     /// Receiving buffers
-    Array<BoutReal> umsg_recvbuff, dmsg_recvbuff, imsg_recvbuff, omsg_recvbuff;
+    Array<BoutReal> umsg_recvbuff, dmsg_recvbuff, imsg_recvbuff, omsg_recvbuff,
+                    lowin_corner_recvbuff, upin_corner_recvbuff, lowout_corner_recvbuff,
+                    upout_corner_recvbuff;
     /// Is the communication still going?
     bool in_progress;
     /// List of fields being communicated
     FieldGroup var_list;
   };
   void free_handle(CommHandle* h);
-  CommHandle* get_handle(int xlen, int ylen);
+  CommHandle* get_handle(int xlen, int ylen, int cornerlen = 0);
   void clear_handles();
   std::list<CommHandle*> comm_list; // List of allocated communication handles
 
