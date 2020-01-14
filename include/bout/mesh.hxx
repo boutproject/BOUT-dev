aff0cd109b41f9660e965d9e75c5e6eb44b34ed1
Chris MacMackin <chris.macmackin@ukaea.uk>
2020-01-14 12:43:26 +0000

    Removed methods from Mesh which were used in petsc interface
    These routines (`localSizeXX` and `globalStartIndexXX`) now depend on the
    OperatorStencil being used and thus would not return a single value
    for a given mesh. The necessary calculations are now performed in the
    GlobalIndexer class.

--- include/bout/mesh.hxx
+++ include/bout/mesh.hxx
@@ -530,26 +530,6 @@ class Mesh {
   /// If the global index includes the boundary cells, then so does the local.
   virtual int YLOCAL(int yglo) const = 0;
 
-  /// Returns the number of unique cells (i.e., ones not used for
-  /// communication) on this processor for 3D fields. Boundaries
-  /// are only included to a depth of 1.
-  virtual int localSize3D();
-  /// Returns the number of unique cells (i.e., ones not used for
-  /// communication) on this processor for 2D fields. Boundaries
-  /// are only included to a depth of 1.
-  virtual int localSize2D();
-  /// Returns the number of unique cells (i.e., ones not used for
-  /// communication) on this processor for perpendicular fields.
-  /// Boundaries are only included to a depth of 1.
-  virtual int localSizePerp();
-
-  /// Get the value of the first global 3D index on this processor.
-  virtual int globalStartIndex3D();
-  /// Get the value of the first global 2D index on this processor.
-  virtual int globalStartIndex2D();
-  /// Get the value of the first global perpendicular index on this processor.
-  virtual int globalStartIndexPerp();
-
   /// Returns a global X index given a local index.
   /// Global index includes boundary cells, local index includes boundary or guard cells.
   virtual int getGlobalXIndex(int xlocal) const = 0;
