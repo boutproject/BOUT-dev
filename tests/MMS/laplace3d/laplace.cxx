f24e39a597a1fbb94f1324ee1cd4a094514af9ee
John Omotani <john.omotani@ukaea.uk>
2020-01-21 21:38:23 +0000

    Update tests/MMS/laplace3d/laplace.cxx for v4.3
    Declare solver pointer as 'auto' since Laplace::create() now returns a
    std::unique_ptr

--- tests/MMS/laplace3d/laplace.cxx
+++ tests/MMS/laplace3d/laplace.cxx
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
   }
 
   // Create a Laplacian inversion solver
-  Laplacian *lap = Laplacian::create();
+  auto lap = Laplacian::create();
 
   FieldFactory fact(mesh);
 
