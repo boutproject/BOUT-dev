c76b7aa203b307e9393a3c9a0771c2d3df85549a
John Omotani <john.omotani@ukaea.uk>
2020-01-22 18:35:06 +0000

    Rename macros to avoid leading underscore followed by capital
    Leading underscore followed by capital is reserved. Prefix the macros
    with BOUT_ instead.

--- include/bout/bout_enum_class.hxx
+++ include/bout/bout_enum_class.hxx
@@ -53,53 +53,53 @@
 #define _ec_expand_10(_call, enumname, x, ...) \
   _call(enumname, x) _ec_expand_9(_call, enumname, __VA_ARGS__)
 
-#define _BOUT_ENUM_CLASS_MAP_ARGS(mac, enumname, ...)                              \
-  _GET_FOR_EACH_EXPANSION(__VA_ARGS__,                                             \
-                          _ec_expand_10, _ec_expand_9, _ec_expand_8, _ec_expand_7, \
-                          _ec_expand_6, _ec_expand_5, _ec_expand_4, _ec_expand_3,  \
-                          _ec_expand_2, _ec_expand_1)                              \
+#define BOUT_ENUM_CLASS_MAP_ARGS(mac, enumname, ...)                                   \
+  BOUT_GET_FOR_EACH_EXPANSION(__VA_ARGS__,                                             \
+                              _ec_expand_10, _ec_expand_9, _ec_expand_8, _ec_expand_7, \
+                              _ec_expand_6, _ec_expand_5, _ec_expand_4, _ec_expand_3,  \
+                              _ec_expand_2, _ec_expand_1)                              \
   (mac, enumname, __VA_ARGS__)
 
-#define _ENUM_CLASS_STR(enumname, val) {enumname::val, lowercase(#val)},
-#define _STR_ENUM_CLASS(enumname, val) {lowercase(#val), enumname::val},
+#define BOUT_ENUM_CLASS_STR(enumname, val) {enumname::val, lowercase(#val)},
+#define BOUT_STR_ENUM_CLASS(enumname, val) {lowercase(#val), enumname::val},
 
-#define _MAKE_FROMSTRING_NAME(enumname) enumname ## FromString
+#define BOUT_MAKE_FROMSTRING_NAME(enumname) enumname ## FromString
 
 /// Create an enum class with toString and <enum name>FromString functions, and an
 /// Options::as<enum> overload to read the enum
-#define BOUT_ENUM_CLASS(enumname, ...)                                     \
-enum class enumname { __VA_ARGS__ };                                       \
-                                                                           \
-inline std::string toString(enumname e) {                                  \
-  AUTO_TRACE();                                                            \
-  const static std::map<enumname, std::string> toString_map = {            \
-    _BOUT_ENUM_CLASS_MAP_ARGS(_ENUM_CLASS_STR, enumname, __VA_ARGS__)      \
-  };                                                                       \
-  auto found = toString_map.find(e);                                       \
-  if (found == toString_map.end()) {                                       \
-    throw BoutException("Did not find enum %d", static_cast<int>(e));      \
-  }                                                                        \
-  return found->second;                                                    \
-}                                                                          \
-                                                                           \
-inline enumname _MAKE_FROMSTRING_NAME(enumname)(const std::string& s) {    \
-  AUTO_TRACE();                                                            \
-  const static std::map<std::string, enumname> fromString_map = {          \
-    _BOUT_ENUM_CLASS_MAP_ARGS(_STR_ENUM_CLASS, enumname, __VA_ARGS__)      \
-  };                                                                       \
-  auto found = fromString_map.find(s);                                     \
-  if (found == fromString_map.end()) {                                     \
-    throw BoutException("Did not find enum %s", s.c_str());                \
-  }                                                                        \
-  return found->second;                                                    \
-}                                                                          \
-                                                                           \
-template <> inline enumname Options::as<enumname>(const enumname&) const { \
-  return _MAKE_FROMSTRING_NAME(enumname)(this->as<std::string>());         \
-}                                                                          \
-                                                                           \
-inline std::ostream& operator<<(std::ostream& out, const enumname& e) {    \
-  return out << toString(e);                                               \
+#define BOUT_ENUM_CLASS(enumname, ...)                                      \
+enum class enumname { __VA_ARGS__ };                                        \
+                                                                            \
+inline std::string toString(enumname e) {                                   \
+  AUTO_TRACE();                                                             \
+  const static std::map<enumname, std::string> toString_map = {             \
+    BOUT_ENUM_CLASS_MAP_ARGS(_ENUM_CLASS_STR, enumname, __VA_ARGS__)        \
+  };                                                                        \
+  auto found = toString_map.find(e);                                        \
+  if (found == toString_map.end()) {                                        \
+    throw BoutException("Did not find enum %d", static_cast<int>(e));       \
+  }                                                                         \
+  return found->second;                                                     \
+}                                                                           \
+                                                                            \
+inline enumname BOUT_MAKE_FROMSTRING_NAME(enumname)(const std::string& s) { \
+  AUTO_TRACE();                                                             \
+  const static std::map<std::string, enumname> fromString_map = {           \
+    BOUT_ENUM_CLASS_MAP_ARGS(_STR_ENUM_CLASS, enumname, __VA_ARGS__)        \
+  };                                                                        \
+  auto found = fromString_map.find(s);                                      \
+  if (found == fromString_map.end()) {                                      \
+    throw BoutException("Did not find enum %s", s.c_str());                 \
+  }                                                                         \
+  return found->second;                                                     \
+}                                                                           \
+                                                                            \
+template <> inline enumname Options::as<enumname>(const enumname&) const {  \
+  return BOUT_MAKE_FROMSTRING_NAME(enumname)(this->as<std::string>());      \
+}                                                                           \
+                                                                            \
+inline std::ostream& operator<<(std::ostream& out, const enumname& e) {     \
+  return out << toString(e);                                                \
 }
 
 #endif // __BOUT_ENUM_CLASS_H__
