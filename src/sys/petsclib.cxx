4ec92d21ce2e9812613451a053b52bc93c94a388
John Omotani <john.omotani@ukaea.uk>
2020-02-16 22:34:16 +0000

    BOUT_OMP(critical) in PetscLib
    Makes 'count', which is incremented in the PetscLib constructor and
    decremented in the destructor, thread-safe.

--- src/sys/petsclib.cxx
+++ src/sys/petsclib.cxx
@@ -2,6 +2,7 @@
 #ifdef BOUT_HAS_PETSC
 
 #include "boutcomm.hxx"
+#include "bout/openmpwrap.hxx"
 #include <bout/petsclib.hxx>
 
 #include <output.hxx>
@@ -14,19 +15,23 @@ char ***PetscLib::pargv = nullptr;
 PetscLogEvent PetscLib::USER_EVENT = 0;
 
 PetscLib::PetscLib() {
-  if(count == 0) {
-    // Initialise PETSc
-    
-    output << "Initialising PETSc\n";
-    PETSC_COMM_WORLD = BoutComm::getInstance()->getComm();
-    PetscInitialize(pargc,pargv,PETSC_NULL,help);
-    PetscLogEventRegister("Total BOUT++",0,&USER_EVENT);
-    PetscLogEventBegin(USER_EVENT,0,0,0,0);
+  BOUT_OMP(critical)
+  {
+    if(count == 0) {
+      // Initialise PETSc
+
+      output << "Initialising PETSc\n";
+      PETSC_COMM_WORLD = BoutComm::getInstance()->getComm();
+      PetscInitialize(pargc,pargv,PETSC_NULL,help);
+      PetscLogEventRegister("Total BOUT++",0,&USER_EVENT);
+      PetscLogEventBegin(USER_EVENT,0,0,0,0);
+    }
+    count++;
   }
-  count++;
 }
 
 PetscLib::~PetscLib() {
+  BOUT_OMP(critical)
   count--;
   if(count == 0) {
     // Finalise PETSc
