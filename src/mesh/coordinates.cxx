932dfe0afb0b344a5048cede05ee360ecb899ddf
Brendan Shanahan <brendan.shanahan@ipp.mpg.de>
2020-02-28 10:54:47 +0100

    Use local transform in indexDDY

--- src/mesh/coordinates.cxx
+++ src/mesh/coordinates.cxx
@@ -1767,8 +1767,9 @@ Field3D Coordinates::indexDDY(const Field3D& f, CELL_LOC outloc,
     if (transform->canToFromFieldAligned()){
       f_aligned = is_unaligned ? transform->toFieldAligned(f, "RGN_NOX") : f;
     } else {
-      f.calcParallelSlices();
-      return bout::derivatives::index::DDY(f, outloc, method, region);
+      Field3D f_parallel = f;
+      transform->calcParallelSlices(f_parallel);
+      return bout::derivatives::index::DDY(f_parallel, outloc, method, region);
     }
     Field3D result = bout::derivatives::index::DDY(f_aligned, outloc, method, region);
     return (is_unaligned ? maybeFromFieldAligned(result, region) : result );
