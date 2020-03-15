44cec86d06757a002e30ce6a6bdd48faee3e8087
David Schwörer <schword2@mail.dcu.ie>
2020-03-15 21:55:00 +0000

    use custom main

--- examples/performance/boundary/boundary.cxx
+++ examples/performance/boundary/boundary.cxx
@@ -1,24 +1,18 @@
 #include <bout/physicsmodel.hxx>
 #include <bout/sys/timer.hxx>
 
-class BoundaryOp_timing : public PhysicsModel {
-  int init(bool restarting);
-  int rhs(BoutReal UNUSED(t)) {
-    ddt(f)=0;
-    return 0; }
-  Field3D f;
-};
+int main(int argc, char **argv) {
+  BoutInitialise(argc, argv);
 
-int BoundaryOp_timing::init(bool UNUSED(restarting)) {
-  SOLVE_FOR(f);
-
+  Field3D f{0.};
+  f.setBoundary("f");
   Options* opt = Options::getRoot();
   int ntests;
   OPTION(opt, ntests, 10000);
 
 
   {
-    Timer timer("dirichlet_o3");
+    Timer timer("boundary");
     for (int i=0; i<ntests; i++) {
       f.applyBoundary();
     }
@@ -27,10 +21,9 @@ int BoundaryOp_timing::init(bool UNUSED(restarting)) {
   std::string name;
   opt->getSection("f")->get("bndry_all",name,"error");
 
-  double time = Timer::getTime("dirichlet_o3");
+  double time = Timer::getTime("boundary");
   printf("%-20s | %12.8f s", name.c_str(), time);
   
   return 0;
 }
 
-BOUTMAIN(BoundaryOp_timing);
