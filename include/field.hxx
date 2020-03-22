a7705fed2ffedb866991bbe0c50f358fb94962c7
David Schwörer <schword2@mail.dcu.ie>
2020-03-22 21:11:46 +0000

    Better error if field is not positive

--- include/field.hxx
+++ include/field.hxx
@@ -297,7 +297,7 @@ inline void checkPositive(const T& f, const std::string& name="field", const std
 
   BOUT_FOR_SERIAL(i, f.getRegion(rgn)) {
     if (f[i] <= 0.) {
-      throw BoutException("{:s} is not positive at {:s}", name, toString(i));
+      throw BoutException("{:s} ({:s} {:s}) is {:e} (not positive) at {:s}", name, toString(f.getLocation()), toString(f.getDirections()), f[i], toString(i));
     }
   }
 }
