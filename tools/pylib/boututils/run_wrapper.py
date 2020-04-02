7dcbfa22cbe2a8012bf1350d196f2e27e0fadc18
David Schwörer <schword2@mail.dcu.ie>
2020-04-02 16:09:52 +0100

    Drop pre python2.4 stuff

--- tools/pylib/boututils/run_wrapper.py
+++ tools/pylib/boututils/run_wrapper.py
@@ -5,16 +5,8 @@ import os
 import pathlib
 import re
 import subprocess
+from subprocess import call, Popen, STDOUT, PIPE
 
-try:
-    # Python 2.4 onwards
-    from subprocess import call, Popen, STDOUT, PIPE
-    lib = "call"
-except ImportError:
-    # FIXME: drop support for python < 2.4!
-    # Use os.system (depreciated)
-    from os import popen4, system
-    lib = "system"
 
 if os.name == "nt":
     # Default on Windows
@@ -60,27 +52,20 @@ def shell(command, pipe=False):
     """
     output = None
     status = 0
-    if lib == "system":
-        if pipe:
-            handle = popen4(command)
-            output = handle[1].read()
-        else:
-            status = system(command)
+    if pipe:
+        child = Popen(command, stderr=STDOUT, stdout=PIPE, shell=True)
+        # This returns a b'string' which is casted to string in
+        # python 2. However, as we want to use f.write() in our
+        # runtest, we cast this to utf-8 here
+        output = child.stdout.read().decode("utf-8", "ignore")
+        # Wait for the process to finish. Note that child.wait()
+        # would have deadlocked the system as stdout is PIPEd, we
+        # therefore use communicate, which in the end also waits for
+        # the process to finish
+        child.communicate()
+        status = child.returncode
     else:
-        if pipe:
-            child = Popen(command, stderr=STDOUT, stdout=PIPE, shell=True)
-            # This returns a b'string' which is casted to string in
-            # python 2. However, as we want to use f.write() in our
-            # runtest, we cast this to utf-8 here
-            output = child.stdout.read().decode("utf-8", "ignore")
-            # Wait for the process to finish. Note that child.wait()
-            # would have deadlocked the system as stdout is PIPEd, we
-            # therefore use communicate, which in the end also waits for
-            # the process to finish
-            child.communicate()
-            status = child.returncode
-        else:
-            status = call(command, shell=True)
+        status = call(command, shell=True)
 
     return status, output
 
