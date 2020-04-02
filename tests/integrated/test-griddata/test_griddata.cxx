82aecaf0e6f22d8b443638840605763553b4b2f8
David Schwörer <schword2@mail.dcu.ie>
2020-04-02 18:48:19 +0200

    Ensure tests requiring netcdf are skipped

--- tests/integrated/test-griddata/test_griddata.cxx
+++ tests/integrated/test-griddata/test_griddata.cxx
@@ -4,7 +4,8 @@
 int main(int argc, char** argv) {
   BoutInitialise(argc, argv);
 
-  Datafile df(Options::getRoot()->getSection("output"));
+  auto options = Options::getRoot();//options["dump_format"].
+  Datafile df(options->getSection("output"));
   df.add(const_cast<BoutReal&>(bout::version::as_double), "BOUT_VERSION", false);
 
   mesh->outputVars(df);
@@ -16,7 +17,8 @@ int main(int argc, char** argv) {
   df.add(Rxy, "Rxy");
   df.add(Bpxy, "Bpxy");
 
-  df.write("data.nc");
+  std::string ext = (*options)["dump_format"];
+  df.write("data.{:s}",ext);
   
   BoutFinalise();
   return 0;
