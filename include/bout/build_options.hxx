074ddd9d77df3933c03aef204e48e37358bea40f
Peter Hill <zed.three@gmail.com>
2020-05-04 15:25:10 +0100

    Add build_options header with compile-time features as variables

---
+++ include/bout/build_options.hxx
@@ -0,0 +1,33 @@
+#ifndef BOUT_BUILD_OPTIONS_HXX
+#define BOUT_BUILD_OPTIONS_HXX
+
+#include "bout/build_config.hxx"
+
+namespace bout {
+namespace build {
+constexpr auto check_level = BOUT_CHECK_LEVEL;
+constexpr auto openmp_schedule = BOUT_OPENMP_SCHEDULE;
+
+constexpr auto has_fftw = static_cast<bool>(BOUT_HAS_FFTW);
+constexpr auto has_gettext = static_cast<bool>(BOUT_HAS_GETTEXT);
+constexpr auto has_hdf5 = static_cast<bool>(BOUT_HAS_HDF5);
+constexpr auto has_lapack = static_cast<bool>(BOUT_HAS_LAPACK);
+constexpr auto has_netcdf = static_cast<bool>(BOUT_HAS_NETCDF);
+constexpr auto has_petsc = static_cast<bool>(BOUT_HAS_PETSC);
+constexpr auto has_pretty_function = static_cast<bool>(BOUT_HAS_PRETTY_FUNCTION);
+constexpr auto has_pvode = static_cast<bool>(BOUT_HAS_PVODE);
+constexpr auto has_scorep = static_cast<bool>(BOUT_HAS_SCOREP);
+constexpr auto has_slepc = static_cast<bool>(BOUT_HAS_SLEPC);
+constexpr auto has_sundials = static_cast<bool>(BOUT_HAS_SUNDIALS);
+constexpr auto use_backtrace = static_cast<bool>(BOUT_USE_BACKTRACE);
+constexpr auto use_color = static_cast<bool>(BOUT_USE_COLOR);
+constexpr auto use_openmp = static_cast<bool>(BOUT_USE_OPENMP);
+constexpr auto use_output_debug = static_cast<bool>(BOUT_USE_OUTPUT_DEBUG);
+constexpr auto use_sigfpe = static_cast<bool>(BOUT_USE_SIGFPE);
+constexpr auto use_signal = static_cast<bool>(BOUT_USE_SIGNAL);
+constexpr auto use_track = static_cast<bool>(BOUT_USE_TRACK);
+
+} // namespace build
+} // namespace bout
+
+#endif // BOUT_BUILD_OPTIONS_HXX
