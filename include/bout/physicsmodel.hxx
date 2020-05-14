d2cceb1bb698cddc5f95d12c7a6794e8480df111
Peter Hill <zed.three@gmail.com>
2020-05-14 14:45:38 +0100

    Add public `mesh`, `dump` members to PhysicsModel
    Initialise from global mesh, dump file for now

--- include/bout/physicsmodel.hxx
+++ include/bout/physicsmodel.hxx
@@ -45,6 +45,8 @@ class PhysicsModel;
 #include "utils.hxx"
 #include "bout/macro_for_each.hxx"
 
+class Mesh;
+
 /*!
   Base class for physics models
  */
@@ -57,6 +59,9 @@ public:
   
   virtual ~PhysicsModel() = default;
   
+  Mesh* mesh{nullptr};
+  Datafile& dump;
+
   /*!
    * Initialse the model, calling the init() and postInit() methods
    *
@@ -270,9 +275,6 @@ protected:
       // Call user output monitor
       return model->outputMonitor(simtime, iter, nout);
     }
-
-    Mesh * mesh = bout::globals::mesh;
-    Datafile & dump = bout::globals::dump;
 
   private:
     PhysicsModel *model;
