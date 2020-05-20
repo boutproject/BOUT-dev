a43cd455c349109e6d92f5836ebd381faf40aa99
David Schwörer <schword2@mail.dcu.ie>
2020-05-20 12:36:29 +0100

    Apply DC to test-laplacexy-short

--- tests/integrated/test-laplacexy-short/test-laplacexy.cxx
+++ tests/integrated/test-laplacexy-short/test-laplacexy.cxx
@@ -62,9 +62,9 @@ int main(int argc, char** argv) {
 
   Field2D rhs, rhs_check;
   if (include_y_derivs) {
-    rhs = a*Laplace_perp(f) + Grad_perp(a)*Grad_perp(f) + b*f;
+    rhs = a*DC(Laplace_perp(f)) + DC(Grad_perp(a)*Grad_perp(f)) + b*f;
   } else {
-    rhs = a*Delp2(f, CELL_DEFAULT, false) + coords->g11*DDX(a)*DDX(f) + b*f;
+    rhs = a*DC(Delp2(f, CELL_DEFAULT, false)) + DC(coords->g11*DDX(a)*DDX(f)) + b*f;
   }
 
   laplacexy.setCoefs(a, b);
@@ -78,9 +78,9 @@ int main(int argc, char** argv) {
 
   mesh->communicate(sol);
   if (include_y_derivs) {
-    rhs_check = a*Laplace_perp(sol) + Grad_perp(a)*Grad_perp(sol) + b*sol;
+    rhs_check = a*DC(Laplace_perp(sol)) + DC(Grad_perp(a)*Grad_perp(sol)) + b*sol;
   } else {
-    rhs_check = a*Delp2(sol, CELL_DEFAULT, false) + coords->g11*DDX(a)*DDX(sol) + b*sol;
+    rhs_check = a*DC(Delp2(sol, CELL_DEFAULT, false)) + DC(coords->g11*DDX(a)*DDX(sol)) + b*sol;
   }
 
   dump.add(a, "a");
