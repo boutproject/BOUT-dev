c3556757bdcad1ace1274b71ed1b249c47cfdeb8
Peter Hill <zed.three@gmail.com>
2020-06-12 15:35:33 +0100

    Manually cleanup backtrace example

--- examples/backtrace/backtrace.cxx
+++ examples/backtrace/backtrace.cxx
@@ -1,45 +1,32 @@
 /*
- * Check for backtrace after throw 
+ * Check for backtrace after throw
  */
 
-#include <bout.hxx>
 #include <bout/physicsmodel.hxx>
+#include <bout.hxx>
 
-class Backtrace : public PhysicsModel {
-protected:
-  int init(bool UNUSED(restarting)) override;
-  int rhs(BoutReal UNUSED(time)) override;
-};
-
-
-
-void f1(){
-  BoutReal a=1;
-  BoutReal b=0;
-  BoutReal c = a/b;
-  output.write("c is {:f}\n",c);
+void f1() {
+  BoutReal a = 1;
+  BoutReal b = 0;
+  BoutReal c = a / b;
+  output.write("c is {:f}\n", c);
   throw BoutException("Tomatoes are red?\n");
 }
-void f2(int UNUSED(a)) {
-  f1();
-}
+void f2(int UNUSED(a)) { f1(); }
 
-int f3(){
+int f3() {
   f2(1);
   return 0;
 }
 
-int Backtrace::init(bool UNUSED(restarting)) {
-  f3();
-  
-  return 1;
-}
+class Backtrace : public PhysicsModel {
+protected:
+  int init(bool UNUSED(restarting)) override {
+    f3();
 
-int Backtrace::rhs(BoutReal UNUSED(time)) {
-  
-  
-  return 1;
-}
-
+    return 1;
+  }
+  int rhs(BoutReal UNUSED(time)) override { return 1; }
+};
 
 BOUTMAIN(Backtrace)
