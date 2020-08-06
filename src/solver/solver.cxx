09b5fa2522850da559fa03d922e7944c21c5c3d3
John Omotani <john.omotani@ukaea.uk>
2020-08-06 18:20:12 +0100

    Optional arguments to give descriptions for output variables in Solver
    Allows output dump files to be more self-documenting.

--- src/solver/solver.cxx
+++ src/solver/solver.cxx
@@ -101,7 +101,7 @@ void Solver::setModel(PhysicsModel *m) {
  * Add fields
  **************************************************************************/
 
-void Solver::add(Field2D& v, const std::string& name) {
+void Solver::add(Field2D& v, const std::string& name, const std::string& description) {
   TRACE("Adding 2D field: Solver::add({:s})", name);
 
 #if CHECK > 0
@@ -122,6 +122,7 @@ void Solver::add(Field2D& v, const std::string& name) {
   d.F_var = &ddt(v);
   d.location = v.getLocation();
   d.name = name;
+  d.description = description;
 
 #if BOUT_USE_TRACK
   v.name = name;
@@ -158,7 +159,7 @@ void Solver::add(Field2D& v, const std::string& name) {
   f2d.emplace_back(std::move(d));
 }
 
-void Solver::add(Field3D& v, const std::string& name) {
+void Solver::add(Field3D& v, const std::string& name, const std::string& description) {
   TRACE("Adding 3D field: Solver::add({:s})", name);
 
   Mesh* mesh = v.getMesh();
@@ -186,6 +187,7 @@ void Solver::add(Field3D& v, const std::string& name) {
   d.F_var = &ddt(v);
   d.location = v.getLocation();
   d.name = name;
+  d.description = description;
 
 #if BOUT_USE_TRACK
   v.name = name;
@@ -217,7 +219,7 @@ void Solver::add(Field3D& v, const std::string& name) {
   f3d.emplace_back(std::move(d));
 }
 
-void Solver::add(Vector2D& v, const std::string& name) {
+void Solver::add(Vector2D& v, const std::string& name, const std::string& description) {
   TRACE("Adding 2D vector: Solver::add({:s})", name);
 
   if (varAdded(name))
@@ -236,6 +238,7 @@ void Solver::add(Vector2D& v, const std::string& name) {
   d.F_var = &ddt(v);
   d.covariant = v.covariant;
   d.name = name;
+  d.description = description;
 
   /// NOTE: No initial_profile call, because this will be done for each
   ///       component individually.
@@ -256,7 +259,7 @@ void Solver::add(Vector2D& v, const std::string& name) {
   v2d.emplace_back(std::move(d));
 }
 
-void Solver::add(Vector3D& v, const std::string& name) {
+void Solver::add(Vector3D& v, const std::string& name, const std::string& description) {
   TRACE("Adding 3D vector: Solver::add({:s})", name);
 
   if (varAdded(name))
@@ -275,6 +278,7 @@ void Solver::add(Vector3D& v, const std::string& name) {
   d.F_var = &ddt(v);
   d.covariant = v.covariant;
   d.name = name;
+  d.description = description;
 
   // Add suffix, depending on co- /contravariance
   if (v.covariant) {
@@ -562,10 +566,16 @@ void Solver::outputVars(Datafile &outputfile, bool save_repeat) {
   for(const auto& f : f2d) {
     // Add to dump file (appending)
     outputfile.add(*(f.var), f.name.c_str(), save_repeat);
+    if (not f.description.empty()) {
+      outputfile.setAttribute(f.name, "description", f.description);
+    }
   }  
   for(const auto& f : f3d) {
     // Add to dump file (appending)
     outputfile.add(*(f.var), f.name.c_str(), save_repeat);
+    if (not f.description.empty()) {
+      outputfile.setAttribute(f.name, "description", f.description);
+    }
     
     if(mms) {
       // Add an error variable
@@ -577,10 +587,16 @@ void Solver::outputVars(Datafile &outputfile, bool save_repeat) {
   for (const auto &d : diagnostic_int) {
     // Add to dump file (appending)
     outputfile.add(*(d.var), d.name.c_str(), save_repeat);
+    if (not d.description.empty()) {
+      outputfile.setAttribute(d.name, "description", d.description);
+    }
   }
   for (const auto &d : diagnostic_BoutReal) {
     // Add to dump file (appending)
     outputfile.add(*(d.var), d.name.c_str(), save_repeat);
+    if (not d.description.empty()) {
+      outputfile.setAttribute(d.name, "description", d.description);
+    }
   }
 }
 
