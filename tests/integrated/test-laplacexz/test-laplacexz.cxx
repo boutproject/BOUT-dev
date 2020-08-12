9291838e3f4f5ac9bf907c0e03d08faab9d3fb6a
BS <brendan.shanahan@ipp.mpg.de>
2020-08-12 14:43:38 +0200

    Update test-laplacexz * correctly import build_and_log in runtest * add tolerances, hyper preconditioner to input * fix calls to global mesh in model

--- tests/integrated/test-laplacexz/test-laplacexz.cxx
+++ tests/integrated/test-laplacexz/test-laplacexz.cxx
@@ -17,13 +17,13 @@
 int main(int argc, char** argv) {
   BoutInitialise(argc, argv);
 
-  auto inv = LaplaceXZ::create(mesh);
+  auto inv = LaplaceXZ::create(bout::globals::mesh);
 
   auto coord = bout::globals::mesh->getCoordinates();
   coord->g13 = 1.8; // test off-diagonal components with nonzero value
 
   // create some input field
-  Field3D f = FieldFactory::get()->create3D("f", Options::getRoot(), mesh);
+  Field3D f = FieldFactory::get()->create3D("f", Options::getRoot(), bout::globals::mesh);
 
   // Calculate the Laplacian with non-zero g13
   Field3D g = coord->g11 * D2DX2(f) + coord->g13 * D2DXDZ(f) + coord->g33 * D2DZ2(f);
@@ -43,7 +43,7 @@ int main(int argc, char** argv) {
 
   output.write("First solve\n");
 
-  Field3D rhs = FieldFactory::get()->create3D("rhs", Options::getRoot(), mesh);
+  Field3D rhs = FieldFactory::get()->create3D("rhs", Options::getRoot(), bout::globals::mesh);
   Field3D x = inv->solve(rhs, 0.0);
 
   SAVE_ONCE2(rhs, x);
@@ -52,11 +52,11 @@ int main(int argc, char** argv) {
 
   inv->setCoefs(Field3D(2.0),Field3D(0.1));
 
-  Field3D rhs2 = FieldFactory::get()->create3D("rhs", Options::getRoot(), mesh);
+  Field3D rhs2 = FieldFactory::get()->create3D("rhs", Options::getRoot(), bout::globals::mesh);
   Field3D x2 = inv->solve(rhs2, 0.0);
   SAVE_ONCE2(rhs2, x2);
 
-  dump.write();
+  bout::globals::dump.write();
 
   BoutFinalise();
   return 0;
