6706f7882325a216df02dc64df25196d70253c85
Peter Hill <zed.three@gmail.com>
2021-03-23 09:43:38 +0000

    Avoid warning about null `this` in GCC11

--- src/mesh/impls/bout/boutmesh.cxx
+++ src/mesh/impls/bout/boutmesh.cxx
@@ -2342,8 +2342,8 @@ int BoutMesh::pack_data(const std::vector<FieldData *> &var_list, int xge, int x
   for (const auto &var : var_list) {
     if (var->is3D()) {
       // 3D variable
-      ASSERT2(dynamic_cast<Field3D*>(var)->isAllocated());
       auto& var3d_ref = *dynamic_cast<Field3D*>(var);
+      ASSERT2(var3d_ref.isAllocated());
       for (int jx = xge; jx != xlt; jx++) {
         for (int jy = yge; jy < ylt; jy++) {
           for (int jz = 0; jz < LocalNz; jz++, len++) {
@@ -2353,8 +2353,8 @@ int BoutMesh::pack_data(const std::vector<FieldData *> &var_list, int xge, int x
       }
     } else {
       // 2D variable
-      ASSERT2(dynamic_cast<Field2D*>(var)->isAllocated());
       auto& var2d_ref = *dynamic_cast<Field2D*>(var);
+      ASSERT2(var2d_ref.isAllocated());
       for (int jx = xge; jx != xlt; jx++) {
         for (int jy = yge; jy < ylt; jy++, len++) {
           buffer[len] = var2d_ref(jx, jy);
