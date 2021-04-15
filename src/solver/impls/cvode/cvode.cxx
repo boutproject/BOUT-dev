9986ba34f06baedebd75031f67fc0f5ec9cea2df
Peter Hill <zed.three@gmail.com>
2021-04-15 13:49:24 +0100

    Fix some time Solvers not using user preconditioner/Jacobian
    If preconditioner or Jacobian was set using
    `PhysicsModel::setPrecon/setJacobian` then CVODE, ARKODE and PETSc
    `Solver`s would not use them

--- src/solver/impls/cvode/cvode.cxx
+++ src/solver/impls/cvode/cvode.cxx
@@ -359,7 +359,7 @@ int CvodeSolver::init(int nout, BoutReal tstep) {
 
     /// Set Jacobian-vector multiplication function
     const auto use_jacobian = (*options)["use_jacobian"].withDefault(false);
-    if ((use_jacobian) && (jacfunc != nullptr)) {
+    if (use_jacobian and hasUserJacobian()) {
       output_info.write("\tUsing user-supplied Jacobian function\n");
 
       if (CVSpilsSetJacTimes(cvode_mem, nullptr, cvode_jac) != CV_SUCCESS)
@@ -577,7 +577,7 @@ void CvodeSolver::pre(BoutReal t, BoutReal gamma, BoutReal delta, BoutReal* udat
 void CvodeSolver::jac(BoutReal t, BoutReal* ydata, BoutReal* vdata, BoutReal* Jvdata) {
   TRACE("Running Jacobian: CvodeSolver::jac({})", t);
 
-  if (jacfunc == nullptr)
+  if (not hasUserJacobian())
     throw BoutException("No jacobian function supplied!\n");
 
   // Load state from ydate
@@ -587,7 +587,7 @@ void CvodeSolver::jac(BoutReal t, BoutReal* ydata, BoutReal* vdata, BoutReal* Jv
   load_derivs(vdata);
 
   // Call function
-  (*jacfunc)(t);
+  runJacobian(t);
 
   // Save Jv from vars
   save_derivs(Jvdata);
