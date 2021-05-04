413da69b81edf079a2698672205c4bfe57652eb9
David Schwörer <dave@ipp.mpg.de>
2021-05-04 15:11:16 +0200

    Add region ypar

--- src/mesh/parallel/fci.cxx
+++ src/mesh/parallel/fci.cxx
@@ -239,6 +239,16 @@ FCIMap::FCIMap(Mesh& mesh, const Field2D& dy, Options& options, int offset_,
   }
 
   interp->setMask(boundary_mask);
+
+  const auto region = fmt::format("RGN_YPAR_{:+d}", offset);
+  if (not map_mesh.hasRegion3D(region)) {
+    // The valid region for this slice
+    map_mesh.addRegion3D(region,
+			 Region<Ind3D>(map_mesh.xstart, map_mesh.xend,
+				       map_mesh.ystart+offset, map_mesh.yend+offset,
+				       0, map_mesh.LocalNz-1,
+				       map_mesh.LocalNy, map_mesh.LocalNz));
+  }
 }
 
 Field3D FCIMap::integrate(Field3D &f) const {
