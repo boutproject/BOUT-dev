8cd7817ad2ad0db39cea32ff5418a0b0f6eddfcf
Peter Hill <zed.three@gmail.com>
2021-05-10 15:55:32 +0100

    Docs: Fix use of deprecated/removed Sphinx method
    [skip ci]

--- manual/sphinx/conf.py
+++ manual/sphinx/conf.py
@@ -193,7 +193,7 @@ html_static_path = ['_static']
 
 
 def setup(app):
-    app.add_stylesheet('css/custom.css')
+    app.add_css_file('css/custom.css')
 
 
 # -- Options for HTMLHelp output ------------------------------------------
