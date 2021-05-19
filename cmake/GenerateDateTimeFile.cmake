7efcbe3ff261fa87c5905f59282f150ed78358dd
Peter Hill <zed.three@gmail.com>
2021-05-19 14:11:04 +0100

    CMake: Use __DATE__ and __TIME__ instead of CMake timestamp

--- cmake/GenerateDateTimeFile.cmake
+++ cmake/GenerateDateTimeFile.cmake
@@ -1,10 +1,7 @@
 # Creates "bout++-time.cxx" in the build directory with the
 # compilation date and time as variables
 
-string(TIMESTAMP bout_date "%b %d %Y")
-string(TIMESTAMP bout_time "%H:%M:%S")
-
 set(bout_date_time_file
-  "const char* boutcompiledate{\"${bout_date}\"}; const char* boutcompiletime{\"${bout_time}\"};")
+  "const char* boutcompiledate{__DATE__}; const char* boutcompiletime{__TIME__};")
 
 file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/bout++-time.cxx" "${bout_date_time_file}")
