7cf51665b6a8baae3a2103ca0a39fa0dd5f7b983
David Schwörer <dave@ipp.mpg.de>
2021-06-04 18:08:26 +0200

    Further improve test-invpar
    Thanks @ZedThree for the suggestions.
    
    Now the test results are printed from the C++ code.

--- tests/integrated/test-invpar/test_invpar.cxx
+++ tests/integrated/test-invpar/test_invpar.cxx
@@ -11,9 +11,12 @@
 
 using bout::globals::mesh;
 
-int test(std::string acoef, std::string bcoef, std::string ccoef, std::string dcoef,
-         std::string ecoef, std::string func, BoutReal tol, FieldFactory& f,
-         CELL_LOC location) {
+int test(const std::string& acoef, const std::string& bcoef, const std::string& ccoef,
+         const std::string& dcoef, const std::string& ecoef, const std::string& func,
+         BoutReal tol, FieldFactory& f, CELL_LOC location) {
+  output_error.write(
+      "acoef={:s} bcoef={:s} ccoef={:s} dcoef={:s} ecoef{:s} with {:s} at {:s} ...",
+      acoef, bcoef, ccoef, dcoef, ecoef, func, toString(location));
   auto inv = InvertPar::create(nullptr, location, mesh);
 
   Field2D A = f.create2D(acoef, nullptr, nullptr, location);
@@ -36,7 +39,7 @@ int test(std::string acoef, std::string bcoef, std::string ccoef, std::string dc
 	  + D*D2DZ2(result) + E*DDY(result);
 
   // Check the result
-  int exit = 0;
+  bool success{true};
   int local_ystart = mesh->ystart;
   if (location == CELL_YLOW) {
     // Point at mesh->ystart in 'result' is set by the Neumann boundary condition, so may
@@ -49,19 +52,16 @@ int test(std::string acoef, std::string bcoef, std::string ccoef, std::string dc
                    input(mesh->xstart, y, z), result(mesh->xstart, y, z),
                    deriv(mesh->xstart, y, z));
       if (std::abs(input(mesh->xstart, y, z) - deriv(mesh->xstart, y, z)) > tol) {
-        exit = 1;
+        success = false;
       }
     }
   }
-  return exit;
-}
-
-std::string get(Options& options, const char* name, int i, const std::string& def) {
-  char* fname = new char[64];
-  snprintf(fname, 63, "%s_%d", name, i);
-  std::string val;
-  options.get(fname, val, def);
-  return val;
+  if (success) {
+    output_error.write(" success\n");
+  } else {
+    output_error.write(" failure\n");
+  }
+  return success;
 }
 
 int main(int argc, char** argv) {
@@ -73,41 +73,44 @@ int main(int argc, char** argv) {
 
   // Get options
   Options& options = Options::root();
-  std::string acoef, bcoef, ccoef, dcoef, ecoef, func;
 
   BoutReal tol = options["tol"].withDefault(1e-10);
 
-  int exit = 0;
+  bool success{true};
   int i = 0;
   while (true) {
-    acoef = get(options, "acoef", i, "not_set");
-    if (acoef == "not_set") {
+    if (not options.isSet(fmt::format("{}_{}", "acoef", i))) {
       break;
     }
-    bcoef = get(options, "bcoef", i, "-1.0");
-    ccoef = get(options, "ccoef", i, "0.0");
-    dcoef = get(options, "dcoef", i, "0.0");
-    ecoef = get(options, "ecoef", i, "0.0");
-    func = get(options, "input_field", i, "sin(2*y)*(1. + 0.2*exp(cos(z)))");
+    const std::string acoef = options[fmt::format("{}_{}", "acoef", i)].as<std::string>();
+    const std::string bcoef =
+        options[fmt::format("{}_{}", "bcoef", i)].withDefault("-1.0");
+    const std::string ccoef =
+        options[fmt::format("{}_{}", "ccoef", i)].withDefault("0.0");
+    const std::string dcoef =
+        options[fmt::format("{}_{}", "dcoef", i)].withDefault("0.0");
+    const std::string ecoef =
+        options[fmt::format("{}_{}", "ecoef", i)].withDefault("0.0");
+    const std::string func = options[fmt::format("{}_{}", "input_field", i)].withDefault(
+        "sin(2*y)*(1. + 0.2*exp(cos(z)))");
 
     for (auto location : {CELL_CENTRE, CELL_YLOW, CELL_XLOW, CELL_ZLOW}) {
-      exit += test(acoef, bcoef, ccoef, dcoef, ecoef, func, tol, f, location);
+      success &= test(acoef, bcoef, ccoef, dcoef, ecoef, func, tol, f, location);
     }
     i += 1;
   }
-  int allexit;
-  MPI_Allreduce(&exit, &allexit, 1, MPI_INT, MPI_MAX, BoutComm::get());
+  bool allsuccess{true};
+  MPI_Allreduce(&success, &allsuccess, 1, MPI_C_BOOL, MPI_LAND, BoutComm::get());
 
   output << "******* Parallel inversion test case: ";
-  if (allexit == 0) {
+  if (allsuccess) {
     output << "PASSED" << endl;
   } else {
     output << "FAILED" << endl;
-    allexit = 1;
   }
 
   MPI_Barrier(BoutComm::get());
 
   BoutFinalise();
-  return allexit;
+  return !allsuccess;
 }
