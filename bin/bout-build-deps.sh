1dc7bf00f9bb92079a38f5efa684ce092af5636a
David Schwörer <dave@ipp.mpg.de>
2021-06-11 13:59:22 +0200

    Add help

--- bin/bout-build-deps.sh
+++ bin/bout-build-deps.sh
@@ -1,22 +1,54 @@
 #!/usr/bin/env bash
 
-test $PREFIX || PREFIX=$HOME/.local/bout-deps
-test $BUILD || BUILD=$(pwd)/bout-deps
-test $BOUT_TOP || BOUT_TOP=$(pwd)
-test $MKFLAGS || MKFLAGS="-j 16"
+PREFIX=${PREFIX:-$HOME/.local/bout-deps}
+BUILD=${BUILD:-$HOME/soft/bout-deps}
+MKFLAGS=${MKFLAGS:-"-j 16"}
+BOUT_TOP=${BOUT_TOP:-$(pwd)}
 
-HDF5VER=1.12.0
-NCVER=4.7.4
-NCCXXVER=4.3.1
-FFTWVER=3.3.9
-SUNVER=5.7.0
-PETSCVER=3.15.0
+HDF5VER=${HDF5VER:-1.12.0}
+NCVER=${NCVER:-4.7.4}
+NCCXXVER=${NCCXXVER:-4.3.1}
+FFTWVER=${FFTWVER:-3.3.9}
+SUNVER=${SUNVER:-5.7.0}
+PETSCVER=${PETSCVER:-3.15.0}
 
+
+HDF5FLAGS=${HDF5FLAGS:-}
+NCFLAGS=${NCFLAGS:-}
+NCCXXFLAGS=${NCCXXFLAGS:-}
+FFTWFLAGS=${FFTWFLAGS:---enable-avx512 --enable-avx-128-fma}
+SUNFLAGS=${SUNFLAGS:-}
+PETSCFLAGS=${PETSCFLAGS:-}
+
+
+help() {
+echo The following options are available:
+echo
+grep '=${.*:-' $0 | grep -v grep| sed -E "s/.*\\\$\{([A-Z0-9a-z_]*):-(.*)\}/Option \$\1 with default '\2'/"
+cat <<EOF
+
+Set any of the options by setting the respective environmente
+variables, e.g.
+
+   FFTWVER=3.3.8 $0
+
+if you want to set FFTW to an older version.
+
+For more options edit the script or use as a starting point for your
+own scripts.  Especially feel free to comment some sections towards
+the end of the file, to disable building some parts.
+
+EOF
+
+# Exit of 0 if -h or --help - else 1
+test ".$1" = .-h || test ".$1" = .--help
+exit
+}
 
 setup() {
     mkdir -p $PREFIX
     mkdir -p $PREFIX/lib
-	mkdir -p $PREFIX/bin
+    mkdir -p $PREFIX/bin
     if test -e $PREFIX/lib64 ; then
         if ! test -L $PREFIX/lib64 ; then
             echo "$PREFIX/lib64 exists and is not a symlink to lib - aborting"
@@ -33,7 +65,7 @@ hdf5() {
     wget -c https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-${HDF5VER}/src/hdf5-${HDF5VER}.tar.bz2 || :
     tar -xvf hdf5-$HDF5VER.tar.bz2
     cd hdf5-${HDF5VER}
-    ./configure --prefix $PREFIX --enable-build-mode=production
+    ./configure --prefix $PREFIX --enable-build-mode=production $HDF5FLAGS
     make $MKFLAGS
     make install
 }
@@ -43,7 +75,7 @@ netcdf() {
     wget -c https://github.com/Unidata/netcdf-c/archive/v$NCVER/netcdf-$NCVER.tar.gz || :
     tar -xf netcdf-$NCVER.tar.gz
     cd netcdf-c-$NCVER
-    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib/" ./configure --prefix=$PREFIX
+    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib/" ./configure --prefix=$PREFIX $NCFLAGS
     make $MKFLAGS
     make install
 }
@@ -53,7 +85,7 @@ nccxx() {
     wget -c ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-cxx4-$NCCXXVER.tar.gz || :
     tar -xf netcdf-cxx4-$NCCXXVER.tar.gz
     cd netcdf-cxx4-$NCCXXVER
-    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib/" ./configure --prefix=$PREFIX
+    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib/" ./configure --prefix=$PREFIX $NCCXXFLAGS
     make $MKFLAGS
     make install
 }
@@ -63,7 +95,7 @@ fftw() {
     wget -c http://www.fftw.org/fftw-$FFTWVER.tar.gz || :
     tar -xf fftw-$FFTWVER.tar.gz
     cd fftw-$FFTWVER
-    ./configure --prefix $PREFIX --enable-shared --enable-sse2 --enable-avx --enable-avx2 --enable-avx512 --enable-avx-128-fma
+    ./configure --prefix $PREFIX --enable-shared --enable-sse2 --enable-avx --enable-avx2  $FFTWFLAGS
     make $MKFLAGS
     make install
 }
@@ -75,7 +107,7 @@ sundials() {
     cd sundials-$SUNVER
     mkdir -p build
     cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DMPI_ENABLE=ON ..
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DMPI_ENABLE=ON .. $SUNFLAGS
     make $MKFLAGS
     make install
 }
@@ -87,15 +119,15 @@ error () {
 }
 
 petsc() {
-    test $PETSC_DIR || error "\$PETSC_DIR is set - please unset"
-    test $PETSC_ARCH || error "\$PETSC_ARCH is set - please unset"
+    test -z $PETSC_DIR || error "\$PETSC_DIR is set ($PETSC_DIR) - please unset"
+    test -z $PETSC_ARCH || error "\$PETSC_ARCH is set ($PETSC_ARCH) - please unset"
     cd $BUILD
     wget -c https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-$PETSCVER.tar.gz || :
     tar -xf petsc-$PETSCVER.tar.gz
     cd petsc-$PETSCVER
     unset PETSC_DIR
     ./configure COPTFLAGS="-O3" CXXOPTFLAGS="-O3" FOPTFLAGS="-O3" --with-batch --known-mpi-shared-libraries=1 --with-mpi-dir=$OPENMPI_HOME --download-fblaslapack \
-        --known-64-bit-blas-indices=0 --download-hypre --with-debugging=0 --prefix=$PREFIX
+        --known-64-bit-blas-indices=0 --download-hypre --with-debugging=0 --prefix=$PREFIX $PETSCFLAGS
     make $MKFLAGS
     make install
 }
@@ -132,6 +164,7 @@ prepend-path  LD_LIBRARY_PATH  $PREFIX/lib
 
 # Uncomment this if want to use it as a script to detect errors
 set -ex
+test ".$1" = . || help $@
 
 setup
 hdf5
