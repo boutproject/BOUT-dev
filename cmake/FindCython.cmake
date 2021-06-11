4f4af96fe5540032d6f723e15d19ecb1d67ee9c5
Peter Hill <zed.three@gmail.com>
2021-06-11 15:02:22 +0100

    CMake: Use FindPackageHandleStandardArgs in FindCython, FindNumpy

--- cmake/FindCython.cmake
+++ cmake/FindCython.cmake
@@ -11,12 +11,19 @@
 #   CYTHON_VERSION - Cython version
 
 execute_process(COMMAND ${Python_EXECUTABLE} -c "import cython ; print(cython.__version__)"
-  RESULT_VARIABLE CYTHON_FOUND
+  RESULT_VARIABLE _cython_runs
   OUTPUT_VARIABLE CYTHON_VERSION
+  OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
-if (CYTHON_FOUND EQUAL 0)
-  set(CYTHON_FOUND ON)
+if (${_cython_runs} EQUAL 0)
+  set(CYTHON_RUNS TRUE)
 else()
-  set(CYTHON_FOUND OFF)
+  set(CYTHON_RUNS FALSE)
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Cython
+  VERSION_VAR CYTHON_VERSION
+  REQUIRED_VARS CYTHON_RUNS
+  )
--- cmake/FindNumpy.cmake
+++ cmake/FindNumpy.cmake
@@ -7,24 +7,44 @@
 #
 # ::
 #
-#   NUMPY_FOUND - true if FFTW was found
-#   NUMPY_VERSION - Location of the FFTW includes
-#   NUMPY_HEADER - Required libraries
+#   Numpy_FOUND - true if FFTW was found
+#   Numpy_VERSION - Location of the FFTW includes
+#   Numpy_INCLUDE_DIR - Required libraries
 
 
 find_package(Python 3.6 REQUIRED COMPONENTS Interpreter Development)
 
-execute_process(COMMAND ${Python_EXECUTABLE} -c "import numpy ; print(numpy.__version__)"
-  RESULT_VARIABLE NUMPY_FOUND
-  OUTPUT_VARIABLE NUMPY_VERSION
-  )
-execute_process(COMMAND ${Python_EXECUTABLE} -c "import numpy ; print(numpy.get_include())"
-  RESULT_VARIABLE NUMPY_FOUND2
-  OUTPUT_VARIABLE NUMPY_HEADER
+if (NOT Numpy_FOUND)
+  execute_process(COMMAND ${Python_EXECUTABLE} -c "import numpy ; print(numpy.__version__)"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE Numpy_VERSION
+    )
+  execute_process(COMMAND ${Python_EXECUTABLE} -c "import numpy ; print(numpy.get_include())"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE _numpy_include_dirs
+    )
+endif()
+
+if (Numpy_DEBUG)
+  message(STATUS "Looking for numpy headers in: ${_numpy_include_dirs} ${PYTHON_INCLUDE_DIR}")
+endif()
+
+find_path(Numpy_INCLUDE_DIR
+  numpy/arrayobject.h
+  PATHS "${_numpy_include_dirs}" "${PYTHON_INCLUDE_DIR}"
+  PATH_SUFFIXES numpy/core/include
   )
 
-if (NUMPY_FOUND EQUAL 0 AND NUMPY_FOUND2 EQUAL 0)
-  set(NUMPY_FOUND ON)
-else()
-  set(NUMPY_FOUND OFF)
+if (NOT Numpy_INCLUDE_DIR)
+  message(STATUS "Numpy headers not found -- do you need to install the development package?")
 endif()
+
+set(Numpy_INCLUDE_DIRS ${Numpy_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Numpy
+  VERSION_VAR Numpy_VERSION
+  REQUIRED_VARS Numpy_INCLUDE_DIR
+  )
+
+mark_as_advanced(Numpy_INCLUDE_DIR)
