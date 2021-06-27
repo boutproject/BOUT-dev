3f37bcbb9b4110770e96bf7b76282b25c4cd6b26
David Schwörer <dave@ipp.mpg.de>
2021-06-27 21:31:13 +0200

    further speed up test-delp2
    The test essentially just checks that running on several cores gives the
    same result as on one.
    We can achieve a similar result by just checking some random functions,
    without having to use a time solver.
    That makes things a bit faster.

--- tests/integrated/test-delp2/test_delp2.py
+++ tests/integrated/test-delp2/test_delp2.py
@@ -3,32 +3,19 @@ import sys
 from boutdata.collect import collect
 import numpy as np
 
-bc.init(sys.argv[2:])
-
-
-class MyModel(bc.PhysicsModel):
-    def __init__(self, useFFT, mesh):
-        self.useFFT = useFFT
-        self.mesh = mesh
-        bc.PhysicsModel.__init__(self)
-
-    def init(self, restart):
-        opt = bc.Options("diffusion")
-        self.D = opt.get("D", 0.1)
-        self.n = bc.create3D("n:function", msh=self.mesh)
-        self.solve_for(n=self.n)
-
-    def rhs(self, time):
-        self.mesh.communicate(self.n)
-
-        self.n.ddt(self.D * bc.Delp2(self.n, "CELL_DEFAULT", self.useFFT))
+bc.init(sys.argv[1:])
 
 
 opt = bc.Options()
 i = 0
 for mesh in [bc.Mesh.getGlobal(), bc.Mesh(section="mesh2")]:
+    infuncs = [bc.create3D(f"n{k}:function", mesh=mesh) for k in range(4)]
+    dump = bc.Datafile.new(opt, mesh, asGlobal=True)
+    dump.openw(f"run{i}.dmp.nc")
+    i += 1
+    out = bc.create3D("0", mesh=mesh)
+    dump.add(save_repeat=True, n=out)
     for useFFT in [True, False]:
-        dump = bc.Datafile.new(opt, mesh, asGlobal=True)
-        dump.openw(f"run{i}.dmp.nc")
-        MyModel(useFFT, mesh).solve()
-        i += 1
+        for f in infuncs:
+            out.set(bc.Delp2(f, "CELL_DEFAULT", useFFT).get())
+            dump.write()
