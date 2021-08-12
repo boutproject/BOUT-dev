a31ea4ccc66ef4a30f51157446fb1f7b3656c2cc
Ben Dudson <bd512@york.ac.uk>
2021-08-12 06:34:46 +0100

    Clang format haswgawa-wakatani-3d
    Compiles with 2D metrics and no RAJA (CPU only).

--- examples/hasegawa-wakatani-3d/hw.cxx
+++ examples/hasegawa-wakatani-3d/hw.cxx
@@ -7,23 +7,14 @@
 /////  Profiling markers and ranges are set if USE_NVTX is defined
 /////  Based on Ben Dudson, Steven Glenn code, Yining Qin update 0521-2020
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
-/// 3D simulations of HW
-/////
-///// This version uses indexed operators
-///// which reduce the number of loops over the domain
-/////
-/////  Profiling markers and ranges are set if USE_NVTX is defined
-/////  Baesed on Ben Duddson, Steven Glenn code, Yining Qin update 0521-2020
-
-
-#include <bout/single_index_ops.hxx>
 #include <bout/physicsmodel.hxx>
-#include <smoothing.hxx>
-#include <invert_laplace.hxx>
+#include <bout/single_index_ops.hxx>
 #include <derivs.hxx>
+#include <invert_laplace.hxx>
+#include <smoothing.hxx>
 
 #ifdef BOUT_HAS_RAJA
 #include "RAJA/RAJA.hpp" // using RAJA lib
@@ -35,13 +26,13 @@
 
 class HW3D : public PhysicsModel {
 public:
-  Field3D n, vort;  // Evolving density and vorticity
-  Field3D phi;      // Electrostatic potential
+  Field3D n, vort; // Evolving density and vorticity
+  Field3D phi;     // Electrostatic potential
 
   // Model parameters
-  BoutReal alpha;      // Adiabaticity (~conductivity)
-  BoutReal kappa;      // Density gradient drive
-  BoutReal Dvort, Dn;  // Diffusion
+  BoutReal alpha;                       // Adiabaticity (~conductivity)
+  BoutReal kappa;                       // Density gradient drive
+  BoutReal Dvort, Dn;                   // Diffusion
   std::unique_ptr<Laplacian> phiSolver; // Laplacian solver for vort -> phi
 
   int init(bool UNUSED(restart)) {
@@ -54,7 +45,7 @@ public:
 
     SOLVE_FOR(n, vort);
     SAVE_REPEAT(phi);
-    
+
     phiSolver = Laplacian::create();
     phi = 0.; // Starting phi
 
@@ -62,11 +53,8 @@ public:
   }
 
   int rhs(BoutReal UNUSED(time)) {
-
-
-//auto start = std::chrono::steady_clock::now();   
     // Solve for potential
-    phi = phiSolver->solve(vort, phi);    
+    phi = phiSolver->solve(vort, phi);
     Field3D phi_minus_n = phi - n;
     // Communicate variables
     mesh->communicate(n, vort, phi, phi_minus_n);
@@ -75,9 +63,9 @@ public:
     auto n_acc = FieldAccessor<>(n);
     auto vort_acc = FieldAccessor<>(vort);
     auto phi_acc = FieldAccessor<>(phi);
-    auto phi_minus_n_acc = FieldAccessor<>(phi_minus_n); 
+    auto phi_minus_n_acc = FieldAccessor<>(phi_minus_n);
 
-#if 0 //def BOUT_HAS_RAJA
+#if 0 // def BOUT_HAS_RAJA
 //  RAJA code ----------- start
     auto indices = n.getRegion("RGN_NOBNDRY").getIndices();
     Ind3D *ob_i = &(indices)[0];
@@ -102,33 +90,28 @@ public:
 
 //  RAJA code ----------- end
 #else
-// -- CPU code ------------- start
-    //printf("BOUT not using RAJA\n"); 
+    // -- CPU code ------------- start
+    // printf("BOUT not using RAJA\n");
     BOUT_FOR(i, n.getRegion("RGN_NOBNDRY")) {
-      
+
       BoutReal div_current = alpha * Div_par_Grad_par(phi_minus_n_acc, i);
 
-      ddt(n)[i] = -bracket(phi_acc, n_acc, i)
-                  - div_current
-                  - kappa * DDZ(phi_acc, i)
-                  + Dn * Delp2(n_acc, i)
-		;
+      ddt(n)[i] = -bracket(phi_acc, n_acc, i) - div_current - kappa * DDZ(phi_acc, i)
+                  + Dn * Delp2(n_acc, i);
 
-      ddt(vort)[i] = -bracket(phi_acc, vort_acc, i)
-                     - div_current
-                     + Dvort * Delp2(vort_acc, i)
-		;
+      ddt(vort)[i] =
+          -bracket(phi_acc, vort_acc, i) - div_current + Dvort * Delp2(vort_acc, i);
     }
 //--CPU code ---------------- end
 #endif
-//auto end = std::chrono::steady_clock::now();
-//auto  time_taken = std::chrono::duration_cast<std::chrono::nanoseconds     >(end-     start);
-//std::cout << "The operator since start is "<< time_taken.count()<<" nano      se     conds.\n";
+    // auto end = std::chrono::steady_clock::now();
+    // auto  time_taken = std::chrono::duration_cast<std::chrono::nanoseconds     >(end-
+    // start); std::cout << "The operator since start is "<< time_taken.count()<<" nano se
+    // conds.\n";
 
     return 0;
-  }  // end RHS
-}; // end class HW3D
+  } // end RHS
+};  // end class HW3D
 
 // Define a main() function
 BOUTMAIN(HW3D);
-
