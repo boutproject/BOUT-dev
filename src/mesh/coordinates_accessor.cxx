d25e88cc7593562442151c10e370d8115a6923fc
Ben Dudson <bd512@york.ac.uk>
2021-09-09 08:12:05 +0100

    Starting new CoordinatesAccessor design
    If too many CoordinatesFieldAccessors are added to FieldAccessor,
    then the CUDA kernel parameter list runs out of space.
    -> Need to reduce the number of pointers stored in FieldAccessor
    
    This leads to having a struct to hold the Coordinates pointers,
    which is then shared between the FieldAccessors.
    
    That struct must be in memory accessible to the GPU, so either
    allocated using Umpire or a local variable which can be captured by
    value by the RAJA lambda function. The local variable would need
    to be created by the user and passed into FieldAccessor constructors
    or as an extra argument to operators.
    
    This design instead uses a single BoutReal array, allocated in Array
    using Umpire, to store all the Coordinates data. The different fields
    (dx,dy,dz etc) are copied into the array and striped, so that values
    for a single grid cell are next to each other in memory. Hopefully
    this will help with memory access performance.
    
    To avoid doing this copy for every FieldAccessor, the arrays are
    cached in a map from Coordinates* to Array<BoutReal>. Unfortunately
    if Coordinates members are changed, this change will not be updated
    in the CoordinatesAccessor unless this store is cleared.

---
+++ src/mesh/coordinates_accessor.cxx
@@ -0,0 +1,56 @@
+#include "bout/coordinates_accessor.hxx"
+
+#include <map>
+
+CoordinatesAccessor::CoordinatesAccessor(const Coordinates *coords) {
+  ASSERT0(coords != nullptr);
+
+  // Size of the mesh in Z. Used to convert 3D -> 2D index
+  mesh_nz = coords->dx.getMesh()->LocalNz;
+
+  /// Associate each Coordinates object with an Array object
+  /// which contains the coordinates data in striped form.
+  ///
+  /// Note: This association could perhaps be done by putting
+  ///       the Array inside Coordinates, but this keeps things decoupled
+  static std::map<Coordinates*, Array<BoutReal>> store;
+
+  auto search = store.find(coords);
+  if (search != store.end()) {
+    // Found, so get the pointer to the data
+    data = search.second.begin();
+    return;
+  }
+
+  // Not yet created, so create the array and copy the data
+
+  // Work out the size needed
+  int array_size = stripe_size * mesh->LocalNx * mesh->LocalNy;
+#if BOUT_USE_METRIC_3D
+  array_size *= mesh->LocalNz; // 3D metrics
+#endif
+
+  // Create the array and get the underlying data
+  data = store.emplace(coords, array_size).first->begin();
+
+  // Copy data from Coordinates variable into data array
+  // Uses the symbol to look up the corresponding Offset
+#define COPY_STRIPE1(symbol)                                           \
+  data[stripe_size * ind.ind + static_cast<int>(Offset::symbol)] = coords->symbol[ind];
+
+  // Implement copy for each argument
+#define COPY_STRIPE(...)                                \
+  { MACRO_FOR_EACH(COPY_STRIPE1, __VA_ARGS__) }
+
+  // Iterate over all points in the field
+  // Note this could be 2D or 3D, depending on FieldMetric type
+  for (const auto &ind : coords->dx.getRegion("RGN_ALL")) {
+    COPY_STRIPE(dx, dy, dz);
+    COPY_STRIPE(d1_dx, d1_dy, d1_dz);
+    COPY_STRIPE(J);
+    COPY_STRIPE(B, Byup, Bydown);
+    COPY_STRIPE(G1, G3);
+    COPY_STRIPE(g11, g12, g13, g22, g23, g33);
+    COPY_STRIPE(g_11, g_12, g_13, g_22, g_23, g_33);
+  }
+}
