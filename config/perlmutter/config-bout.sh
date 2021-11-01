192715c598430394df39305f6a61e73165f6db59
jonesholger <jonesholger@gmail.com>
2021-10-31 17:16:11 -0700

    add CUDA_ARCH switch for sm80; reverted cmake/SetupCompilers to sm70

--- config/perlmutter/config-bout.sh
+++ config/perlmutter/config-bout.sh
@@ -76,6 +76,7 @@ if [ "$pkg" == "BOUT-dev" ]; then
           -DBOUT_ENABLE_OPENMP=Off \
           -DBOUT_ENABLE_CUDA=On \
           -DCMAKE_CUDA_STANDARD=17 \
+          -DCUDA_ARCH="compute_80,code=sm_80" \
           -DBOUT_USE_HYPRE=On \
           -DHYPRE_DIR="${tpl_prefix}/hypre_dir/hypre_autoconf/install" \
           -DHYPRE_CUDA=On \
