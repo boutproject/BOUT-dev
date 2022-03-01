f5cdabf6a44e100d1954d849dc21822b2674728d
David Bold <dave@ipp.mpg.de>
2022-03-01 15:57:00 +0100

    Fix coverage job

--- .ci_with_cmake.sh
+++ .ci_with_cmake.sh
@@ -8,11 +8,12 @@ cmake -S . -B build $@ -DCMAKE_INSTALL_PREFIX=$(pwd)/installed
 if test ".$UNIT_ONLY" = ".YES" ; then
     make -C build build-check-unit-tests
     make -C build check-unit-tests
-else
-    cmake --build build --target build-check -j 2
-    cd build
-    ctest --output-on-failure --timeout 300
+    exit 0
 fi
+
+cmake --build build --target build-check -j 2
+cd build
+ctest --output-on-failure --timeout 300
 
 export LD_LIBRARY_PATH=/home/runner/local/lib:$LD_LIBRARY_PATH
 
