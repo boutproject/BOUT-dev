c4805559099ed7cea7bc88f18ca3ef80d7a902b2
David Bold <dave@ipp.mpg.de>
2022-03-01 08:49:13 +0100

    Run only unit tests for coverage

--- .ci_with_cmake.sh
+++ .ci_with_cmake.sh
@@ -4,9 +4,15 @@ set -ex
 
 cmake --version
 cmake -S . -B build $@ -DCMAKE_INSTALL_PREFIX=$(pwd)/installed
-cmake --build build --target build-check -j 2
-cd build
-ctest --output-on-failure --timeout 300
+
+if test ".$UNIT_ONLY" = ".YES" ; then
+    make -C build build-check-unit-tests
+    make -C build check-unit-tests
+else
+    cmake --build build --target build-check -j 2
+    cd build
+    ctest --output-on-failure --timeout 300
+fi
 
 # Test bout-config basic functionallity
 cd ../examples/make-script
