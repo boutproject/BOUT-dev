c27ba4471d262aeec78a420c95c3f07443083231
dschwoerer <dschwoerer@users.noreply.github.com>
2022-04-26 05:51:41 +0000

    Apply clang-format changes

--- include/bout/fv_ops.hxx
+++ include/bout/fv_ops.hxx
@@ -14,15 +14,15 @@
 #include <bout/mesh.hxx>
 
 namespace FV {
-  /*!
-   * Div ( a Grad_perp(f) ) -- ∇⊥ ( a ⋅ ∇⊥ f) -- Vorticity
-   */
-  const Field3D Div_a_Grad_perp(const Field3D &a, const Field3D &x);
+/*!
+ * Div ( a Grad_perp(f) ) -- ∇⊥ ( a ⋅ ∇⊥ f) -- Vorticity
+ */
+const Field3D Div_a_Grad_perp(const Field3D& a, const Field3D& x);
 
-  [[deprecated("Please use Div_a_Grad_perp instead")]]
-  inline const Field3D Div_a_Laplace_perp(const Field3D &a, const Field3D &x){
-    return Div_a_Grad_perp(a, x);
-  }
+[[deprecated("Please use Div_a_Grad_perp instead")]] inline const Field3D
+Div_a_Laplace_perp(const Field3D& a, const Field3D& x) {
+  return Div_a_Grad_perp(a, x);
+}
 
   /*!
    * Divergence of a parallel diffusion Div( k * Grad_par(f) )
