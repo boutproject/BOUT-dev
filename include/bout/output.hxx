36cabdfe53644cdf8eeec7a61ea3cfb224ce6655
David Bold <dave@ipp.mpg.de>
2023-02-20 09:07:44 +0100

    Declare variable as unused to avoid warning

--- include/bout/output.hxx
+++ include/bout/output.hxx
@@ -140,8 +140,8 @@ public:
   void write(const S&, const Args&...){};
   template <class S, class... Args>
   void print(const S&, const Args&...){};
-  void write(const std::string& s) override{};
-  void print(const std::string& s) override{};
+  void write(const std::string& UNUSED(s)) override{};
+  void print(const std::string& UNUSED(s)) override{};
   void enable() override{};
   void disable() override{};
   void enable(MAYBE_UNUSED(bool enable)){};
