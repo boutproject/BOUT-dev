f1b2e049c82105fe7b530fce6d35b73a9ee9cf48
David Bold <dschwoerer@users.noreply.github.com>
2023-02-22 19:37:50 +0100

    Do not check for PVEC_REAL_MPI_TYPE change
    MPI_DOUBLE is a macro, so this check can break, even if there is no mismatch.

--- externalpackages/PVODE/include/pvode/nvector.h
+++ externalpackages/PVODE/include/pvode/nvector.h
@@ -94,16 +94,6 @@ namespace pvode {
 #else
 #define PVEC_REAL_MPI_TYPE MPI_FLOAT
 #endif
-#else
-#if (LLNL_DOUBLE == 1)
-#if PVEC_REAL_MPI_TYPE != MPI_DOUBLE
-#error PVEC_REAL_MPI_TYPE changed
-#endif
-#else
-#if PVEC_REAL_MPI_TYPE != MPI_FLOAT
-#error PVEC_REAL_MPI_TYPE changed
-#endif
-#endif
 #endif
 
 // Ugh, potentionanlly bad but need to do this for now when compiling with both SUNDIALS and PVODE
