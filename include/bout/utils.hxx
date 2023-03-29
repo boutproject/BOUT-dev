2f30d2bb9d89b7ed0affee1acac282f447845895
dschwoerer <dschwoerer@users.noreply.github.com>
2023-03-29 09:08:28 +0000

    Apply clang-format changes

--- include/bout/utils.hxx
+++ include/bout/utils.hxx
@@ -662,15 +662,14 @@ std::string trimComments(const std::string& s, const std::string& c = "#;");
 std::string::size_type editDistance(const std::string& str1, const std::string& str2);
 
 // from https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
-template<class T>
+template <class T>
 typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
-    almost_equal(T x, T y, int ulp = 2)
-{
-    // the machine epsilon has to be scaled to the magnitude of the values used
-    // and multiplied by the desired precision in ULPs (units in the last place)
-    return std::fabs(x - y) <= std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
-        // unless the result is subnormal
-        || std::fabs(x - y) < std::numeric_limits<T>::min();
+almost_equal(T x, T y, int ulp = 2) {
+  // the machine epsilon has to be scaled to the magnitude of the values used
+  // and multiplied by the desired precision in ULPs (units in the last place)
+  return std::fabs(x - y) <= std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
+         // unless the result is subnormal
+         || std::fabs(x - y) < std::numeric_limits<T>::min();
 }
 
 /// Convert pointer or reference to pointer
