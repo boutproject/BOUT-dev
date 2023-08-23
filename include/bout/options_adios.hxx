4f5f52c0f784b3569107710bdbd4a38e2c140121
Norbert Podhorszki <pnorbert@ornl.gov>
2023-08-23 12:27:31 -0400

    Add adios2 dependency (BOUT_HAS_ADIOS flag), and starting .hxx file

---
+++ include/bout/options_adios.hxx
@@ -0,0 +1,118 @@
+
+#pragma once
+
+#ifndef __OPTIONS_ADIOS_H__
+#define __OPTIONS_ADIOS_H__
+
+#include "bout/build_config.hxx"
+
+#if !BOUT_HAS_ADIOS 
+
+#include <string>
+
+#include "bout/boutexception.hxx"
+#include "bout/options.hxx"
+
+namespace bout {
+
+class OptionsADIOS {
+public:
+  enum class FileMode {
+    replace, ///< Overwrite file when writing
+    append   ///< Append to file when writing
+  };
+
+  OptionsADIOS(const std::string& filename, FileMode mode = FileMode::replace) {}
+  OptionsADIOS(const OptionsADIOS&) = default;
+  OptionsADIOS(OptionsADIOS&&) = default;
+  OptionsADIOS& operator=(const OptionsADIOS&) = default;
+  OptionsADIOS& operator=(OptionsADIOS&&) = default;
+
+  /// Read options from file
+  Options read() { throw BoutException("OptionsADIOS not available\n"); }
+
+  /// Write options to file
+  void write(const Options& options) {
+    throw BoutException("OptionsADIOS not available\n");
+  }
+};
+
+} // namespace bout
+
+#else
+
+#include <memory>
+#include <string>
+
+#include "bout/options.hxx"
+
+/// Forward declare ADIOS file type so we don't need to depend
+/// directly on ADIOS
+namespace ADIOS {
+class BPStream;
+}
+
+namespace bout {
+
+class OptionsADIOS {
+public:
+  enum class FileMode {
+    replace, ///< Overwrite file when writing
+    append   ///< Append to file when writing
+  };
+
+  // Constructors need to be defined in implementation due to forward
+  // declaration of NcFile
+  OptionsADIOS();
+  explicit OptionsADIOS(std::string filename, FileMode mode = FileMode::replace);
+  ~OptionsADIOS();
+  OptionsADIOS(const OptionsADIOS&) = delete;
+  OptionsADIOS(OptionsADIOS&&) noexcept;
+  OptionsADIOS& operator=(const OptionsADIOS&) = delete;
+  OptionsADIOS& operator=(OptionsADIOS&&) noexcept;
+
+  /// Read options from file
+  Options read();
+
+  /// Write options to file
+  void write(const Options& options) { write(options, "t"); }
+  void write(const Options& options, const std::string& time_dim);
+
+  /// Check that all variables with the same time dimension have the
+  /// same size in that dimension. Throws BoutException if there are
+  /// any differences, otherwise is silent
+  void verifyTimesteps() const;
+
+private:
+  /// Name of the file on disk
+  std::string filename;
+  /// How to open the file for writing
+  FileMode file_mode{FileMode::replace};
+  /// Pointer to ADIOS file so we don't introduce direct dependence
+  std::unique_ptr<ADIOS::BPStream> data_file;
+};
+
+} // namespace bout
+
+#endif
+
+namespace bout {
+/// Name of the directory for restart files
+std::string getRestartDirectoryName(Options& options);
+/// Name of the restart file on this rank
+std::string getRestartFilename(Options& options);
+/// Name of the restart file on \p rank
+std::string getRestartFilename(Options& options, int rank);
+/// Name of the main output file on this rank
+std::string getOutputFilename(Options& options);
+/// Name of the main output file on \p rank
+std::string getOutputFilename(Options& options, int rank);
+/// Write `Options::root()` to the main output file, overwriting any
+/// existing files
+void writeDefaultOutputFile();
+/// Write \p options to the main output file, overwriting any existing
+/// files
+void writeDefaultOutputFile(Options& options);
+} // namespace bout
+
+#endif //  __OPTIONS_ADIOS_H__
