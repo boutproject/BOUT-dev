dc1c812ef5d830f6e1af373066debd03c4dc599a
tomc271 <tomc271@gmail.com>
2023-10-22 19:45:02 +0100

    Remove usages of getCovariantMetricTensor method from integrated tests.

--- tests/integrated/test-naulin-laplace/test_naulin_laplace.cxx
+++ tests/integrated/test-naulin-laplace/test_naulin_laplace.cxx
@@ -246,7 +246,7 @@ int main(int argc, char** argv) {
       x0(mesh->xstart - 1, mesh->ystart, k) =
           (f4(mesh->xstart, mesh->ystart, k) - f4(mesh->xstart - 1, mesh->ystart, k))
           / mesh->getCoordinates()->dx(mesh->xstart, mesh->ystart, k)
-          / sqrt(mesh->getCoordinates()->getCovariantMetricTensor().g_11(mesh->xstart, mesh->ystart, k));
+          / sqrt(mesh->getCoordinates()->g_11()(mesh->xstart, mesh->ystart, k));
     }
   }
   if (mesh->lastX()) {
@@ -254,7 +254,7 @@ int main(int argc, char** argv) {
       x0(mesh->xend + 1, mesh->ystart, k) =
           (f4(mesh->xend + 1, mesh->ystart, k) - f4(mesh->xend, mesh->ystart, k))
           / mesh->getCoordinates()->dx(mesh->xend, mesh->ystart, k)
-          / sqrt(mesh->getCoordinates()->getCovariantMetricTensor().g_11(mesh->xend, mesh->ystart, k));
+          / sqrt(mesh->getCoordinates()->g_11()(mesh->xend, mesh->ystart, k));
     }
   }
 
@@ -305,7 +305,8 @@ int main(int argc, char** argv) {
 Field3D this_Grad_perp_dot_Grad_perp(const Field3D& f, const Field3D& g) {
   const auto* coords = f.getCoordinates();
   const auto metric_tensor = coords->getContravariantMetricTensor();
-  Field3D result = metric_tensor.g11 * ::DDX(f) * ::DDX(g) + metric_tensor.g33 * ::DDZ(f) * ::DDZ(g)
+  Field3D result = metric_tensor.g11 * ::DDX(f) * ::DDX(g)
+                   + metric_tensor.g33 * ::DDZ(f) * ::DDZ(g)
                    + metric_tensor.g13 * (DDX(f) * DDZ(g) + DDZ(f) * DDX(g));
 
   return result;
