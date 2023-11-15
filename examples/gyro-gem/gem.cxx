81ee5edffd760d8526d2db2edf84e75c82f29c98
tomc271 <tomc271@gmail.com>
2023-11-15 14:19:04 +0000

    Update more tests.

--- examples/gyro-gem/gem.cxx
+++ examples/gyro-gem/gem.cxx
@@ -369,22 +369,26 @@ class GEM : public PhysicsModel {
 
     // Metric components
 
-    coord->g11 = SQ(Rxy * Bpxy);
-    coord->g22 = 1.0 / SQ(hthe);
-    coord->g33 = SQ(Bxy) / coord->g11;
-    coord->g12 = 0.0;
-    coord->g13 = 0.;
-    coord->g23 = -Btxy / (hthe * Bpxy * Rxy);
+    MetricTensor::FieldMetric g11, g22, g33, g12, g13, g23;
+    g11 = SQ(Rxy * Bpxy);
+    g22 = 1.0 / SQ(hthe);
+    g33 = SQ(Bxy) / coord->g11();
+    g12 = 0.0;
+    g13 = 0.;
+    g23 = -Btxy / (hthe * Bpxy * Rxy);
+    coord->setContravariantMetricTensor(MetricTensor(g11, g22, g33, g12, g13, g23));
 
     coord->J = hthe / Bpxy;
     coord->Bxy = Bxy;
 
-    coord->g_11 = 1.0 / coord->g11;
-    coord->g_22 = SQ(Bxy * hthe / Bpxy);
-    coord->g_33 = Rxy * Rxy;
-    coord->g_12 = 0.;
-    coord->g_13 = 0.;
-    coord->g_23 = Btxy * hthe * Rxy / Bpxy;
+    MetricTensor::FieldMetric g_11, g_22, g_33, g_12, g_13, g_23;
+    g_11 = 1.0 / coord->g11();
+    g_22 = SQ(Bxy * hthe / Bpxy);
+    g_33 = Rxy * Rxy;
+    g_12 = 0.;
+    g_13 = 0.;
+    g_23 = Btxy * hthe * Rxy / Bpxy;
+    coord->setCovariantMetricTensor(MetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
 
     coord->geometry();
 
