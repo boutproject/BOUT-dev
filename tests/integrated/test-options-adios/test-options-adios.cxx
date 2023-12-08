eeb91777a2957a74554a5728de04798322dad179
Ben Dudson <dudson2@llnl.gov>
2023-12-08 12:58:18 -0800

    More integrated test fixes
    Changing from OptionsNetCDF to OptionsIO interface.

--- tests/integrated/test-options-adios/test-options-adios.cxx
+++ tests/integrated/test-options-adios/test-options-adios.cxx
@@ -1,20 +1,18 @@
 
 #include "bout/bout.hxx"
 
-#include "bout/options_adios.hxx"
 #include "bout/options_io.hxx"
-#include "bout/options_netcdf.hxx"
 #include "bout/optionsreader.hxx"
 
-using bout::OptionsADIOS;
+using bout::OptionsIO;
 
 int main(int argc, char** argv) {
   BoutInitialise(argc, argv);
 
   // Read values from a NetCDF file
-  bout::OptionsNetCDF file("test.nc");
+  auto file = bout::OptionsIO::create("test.nc");
 
-  auto values = file.read();
+  auto values = file->read();
 
   values.printUnused();
 
@@ -22,17 +20,24 @@ int main(int argc, char** argv) {
   OptionsReader* reader = OptionsReader::getInstance();
   reader->write(&values, "test-out.ini");
 
-  // Write to ADIOS file
-  OptionsADIOS("test-out.bp", bout::OptionsIO::FileMode::replace, true).write(values);
+  // Write to ADIOS file, by setting file type "adios"
+  OptionsIO::create({{"file", "test-out.bp"},
+                     {"type", "adios"},
+                     {"append", false},
+                     {"singleWriteFile", true}})
+      ->write(values);
 
   ///////////////////////////
 
   // Write the BOUT.inp settings to NetCDF file
-  OptionsADIOS("settings.bp", bout::OptionsIO::FileMode::replace, true)
-      .write(Options::root());
+  OptionsIO::create({{"file", "settings.bp"},
+                     {"type", "adios"},
+                     {"append", false},
+                     {"singleWriteFile", true}})
+      ->write(Options::root());
 
   // Read back in
-  auto settings = OptionsADIOS("settings.bp").read();
+  auto settings = OptionsIO::create({{"file", "settings.bp"}, {"type", "adios"}})->read();
 
   // Write to INI file
   reader->write(&settings, "settings.ini");
@@ -44,19 +49,20 @@ int main(int argc, char** argv) {
   fields["f2d"] = Field2D(1.0);
   fields["f3d"] = Field3D(2.0);
   fields["fperp"] = FieldPerp(3.0);
-  auto f = OptionsADIOS("fields.bp");
+  auto f = OptionsIO::create({{"file", "fields.bp"}, {"type", "adios"}});
   /*
      write() for adios only buffers data but does not guarantee writing to disk
      unless singleWriteFile is set to true 
      */
-  f.write(fields);
+  f->write(fields);
   // indicate completion of step, required to get data on disk
-  f.verifyTimesteps();
+  f->verifyTimesteps();
 
   ///////////////////////////
   // Read fields
 
-  Options fields_in = OptionsADIOS("fields.bp").read();
+  Options fields_in =
+      OptionsIO::create({{"file", "fields.bp"}, {"type", "adios"}})->read();
 
   auto f2d = fields_in["f2d"].as<Field2D>(bout::globals::mesh);
   auto f3d = fields_in["f3d"].as<Field3D>(bout::globals::mesh);
@@ -68,8 +74,10 @@ int main(int argc, char** argv) {
   fields2["fperp"] = fperp;
 
   // Write out again
-  auto f2 = bout::OptionsIOFactory("fields2.bp", bout::OptionsIO::FileMode::replace,
-                                   bout::OptionsIO::Library::ADIOS, true);
+  auto f2 = OptionsIO::create({{"file", "fields2.bp"},
+                               {"type", "adios"},
+                               {"append", false},
+                               {"singleWriteFile", true}});
   f2->write(fields2);
 
   ///////////////////////////
@@ -82,14 +90,22 @@ int main(int argc, char** argv) {
   data["field"] = Field3D(2.0);
   data["field"].attributes["time_dimension"] = "t";
 
-  OptionsADIOS("time.bp", bout::OptionsIO::FileMode::replace, true).write(data);
+  OptionsIO::create({{"file", "time.bp"},
+                     {"type", "adios"},
+                     {"append", false},
+                     {"singleWriteFile", true}})
+      ->write(data);
 
   // Update time-dependent values
   data["scalar"] = 2.0;
   data["field"] = Field3D(3.0);
 
   // Append data to file
-  OptionsADIOS("time.bp", bout::OptionsIO::FileMode::append, true).write(data);
+  OptionsIO::create({{"file", "time.bp"},
+                     {"type", "adios"},
+                     {"append", true},
+                     {"singleWriteFile", true}})
+      ->write(data);
 
   BoutFinalise();
 };
