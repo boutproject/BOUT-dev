55d887c4a648808cd61c6ed5e58144266c679ef8
tomc271 <tomc271@gmail.com>
2024-06-07 15:30:49 +0100

    Remove 'shim' header file - options_netcdf.hxx no longer exists.

--- include/options_netcdf.hxx
+++
@@ -1,4 +0,0 @@
-#pragma once
-// BOUT++ header shim
-#warning Header "options_netcdf.hxx" has moved to "bout/options_netcdf.hxx". Run `bin/bout-v5-header-upgrader.py` to fix
-#include "bout/options_netcdf.hxx"
