aeccb60c960d0d155eb1f3187a233a80cb66d5ef
Peter Hill <peter.hill@york.ac.uk>
2024-07-03 11:34:40 +0100

    Reset output `Options` after writing to disk
    Avoids rewriting time-independent data, gives slight performance boost

--- src/physics/physicsmodel.cxx
+++ src/physics/physicsmodel.cxx
@@ -257,6 +257,9 @@ int PhysicsModel::PhysicsModelMonitor::call(Solver* solver, BoutReal simtime,
   model->outputVars(model->output_options);
   model->writeOutputFile();
 
+  // Reset output options, this avoids rewriting time-independent data
+  model->output_options = Options{};
+
   // Call user output monitor
   return model->outputMonitor(simtime, iteration, nout);
 }
