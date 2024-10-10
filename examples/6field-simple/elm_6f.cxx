240bf4f69511a2b6242d23321686f32498afa255
tomc271 <tomc271@gmail.com>
2024-10-10 11:48:16 +0100

    Extract function tokamak_coordinates(coord, Rxy, Bpxy, hthe, I, B0, Btxy)
    to new file examples/common.hxx

--- examples/6field-simple/elm_6f.cxx
+++ examples/6field-simple/elm_6f.cxx
@@ -6,7 +6,7 @@
  * T. Xia
  *******************************************************************************/
 
-#include "bout/bout.hxx"
+#include "../common.hxx"
 #include "bout/constants.hxx"
 #include "bout/derivs.hxx"
 #include "bout/initialprofiles.hxx"
@@ -1051,25 +1051,7 @@ protected:
 
     /**************** CALCULATE METRICS ******************/
 
-    const auto g11 = SQ(Rxy * Bpxy);
-    const auto g22 = 1.0 / SQ(hthe);
-    const auto g33 = SQ(I) * g11 + SQ(B0) / g11;
-    const auto g12 = 0.0;
-    const auto g13 = -I * g11;
-    const auto g23 = -Btxy / (hthe * Bpxy * Rxy);
-
-    const auto g_11 = 1.0 / g11 + SQ(I * Rxy);
-    const auto g_22 = SQ(B0 * hthe / Bpxy);
-    const auto g_33 = Rxy * Rxy;
-    const auto g_12 = Btxy * hthe * I * Rxy / Bpxy;
-    const auto g_13 = I * Rxy * Rxy;
-    const auto g_23 = Btxy * hthe * Rxy / Bpxy;
-
-    coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
-                           CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
-
-    coord->setJ(hthe / Bpxy);
-    coord->setBxy(B0);
+    tokamak_coordinates(coord, Rxy, Bpxy, hthe, I, B0, Btxy);
 
     // Set B field vector
 
--- examples/wave-slab/wave_slab.cxx
+++ examples/wave-slab/wave_slab.cxx
@@ -10,6 +10,7 @@
   
  */
 
+#include "../common.hxx"
 #include <bout/physicsmodel.hxx>
 
 class WaveTest : public PhysicsModel {
@@ -31,24 +32,7 @@ public:
       mesh->get(I, "sinty");
     }
 
-    const auto g11 = pow(Rxy * Bpxy, 2.0);
-    const auto g22 = 1.0 / pow(hthe, 2.0);
-    const auto g33 = pow(I, 2.0) * g11 + pow(coords->Bxy(), 2.0) / g11;
-    const auto g12 = 0.0;
-    const auto g13 = -I * g11;
-    const auto g23 = -Btxy / (hthe * Bpxy * Rxy);
-
-    const auto g_11 = 1.0 / g11 + (pow(I * Rxy, 2.0));
-    const auto g_22 = pow(coords->Bxy() * hthe / Bpxy, 2.0);
-    const auto g_33 = Rxy * Rxy;
-    const auto g_12 = Btxy * hthe * I * Rxy / Bpxy;
-    const auto g_13 = I * Rxy * Rxy;
-    const auto g_23 = Btxy * hthe * Rxy / Bpxy;
-
-    coords->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
-                            CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
-
-    coords->setJ(hthe / Bpxy);
+    tokamak_coordinates(coords, Rxy, Bpxy, hthe, I, coords->Bxy(), Btxy);
 
     solver->add(f, "f");
     solver->add(g, "g");
