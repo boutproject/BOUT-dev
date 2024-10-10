ef81a05675a92fa6544824ee638c0587dffc6cd6
tomc271 <tomc271@gmail.com>
2024-10-10 13:09:58 +0100

    Use function in more examples
    elm-pb
    laplacexy/laplace_perp
    gyro-gem example (using zero for I parameter)
    alfven-wave example (using sinty for I parameter)

--- examples/constraints/alfven-wave/alfven.cxx
+++ examples/constraints/alfven-wave/alfven.cxx
@@ -2,7 +2,7 @@
 #include <bout/field_factory.hxx>
 #include <bout/invert/laplacexz.hxx>
 #include <bout/invert_laplace.hxx>
-#include <bout/physicsmodel.hxx>
+#include "../../common.hxx"
 
 /// Fundamental constants
 const BoutReal PI = 3.14159265;
@@ -199,26 +199,7 @@ protected:
       sbp = -1.0;
     }
 
-    // Calculate metric components
-
-    const auto g11 = SQ(Rxy * Bpxy);
-    const auto g22 = 1.0 / SQ(hthe);
-    const auto g33 = SQ(sinty) * g11 + SQ(coord->Bxy()) / g11;
-    const auto g12 = 0.0;
-    const auto g13 = -sinty * g11;
-    const auto g23 = -sbp * Btxy / (hthe * Bpxy * Rxy);
-
-    const auto g_11 = 1.0 / g11 + SQ(sinty * Rxy);
-    const auto g_22 = SQ(coord->Bxy() * hthe / Bpxy);
-    const auto g_33 = Rxy * Rxy;
-    const auto g_12 = sbp * Btxy * hthe * sinty * Rxy / Bpxy;
-    const auto g_13 = sinty * Rxy * Rxy;
-    const auto g_23 = sbp * Btxy * hthe * Rxy / Bpxy;
-
-    coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
-                           CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
-
-    coord->setJ(hthe / Bpxy);
+    tokamak_coordinates(coord, Rxy, Bpxy, hthe, sinty, coord->Bxy(), Btxy, sbp);
   }
 };
 
