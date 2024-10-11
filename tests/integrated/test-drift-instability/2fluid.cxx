f5f93debc46f3fd78b9b0a55e5138156f35defbd
tomc271 <tomc271@gmail.com>
2024-10-11 13:52:56 +0100

    Use function `tokamak_coordinates` in tests

--- tests/integrated/test-drift-instability/2fluid.cxx
+++ tests/integrated/test-drift-instability/2fluid.cxx
@@ -3,7 +3,7 @@
  * Same as Maxim's version of BOUT - simplified 2-fluid for benchmarking
  *******************************************************************************/
 
-#include <bout/bout.hxx>
+#include <bout/tokamak_coordinates.hxx>
 #include <bout/physicsmodel.hxx>
 
 #include <bout/derivs.hxx>
@@ -215,26 +215,7 @@ protected:
     pei0 = (Ti0 + Te0) * Ni0;
     pe0 = Te0 * Ni0;
 
-    /**************** CALCULATE METRICS ******************/
-
-    const auto g11 = SQ(Rxy * Bpxy);
-    const auto g22 = 1.0 / SQ(hthe);
-    const auto g33 = SQ(I) * g11 + SQ(coord->Bxy()) / g11;
-    const auto g12 = 0.0;
-    const auto g13 = -I * g11;
-    const auto g23 = -Btxy / (hthe * Bpxy * Rxy);
-
-    const auto g_11 = 1.0 / g11 + SQ(I * Rxy);
-    const auto g_22 = SQ(coord->Bxy() * hthe / Bpxy);
-    const auto g_33 = Rxy * Rxy;
-    const auto g_12 = Btxy * hthe * I * Rxy / Bpxy;
-    const auto g_13 = I * Rxy * Rxy;
-    const auto g_23 = Btxy * hthe * Rxy / Bpxy;
-
-    coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
-                           CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
-
-    coord->setJ(hthe / Bpxy);
+    tokamak_coordinates(coord, Rxy, Bpxy, hthe, I, coord->Bxy(), Btxy);
 
     /**************** SET EVOLVING VARIABLES *************/
 
