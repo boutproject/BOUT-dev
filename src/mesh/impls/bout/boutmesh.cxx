878cf37a99574fe6dd11189264cc2ff8425929ae
David Bold <dschwoerer@users.noreply.github.com>
2024-10-22 12:10:59 +0200

    prefer `auto *` for pointer types
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/mesh/impls/bout/boutmesh.cxx
+++ src/mesh/impls/bout/boutmesh.cxx
@@ -2296,7 +2296,7 @@ int BoutMesh::pack_data(const std::vector<FieldData*>& var_list, int xge, int xl
   for (const auto& var : var_list) {
     if (var->is3D()) {
       // 3D variable
-      auto var3d_ref_ptr = dynamic_cast<Field3D*>(var);
+      auto *var3d_ref_ptr = dynamic_cast<Field3D*>(var);
       ASSERT0(var3d_ref_ptr != nullptr);
       auto& var3d_ref = *var3d_ref_ptr;
       ASSERT2(var3d_ref.isAllocated());
@@ -2309,7 +2309,7 @@ int BoutMesh::pack_data(const std::vector<FieldData*>& var_list, int xge, int xl
       }
     } else {
       // 2D variable
-      auto var2d_ref_ptr = dynamic_cast<Field2D*>(var);
+      auto *var2d_ref_ptr = dynamic_cast<Field2D*>(var);
       ASSERT0(var2d_ref_ptr != nullptr);
       auto& var2d_ref = *var2d_ref_ptr;
       ASSERT2(var2d_ref.isAllocated());
