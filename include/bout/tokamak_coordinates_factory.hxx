b524064dcf58f2e3ea9b3364730396bd91b7d231
tomc271 <tomc271@gmail.com>
2024-11-06 14:35:39 +0000

    Rename variable (Sign of Bp)

--- include/bout/tokamak_coordinates_factory.hxx
+++ include/bout/tokamak_coordinates_factory.hxx
@@ -19,7 +19,7 @@ public:
                                         const FieldMetric& Bpxy, const FieldMetric& hthe,
                                         const FieldMetric& I, const FieldMetric& B,
                                         const FieldMetric& Btxy,
-                                        const BoutReal& sbp = 1.0) const // Sign of Bp
+                                        const BoutReal& sign_of_bp = 1.0) const
   {
     auto* coord = mesh_m->getCoordinates();
 
@@ -28,14 +28,14 @@ public:
     const auto g33 = SQ(I) * g11 + SQ(B) / g11;
     const auto g12 = 0.0;
     const auto g13 = -I * g11;
-    const auto g23 = -sbp * Btxy / (hthe * Bpxy * Rxy);
+    const auto g23 = -sign_of_bp * Btxy / (hthe * Bpxy * Rxy);
 
     const auto g_11 = 1.0 / g11 + SQ(I * Rxy);
     const auto g_22 = SQ(B * hthe / Bpxy);
     const auto g_33 = Rxy * Rxy;
-    const auto g_12 = sbp * Btxy * hthe * I * Rxy / Bpxy;
+    const auto g_12 = sign_of_bp * Btxy * hthe * I * Rxy / Bpxy;
     const auto g_13 = I * Rxy * Rxy;
-    const auto g_23 = sbp * Btxy * hthe * Rxy / Bpxy;
+    const auto g_23 = sign_of_bp * Btxy * hthe * Rxy / Bpxy;
 
     coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
                            CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));
