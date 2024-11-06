f7919e0ee1bf7b14cd34a348b5bad4dd22302530
David Bold <dave@ipp.mpg.de>
2024-11-06 14:52:41 +0100

    Fix bad merge

--- include/bout/mask.hxx
+++ include/bout/mask.hxx
@@ -66,8 +66,6 @@ public:
 
   inline bool& operator()(int jx, int jy, int jz) { return mask(jx, jy, jz); }
   inline const bool& operator()(int jx, int jy, int jz) const { return mask(jx, jy, jz); }
-
-  inline bool& operator[](const Ind3D& i) { return mask[i]; }
   inline const bool& operator[](const Ind3D& i) const { return mask[i]; }
   inline bool& operator[](const Ind3D& i) { return mask[i]; }
 };
