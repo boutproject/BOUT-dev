6bcb8e92dd292c2d8b1a96f427cfd16ee8a49a29
Peter Hill <peter.hill@york.ac.uk>
2025-01-23 14:46:49 +0000

    CMake: Bump downloaded SUNDIALS version
    Importantly, includes fixes that avoid clobbering our CMake variables

--- cmake/SetupBOUTThirdParty.cmake
+++ cmake/SetupBOUTThirdParty.cmake
@@ -282,7 +282,7 @@ if (BOUT_USE_SUNDIALS)
     FetchContent_Declare(
       sundials
       GIT_REPOSITORY https://github.com/LLNL/sundials
-      GIT_TAG        v7.0.0
+      GIT_TAG        v7.2.1
       )
     # Note: These are settings for building SUNDIALS
     set(EXAMPLES_ENABLE_C OFF CACHE BOOL "" FORCE)
