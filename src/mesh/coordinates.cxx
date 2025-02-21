d52a724966d1ee64bbe60cb987b089262a8e96f9
tomc271 <tomc271@gmail.com>
2025-02-21 10:30:32 +0000

    Fix for failing fci tests. Need to use Coordinates::DDY() not bout::derivatives::index::DDY() as the former checks canToFromFieldAligned().

--- src/mesh/coordinates.cxx
+++ src/mesh/coordinates.cxx
@@ -857,7 +857,7 @@ void Coordinates::correctionForNonUniformMeshes(bool force_interpolate_from_cent
   if (localmesh->get(d2y, "d2y" + suffix, 0.0, false, location) != 0) {
     output_warn.write("\tWARNING: differencing quantity 'd2y' not found. "
                       "Calculating from dy\n");
-    d1_dy_ = bout::derivatives::index::DDY(1. / dy()); // d/di(1/dy)
+    d1_dy_ = DDY(1. / dy()); // d/di(1/dy)
 
     communicate(d1_dy_);
     d1_dy_ =
