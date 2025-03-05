91ddbfd2af7cf85b442c70b1df250d0f282a9c0c
Ben Dudson <dudson2@llnl.gov>
2025-03-05 14:52:03 -0800

    Mesh, BoutMesh, Coordinates: update header comment
    Update copyright statement, contact email.

--- include/bout/mesh.hxx
+++ include/bout/mesh.hxx
@@ -17,9 +17,9 @@
  *     * Incorporates code from topology.cpp and Communicator
  *
  **************************************************************************
- * Copyright 2010 B.D.Dudson, S.Farley, M.V.Umansky, X.Q.Xu
+ * Copyright 2010-2025 BOUT++ contributors
  *
- * Contact: Ben Dudson, bd512@york.ac.uk
+ * Contact: Ben Dudson, dudson2@llnl.gov
  * 
  * This file is part of BOUT++.
  *
