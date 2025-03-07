5755012e9886b561af319e1cc292080e4cc35216
David Bold <dave@ipp.mpg.de>
2025-03-07 10:39:28 +0100

    Ensure the field has parallel slices

--- include/bout/parallel_boundary_region.hxx
+++ include/bout/parallel_boundary_region.hxx
@@ -262,6 +262,7 @@ public:
 
   template <bool check = true>
   BoutReal& getAt(Field3D& f, int off) const {
+    ASSERT4(f.hasParallelSlices());
     if constexpr (check) {
       ASSERT3(valid() > -off - 2);
     }
@@ -270,6 +271,7 @@ public:
   }
   template <bool check = true>
   const BoutReal& getAt(const Field3D& f, int off) const {
+    ASSERT4(f.hasParallelSlices());
     if constexpr (check) {
       ASSERT3(valid() > -off - 2);
     }
