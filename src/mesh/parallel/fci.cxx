242b9b2be958e87dccc1c38ca9149a666bed5ae1
David Bold <dave@ipp.mpg.de>
2025-03-19 10:51:32 +0100

    Add y-shifted regions by default

--- src/mesh/parallel/fci.cxx
+++ src/mesh/parallel/fci.cxx
@@ -340,15 +340,6 @@ FCIMap::FCIMap(Mesh& mesh, const Coordinates::FieldMetric& UNUSED(dy), Options& 
   region_no_boundary = region_no_boundary.mask(to_remove);
 
   interp->setRegion(region_no_boundary);
-
-  const auto region = fmt::format("RGN_YPAR_{:+d}", offset);
-  if (not map_mesh.hasRegion3D(region)) {
-    // The valid region for this slice
-    map_mesh.addRegion3D(
-        region, Region<Ind3D>(map_mesh.xstart, map_mesh.xend, map_mesh.ystart + offset,
-                              map_mesh.yend + offset, 0, map_mesh.LocalNz - 1,
-                              map_mesh.LocalNy, map_mesh.LocalNz));
-  }
 }
 
 Field3D FCIMap::integrate(Field3D& f) const {
