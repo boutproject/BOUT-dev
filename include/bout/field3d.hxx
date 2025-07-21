0ecf55cb8cd71d4356029552e6079316f0ee0a2d
David Bold <dave@ipp.mpg.de>
2025-07-21 11:14:10 +0200

    Move function definition after class definition

--- include/bout/field3d.hxx
+++ include/bout/field3d.hxx
@@ -600,32 +600,6 @@ Field3DParallel operator-(BoutReal lhs, const Field3DParallel& rhs);
 Field3DParallel operator*(BoutReal lhs, const Field3DParallel& rhs);
 Field3DParallel operator/(BoutReal lhs, const Field3DParallel& rhs);
 
-Field3D operator+(const Field2D& lhs, const Field3DParallel& rhs) {
-  return lhs + rhs.asField3D();
-}
-Field3D operator-(const Field2D& lhs, const Field3DParallel& rhs) {
-  return lhs + rhs.asField3D();
-}
-Field3D operator*(const Field2D& lhs, const Field3DParallel& rhs) {
-  return lhs + rhs.asField3D();
-}
-Field3D operator/(const Field2D& lhs, const Field3DParallel& rhs) {
-  return lhs + rhs.asField3D();
-}
-
-Field3D operator+(const Field3DParallel& lhs, const Field2D& rhs) {
-  return lhs.asField3D() + rhs;
-}
-Field3D operator-(const Field3DParallel& lhs, const Field2D& rhs) {
-  return lhs.asField3D() - rhs;
-}
-Field3D operator*(const Field3DParallel& lhs, const Field2D& rhs) {
-  return lhs.asField3D() * rhs;
-}
-Field3D operator/(const Field3DParallel& lhs, const Field2D& rhs) {
-  return lhs.asField3D() / rhs;
-}
-
 /*!
  * Unary minus. Returns the negative of given field,
  * iterates over whole domain including guard/boundary cells.
@@ -807,4 +781,32 @@ private:
 };
 
 Field3DParallel Field3D::asField3DParallel() { return Field3DParallel(*this); }
+
+
+inline Field3D operator+(const Field2D& lhs, const Field3DParallel& rhs) {
+  return lhs + rhs.asField3D();
+}
+inline Field3D operator-(const Field2D& lhs, const Field3DParallel& rhs) {
+  return lhs + rhs.asField3D();
+}
+inline Field3D operator*(const Field2D& lhs, const Field3DParallel& rhs) {
+  return lhs + rhs.asField3D();
+}
+inline Field3D operator/(const Field2D& lhs, const Field3DParallel& rhs) {
+  return lhs + rhs.asField3D();
+}
+
+inline Field3D operator+(const Field3DParallel& lhs, const Field2D& rhs) {
+  return lhs.asField3D() + rhs;
+}
+inline Field3D operator-(const Field3DParallel& lhs, const Field2D& rhs) {
+  return lhs.asField3D() - rhs;
+}
+inline Field3D operator*(const Field3DParallel& lhs, const Field2D& rhs) {
+  return lhs.asField3D() * rhs;
+}
+inline Field3D operator/(const Field3DParallel& lhs, const Field2D& rhs) {
+  return lhs.asField3D() / rhs;
+}
+
 #endif /* BOUT_FIELD3D_H */
