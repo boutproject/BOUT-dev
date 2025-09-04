af37dff14f1875ad2ace70f3f260a246dc8ec010
David Bold <dave@ipp.mpg.de>
2025-09-04 13:22:32 +0200

    Fix spacing

--- tools/pylib/_boutpp_build/backend.py
+++ tools/pylib/_boutpp_build/backend.py
@@ -223,7 +223,7 @@ Version: {getversion()}
             for l in src:
                 f.write(f"{pre}{l}")
                 pre = "         "
-        f.write("\nDescription-Content-Type: text/markdown\n\n")
+        f.write("Description-Content-Type: text/markdown\n\n")
         with open("README.md") as src:
             f.write(src.read())
     run(
