560b005cb95826a4e86c6d1003def6edd3e3e803
Peter Hill <peter.hill@york.ac.uk>
2025-10-07 16:34:44 +0100

    tests: Add test for FCI `Div_par_K_Grad_par`

--- tests/MMS/spatial/fci/mms.py
+++ tests/MMS/spatial/fci/mms.py
@@ -9,11 +9,12 @@ import warnings
 from boututils.boutwarnings import AlwaysWarning
 from boutdata.data import BoutOptionsFile
 from boutdata.mms import diff, exprToStr, x, y, z
-from sympy import sin, sqrt, Expr
+from sympy import sin, cos, sqrt, Expr
 
 warnings.simplefilter("ignore", AlwaysWarning)
 
 f = sin(y - z) + sin(y - 2 * z)
+K = cos(z - y)
 
 Lx = 0.1
 Ly = 10.0
@@ -27,23 +28,33 @@ Bpx = Bp + (x - 0.5) * Lx * Bpprime  # Note: x in range [0,1]
 B = sqrt(Bpx**2 + Bt**2)
 
 
-def FCI_ddy(f: Expr) -> Expr:
+def FCI_grad_par(f: Expr) -> Expr:
     return (Bt * diff(f, y) * 2.0 * pi / Ly + Bpx * diff(f, z) * 2.0 * pi / Lz) / B
 
 
+def FCI_grad2_par2(f: Expr) -> Expr:
+    return FCI_grad_par(FCI_grad_par(f))
+
+
 def FCI_div_par(f: Expr) -> Expr:
-    return Bpx * FCI_ddy(f / Bpx)
+    return Bpx * FCI_grad_par(f / Bpx)
+
+
+def FCI_div_par_K_grad_par(f: Expr, K: Expr) -> Expr:
+    return (K * FCI_grad2_par2(f)) + (FCI_div_par(K) * FCI_grad_par(f))
 
 
 ############################################
 # Equations solved
 
 input_field = exprToStr(f)
-grad_par_solution = exprToStr(FCI_ddy(f))
-grad2_par2_solution = exprToStr(FCI_ddy(FCI_ddy(f)))
+grad_par_solution = exprToStr(FCI_grad_par(f))
+grad2_par2_solution = exprToStr(FCI_grad2_par2(f))
 div_par_solution = exprToStr(FCI_div_par(f))
+div_par_K_grad_par_solution = exprToStr(FCI_div_par_K_grad_par(f, K))
 
 print(f"input_field = {input_field}")
+print(f"K = {K}")
 print(f"grad_par_solution = {grad_par_solution}")
 print(f"grad2_par2_solution = {grad2_par2_solution}")
 print(f"div_par_solution = {div_par_solution}")
@@ -51,7 +62,9 @@ print(f"div_par_K_grad_par_solution = {div_par_K_grad_par_solution}")
 
 options = BoutOptionsFile("data/BOUT.inp")
 options["input_field"] = input_field
+options["K"] = K
 options["grad_par_solution"] = grad_par_solution
 options["grad2_par2_solution"] = grad2_par2_solution
 options["div_par_solution"] = div_par_solution
+options["div_par_K_grad_par_solution"] = div_par_K_grad_par_solution
 options.write("data/BOUT.inp", overwrite=True)
