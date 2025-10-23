96c86b85cd4f50bc640bfe5c14f6f026bae62e90
ZedThree <1486942+ZedThree@users.noreply.github.com>
2025-10-23 17:22:49 +0000

    Apply clang-format changes

--- src/invert/laplacexy/impls/hypre/laplacexy-hypre.cxx
+++ src/invert/laplacexy/impls/hypre/laplacexy-hypre.cxx
@@ -12,10 +12,8 @@
 #include "bout/sys/timer.hxx"
 
 #if BOUT_HAS_CUDA && defined(__CUDACC__)
-#define gpuErrchk(ans)                    \
-  {                                       \
-    gpuAssert((ans), __FILE__, __LINE__); \
-  }
+#define gpuErrchk(ans) \
+  { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true) {
   if (code != cudaSuccess) {
     fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
