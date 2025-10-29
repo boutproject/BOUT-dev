72f8e95101d2adca402becef54163d24bfea63e7
David Bold <dave@ipp.mpg.de>
2025-10-29 11:32:38 +0100

    Mark Field3DParallel constructors explicit

--- include/bout/field3d.hxx
+++ include/bout/field3d.hxx
@@ -738,13 +738,13 @@ inline Field3D copy(const Field3D& f) {
 /// "main" field, Field3DParallel will retain the parallel slices.
 class Field3DParallel : public Field3D {
 public:
-  template <class... Types>
+  explicit template <class... Types>
   Field3DParallel(Types... args) : Field3D(std::move(args)...) {
     ensureFieldAligned();
   }
   // Explicitly needed, as DirectionTypes is sometimes constructed from a
   // brace enclosed list
-  Field3DParallel(Mesh* localmesh = nullptr, CELL_LOC location_in = CELL_CENTRE,
+  explicit Field3DParallel(Mesh* localmesh = nullptr, CELL_LOC location_in = CELL_CENTRE,
                   DirectionTypes directions_in = {YDirectionType::Standard,
                                                   ZDirectionType::Standard},
                   std::optional<size_t> regionID = {})
@@ -752,13 +752,13 @@ public:
     splitParallelSlices();
     ensureFieldAligned();
   }
-  Field3DParallel(Array<BoutReal> data, Mesh* localmesh, CELL_LOC location = CELL_CENTRE,
+  explicit Field3DParallel(Array<BoutReal> data, Mesh* localmesh, CELL_LOC location = CELL_CENTRE,
                   DirectionTypes directions_in = {YDirectionType::Standard,
                                                   ZDirectionType::Standard})
       : Field3D(std::move(data), localmesh, location, directions_in) {
     ensureFieldAligned();
   }
-  Field3DParallel(BoutReal, Mesh* mesh = nullptr);
+  explicit Field3DParallel(BoutReal, Mesh* mesh = nullptr);
   Field3D& asField3D() { return *this; }
   const Field3D& asField3D() const { return *this; }
 
