2699cafcfdcf2ee546fca1891086e96022a5f206
Peter Hill <peter.hill@york.ac.uk>
2025-11-03 10:26:59 +0000

    Delete v5 header shims

--- include/optionsreader.hxx
+++
@@ -1,4 +0,0 @@
-#pragma once
-// BOUT++ header shim
-#warning Header "optionsreader.hxx" has moved to "bout/optionsreader.hxx". Run `bin/bout-v5-header-upgrader.py` to fix
-#include "bout/optionsreader.hxx"
