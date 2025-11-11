01cc49c793c3dbcc08def534263cec5197a100f7
tomc <tomc271@gmail.com>
2025-11-11 13:55:52 +0000

    Convert test_cyclic.py to a valid pytest file

--- tests/integrated/test-cyclic/test_cyclic.py
+++ tests/integrated/test-cyclic/test_cyclic.py
@@ -15,40 +15,41 @@ except:
     pass
 from boututils.run_wrapper import build_and_log, shell, launch
 from boutdata.collect import collect
-from sys import stdout, exit
+from sys import stdout
 
 build_and_log("Cyclic Reduction test")
 
 flags = ["", "nsys=2", "nsys=5 periodic", "nsys=7 n=10"]
 
-code = 0  # Return code
-for nproc in [1, 2, 4]:
-    cmd = "./test_cyclic"
 
-    print("   %d processors...." % (nproc))
-    r = 0
-    for f in flags:
-        stdout.write("\tflags '" + f + "' ... ")
+def test_cyclic():
+    code = 0  # Return code
+    for nproc in [1, 2, 4]:
+        cmd = "./test_cyclic"
 
-        shell("rm data/BOUT.dmp.* 2> err.log")
+        print("   %d processors...." % (nproc))
+        r = 0
+        for f in flags:
+            stdout.write("\tflags '" + f + "' ... ")
 
-        # Run the case
-        status, out = launch(cmd + " " + f, nproc=nproc, mthread=1, pipe=True)
-        with open(f"run.log.{nproc}.{r}", "w") as f:
-            f.write(out)
+            shell("rm data/BOUT.dmp.* 2> err.log")
 
-        r = r + 1
+            # Run the case
+            status, out = launch(cmd + " " + f, nproc=nproc, mthread=1, pipe=True)
+            with open(f"run.log.{nproc}.{r}", "w") as f:
+                f.write(out)
 
-        # Find out if it worked
-        if status:
-            print("PASSED")
-        else:
-            print("FAILED")
-            code = 1
+            r = r + 1
 
-if code == 0:
-    print(" => All cyclic reduction tests passed")
-else:
-    print(" => Some failed tests")
+            # Find out if it worked
+            if status:
+                print("PASSED")
+            else:
+                print("FAILED")
+                code = 1
+            assert status, f"Test failed for flag={f}"
 
-exit(code)
+    if code == 0:
+        print(" => All cyclic reduction tests passed")
+    else:
+        print(" => Some failed tests")
