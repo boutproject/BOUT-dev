6703d0ee10aa820cb0bd2f77c18557596033d282
Peter Hill <peter.hill@york.ac.uk>
2025-12-01 14:21:42 +0000

    Remove explicit use of `MPI_COMM_WORLD`
    Fixes #3215

--- src/mesh/interpolation/hermite_spline_xz.cxx
+++ src/mesh/interpolation/hermite_spline_xz.cxx
@@ -21,6 +21,7 @@
  **************************************************************************/
 
 #include "../impls/bout/boutmesh.hxx"
+#include "bout/bout.hxx"
 #include "bout/globals.hxx"
 #include "bout/index_derivs_interface.hxx"
 #include "bout/interpolation_xz.hxx"
@@ -133,16 +134,9 @@ XZHermiteSpline::XZHermiteSpline(int y_offset, Mesh* mesh)
 #ifdef HS_USE_PETSC
   petsclib = new PetscLib(
       &Options::root()["mesh:paralleltransform:xzinterpolation:hermitespline"]);
-  // MatCreate(MPI_Comm comm,Mat *A)
-  // MatCreate(MPI_COMM_WORLD, &petscWeights);
-  //  MatSetSizes(petscWeights, m, m, M, M);
-  // PetscErrorCode MatCreateAIJ(MPI_Comm comm, PetscInt m, PetscInt n, PetscInt M,
-  // PetscInt N, 			      PetscInt d_nz, const PetscInt d_nnz[],
-  // PetscInt o_nz, const PetscInt o_nnz[], Mat *A)
-  //  MatSetSizes(Mat A,PetscInt m,PetscInt n,PetscInt M,PetscInt N)
   const int m = localmesh->LocalNx * localmesh->LocalNy * localmesh->LocalNz;
   const int M = m * localmesh->getNXPE() * localmesh->getNYPE();
-  MatCreateAIJ(MPI_COMM_WORLD, m, m, M, M, 16, nullptr, 16, nullptr, &petscWeights);
+  MatCreateAIJ(BoutComm::get(), m, m, M, M, 16, nullptr, 16, nullptr, &petscWeights);
 #endif
 #endif
 #ifndef HS_USE_PETSC
