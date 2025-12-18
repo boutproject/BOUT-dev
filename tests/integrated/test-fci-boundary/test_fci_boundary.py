c55ab8f42ab084a20f2950c5680425c1a414dfdc
tomc <tomc271@gmail.com>
2025-12-18 16:50:42 +0000

    Modify test_fci_boundary to run via pytest
    Fails due to missing file `grid.fci.nc`.

--- tests/integrated/test-fci-boundary/test_fci_boundary.py
+++ tests/integrated/test-fci-boundary/test_fci_boundary.py
@@ -2,54 +2,68 @@
 #
 # Python script to run and analyse MMS test
 
+
+import os
+import pathlib
+import numpy as np
 from boututils.run_wrapper import launch_safe
 from boututils.datafile import DataFile
 from boutdata.collect import collect
 
-import numpy as np
-
-
-nprocs = [1]
-mthread = 2
 
 directory = "data"
 
-with DataFile("grid.fci.nc") as grid:
-    MXG = grid.get("MXG", default=1)
-    xfwd = grid.read("forward_xt_prime")[MXG:-MXG]
-    xbwd = grid.read("backward_xt_prime")[MXG:-MXG]
+def collect(var):
+    return _collect(
+        var,
+        info=False,
+        path=directory,
+        xguards=False,
+        yguards=False,
+    )
 
-nx = xfwd.shape[0]
 
-regions = {
-    "xin_fwd": xfwd < MXG,
-    "xout_fwd": xfwd > nx + MXG - 1,
-    "xin_bwd": xbwd < MXG,
-    "xout_bwd": xbwd > nx + MXG - 1,
-}
-regions = {k: v.astype(int) for k, v in regions.items()}
+def test_fci_boundary():
 
-for x in "xout", "xin":
-    regions[x] = regions[f"{x}_fwd"] + regions[f"{x}_bwd"]
-for x in "fwd", "bwd":
-    regions[x] = regions[f"xin_{x}"] + regions[f"xout_{x}"]
-regions["all"] = regions["xin"] + regions["xout"]
+    nprocs = [1]
+    mthread = 2
 
-for nproc in nprocs:
-    cmd = "./get_par_bndry"
-    _, out = launch_safe(cmd, nproc=nproc, mthread=mthread, pipe=True)
+    with DataFile("grid.fci.nc") as grid:
+        MXG = grid.get("MXG", default=1)
+        xfwd = grid.read("forward_xt_prime")[MXG:-MXG]
+        xbwd = grid.read("backward_xt_prime")[MXG:-MXG]
 
-    for k, v in regions.items():
-        data = collect(
-            f"field_{k}",
-            info=False,
-            path=directory,
-            xguards=False,
-            yguards=False,
-        )
-        assert np.allclose(data, v), (
-            f"{k} does not match",
-            np.sum(data),
-            np.sum(v),
-            np.max(data),
-        )
+    nx = xfwd.shape[0]
+
+    regions = {
+        "xin_fwd": xfwd < MXG,
+        "xout_fwd": xfwd > nx + MXG - 1,
+        "xin_bwd": xbwd < MXG,
+        "xout_bwd": xbwd > nx + MXG - 1,
+    }
+    regions = {k: v.astype(int) for k, v in regions.items()}
+
+    for x in "xout", "xin":
+        regions[x] = regions[f"{x}_fwd"] + regions[f"{x}_bwd"]
+    for x in "fwd", "bwd":
+        regions[x] = regions[f"xin_{x}"] + regions[f"xout_{x}"]
+    regions["all"] = regions["xin"] + regions["xout"]
+
+    for nproc in nprocs:
+        cmd = "./get_par_bndry"
+        _, out = launch_safe(cmd, nproc=nproc, mthread=mthread, pipe=True)
+
+        for k, v in regions.items():
+            data = collect(
+                f"field_{k}",
+                info=False,
+                path=directory,
+                xguards=False,
+                yguards=False,
+            )
+            assert np.allclose(data, v), (
+                f"{k} does not match",
+                np.sum(data),
+                np.sum(v),
+                np.max(data),
+            )
