8d87bdafa65a41e158f41536b4f5599e63e47eed
tomc <tomc271@gmail.com>
2025-12-28 19:38:42 +0000

    Modify test_smooth to run via pytest

--- tests/integrated/test-smooth/test_smooth.py
+++ tests/integrated/test-smooth/test_smooth.py
@@ -14,51 +14,47 @@ tol = 1e-7  # Absolute tolerance, benchmark values are floats
 from boututils.run_wrapper import build_and_log, shell, launch_safe
 from boutdata.collect import collect
 import numpy as np
-from sys import stdout, exit
+from sys import stdout
 
-build_and_log("smoothing operator test")
 
-# Read benchmark values
-print("Reading benchmark data")
-bmk = {}
-for v in vars:
-    bmk[v] = collect(v, path="data", prefix="benchmark", info=False)
+def test_smooth():
 
-print("Running smoothing operator test")
-success = True
+    # Read benchmark values
+    print("Reading benchmark data")
+    bmk = {}
+    for v in vars:
+        bmk[v] = collect(v, path="data", prefix="benchmark", info=False)
 
-for nype in [1, 2]:
-    for nxpe in [1, 2]:
-        nproc = nxpe * nype
-        cmd = "./test_smooth"
+    print("Running smoothing operator test")
+    success = True
 
-        shell("rm data/BOUT.dmp.*.nc")
+    for nype in [1, 2]:
+        for nxpe in [1, 2]:
+            nproc = nxpe * nype
+            cmd = "./test_smooth"
 
-        print("   %d processor (%d x %d)...." % (nproc, nxpe, nype))
-        s, out = launch_safe(cmd + " NXPE=" + str(nxpe), nproc=nproc, pipe=True)
-        with open("run.log." + str(nproc), "w") as f:
-            f.write(out)
+            shell("rm data/BOUT.dmp.*.nc")
 
-        # Collect output data
-        for v in vars:
-            stdout.write("      Checking variable " + v + " ... ")
-            result = collect(v, path="data", info=False)
-            # Compare benchmark and output
-            if np.shape(bmk[v]) != np.shape(result):
-                print("Fail, wrong shape")
-                success = False
-                continue
+            print("   %d processor (%d x %d)...." % (nproc, nxpe, nype))
+            s, out = launch_safe(cmd + " NXPE=" + str(nxpe), nproc=nproc, pipe=True)
+            with open("run.log." + str(nproc), "w") as f:
+                f.write(out)
 
-            diff = np.max(np.abs(bmk[v] - result))
-            if diff > tol:
-                print("Fail, maximum difference = " + str(diff))
-                success = False
-            else:
-                print("Pass")
+            # Collect output data
+            for v in vars:
+                stdout.write("      Checking variable " + v + " ... ")
+                result = collect(v, path="data", info=False)
+                # Compare benchmark and output
+                if np.shape(bmk[v]) != np.shape(result):
+                    print("Fail, wrong shape")
+                    success = False
+                    continue
 
-if success:
-    print(" => All smoothing operator tests passed")
-    exit(0)
-else:
-    print(" => Some failed tests")
-    exit(1)
+                diff = np.max(np.abs(bmk[v] - result))
+                if diff > tol:
+                    print("Fail, maximum difference = " + str(diff))
+                    success = False
+                else:
+                    print("Pass")
+
+    assert success, " => Some failed tests"
