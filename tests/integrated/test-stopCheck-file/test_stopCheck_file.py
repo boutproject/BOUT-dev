ada42eb573b2a1f60c50535ee9ee641bb514faf7
tomc <tomc271@gmail.com>
2025-12-30 13:37:12 +0000

    Modify test_stopCheck_file to run via pytest

--- tests/integrated/test-stopCheck-file/test_stopCheck_file.py
+++ tests/integrated/test-stopCheck-file/test_stopCheck_file.py
@@ -9,45 +9,42 @@ from boutdata.collect import collect
 
 nproc = 1
 
-build_and_log("stopCheck test")
+def test_stopCheck_file():
 
-check_values = [True, False]
-expected_steps = [1, 11]
-data_dirs = ["data", "dataSecond"]
-stop_files = ["BOUT.stop", "otherStop.check"]
+    build_and_log("stopCheck test")
 
-print("Running stopCheck test")
-success = True
+    check_values = [True, False]
+    expected_steps = [1, 11]
+    data_dirs = ["data", "dataSecond"]
+    stop_files = ["BOUT.stop", "otherStop.check"]
 
-for data_dir, stop_file in zip(data_dirs, stop_files):
-    for check, expected in zip(check_values, expected_steps):
-        executable = "./test_stopCheck"
-        command = "{} -d {} stopCheck={} stopCheckName={}".format(
-            executable, data_dir, check, stop_file
-        )
+    print("Running stopCheck test")
+    success = True
 
-        s, out = launch(
-            command,
-            nproc=nproc,
-            pipe=True,
-        )
-        with open("run.log.{}.{}".format(data_dir, check), "w") as f:
-            f.write(out)
+    for data_dir, stop_file in zip(data_dirs, stop_files):
+        for check, expected in zip(check_values, expected_steps):
+            executable = "./test_stopCheck"
+            command = "{} -d {} stopCheck={} stopCheckName={}".format(
+                executable, data_dir, check, stop_file
+            )
 
-        result = collect("t_array", path=data_dir, info=False)
+            s, out = launch(
+                command,
+                nproc=nproc,
+                pipe=True,
+            )
+            with open("run.log.{}.{}".format(data_dir, check), "w") as f:
+                f.write(out)
 
-        if result.shape[0] != expected:
-            print("Fail, wrong shape")
-            print("\tOption is {}/{}/{}".format(check, data_dir, stop_file))
-            print("\tshape is {}".format(result.shape[0]))
-            print("\texpecting {}".format(expected))
-            sys.exit(1)
-            success = False
-            continue
+            result = collect("t_array", path=data_dir, info=False)
 
-if success:
-    print(" => All checkStop tests passed")
-    sys.exit(0)
-else:
-    print(" => Some failed tests")
-    sys.exit(1)
+            if result.shape[0] != expected:
+                print("Fail, wrong shape")
+                print("\tOption is {}/{}/{}".format(check, data_dir, stop_file))
+                print("\tshape is {}".format(result.shape[0]))
+                print("\texpecting {}".format(expected))
+                sys.exit(1)
+                success = False
+                continue
+
+    assert success, " => Some failed tests"
