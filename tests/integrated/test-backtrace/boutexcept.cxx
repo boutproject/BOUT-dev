8edb351b1f75382ae014f770676c239ee92e74be
Peter Hill <peter.hill@york.ac.uk>
2026-01-08 16:01:24 +0000

    tests: Fix exception test inlining functions

--- tests/integrated/test-backtrace/boutexcept.cxx
+++ tests/integrated/test-backtrace/boutexcept.cxx
@@ -1,10 +1,12 @@
 #include "bout/boutexception.hxx"
 
-namespace {
+// Can't use anonymous namespace, or the compiler will inline everything,
+// defeating the point of this test.
+// NOLINTBEGIN(misc-use-internal-linkage)
 void troublemaker() { throw BoutException("test"); }
 void f() { troublemaker(); }
 void e() { f(); }
-} // namespace
+// NOLINTEND(misc-use-internal-linkage)
 
 int main() {
   e();
