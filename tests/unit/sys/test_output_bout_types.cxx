459724c1712ead503e83dff52e95e37c593b6b88
Peter Hill <peter.hill@york.ac.uk>
2026-02-24 21:08:22 +0000

    Consolidate Field formatters into one implementation

--- tests/unit/sys/test_output_bout_types.cxx
+++ tests/unit/sys/test_output_bout_types.cxx
@@ -3,8 +3,10 @@
 #include "gtest/gtest.h"
 
 #include "bout/field2d.hxx"
+#include "bout/field3d.hxx"
+#include "bout/fieldperp.hxx"
 #include "bout/globals.hxx"
-#include "bout/output_bout_types.hxx"
+#include "bout/output_bout_types.hxx" // IWYU pragma: keep
 
 #include <string>
 
@@ -20,11 +22,23 @@ TEST_F(FormatFieldTest, Field2D) {
   const auto out = fmt::format("{}", f);
 
   const std::string expected =
-      R"((0, 0): 0; (0, 1): 1; (0, 2): 2; (0, 3): 3; (0, 4): 4;
-(1, 0): 5; (1, 1): 6; (1, 2): 7; (1, 3): 8; (1, 4): 9;
-(2, 0): 10; (2, 1): 11; (2, 2): 12; (2, 3): 13; (2, 4): 14;
+    R"((0, 0): 0;
+(0, 1): 1;
+(0, 2): 2;
+(0, 3): 3;
+(0, 4): 4;
 
-)";
+(1, 0): 5;
+(1, 1): 6;
+(1, 2): 7;
+(1, 3): 8;
+(1, 4): 9;
+
+(2, 0): 10;
+(2, 1): 11;
+(2, 2): 12;
+(2, 3): 13;
+(2, 4): 14;)";
   EXPECT_EQ(out, expected);
 }
 
@@ -36,11 +50,23 @@ TEST_F(FormatFieldTest, Field2DSpec) {
   const auto out = fmt::format("{:3.1e}", f);
 
   const std::string expected =
-      R"((0, 0): 0.0e+00; (0, 1): 1.0e+00; (0, 2): 2.0e+00; (0, 3): 3.0e+00; (0, 4): 4.0e+00;
-(1, 0): 5.0e+00; (1, 1): 6.0e+00; (1, 2): 7.0e+00; (1, 3): 8.0e+00; (1, 4): 9.0e+00;
-(2, 0): 1.0e+01; (2, 1): 1.1e+01; (2, 2): 1.2e+01; (2, 3): 1.3e+01; (2, 4): 1.4e+01;
+    R"((0, 0): 0.0e+00;
+(0, 1): 1.0e+00;
+(0, 2): 2.0e+00;
+(0, 3): 3.0e+00;
+(0, 4): 4.0e+00;
 
-)";
+(1, 0): 5.0e+00;
+(1, 1): 6.0e+00;
+(1, 2): 7.0e+00;
+(1, 3): 8.0e+00;
+(1, 4): 9.0e+00;
+
+(2, 0): 1.0e+01;
+(2, 1): 1.1e+01;
+(2, 2): 1.2e+01;
+(2, 3): 1.3e+01;
+(2, 4): 1.4e+01;)";
   EXPECT_EQ(out, expected);
 }
 
@@ -70,10 +96,7 @@ TEST_F(FormatFieldTest, Field3D) {
 (2, 1, 0): 22; (2, 1, 1): 23;
 (2, 2, 0): 24; (2, 2, 1): 25;
 (2, 3, 0): 26; (2, 3, 1): 27;
-(2, 4, 0): 28; (2, 4, 1): 29;
-
-
-)";
+(2, 4, 0): 28; (2, 4, 1): 29;)";
   EXPECT_EQ(out, expected);
 }
 
@@ -103,10 +126,7 @@ TEST_F(FormatFieldTest, Field3DSpec) {
 (2, 1, 0): 2.2e+01; (2, 1, 1): 2.3e+01;
 (2, 2, 0): 2.4e+01; (2, 2, 1): 2.5e+01;
 (2, 3, 0): 2.6e+01; (2, 3, 1): 2.7e+01;
-(2, 4, 0): 2.8e+01; (2, 4, 1): 2.9e+01;
-
-
-)";
+(2, 4, 0): 2.8e+01; (2, 4, 1): 2.9e+01;)";
   EXPECT_EQ(out, expected);
 }
 
@@ -123,10 +143,10 @@ TEST_F(FormatFieldTest, FieldPerp) {
 
   const std::string expected =
       R"((0, 0): 0; (0, 1): 1;
-(1, 0): 10; (1, 1): 11;
-(2, 0): 20; (2, 1): 21;
 
-)";
+(1, 0): 10; (1, 1): 11;
+
+(2, 0): 20; (2, 1): 21;)";
   EXPECT_EQ(out, expected);
 }
 
@@ -143,9 +163,9 @@ TEST_F(FormatFieldTest, FieldPerpSpec) {
 
   const std::string expected =
       R"((0, 0): 0.0e+00; (0, 1): 1.0e+00;
-(1, 0): 1.0e+01; (1, 1): 1.1e+01;
-(2, 0): 2.0e+01; (2, 1): 2.1e+01;
 
-)";
+(1, 0): 1.0e+01; (1, 1): 1.1e+01;
+
+(2, 0): 2.0e+01; (2, 1): 2.1e+01;)";
   EXPECT_EQ(out, expected);
 }
