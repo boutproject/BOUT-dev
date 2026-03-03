8d5cb39e03c2644715a50684f8cd0318b4e82676
dschwoerer <5637662+dschwoerer@users.noreply.github.com>
2026-03-03 13:33:29 +0000

    [bot] Apply format changes

--- src/mesh/impls/bout/boutmesh.cxx
+++ src/mesh/impls/bout/boutmesh.cxx
@@ -497,8 +497,8 @@ int BoutMesh::load() {
   }
   if (meshHasMyg && MYG != meshMyg) {
     output_warn.write(_("Options changed the number of y-guard cells. Grid has {} but "
-                           "option specified {}! Continuing with {}"),
-                         meshMyg, MYG, MYG);
+                        "option specified {}! Continuing with {}"),
+                      meshMyg, MYG, MYG);
   }
   ASSERT0(MYG >= 0);
 
