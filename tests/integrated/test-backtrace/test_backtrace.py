ca85ceb6c7ed5157b4e6296bf6b76dc9cb6297e6
tomc <tomc271@gmail.com>
2026-03-04 14:33:41 +0000

    Fix test_backtrace

--- tests/integrated/test-backtrace/test_backtrace.py
+++ tests/integrated/test-backtrace/test_backtrace.py
@@ -14,24 +14,10 @@ def test_backtrace():
     except KeyError:
         pass
 
-    success = True
+    _, output = shell(["BOUT_SHOW_BACKTRACE=0 ./boutexcept"], pipe=True)
+
+    assert "troublemaker" not in output, f"Fail: detected offending function name in output when not expected\n{output}"
 
     _, output = shell(["./boutexcept"], pipe=True)
 
-    if "troublemaker" in output:
-        success = False
-        pytest.fail("Fail: detected offending function name in output when not expected")
-
-    _, output = shell(["BOUT_SHOW_BACKTRACE=yes ./boutexcept"], pipe=True)
-
-    if "troublemaker" not in output:
-        success = False
-        print("Fail: did not detect offending function name in output when expected")
-
-    if success:
-        print("=> All BoutException backtrace tests passed")
-        exit(0)
-    assert success, f"Test failed"
-
-    print("=> Some failed tests")
-    exit(1)
+    assert "troublemaker" in output, f"Fail: did not detect offending function name in output when expected\n{output}"
