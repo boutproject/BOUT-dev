22ec1526a831a7a6e35ab682ea880addfcf54c1e
tomc <tomc271@gmail.com>
2026-03-13 11:09:10 +0000

    Convert test_boutpp_print to from a shell script to python

--- tests/integrated/test-boutpp/print/test_boutpp_print.py
+++ tests/integrated/test-boutpp/print/test_boutpp_print.py
@@ -1,4 +1,17 @@
+import subprocess
 
 
-def test_runtest(assert_success_in_shell):
-    assert_success_in_shell("./runtest")
+def test_boutpp_print():
+
+    msg = 'Can we print to the log from python? 🎉 __does_it_still__work {} {:s}'
+    cmd = ["python3", "test.py"] + msg.split()
+
+    print(f"+ python3 test.py {msg} > out.log")
+    with open("out.log", "w") as f:
+        subprocess.run(cmd, stdout=f, check=True)
+
+    with open("out.log", "r") as f:
+        assert msg in f.read(), f"Error: '{msg}' not found in out.log"
+
+    with open("test/BOUT.log.0", "r") as f:
+        assert msg in f.read(), f"Error: '{msg}' not found in test/BOUT.log.0"
