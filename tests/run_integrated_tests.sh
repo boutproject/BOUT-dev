4681b195057d35b48a30ea430b16f569c758c2a1
tomc <tomc271@gmail.com>
2026-03-14 20:37:35 +0000

    Refactor run_integrated_tests.sh (to use `PROJECT_ROOT`)

--- tests/run_integrated_tests.sh
+++ tests/run_integrated_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-HERE=$(cd "$(dirname "$0")" && pwd)
-export PYTHONPATH="$HERE/../tools/pylib"
+PROJECT_ROOT=$(cd "$(dirname "$0")/.." && pwd)
+
+export PYTHONPATH="$PROJECT_ROOT/tools/pylib"
 
 # Pre-build the project to prevent concurrent CMake race conditions
-cmake --build "$HERE/.."
+cmake --build "$PROJECT_ROOT"
 
-pytest -m "not serial" --cache-clear -n auto --dist=loadgroup -q "$HERE/integrated" "$@"
-pytest -m serial "$HERE/integrated" "$@"
+pytest -m "not serial" --cache-clear -n auto --dist=loadgroup -q "$PROJECT_ROOT/tests/integrated" "$@"
+pytest -m serial "$PROJECT_ROOT/tests/integrated" "$@"
