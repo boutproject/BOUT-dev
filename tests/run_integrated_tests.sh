0b3d631816d8dc2f02cade2819b2144b8da99845
tomc <tomc271@gmail.com>
2026-03-14 19:27:23 +0000

    Set absolute path to boupp in PYTHONPATH

--- tests/run_integrated_tests.sh
+++ tests/run_integrated_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-export PYTHONPATH='../tools/pylib'
+HERE=$(cd "$(dirname "$0")" && pwd)
+export PYTHONPATH="$HERE/../tools/pylib"
 
 pytest -m "not serial" --cache-clear -n auto --dist=loadgroup integrated $@
 pytest -m serial integrated $@
