d4e137c7536a0e8ae183a65e73e1ea0608af0b67
David Bold <dave@ipp.mpg.de>
2026-03-18 13:08:03 +0100

    CI: ensure pip is installed

--- .ci_fedora.sh
+++ .ci_fedora.sh
@@ -47,6 +47,7 @@ test . != ".$1" && mpi="$1" || mpi=openmpi
     export OMP_NUM_THREADS=1
     cd
     cd BOUT-dev
+    python3 -m ensurepip
     python3 -m pip install git+https://github.com/boutproject/zoidberg@better-metric
     echo "starting configure"
     time cmake -S . -B build -DBOUT_USE_PETSC=ON \
