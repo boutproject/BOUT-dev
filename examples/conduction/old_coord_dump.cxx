83ecdfa78c9426992773bab864fd638170cae390
wearysebas <sebichito@gmail.com>
2026-03-20 11:26:28 +0000

    fixed old_coord_dmp.cxx

--- examples/conduction/old_coord_dump.cxx
+++ examples/conduction/old_coord_dump.cxx
@@ -11,7 +11,11 @@ int main(int argc, char** argv) {
   FieldFactory factory {mesh, &Options::root()};
   dump["x"] = factory.create3D("x");
   dump["y"] = factory.create3D("y");
-  bout::writeDefaultOutputFile(dump);
+
+  // Write to BOUT_coord.dmp.* instead of the default BOUT.dmp.*
+  Options coord_out_opts;
+  coord_out_opts["prefix"] = "BOUT_coord.dmp";
+  bout::OptionsIOFactory::getInstance().createOutput(&coord_out_opts)->write(dump);
 
   BoutFinalise();
 
