2dfa9503d3a3c705d547cb5e0975fec6431e8bd5
David Bold <dave@ipp.mpg.de>
2026-04-15 14:05:28 +0200

    Do not move trivially-copyable variable

--- include/bout/parallel_boundary_region.hxx
+++ include/bout/parallel_boundary_region.hxx
@@ -50,9 +50,9 @@ struct Indices {
   signed char valid;
   signed char offset;
   unsigned char abs_offset;
-  Indices(Ind3D index, RealPoint&& intersection, BoutReal length, signed char valid,
+  Indices(Ind3D index, RealPoint intersection, BoutReal length, signed char valid,
           signed char offset, unsigned char abs_offset)
-      : index(index), intersection(std::move(intersection)), length(length), valid(valid),
+      : index(index), intersection(intersection), length(length), valid(valid),
         offset(offset), abs_offset(abs_offset) {};
 };
 
