2f99557f77123f40ef5a832f11a4f101c4681ba4
tomc271 <tomc271@gmail.com>
2026-04-28 15:58:44 +0100

    Add autouse fixture to call boutpp.finalise()

--- tests/integrated/test-boutpp/conftest.py
+++ tests/integrated/test-boutpp/conftest.py
@@ -5,3 +5,9 @@ def unique_xdist_group(request):
     # Unique group per test function (nodeid is unique, e.g., test_file.py::test_func)
     group_name = f"boutpp_isolated_{request.node.nodeid.replace('/', '_').replace('::', '_')}"
     request.node.add_marker(pytest.mark.xdist_group(name=group_name))
+
+@pytest.fixture(autouse=True, scope="function")
+def cleanup_boutpp():
+    yield
+    import boutpp
+    boutpp.finalise()
