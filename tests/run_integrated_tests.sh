04c5837d4d2da12f51dc32ac2c05dff13bb72778
tomc271 <tomc271@gmail.com>
2026-04-29 10:58:04 +0100

    Run pytest verbosely when CI flag is true

--- tests/run_integrated_tests.sh
+++ tests/run_integrated_tests.sh
@@ -2,12 +2,18 @@
 set -e
 
 PROJECT_ROOT="@PROJECT_BINARY_DIR@"
-
 export PYTHONPATH="@PROJECT_BINARY_DIR@/tools/pylib:@PROJECT_SOURCE_DIR@/tools/pylib:$PYTHONPATH"
 
 # Pre-build the project to prevent concurrent CMake race conditions
 cmake --build "$PROJECT_ROOT"
 
+PYTEST_ARGS=("$@")
+if [ "$CI" == "true" ]; then
+    TEST_FLAGS="-v"
+else
+    TEST_FLAGS="-q"
+fi
+
 # Use the Python executable that CMake discovered during configuration
-"@Python3_EXECUTABLE@" -m pytest -m "not serial" --cache-clear -n auto --dist=loadgroup -q "$PROJECT_ROOT/tests/integrated" "$@"
-"@Python3_EXECUTABLE@" -m pytest -m serial "$PROJECT_ROOT/tests/integrated" "$@"
+"@Python3_EXECUTABLE@" -m pytest -m "not serial" --cache-clear -n auto --dist=loadgroup $TEST_FLAGS "$PROJECT_ROOT/tests/integrated" "${PYTEST_ARGS[@]}"
+"@Python3_EXECUTABLE@" -m pytest -m serial $TEST_FLAGS "$PROJECT_ROOT/tests/integrated" "${PYTEST_ARGS[@]}"
