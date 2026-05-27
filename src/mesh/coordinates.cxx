503c25c4b00e4b4e702107db0a7ec7442cde3d83
David Bold <dave@ipp.mpg.de>
2026-05-27 11:05:10 +0200

    Use getLegacyPointer

--- src/mesh/coordinates.cxx
+++ src/mesh/coordinates.cxx
@@ -55,8 +55,7 @@ Field2D interpolateAndExtrapolate(const Field2D& f, CELL_LOC location, bool extr
   // Also, here we interpolate for the boundary points at xstart/ystart and
   // (xend+1)/(yend+1) instead of extrapolating.
   for (auto& newbndry : localmesh->getBoundaries()) {
-    auto bndry = dynamic_cast<BoundaryRegion*>(newbndry);
-    ASSERT0(bndry != nullptr);
+    auto bndry = newbndry->getLegacyPointer();
     if ((extrapolate_x and bndry->bx != 0) or (extrapolate_y and bndry->by != 0)) {
       int extrap_start = 0;
       if (not no_extra_interpolate) {
@@ -206,8 +205,7 @@ Field3D interpolateAndExtrapolate(const Field3D& f_, CELL_LOC location,
   // Also, here we interpolate for the boundary points at xstart/ystart and
   // (xend+1)/(yend+1) instead of extrapolating.
   for (auto& newbndry : localmesh->getBoundaries()) {
-    auto bndry = dynamic_cast<BoundaryRegion*>(newbndry);
-    ASSERT0(bndry != nullptr);
+    auto bndry = newbndry->getLegacyPointer();
     if ((extrapolate_x and bndry->bx != 0) or (extrapolate_y and bndry->by != 0)) {
       int extrap_start = 0;
       if (not no_extra_interpolate) {
