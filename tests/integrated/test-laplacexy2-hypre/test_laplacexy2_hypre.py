7f920ac02550edf26ad613a62dc0d7627229fa7f
tomc271 <tomc271@gmail.com>
2026-06-11 10:47:32 +0100

    Refactor test_laplacexy2_hypre for pytest

--- tests/integrated/test-laplacexy2-hypre/test_laplacexy2_hypre.py
+++ tests/integrated/test-laplacexy2-hypre/test_laplacexy2_hypre.py
@@ -7,11 +7,12 @@
 # requires: hypre
 # cores: 8
 
-
+import pytest
 from boutdata.collect import collect
 from boututils.run_wrapper import launch_safe, shell
 
 tol = 5.0e-8
+nproc = 8
 
 argslist = [
     "laplacexy:core_bndry_dirichlet=true laplacexy:pf_bndry_dirichlet=true laplacexy:y_bndry_dirichlet=true "
@@ -37,32 +38,24 @@ argslist = [
 ]
 
 
-def test_laplacexy2_hypre():
+@pytest.mark.parametrize("args", argslist)
+def test_laplacexy2_hypre(args):
 
-    print("Running LaplaceXY inversion test")
-    success = True
+    print("   %d processors...." % nproc)
 
-    for nproc in [8]:
-        print("   %d processors...." % nproc)
-        for args in argslist:
-            cmd = "./test-laplacexy " + args
+    cmd = f"./test-laplacexy {args}"
 
-            shell(["rm data/BOUT.dmp.*.nc > /dev/null 2>&1"])
+    shell(["rm data/BOUT.dmp.*.nc > /dev/null 2>&1"])
 
-            s, out = launch_safe(cmd, nproc=nproc, pipe=True, verbose=True)
+    s, out = launch_safe(cmd, nproc=nproc, pipe=True, verbose=True)
 
-            with open(f"run.log.{nproc}", "w") as f:
-                f.write(out)
+    with open(f"run.log.{nproc}", "w") as f:
+        f.write(out)
 
-            # Collect output data
-            error = collect("max_error", path="data", info=False)
-            if error <= 0:
-                print("Convergence error")
-                success = False
-            elif error > tol:
-                print("Fail, maximum error is = " + str(error))
-                success = False
-            else:
-                print("Pass")
+    # Collect output data
+    error = collect("max_error", path="data", info=False)
 
-    assert success, " => Some failed tests"
+    assert error > 0, f"Convergence error: maximum error is {error}"
+    assert error <= tol, (
+        f"Fail: maximum error is {error}, which exceeds tolerance {tol}"
+    )
