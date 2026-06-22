70e341d33f81a5acff5baa9feb6d2201c06c9692
tomc271 <tomc271@gmail.com>
2026-06-22 14:57:13 +0100

    Refactor test_fieldgroupComm for pytest

--- tests/integrated/test-fieldgroupComm/test_fieldgroupComm.py
+++ tests/integrated/test-fieldgroupComm/test_fieldgroupComm.py
@@ -9,52 +9,43 @@
 # Requires: not metric_3d
 # Cores: 4
 
-
+import pytest
 from boututils.run_wrapper import shell, launch_safe
 from boutdata.collect import collect
 from numpy import abs, seterr
 
 
-def test_fieldgroupcomm():
-
+@pytest.mark.parametrize("nproc", [1, 2, 4])
+def test_fieldgroupcomm(nproc):
     # Good chance we'll do 0.0/0.0, which generates a warning
     # Ignore this warning
     seterr(divide="ignore", invalid="ignore")
 
-    varCorrect = "fld1"
-    varsComp = ["fld2", "fld3"]
-    name = "FieldGroup comm"
-    exeName = "test_fieldgroupcomm"
+    var_correct = "fld1"
+    vars_comp = ["fld2", "fld3"]
+    exe_name = "test_fieldgroupcomm"
     tol = 1e-10  # Relative tolerance
 
-    print("Running {nm} test".format(nm=name))
-    success = True
+    cmd = f"./{exe_name}"
 
-    for nproc in [1, 2, 4]:
-        cmd = "./{exe} ".format(exe=exeName)
+    shell(["rm -f data/BOUT.dmp.*.nc"])
 
-        shell("rm data/BOUT.dmp.*.nc")
+    print(f"Running FieldGroup comm test on {nproc} processor(s)...")
+    s, out = launch_safe(cmd, nproc=nproc, pipe=True)
+    with open(f"run.log.{nproc}", "w") as f:
+        f.write(out)
 
-        print("   %d processors ...." % (nproc))
-        s, out = launch_safe(cmd, nproc=nproc, pipe=True)
-        with open("run.log." + str(nproc), "w") as f:
-            f.write(out)
+    # Analyse result
+    # /"Correct" answer
+    f1 = collect(var_correct, path="data", info=False)
+    f1max = abs(f1).max()
+    # /Two different fields which should be identical to correct
+    err = []
+    for v in vars_comp:
+        tmp = collect(v, path="data", info=False)
+        relative_error = abs(f1 - tmp).max() / f1max
 
-        # Analyse result
-        # /"Correct" answer
-        f1 = collect(varCorrect, path="data", info=False)
-        f1max = abs(f1).max()
-        # /Two different fields which should be identical to correct
-        err = []
-        for v in varsComp:
-            tmp = collect(v, path="data", info=False)
-            err.append(abs((f1 - tmp)).max() / f1max)
+        if relative_error > tol:
+            err.append(f"Field '{v}' mismatch: relative error is {relative_error:.4e})")
 
-        for i, e in enumerate(err):
-            if e > tol:
-                print(
-                    "Fail, in {i}th comparison relative error is {re}".format(i=i, re=e)
-                )
-                success = False
-
-    assert success, " => Some failed tests"
+    assert not err, f"Failures encountered on {nproc} processor(s):\n" + "\n".join(err)
