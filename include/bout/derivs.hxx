b89867dd3a54d17d1c7b4174a79baabebec44c54
David Bold <dave@ipp.mpg.de>
2026-06-24 14:11:38 +0200

    Add DDY template for expression

--- include/bout/derivs.hxx
+++ include/bout/derivs.hxx
@@ -86,6 +86,13 @@ Field3D DDY(const Field3DParallel& f, CELL_LOC outloc = CELL_DEFAULT,
             const std::string& method = "DEFAULT",
             const std::string& region = "RGN_NOBNDRY");
 
+template <typename E>
+std::enable_if_t<is_expr_field3d_v<E> && !bout::utils::is_Field3D_v<E>, const Field3D>
+DDY(const E& expr, CELL_LOC outloc = CELL_DEFAULT, const std::string& method = "DEFAULT",
+    const std::string& region = "RGN_NOBNDRY") {
+  return DDY(Field3D{expr}, outloc, method, region);
+}
+
 /// Calculate first partial derivative in Y
 ///
 ///   \f$\partial / \partial y\f$
