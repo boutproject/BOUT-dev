fdf757af26af8036fdfc75d7525c10d0c75807e9
Ben Dudson <dudson2@llnl.gov>
2026-06-24 17:26:07 -0700

    Fix 2D metric build with BinaryExpr
    Field2D expressions need to be evaluated before assignment to Field3D.

--- src/mesh/petsc_operators.cxx
+++ src/mesh/petsc_operators.cxx
@@ -359,14 +359,14 @@ PetscOperators::Parallel PetscOperators::getParallel() const {
   auto* coords = mesh->getCoordinates();
 
   // Parallel spacing in cell space
-  Field3D dl = coords->dy * sqrt(coords->g_22);
+  Field3D dl = Coordinates::FieldMetric{coords->dy * sqrt(coords->g_22)};
   dl.splitParallelSlices();
   dl.yup() = 0.0;
   dl.ydown() = 0.0;
   dl.applyParallelBoundary("parallel_neumann_o1");
 
   // Cell volume
-  Field3D dV = coords->J * coords->dx * coords->dy * coords->dz;
+  Field3D dV = Coordinates::FieldMetric{coords->J * coords->dx * coords->dy * coords->dz};
   dV.splitParallelSlices();
   dV.yup() = 0.0;
   dV.ydown() = 0.0;
