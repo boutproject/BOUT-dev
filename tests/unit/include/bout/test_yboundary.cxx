c47e72c76736c18e7499dc0166c6b21279438fcc
David Bold <dave@ipp.mpg.de>
2026-06-26 16:07:30 +0200

    Add interpolate_boundary_o2 test

--- tests/unit/include/bout/test_yboundary.cxx
+++ tests/unit/include/bout/test_yboundary.cxx
@@ -17,3 +17,18 @@ TEST_F(YBTest, dirichlet_o2) {
   sheath.iter([&](auto& point) { point.dirichlet_o2(test, 2); });
   EXPECT_TRUE(IsFieldEqual(test, 3.0, "RGN_YGUARDS"));
 }
+
+// Not yet implemented for FA
+// TEST_F(YBTest, dirichlet_o3) {
+//   Field3D test = 1.0;
+//   YBoundary sheath(YBndryType::all, nullptr, *mesh);
+//   sheath.iter([&](auto& point) { point.dirichlet_o3(test, 2); });
+//   EXPECT_TRUE(IsFieldEqual(test, 3.0, "RGN_YGUARDS"));
+// }
+
+TEST_F(YBTest, interpolate_boundary_o2) {
+  Field3D test = 1.0;
+  YBoundary sheath(YBndryType::all, nullptr, *mesh);
+  sheath.iter(
+      [&](auto& point) { EXPECT_DOUBLE_EQ(point.interpolate_boundary_o2(test), 1.0); });
+}
