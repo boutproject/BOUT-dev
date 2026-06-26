13d4719e00795ff6c1ec82446ad873da931f7dcb
David Bold <dave@ipp.mpg.de>
2026-06-26 11:38:20 +0200

    Cast as Field

--- src/mesh/petsc_operators.cxx
+++ src/mesh/petsc_operators.cxx
@@ -332,20 +332,20 @@ PetscOperators::Parallel PetscOperators::getParallel() const {
   auto* coords = mesh->getCoordinates();
 
   // Parallel spacing in cell space
-  auto dl = coords->dy * sqrt(coords->g_22);
+  Coorinates::FieldMetric dl = coords->dy * sqrt(coords->g_22);
   dl.splitParallelSlices();
   dl.yup() = 0.0;
   dl.ydown() = 0.0;
   dl.applyParallelBoundary("parallel_neumann_o1");
 
   // Cell volume
-  auto dV = coords->J * coords->dx * coords->dy * coords->dz;
+  Coorinates::FieldMetric dV = coords->J * coords->dx * coords->dy * coords->dz;
   dV.splitParallelSlices();
   dV.yup() = 0.0;
   dV.ydown() = 0.0;
   dV.applyParallelBoundary("parallel_neumann_o1");
 
-  auto neg_inv_dV = -1.0 / dV;
+  Coorinates::FieldMetric neg_inv_dV = -1.0 / dV;
   neg_inv_dV.splitParallelSlices();
   neg_inv_dV.yup() = 0.0;
   neg_inv_dV.ydown() = 0.0;
