44700a2a41802ebab9304952e5cbf0db66e0c826
David Bold <dave@ipp.mpg.de>
2026-06-30 14:28:45 +0200

    status is always true

--- tests/integrated/test-vec/test_vec.py
+++ tests/integrated/test-vec/test_vec.py
@@ -5,9 +5,7 @@ from boututils.run_wrapper import launch_safe
 
 def test_vec():
 
-    status, out = launch_safe("./testVec", nproc=4, pipe=True)
+    _, out = launch_safe("./testVec", nproc=4, pipe=True)
 
     with open("run.log.4", "w") as f:
         f.write(out)
-
-    assert not status, " => Test failed"
