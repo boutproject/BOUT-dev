88af6c53288a7ab3cb69029129a4a243609ef922
David Bold <dave@ipp.mpg.de>
2026-06-30 15:29:02 +0200

    Revert "prefer launch for MPI aware jobs"
    This reverts commit 3e91960bc8849c9dcec64fdde1d102fe4ee2e8ed.
    
    We do not call MPI_finalize.
    This make mpirun unhappy.

--- tests/integrated/test-beuler/test_beuler.py
+++ tests/integrated/test-beuler/test_beuler.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-from boututils.run_wrapper import launch_safe
+# requires: petsc
+
+from boututils.run_wrapper import shell_safe
 
 
 def test_beuler():
     print("Running solver test")
-    status, out = launch_safe("./test_beuler", nproc=1, mthread=1, pipe=True)
+    status, out = shell_safe("./test_beuler", pipe=True)
     with open("run.log", "w") as f:
         f.write(out)
 
