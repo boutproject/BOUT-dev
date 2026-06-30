b7dc440e85f945919ea6c42d318e2928b534e2cb
David Bold <dave@ipp.mpg.de>
2026-06-30 14:17:18 +0200

    Do not duplicate runs for different variables

--- tests/integrated/test-yupdown/test_yupdown.py
+++ tests/integrated/test-yupdown/test_yupdown.py
@@ -6,7 +6,6 @@ from boutdata.collect import collect
 from numpy import max, abs
 
 shift_types = ["shifted", "shiftedinterp"]
-variables = [("ddy", "ddy_check"), ("ddy2", "ddy_check")]
 
 
 def run_case(shift_type, variable):
@@ -20,24 +19,26 @@ def run_case(shift_type, variable):
     with open("run.log", "w") as f:
         f.write(out)
 
-    v, v_check = variable
-    print("Testing %s and %s ... " % (v, v_check))
-    ddy = collect(v, path="data", xguards=False, yguards=False, info=False)
-    ddy_check = collect(v_check, path="data", xguards=False, yguards=False, info=False)
+    ret = True
+    for v, v_check in [("ddy", "ddy_check"), ("ddy2", "ddy_check")]:
+        print("Testing %s and %s ... " % (v, v_check))
+        ddy = collect(v, path="data", xguards=False, yguards=False, info=False)
+        ddy_check = collect(
+            v_check, path="data", xguards=False, yguards=False, info=False
+        )
 
-    diff = max(abs(ddy - ddy_check))
+        diff = max(abs(ddy - ddy_check))
 
-    print(f"shifttype: {shift_type} Max difference {diff}")
-    if diff < 2e-5:
-        return True
-    return False
+        print(f"shifttype: {shift_type} Max difference {diff}")
+        if diff >= 2e-5:
+            ret = False
+    return ret
 
 
 @pytest.mark.parametrize("shift_type", shift_types)
-@pytest.mark.parametrize("variable", variables)
 def test_case(shift_type, variable):
     # MPI oversubscribe
     os.environ["OMPI_MCA_rmaps_base_oversubscribe"] = "1"
 
     success = run_case(shift_type, variable)
-    assert success, f"Test failed for shift_type={shift_type}, variable={variable}"
+    assert success, f"Test failed for shift_type={shift_type}"
