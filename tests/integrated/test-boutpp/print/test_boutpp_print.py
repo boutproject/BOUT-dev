6e0c09a97805e9fbc280286f2f0b44eb13f8eb49
David Bold <dave@ipp.mpg.de>
2026-06-30 14:19:26 +0200

    Remove print statement

--- tests/integrated/test-boutpp/print/test_boutpp_print.py
+++ tests/integrated/test-boutpp/print/test_boutpp_print.py
@@ -9,7 +9,6 @@ def test_boutpp_print(run_isolated):
     msg = "Can we print to the log from python? 🎉 __does_it_still__work {} {:s}"
     cmd = ["python3", "test.py", msg]
 
-    print(f"+ python3 test.py {msg} > out.log")
     with open("out.log", "w") as f:
         subprocess.run(cmd, stdout=f, check=True)
 
