4e2014f5843f041887e3e96b5e691627425d8dfe
tomc271 <tomc271@gmail.com>
2026-07-03 13:36:33 +0100

    Refactor typeName<T> implementation
    Replaced template specializations with a single `if constexpr` block.

--- include/bout/sys/type_name.hxx
+++ include/bout/sys/type_name.hxx
@@ -8,8 +8,10 @@
 #include "bout/bout_types.hxx"
 
 #include <string>
+#include <type_traits> // Required for std::is_same_v
 #include <typeinfo>
 
+// Forward declarations
 class Field2D;
 class Field3D;
 class FieldPerp;
@@ -23,44 +25,39 @@ namespace utils {
 
 template <typename T>
 std::string typeName() {
-  return typeid(T).name();
+  if constexpr (std::is_same_v<T, bool>)
+    return "bool";
+  else if constexpr (std::is_same_v<T, int>)
+    return "int";
+  else if constexpr (std::is_same_v<T, std::string>)
+    return "string";
+  // Specialised for BOUT++ types to ensure that the result is human-readable
+  else if constexpr (std::is_same_v<T, BoutReal>)
+    return "BoutReal";
+  else if constexpr (std::is_same_v<T, Field2D>)
+    return "Field2D";
+  else if constexpr (std::is_same_v<T, Field3D>)
+    return "Field3D";
+  else if constexpr (std::is_same_v<T, FieldPerp>)
+    return "FieldPerp";
+  else if constexpr (std::is_same_v<T, Array<int>>)
+    return "Array<int>";
+  else if constexpr (std::is_same_v<T, Array<BoutReal>>)
+    return "Array<BoutReal>";
+  else if constexpr (std::is_same_v<T, Matrix<int>>)
+    return "Matrix<int>";
+  else if constexpr (std::is_same_v<T, Matrix<BoutReal>>)
+    return "Matrix<BoutReal>";
+  else if constexpr (std::is_same_v<T, Tensor<int>>)
+    return "Tensor<int>";
+  else if constexpr (std::is_same_v<T, Tensor<BoutReal>>)
+    return "Tensor<BoutReal>";
+  else {
+    return typeid(T).name();
+  }
 }
 
-template <>
-std::string typeName<bool>();
-
-template <>
-std::string typeName<int>();
-
-template <>
-std::string typeName<std::string>();
-
-// Specialised for BOUT++ types to ensure that the result is human-readable
-template <>
-std::string typeName<BoutReal>();
-
-template <>
-std::string typeName<Field2D>();
-
-template <>
-std::string typeName<Field3D>();
-
-template <>
-std::string typeName<FieldPerp>();
-
-template <>
-std::string typeName<Array<int>>();
-template <>
-std::string typeName<Array<BoutReal>>();
-template <>
-std::string typeName<Matrix<int>>();
-template <>
-std::string typeName<Matrix<BoutReal>>();
-template <>
-std::string typeName<Tensor<int>>();
-template <>
-std::string typeName<Tensor<BoutReal>>();
 } // namespace utils
 } // namespace bout
 
-#endif //TYPE_NAME_HXX
+#endif // TYPE_NAME_HXX
