45fd7a369eb0ce909cb6b7368b3a070a412df75d
David Bold <dave@ipp.mpg.de>
2026-08-13 13:19:44 +0200

    Add unit tests for namespaced enum class

--- tests/unit/include/bout/test_bout_enum_class.cxx
+++ tests/unit/include/bout/test_bout_enum_class.cxx
@@ -6,10 +6,22 @@
 #include "bout/output.hxx"
 
 BOUT_ENUM_CLASS(TestEnum, foo, bar);
+BOUT_ENUM_CLASS_NS(bout, TestEnum, foo, bar);
+
+TEST(BoutEnumClassNS, toString) {
+  EXPECT_EQ(toString(bout::TestEnum::foo), "foo");
+  EXPECT_EQ(toString(bout::TestEnum::bar), "bar");
+}
 
 TEST(BoutEnumClass, toString) {
   EXPECT_EQ(toString(TestEnum::foo), "foo");
   EXPECT_EQ(toString(TestEnum::bar), "bar");
+}
+
+TEST(BoutEnumClassNS, fromString) {
+  EXPECT_EQ(bout::TestEnumFromString("foo"), bout::TestEnum::foo);
+  EXPECT_EQ(bout::TestEnumFromString("bar"), bout::TestEnum::bar);
+  EXPECT_THROW(bout::TestEnumFromString("expect_fail"), BoutException);
 }
 
 TEST(BoutEnumClass, fromString) {
@@ -38,10 +50,38 @@ TEST(BoutEnumClass, options) {
   EXPECT_THROW(options["optfail"].as<TestEnum>(), BoutException);
 }
 
+TEST(BoutEnumClassNS, options) {
+  WithQuietOutput quiet_info{output_info};
+
+  Options options;
+
+  auto opt1 = options["opt"].withDefault(bout::TestEnum::foo);
+  EXPECT_EQ(opt1, bout::TestEnum::foo);
+  EXPECT_NE(opt1, bout::TestEnum::bar);
+
+  options["opt"] = "bar";
+
+  auto opt2 = options["opt"].as<bout::TestEnum>();
+  EXPECT_EQ(opt2, bout::TestEnum::bar);
+  EXPECT_NE(opt2, bout::TestEnum::foo);
+
+  options["optfail"] = "expect_fail";
+
+  EXPECT_THROW(options["optfail"].as<bout::TestEnum>(), BoutException);
+}
+
 TEST(BoutEnumClass, ostream) {
   auto sstream = std::stringstream();
 
   sstream << TestEnum::foo << TestEnum::bar;
+
+  EXPECT_EQ(sstream.str(), "foobar");
+}
+
+TEST(BoutEnumClassNS, ostream) {
+  auto sstream = std::stringstream();
+
+  sstream << bout::TestEnum::foo << bout::TestEnum::bar;
 
   EXPECT_EQ(sstream.str(), "foobar");
 }
