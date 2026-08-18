bb1d5c9b33a2678d8ccf1a9333d7cf295d295f69
Peter Hill <peter.hill@york.ac.uk>
2026-08-18 18:06:25 +0100

    Small refactor of `YBoundary`
    - Move body of ctor to .cxx file
    - Inline two-element vector

--- include/bout/yboundary_regions.hxx
+++ include/bout/yboundary_regions.hxx
@@ -2,18 +2,16 @@
 
 #include "bout/boundary_region_iter.hxx"
 
-#include "bout/boundary_iterator.hxx"
 #include "bout/boutexception.hxx"
 #include "bout/field_data.hxx"
-#include "bout/globals.hxx"
 #include "bout/mask.hxx"
-#include "bout/options.hxx"
-#include "bout/parallel_boundary_region.hxx"
 #include "bout/region.hxx"
 
 #include <concepts>
 #include <memory>
 #include <vector>
+
+class Options;
 
 namespace bout {
 namespace boundary {
@@ -36,6 +34,8 @@ concept YBoundaryPoint = std::derived_from<T, BoundaryRegionIterBase<T>>;
 
 class YBoundary {
 public:
+  YBoundary(YBndryType type, Options* options_ptr, const Mesh& mesh);
+
   /// Iterate over the boundary.
   /// This function takes a lamda / templated function, that applies the boundary on the given point.
   /// The function must take a `const YBoundaryPoint auto& point` as its only argument.
@@ -56,86 +56,25 @@ public:
     }
   }
 
-  YBoundary(YBndryType type, Options* options_ptr, const Mesh& mesh) {
-    bool lower_y = true;
-    bool upper_y = true;
-    bool outer_x = true;
-    bool inner_x = false;
-    if (options_ptr != nullptr) {
-      auto& options = *options_ptr;
-      if (!mesh.isFci()) {
-        lower_y =
-            options["lower_y"].doc("Boundary on lower y?").withDefault<bool>(lower_y);
-        upper_y =
-            options["upper_y"].doc("Boundary on upper y?").withDefault<bool>(upper_y);
-      } else {
-        outer_x =
-            options["outer_x"].doc("Boundary on outer x?").withDefault<bool>(outer_x);
-        inner_x =
-            options["inner_x"].doc("Boundary on inner x?").withDefault<bool>(inner_x);
-      }
-    }
-    switch (type) {
-    case YBndryType::sheath:
-      break;
-    case YBndryType::not_sheath:
-      lower_y = !lower_y;
-      upper_y = !upper_y;
-      outer_x = !outer_x;
-      inner_x = !inner_x;
-      break;
-    case YBndryType::all:
-      lower_y = true;
-      upper_y = true;
-      outer_x = true;
-      inner_x = true;
-    }
+  /// Return true if this boundary in the lower direction contains point \p ind
+  bool contains_low(const Ind3D& ind) const { return _contains_low[ind]; }
+  /// Return true if this boundary in the upper direction contains point \p ind
+  bool contains_high(const Ind3D& ind) const { return _contains_high[ind]; }
 
-    if (mesh.isFci()) {
-      if (outer_x) {
-        for (auto& bndry : mesh.getBoundariesPar(BoundaryParType::xout)) {
-          boundary_regions_par.push_back(bndry);
-        }
-      }
-      if (inner_x) {
-        for (auto& bndry : mesh.getBoundariesPar(BoundaryParType::xin)) {
-          boundary_regions_par.push_back(bndry);
-        }
-      }
-    } else {
-      for (auto& bndry : mesh.getBoundaries()) {
-        if ((lower_y && bndry->location == BndryLoc::ydown)
-            or (upper_y && bndry->location == BndryLoc::yup)) {
-          boundary_regions.push_back(
-              std::dynamic_pointer_cast<bout::boundary::BoundaryRegionY>(bndry));
-        }
-      }
-    }
-    // Cache boundary regions
-    _contains.emplace_back(&mesh, false);
-    _contains.emplace_back(&mesh, false);
-    iter([&](const YBoundaryPoint auto& point) {
-      if (point.dir() == 1) {
-        _contains[1][point.ind()] = true;
-      } else if (point.dir() == -1) {
-        _contains[0][point.ind()] = true;
-      }
-    });
-  }
-
-  bool contains_low(Ind3D ind) const { return _contains[0][ind]; }
-  bool contains_high(Ind3D ind) const { return _contains[1][ind]; }
+  /// Return true if this boundary in direction \p dir contains point \p ind
   template <int dir>
-  bool contains(Ind3D ind) const {
+  bool contains(const Ind3D& ind) const {
     static_assert(dir == 1 || dir == -1);
     if constexpr (dir == 1) {
-      return _contains[1][ind];
+      return _contains_high[ind];
     }
     if constexpr (dir == -1) {
-      return _contains[0][ind];
+      return _contains_low[ind];
     }
   }
-  bool contains(int dir, Ind3D ind) const {
+
+  /// Return true if this boundary in direction \p dir contains point \p ind
+  bool contains(int dir, const Ind3D& ind) const {
     if (dir == 1) {
       return contains<+1>(ind);
     }
@@ -149,7 +88,8 @@ private:
   std::vector<std::shared_ptr<BoundaryRegionFCI>> boundary_regions_par;
   std::vector<std::shared_ptr<BoundaryRegionY>> boundary_regions;
 
-  std::vector<BoutMask> _contains;
+  BoutMask _contains_low;
+  BoutMask _contains_high;
 };
 
 } // namespace boundary
---
+++ src/mesh/yboundary_regions.cxx
@@ -0,0 +1,72 @@
+#include "bout/yboundary_regions.hxx"
+
+#include "bout/boundary_region_iter.hxx"
+#include "bout/field_data.hxx"
+#include "bout/options.hxx"
+#include "bout/parallel_boundary_region.hxx"
+
+#include <memory>
+
+namespace bout::boundary {
+YBoundary::YBoundary(YBndryType type, Options* options_ptr, const Mesh& mesh)
+    : _contains_low(&mesh, false), _contains_high(&mesh, false) {
+  bool lower_y = true;
+  bool upper_y = true;
+  bool outer_x = true;
+  bool inner_x = false;
+  if (options_ptr != nullptr) {
+    auto& options = *options_ptr;
+    if (!mesh.isFci()) {
+      lower_y = options["lower_y"].doc("Boundary on lower y?").withDefault<bool>(lower_y);
+      upper_y = options["upper_y"].doc("Boundary on upper y?").withDefault<bool>(upper_y);
+    } else {
+      outer_x = options["outer_x"].doc("Boundary on outer x?").withDefault<bool>(outer_x);
+      inner_x = options["inner_x"].doc("Boundary on inner x?").withDefault<bool>(inner_x);
+    }
+  }
+  switch (type) {
+  case YBndryType::sheath:
+    break;
+  case YBndryType::not_sheath:
+    lower_y = !lower_y;
+    upper_y = !upper_y;
+    outer_x = !outer_x;
+    inner_x = !inner_x;
+    break;
+  case YBndryType::all:
+    lower_y = true;
+    upper_y = true;
+    outer_x = true;
+    inner_x = true;
+  }
+
+  if (mesh.isFci()) {
+    if (outer_x) {
+      for (auto& bndry : mesh.getBoundariesPar(BoundaryParType::xout)) {
+        boundary_regions_par.push_back(bndry);
+      }
+    }
+    if (inner_x) {
+      for (auto& bndry : mesh.getBoundariesPar(BoundaryParType::xin)) {
+        boundary_regions_par.push_back(bndry);
+      }
+    }
+  } else {
+    for (auto& bndry : mesh.getBoundaries()) {
+      if ((lower_y && bndry->location == BndryLoc::ydown)
+          or (upper_y && bndry->location == BndryLoc::yup)) {
+        boundary_regions.push_back(
+            std::dynamic_pointer_cast<bout::boundary::BoundaryRegionY>(bndry));
+      }
+    }
+  }
+  // Cache boundary regions
+  iter([&](const YBoundaryPoint auto& point) {
+    if (point.dir() == 1) {
+      _contains_high[point.ind()] = true;
+    } else if (point.dir() == -1) {
+      _contains_low[point.ind()] = true;
+    }
+  });
+}
+} // namespace bout::boundary
