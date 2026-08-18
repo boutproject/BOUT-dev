48914ced4cd7dcdc685736960f7f0f69761dcc43
tomc271 <tomc271@gmail.com>
2026-08-18 16:18:02 +0100

    Remove redundant `else` following `return`

--- include/bout/sys/type_name.hxx
+++ include/bout/sys/type_name.hxx
@@ -24,36 +24,47 @@ namespace bout::utils {
 
 template <typename T>
 std::string typeName() {
-  if constexpr (std::is_same_v<T, bool>)
+  if constexpr (std::is_same_v<T, bool>) {
     return "bool";
-  else if constexpr (std::is_same_v<T, int>)
-    return "int";
-  else if constexpr (std::is_same_v<T, std::string>)
-    return "string";
-  // Specialised for BOUT++ types to ensure that the result is human-readable
-  else if constexpr (std::is_same_v<T, BoutReal>)
-    return "BoutReal";
-  else if constexpr (std::is_same_v<T, Field2D>)
-    return "Field2D";
-  else if constexpr (std::is_same_v<T, Field3D>)
-    return "Field3D";
-  else if constexpr (std::is_same_v<T, FieldPerp>)
-    return "FieldPerp";
-  else if constexpr (std::is_same_v<T, Array<int>>)
-    return "Array<int>";
-  else if constexpr (std::is_same_v<T, Array<BoutReal>>)
-    return "Array<BoutReal>";
-  else if constexpr (std::is_same_v<T, Matrix<int>>)
-    return "Matrix<int>";
-  else if constexpr (std::is_same_v<T, Matrix<BoutReal>>)
-    return "Matrix<BoutReal>";
-  else if constexpr (std::is_same_v<T, Tensor<int>>)
-    return "Tensor<int>";
-  else if constexpr (std::is_same_v<T, Tensor<BoutReal>>)
-    return "Tensor<BoutReal>";
-  else {
-    return typeid(T).name();
   }
+  if constexpr (std::is_same_v<T, int>) {
+    return "int";
+  }
+  if constexpr (std::is_same_v<T, std::string>) {
+    return "string";
+  }
+  // Specialised for BOUT++ types to ensure that the result is human-readable
+  if constexpr (std::is_same_v<T, BoutReal>) {
+    return "BoutReal";
+  }
+  if constexpr (std::is_same_v<T, Field2D>) {
+    return "Field2D";
+  }
+  if constexpr (std::is_same_v<T, Field3D>) {
+    return "Field3D";
+  }
+  if constexpr (std::is_same_v<T, FieldPerp>) {
+    return "FieldPerp";
+  }
+  if constexpr (std::is_same_v<T, Array<int>>) {
+    return "Array<int>";
+  }
+  if constexpr (std::is_same_v<T, Array<BoutReal>>) {
+    return "Array<BoutReal>";
+  }
+  if constexpr (std::is_same_v<T, Matrix<int>>) {
+    return "Matrix<int>";
+  }
+  if constexpr (std::is_same_v<T, Matrix<BoutReal>>) {
+    return "Matrix<BoutReal>";
+  }
+  if constexpr (std::is_same_v<T, Tensor<int>>) {
+    return "Tensor<int>";
+  }
+  if constexpr (std::is_same_v<T, Tensor<BoutReal>>) {
+    return "Tensor<BoutReal>";
+  }
+  return typeid(T).name();
 }
 
 } // namespace bout::utils
