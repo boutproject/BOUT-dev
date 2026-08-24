8ca53f02ecd2e91fecda2e66ce7c49f79572a823
David Bold <dave@ipp.mpg.de>
2026-08-24 12:09:33 +0200

    Add MetricNormaliser class

--- include/bout/coordinates.hxx
+++ include/bout/coordinates.hxx
@@ -46,6 +46,7 @@
 
 class Mesh;
 class YBoundary;
+class MetricNormaliser;
 
 /*!
  * Represents a coordinate system, and associated operators
@@ -551,5 +552,24 @@ protected:
 namespace bout {
 std::string parallelSliceFieldName(std::string_view field, int offset);
 }
+/// Represents a way to normalise the coordinate system
+/// If a component returns nothing, no normalisation is performed.
+/// Coordinate values are divided by the respective component from
+/// MetricNormaliser, with the exception of the contravariant metric
+/// tensor, which is multiplied by the normalisation factor.
+struct MetricNormaliser {
+  std::optional<BoutReal> g() const { return {}; };
+  std::optional<BoutReal> g11() const { return {}; };
+  std::optional<BoutReal> g22() const { return {}; };
+  std::optional<BoutReal> g33() const { return {}; };
+  std::optional<BoutReal> g12() const { return {}; };
+  std::optional<BoutReal> g13() const { return {}; };
+  std::optional<BoutReal> g23() const { return {}; };
+  std::optional<BoutReal> dx() const { return {}; };
+  std::optional<BoutReal> dy() const { return {}; };
+  std::optional<BoutReal> dz() const { return {}; };
+  std::optional<BoutReal> J() const { return {}; };
+  std::optional<BoutReal> Bxy() const { return {}; };
+};
 
 #endif // BOUT_COORDINATES_H
