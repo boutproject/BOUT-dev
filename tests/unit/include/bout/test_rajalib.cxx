f5c021d1712df397cbe421034e4c99d0bf176092
Jony Castagna <jony.castagna@stfc.ac.uk>
2026-09-17 10:26:03 +0100

    Fixed format via prek

--- tests/unit/include/bout/test_rajalib.cxx
+++ tests/unit/include/bout/test_rajalib.cxx
@@ -1,5 +1,5 @@
-#include "gtest/gtest.h"
 #include "bout/build_config.hxx"
+#include "gtest/gtest.h"
 
 #if BOUT_HAS_RAJA
 #include "bout/rajalib.hxx"
@@ -17,7 +17,7 @@ TEST(RajaExecution, HostDeviceRoundTrip) {
   int* data = &values[0];
   // EXEC_POL is synchronous: results must be visible to the host on return.
   RAJA::forall<EXEC_POL>(RAJA::RangeSegment(0, values.size()),
-                        [=] RAJA_DEVICE(int i) { data[i] = 3 * data[i] + 1; });
+                         [=] RAJA_DEVICE(int i) { data[i] = 3 * data[i] + 1; });
   for (int i = 0; i < values.size(); ++i) {
     EXPECT_EQ(values[i], 3 * i + 1);
   }
